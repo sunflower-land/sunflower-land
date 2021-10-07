0e1ed598c078bdbdb3cc8068c0cf401214e15b8d
Spencer <spencer.dezart.smith@gmail.com>
2021-10-07 20:06:02 +1100

    Update info modal

--- src/dapp/components/modals/Welcome.tsx
+++ src/dapp/components/modals/Welcome.tsx
@@ -14,79 +14,82 @@ import { service } from '../../machine'
 import { getMarketRate } from '../../utils/supply'
 
 interface Props {
-	onGetStarted: () => void
+    onGetStarted: () => void
 }
 
 // TODO: Hardcoded from reports, read from live API
 const predictedDate = new Date(2021, 9, 19, 8, 44, 55)
 
 const makeTimeLeft = () => {
-	const difference = predictedDate.getTime() - Date.now()
+    const difference = predictedDate.getTime() - Date.now()
 
-	const display = secondsToLongString(difference / 1000)
-	return display
+    const display = secondsToLongString(difference / 1000)
+    return display
 }
 
 export const Welcome: React.FC<Props> = ({ onGetStarted }) => {
-	const [timeLeft, setTimeLeft] = React.useState(makeTimeLeft())
-	const [totalSupply, setTotalSupply] = React.useState<number>(1)
+    const [timeLeft, setTimeLeft] = React.useState(makeTimeLeft())
+    const [totalSupply, setTotalSupply] = React.useState<number>(1)
 
-	React.useEffect(() => {
-		const interval = window.setInterval(() => {
-			setTimeLeft(makeTimeLeft())
-		}, 1000)
+    React.useEffect(() => {
+        const interval = window.setInterval(() => {
+            setTimeLeft(makeTimeLeft())
+        }, 1000)
 
-		return () => window.clearInterval(interval)
-	}, [])
+        return () => window.clearInterval(interval)
+    }, [])
 
-	React.useEffect(() => {
-		const load = async () => {
-			const supply = await service.machine.context.blockChain.totalSupply()
-			setTotalSupply(supply)
-		}
+    React.useEffect(() => {
+        const load = async () => {
+            const supply = await service.machine.context.blockChain.totalSupply()
+            setTotalSupply(supply)
+        }
 
-		load()
-	}, [])
+        load()
+    }, [])
 
-	const marketRate = getMarketRate(totalSupply)
+    const marketRate = getMarketRate(totalSupply)
 
-	const sunflowerPrice = Big(0.01).div(marketRate)
+    const sunflowerPrice = Big(0.01).div(marketRate)
 
-	return (
-		<Panel>
-			<div id="welcome">
-				<img id="logo" src={logo} />
-				<Button onClick={onGetStarted}>
-					<span>Get Started</span>
-				</Button>
-				<Button
-					onClick={() =>
-						window.open(
-							'https://adamhannigan81.gitbook.io/sunflower-coin/'
-						)
-					}
-				>
-					About
-					<img src={questionMark} id="question" />
-				</Button>
+    return (
+        <Panel>
+            <div id="welcome">
+                <img id="logo" src={logo} />
+                <Button onClick={onGetStarted}>
+                    <span>Get Started</span>
+                </Button>
+                <Button
+                    onClick={() =>
+                        window.open(
+                            'https://adamhannigan81.gitbook.io/sunflower-coin/'
+                        )
+                    }
+                >
+                    About
+                    <img src={questionMark} id="question" />
+                </Button>
 
-				<div>
-					<h3 className="current-price-header">
-						We are reaching the first halvening event. Rewards will become 5x scarcer
-					</h3>
-					<div className="current-price-container ">
-						<img className="sunflower-price" src={sunflower} />
-						<span className="current-price">
-							= {`$${sunflowerPrice}`}
-						</span>
-					</div>
-					<a href="https://adamhannigan81.gitbook.io/sunflower-coin/#supply-and-demand">
-						<h3 className="current-price-supply-demand">
-							Read more about the supply & demand
-						</h3>
-					</a>
-				</div>
-			</div>
-		</Panel>
-	)
+                <div>
+                    <h3 className="current-price-header">
+                        We are reaching the first halvening event. Rewards will
+                        become 5x scarcer
+                    </h3>
+                    <br />
+                    <div className="current-price-container">
+                        <img className="sunflower-price" src={sunflower} />
+                        <span className="current-price">
+                            = {`$${sunflowerPrice}`}
+                        </span>
+                    </div>
+                    <br />
+                    <a href="https://adamhannigan81.gitbook.io/sunflower-coin/#supply-and-demand">
+                        <h3 className="current-price-supply-demand">
+                            Read more about the supply & demand
+                        </h3>
+                    </a>
+                </div>
+            </div>
+        </Panel>
+    )
 }
