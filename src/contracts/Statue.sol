d52d868b5b179e3215a4fe9611b9c7cba291026a
Adam Hannigan <adam@payble.com.au>
2021-11-21 09:30:32 +1100

    [ENG] Add NFT metadata folder

--- src/contracts/Statue.sol
+++ src/contracts/Statue.sol
@@ -1,17 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.8.0;
 
-//import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-//import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.3.0/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.3.0/contracts/access/Ownable.sol";
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.3.0/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.3.0/contracts/token/ERC20/ERC20Burnable.sol";
+contract SunflowerFarmersStatue is ERC721, Ownable {
+    constructor() ERC721("Sunflower Farmers Statue", "SFS") {}
 
+    function baseTokenURI() public view returns (string memory) {
+        return "";
+    }
 
-contract Stone is ERC20, ERC20Burnable {
-    constructor(address _proxyRegistryAddress) TradeableERC721Token("Sunflower Farmers Statue", "SFS", _proxyRegistryAddress) public {  }
-
-  function baseTokenURI() public view returns (string memory) {
-    return "https://sunflower-farmers/";
-  }
+    function tokenURI(uint256 _tokenId) external view returns (string memory) {
+        return Strings.strConcat(
+            baseTokenURI(),
+            Strings.uint2str(_tokenId)
+        );
+    }
 }
