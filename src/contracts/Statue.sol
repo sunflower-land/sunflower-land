401cee4515fbf1534eb1ef690b8a959287f136ee
Adam Hannigan <adam@payble.com.au>
2021-11-21 09:59:09 +1100

    [ENG] Support multiple URIs

--- src/contracts/Statue.sol
+++ src/contracts/Statue.sol
@@ -5,16 +5,39 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.3.0/contr
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.3.0/contracts/access/Ownable.sol";
 
 contract SunflowerFarmersStatue is ERC721, Ownable {
+    address public minter;
+
     constructor() ERC721("Sunflower Farmers Statue", "SFS") {}
 
+    function passMinterRole(address farm) public returns (bool) {
+        require(msg.sender==minter, "You are not minter");
+        minter = farm;
+
+        return true;
+    }
+
     function baseTokenURI() public view returns (string memory) {
-        return "";
+        return "https://sunflower-farmers.com/play/nfts/";
     }
 
     function tokenURI(uint256 _tokenId) external view returns (string memory) {
         return Strings.strConcat(
             baseTokenURI(),
-            Strings.uint2str(_tokenId)
+            // Only support a single statue
+            "statue"
         );
     }
+
+    function mint(address account, uint256 amount) public {
+        require(msg.sender == minter, "You are not the minter");
+
+        uint supply = totalSupply();
+        require(supply < 1000, "Only 1000 statues can be minted");
+
+
+        require(balanceOf(account) < 1, "A farm can only have 1 statue");
+
+        uint256 tokenId = supply + 1;
+        _mint(account, tokenId);
+	}
 }
