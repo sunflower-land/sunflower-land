f05927b25e76b92d330874a1e733a38933917938
Adam Hannigan <adam@payble.com.au>
2021-11-28 07:00:32 +1100

    [FIX] Fix statue metadata for Open Sea

--- src/contracts/Statue.sol
+++ src/contracts/Statue.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.0 <0.8.0;
+pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.3.0/contracts/token/ERC721/ERC721.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.3.0/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 contract SunflowerFarmersStatue is ERC721, Ownable {
     address public minter;
+    uint public totalSupply;
 
     constructor() public ERC721("Sunflower Farmers Statue", "SFS") {}
 
@@ -16,20 +17,21 @@ contract SunflowerFarmersStatue is ERC721, Ownable {
         return true;
     }
 
-    function baseTokenURI() public view returns (string memory) {
-        return "https://sunflower-farmers.com/play/nfts/statue/";
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(tokenId <= 1000);
+
+        return "https://sunflower-farmers.com/play/nfts/statue/metadata";
     }
+
 
     function mint(address account, uint256 amount) public {
         require(msg.sender == minter, "You are not the minter");
-
-        uint supply = totalSupply();
-        require(supply < 1000, "Only 1000 statues can be minted");
-
-
+        require(totalSupply < 1000, "Only 1000 statues can be minted");
         require(balanceOf(account) < 1, "A farm can only have 1 statue");
 
-        uint256 tokenId = supply + 1;
+        uint256 tokenId = totalSupply + 1;
         _mint(account, tokenId);
+
+        totalSupply = totalSupply + 1;
 	}
 }
