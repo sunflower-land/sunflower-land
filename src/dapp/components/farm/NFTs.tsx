b02d798847602637c76019e2b23226839679ca45
Adam Hannigan <adam@payble.com.au>
2021-12-01 22:16:51 +1100

    Include scarecrow sol and front end

--- src/dapp/components/farm/NFTs.tsx
+++ src/dapp/components/farm/NFTs.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Prop } from "xstate/lib/model.types";
 
 import statue from "../../images/ui/sunflower_statue.png";
+import scarecrow from "../../images/ui/scarecrow.gif";
 import { Inventory } from "../../types/crafting";
 
 import "./NFTs.css";
@@ -14,6 +15,10 @@ export const NFTs: React.FC<Props> = ({ inventory }) => {
     <>
       <div id="statue">
         {inventory["Sunflower Statue"] > 0 && <img src={statue} alt="statue" />}
+      </div>
+
+      <div id="scarecrow">
+        {inventory["Scarecrow"] > 0 && <img src={scarecrow} alt="statue" />}
       </div>
       <div className="dirt" style={{ gridColumn: 6, gridRow: 7 }} />
       <div className="dirt" style={{ gridColumn: 6, gridRow: 6 }} />
