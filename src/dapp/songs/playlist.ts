8287405bdc421e11e683b8fc2fb2e91737131940
Adam Hannigan <adam@payble.com.au>
2022-01-06 13:32:26 -0300

    [FIX] Update willow tree origin

--- src/dapp/songs/playlist.ts
+++ src/dapp/songs/playlist.ts
@@ -1,7 +1,7 @@
 export const playlist = [
   {
     name: "Willow tree",
-    file: "https://s3.ap-southeast-2.amazonaws.com/sunflower-farmers.com/play/static/media/willow_tree.70fb5690.mp3",
+    file: "https://s3.ap-southeast-2.amazonaws.com/sunflower-farmers.com/play/media/willow_tree.mp3",
     artist: "Romy",
   },
 ];
