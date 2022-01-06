d28f5d4817e304c8ff56c20ebb0564ca64b0b1d9
Adam Hannigan <adam@payble.com.au>
2022-01-06 12:41:44 -0300

    [ENG] Use S3 version for music

--- src/dapp/songs/playlist.ts
+++ src/dapp/songs/playlist.ts
@@ -1,9 +1,7 @@
-const willowTree = require("./willow_tree.mp3");
-
 export const playlist = [
   {
     name: "Willow tree",
-    file: willowTree,
+    file: "https://s3.ap-southeast-2.amazonaws.com/sunflower-farmers.com/play/static/media/willow_tree.70fb5690.mp3",
     artist: "Romy",
   },
 ];
