b1ce9cb834ea96fd65dea921e42496aba410e180
Andrea Valla <valla.andrea@gmail.com>
2022-01-25 22:24:01 +0100

    bugfix: issue #171 favicon counter is buggy on first load - fixed refactoring how counting works, now it checks harvestable fields in state

--- src/features/crops/AppIconProvider.tsx
+++ src/features/crops/AppIconProvider.tsx
@@ -2,31 +2,45 @@
  * A wrapper that provides crops favicon harvestable items management
  */
 
-import React from "react";
-import { useState } from "react";
+import React, {useContext} from "react";
 import Tinycon from "tinycon";
+import {CROPS} from "features/game/types/crops";
+import {getTimeLeft} from "lib/utils/time";
+import {Context} from "features/game/GameProvider";
+import {useActor} from "@xstate/react";
+import {FieldItem} from "features/game/types/game";
 
 
 interface AppIconContext {
-  incrementHarvestable: (value: number) => void;
+  updateHarvestable: (value: number) => void,
 }
 
 export const AppIconContext = React.createContext<AppIconContext>({} as AppIconContext);
 
-export const AppIconProvider: React.FC = ({ children }) => {
-  const [harvestable, setHarvestable] = useState<number>(0);
+const isHarvestable = (field: FieldItem) : boolean => {
+  if (!field.crop) {
+    return false;
+  }
+  const crop = CROPS[field.crop.name];
+  const timeLeft = getTimeLeft(field.crop.plantedAt, crop.harvestSeconds);
+  return timeLeft <= 0;
+}
 
-  const incrementHarvestable = (value: number) => {
-    if (value != -1 && value !== 1) {
-      throw new Error("This counter should increment or decrement by 1 only");
-    }
-    const incrementedHarvestable = harvestable + value;
-    setHarvestable(incrementedHarvestable);
-    Tinycon.setBubble(incrementedHarvestable || null);
+export const AppIconProvider: React.FC = ({ children }) => {
+  const { gameService } = useContext(Context);
+  const [
+    {
+      context: { state },
+    },
+  ] = useActor(gameService);
+
+  const updateHarvestable = () : void => {
+    const total = state.fields.filter(isHarvestable).length;
+    Tinycon.setBubble(total || null);
   }
 
   return (
-    <AppIconContext.Provider value={{ incrementHarvestable }}>
+    <AppIconContext.Provider value={{ updateHarvestable }}>
       {children}
     </AppIconContext.Provider>
   );
