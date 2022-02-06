454f0c739f921714c26d9133349c35db50ef68b5
Pranav Yadav <pranavyadav3912@gmail.com>
2022-02-07 00:10:28 +0530

    add ScreenshotButton into Hud

--- src/features/hud/Hud.tsx
+++ src/features/hud/Hud.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-import {Balance} from "./components/Balance";
-import {Inventory} from "./components/Inventory";
-import {Menu} from "./components/Menu";
-import {Address} from "./components/Address";
-import {AudioPlayer} from "components/ui/AudioPlayer";
+import { Balance } from "./components/Balance";
+import { Inventory } from "./components/Inventory";
+import { Menu } from "./components/Menu";
+import { Address } from "./components/Address";
+import { AudioPlayer } from "components/ui/AudioPlayer";
+import { ScreenshotButton } from "./components/ScreenshotButton";
 
 /**
  * Heads up display - a concept used in games for the small overlayed display of information.
@@ -13,10 +14,11 @@ import {AudioPlayer} from "components/ui/AudioPlayer";
 export const Hud: React.FC = () => {
   return (
     <>
-      <Menu/>
-      <Balance/>
-      <Inventory/>
-      <AudioPlayer/>
+      <Menu />
+      <Balance />
+      <Inventory />
+      <ScreenshotButton />
+      <AudioPlayer />
       <Address />
     </>
   );
