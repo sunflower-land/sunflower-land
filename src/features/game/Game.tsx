78e4cc728ad648194cb6646b53d36db187b015ce
Spencer <spencer.dezart.smith@gmail.com>
2022-02-06 22:25:08 +1100

    Implement saving UI

--- src/features/game/Game.tsx
+++ src/features/game/Game.tsx
@@ -15,10 +15,15 @@ import { ToastManager } from "./toast/ToastManager";
 import { GameError } from "./components/GameError";
 import { Decorations } from "./components/Decorations";
 import { Animals } from "features/animals/Animals";
+import { useInterval } from "lib/utils/useInterval";
+
+const AUTO_SAVE_INTERVAL = 1000 * 30; // autosave every 30 seconds
 
 export const Game: React.FC = () => {
   const { gameService } = useContext(Context);
-  const [gameState] = useActor(gameService);
+  const [gameState, send] = useActor(gameService);
+
+  useInterval(() => send("SAVE"), AUTO_SAVE_INTERVAL);
 
   return (
     <>
