49c0b83e54aabc7a15e27478872727d87ecee4e6
Spencer <spencer.dezart.smith@gmail.com>
2022-02-06 13:20:22 +1100

    Use FarmID instead of address

--- src/features/auth/components/StartFarm.tsx
+++ src/features/auth/components/StartFarm.tsx
@@ -4,7 +4,6 @@ import { useActor } from "@xstate/react";
 import * as Auth from "features/auth/lib/Provider";
 
 import { Button } from "components/ui/Button";
-import { shortAddress } from "features/hud/components/Address";
 
 export const StartFarm: React.FC = () => {
   const { authService } = useContext(Auth.Context);
@@ -15,13 +14,11 @@ export const StartFarm: React.FC = () => {
   };
 
   // We can only ever show this state if the address is not undefin
-  const farmAddress = authState.context.address!;
+  const farmId = authState.context.farmId!;
 
   return (
     <>
-      <p className="text-shadow text-small mb-2 px-1">
-        Farm: {shortAddress(farmAddress)}
-      </p>
+      <p className="text-shadow text-small mb-2 px-1">Farm ID: {farmId}</p>
       <Button onClick={start} className="overflow-hidden mb-2">
         Lets go!
       </Button>
