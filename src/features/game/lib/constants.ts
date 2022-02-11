29711f4b1853fa49b43f40258a6bcceda505490d
Adam Hannigan <adam@payble.com.au>
2022-02-11 16:40:07 -0300

    Refactor stock section

--- src/features/game/lib/constants.ts
+++ src/features/game/lib/constants.ts
@@ -1,8 +1,31 @@
 import Decimal from "decimal.js-light";
 import { fromWei } from "web3-utils";
-import { GameState } from "../types/game";
+import { GameState, Inventory } from "../types/game";
 
 export const GRID_WIDTH_PX = 42;
+
+export const INITIAL_STOCK: Inventory = {
+  "Sunflower Seed": new Decimal(20),
+  "Potato Seed": new Decimal(300),
+  "Pumpkin Seed": new Decimal(200),
+  "Carrot Seed": new Decimal(100),
+  "Cabbage Seed": new Decimal(90),
+  "Beetroot Seed": new Decimal(80),
+  "Cauliflower Seed": new Decimal(70),
+  "Parsnip Seed": new Decimal(50),
+  "Radish Seed": new Decimal(40),
+  "Wheat Seed": new Decimal(0),
+
+  Axe: new Decimal(50),
+  Pickaxe: new Decimal(50),
+  "Stone Pickaxe": new Decimal(50),
+  "Iron Pickaxe": new Decimal(50),
+
+  // One off items
+  "Pumpkin Soup": new Decimal(1),
+  Sauerkraut: new Decimal(1),
+  "Roasted Cauliflower": new Decimal(1),
+};
 
 export const INITIAL_FARM: GameState = {
   id: 1,
@@ -64,26 +87,5 @@ export const INITIAL_FARM: GameState = {
     "Gold Egg": new Decimal(1),
     "Roasted Cauliflower": new Decimal(2),
   },
-  stock: {
-    "Sunflower Seed": new Decimal(20),
-    "Potato Seed": new Decimal(300),
-    "Pumpkin Seed": new Decimal(200),
-    "Carrot Seed": new Decimal(100),
-    "Cabbage Seed": new Decimal(90),
-    "Beetroot Seed": new Decimal(80),
-    "Cauliflower Seed": new Decimal(70),
-    "Parsnip Seed": new Decimal(50),
-    "Radish Seed": new Decimal(40),
-    "Wheat Seed": new Decimal(0),
-
-    Axe: new Decimal(50),
-    Pickaxe: new Decimal(50),
-    "Stone Pickaxe": new Decimal(50),
-    "Iron Pickaxe": new Decimal(50),
-
-    // One off items
-    "Pumpkin Soup": new Decimal(1),
-    Sauerkraut: new Decimal(1),
-    "Roasted Cauliflower": new Decimal(1),
-  },
+  stock: INITIAL_STOCK,
 };
