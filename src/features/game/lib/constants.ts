d3774fcc6ff56679f01ba9ed4d55b7130f7fd21b
Adam Hannigan <adam@payble.com.au>
2022-02-13 20:57:56 -0300

    [FIX] Initial game values

--- src/features/game/lib/constants.ts
+++ src/features/game/lib/constants.ts
@@ -1,5 +1,6 @@
 import Decimal from "decimal.js-light";
 import { fromWei } from "web3-utils";
+import { FOODS, LimitedItems } from "../types/craftables";
 import { GameState, Inventory } from "../types/game";
 
 export const GRID_WIDTH_PX = 42;
@@ -27,65 +28,51 @@ export const INITIAL_STOCK: Inventory = {
   "Roasted Cauliflower": new Decimal(1),
 };
 
+export const INITIAL_FIELDS: GameState["fields"] = {
+  0: {
+    name: "Sunflower",
+    plantedAt: 0,
+  },
+  1: {
+    name: "Sunflower",
+    plantedAt: 0,
+  },
+  2: {
+    name: "Sunflower",
+    plantedAt: 0,
+  },
+  5: {
+    name: "Carrot",
+    plantedAt: 0,
+  },
+  6: {
+    name: "Cabbage",
+    plantedAt: 0,
+  },
+  10: {
+    name: "Cauliflower",
+    plantedAt: 0,
+  },
+  11: {
+    name: "Beetroot",
+    plantedAt: 0,
+  },
+  16: {
+    name: "Parsnip",
+    plantedAt: 0,
+  },
+  17: {
+    name: "Radish",
+    plantedAt: 0,
+  },
+};
+
 export const INITIAL_FARM: GameState = {
   id: 1,
-  balance: new Decimal(fromWei("2999999999999999990")),
-  fields: {
-    0: {
-      name: "Sunflower",
-      plantedAt: 0,
-    },
-    1: {
-      name: "Sunflower",
-      plantedAt: 0,
-    },
-    2: {
-      name: "Sunflower",
-      plantedAt: 0,
-    },
-    5: {
-      name: "Carrot",
-      plantedAt: 0,
-    },
-    6: {
-      name: "Cabbage",
-      plantedAt: 0,
-    },
-    10: {
-      name: "Cauliflower",
-      plantedAt: 0,
-    },
-    11: {
-      name: "Beetroot",
-      plantedAt: 0,
-    },
-    16: {
-      name: "Parsnip",
-      plantedAt: 0,
-    },
-    17: {
-      name: "Radish",
-      plantedAt: 0,
-    },
-  },
+  balance: new Decimal(fromWei("0")),
+  fields: INITIAL_FIELDS,
   inventory: {
     "Sunflower Seed": new Decimal(3),
-    "Pumpkin Soup": new Decimal(1),
-    "Sunflower Tombstone": new Decimal(1),
-    "Sunflower Rock": new Decimal(1),
-    "Chicken Coop": new Decimal(1),
-    "Carrot Seed": new Decimal(2),
-    "Cabbage Seed": new Decimal(3),
-    "Beetroot Seed": new Decimal(2),
-    "Cauliflower Seed": new Decimal(100),
-    "Sunflower Statue": new Decimal(1),
-    "Christmas Tree": new Decimal(1),
-    Scarecrow: new Decimal(1),
-    "Farm Cat": new Decimal(1),
-    "Farm Dog": new Decimal(1),
-    Gnome: new Decimal(1),
-    "Gold Egg": new Decimal(1),
-    "Roasted Cauliflower": new Decimal(2),
   },
   stock: INITIAL_STOCK,
 };
