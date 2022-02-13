a42761b9191528c31202de714ad4dc41dfb8b269
Spencer <spencer.dezart.smith@gmail.com>
2022-02-13 13:20:59 +1100

    Add game error as default error

--- src/features/auth/Unauthorised.tsx
+++ src/features/auth/Unauthorised.tsx
@@ -10,6 +10,7 @@ import questionMark from "assets/icons/expression_confused.png";
 import { Web3Missing } from "./components/Web3Missing";
 import { WrongChain } from "./components/WrongChain";
 import { Beta } from "./components/Beta";
+import { GameError } from "features/game/components/GameError";
 
 interface Props {}
 
@@ -29,21 +30,5 @@ export const Unauthorised: React.FC<Props> = () => {
     return <Beta />;
   }
 
-  return (
-    <>
-      <span>Catch all other connecting errors :/</span>
-      {/* <div className="flex flex-col justify-center items-center">
-        <div className="w-56">
-          <Button onClick={onGetStarted}>Get Started</Button>
-          <Button
-            onClick={() => window.open("https://docs.sunflower-farmers.com/")}
-            className="mt-2"
-          >
-            About
-            <img className="h-4 ml-2" src={questionMark} />
-          </Button>
-        </div>
-      </div> */}
-    </>
-  );
+  return <GameError />;
 };
