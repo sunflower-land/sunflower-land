9170661e4e891b320fe03883e2a0e213b624c815
Romulo Furtado <rofrtd@gmail.com>
2022-02-15 23:59:39 +1100

    add tab click handler

--- src/features/crops/components/MarketItems.tsx
+++ src/features/crops/components/MarketItems.tsx
@@ -9,6 +9,7 @@ import { Tab } from "components/ui/Tab";
 
 import { Seeds } from "./Seeds";
 import { Plants } from "./Plants";
+import { useTour } from "@reactour/tour";
 
 interface Props {
   onClose: () => void;
@@ -16,19 +17,28 @@ interface Props {
 
 export const MarketItems: React.FC<Props> = ({ onClose }) => {
   const [tab, setTab] = useState<"buy" | "sell">("buy");
+  const { setCurrentStep: setCurrentTourStep, isOpen: tourIsOpen, currentStep: currentTourStep } = useTour()
+
+
+  const handleTabClick = (tab: "buy" | "sell") => {
+    setTab(tab)
+    if (tourIsOpen) {
+      currentTourStep === 4 ? setCurrentTourStep(5) : setCurrentTourStep(6)
+    }
+  }
 
   return (
-    <Panel className="pt-5 relative">
+    <Panel className="pt-5 relative shop">
       <div className="flex justify-between absolute top-1.5 left-0.5 right-0 items-center">
         <div className="flex">
-          <Tab isActive={tab === "buy"} onClick={() => setTab("buy")}>
+          <Tab isActive={tab === "buy"} onClick={() => handleTabClick('buy')}>
             <img src={seeds} className="h-5 mr-2" />
             <span className="text-sm text-shadow">Buy</span>
           </Tab>
-          <Tab isActive={tab === "sell"} onClick={() => setTab("sell")}>
-            <img src={sunflowerPlant} className="h-5 mr-2" />
-            <span className="text-sm text-shadow">Sell</span>
-          </Tab>
+              <Tab isActive={tab === "sell"} onClick={() => handleTabClick("sell")}>
+                <img src={sunflowerPlant} className="h-5 mr-2" />
+                <span className="text-sm text-shadow">Sell</span>
+              </Tab>
         </div>
         <img
           src={close}
