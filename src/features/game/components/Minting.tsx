9b54b3ba302b97d2ff709d2c3a357c375a6e66bc
Spencer <spencer.dezart.smith@gmail.com>
2022-02-16 22:14:05 +1100

    Add minting modal

--- src/features/game/components/Minting.tsx
+++ src/features/game/components/Minting.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 
+import mintingAnimation from "assets/npcs/minting.gif";
+
 export const Minting: React.FC = () => {
-  return <span>Minting your item...</span>;
+  return (
+    <div className="flex flex-col items-center p-2">
+      <span className="text-shadow text-center">Minting</span>
+      <img src={mintingAnimation} className="w-1/2 mt-2 mb-3 ml-8" />
+      <span className="text-shadow text-xs text-center">
+        Please be patient while our minons mint something special for you.
+      </span>
+    </div>
+  );
 };
