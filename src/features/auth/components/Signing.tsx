160ae934cfe70cec5c92181d9f9777051b6b7ab3
Spencer <spencer.dezart.smith@gmail.com>
2022-02-16 22:21:35 +1100

    Add syncing modal

--- src/features/auth/components/Signing.tsx
+++ src/features/auth/components/Signing.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Signing: React.FC = () => {
   return (
     <>
-      <span className="text-shadow loading">Waiting for you...</span>
+      <span className="text-shadow loading">Waiting for you</span>
       <span className="text-shadow block mt-4 text-sm">
         Accept the signature request in your browser wallet to login.
       </span>
