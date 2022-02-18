7730074ce9377fe3f7704ec202d870acbbf31558
Adam Hannigan <adam@payble.com.au>
2022-02-18 16:23:12 -0300

    Add image files and command to script

--- _scripts/erc1155Metadata.ts
+++ _scripts/erc1155Metadata.ts
@@ -13,7 +13,12 @@ type ERC1155Metadata = Record<
   }
 >;
 
+/**
+ * Data that will be displayed on ERC721/ERC1155 compatible platforms
+ * E.g. OpenSea
+ */
 const items: ERC1155Metadata = {
+  // Seeds
   "Sunflower Seed": {
     description: "A seed for a sunflower plant.",
     decimals: 1,
@@ -65,13 +70,227 @@ const items: ERC1155Metadata = {
     image: "src/assets/crops/wheat/seed.png",
   },
 
+  // Crops
   Sunflower: {
     description: "A sunflower that was harvested.",
     decimals: 18,
     image: "src/assets/crops/sunflower/crop.png",
   },
-} as any;
+  Potato: {
+    description: "A potato that was harvested.",
+    decimals: 18,
+    image: "src/assets/crops/potato/crop.png",
+  },
+  Pumpkin: {
+    description: "A pumpkin that was harvested.",
+    decimals: 18,
+    image: "src/assets/crops/pumpkin/crop.png",
+  },
+  Carrot: {
+    description: "A carrot that was harvested.",
+    decimals: 18,
+    image: "src/assets/crops/carrot/crop.png",
+  },
+  Cabbage: {
+    description: "A cavvage that was harvested.",
+    decimals: 18,
+    image: "src/assets/crops/cabbage/crop.png",
+  },
+  Beetroot: {
+    description: "A beetroot that was harvested.",
+    decimals: 18,
+    image: "src/assets/crops/beetroot/crop.png",
+  },
+  Cauliflower: {
+    description: "A cauliflower that was harvested.",
+    decimals: 18,
+    image: "src/assets/crops/cauliflower/crop.png",
+  },
+  Radish: {
+    description: "A radish that was harvested.",
+    decimals: 18,
+    image: "src/assets/crops/radish/crop.png",
+  },
+  Parsnip: {
+    description: "A parsnip that was harvested.",
+    decimals: 18,
+    image: "src/assets/crops/parsnip/crop.png",
+  },
+  Wheat: {
+    description: "Wheat that was harvested.",
+    decimals: 18,
+    image: "src/assets/crops/wheat/crop.png",
+  },
+  Axe: {
+    description: "A tool used to cut down trees.",
+    decimals: 1,
+    image: "src/assets/tools/axe.png",
+  },
+  Pickaxe: {
+    description: "A tool used to mine rocks.",
+    decimals: 1,
+    image: "src/assets/tools/wood_pickaxe.png",
+  },
+  "Stone Pickaxe": {
+    description: "A tool used to mine rocks.",
+    decimals: 1,
+    image: "src/assets/tools/stone_pickaxe.png",
+  },
+  "Iron Pickaxe": {
+    description: "A tool used to mine rocks.",
+    decimals: 1,
+    image: "src/assets/tools/iron_pickaxe.png",
+  },
+  Hammer: {
+    description: "A tool used to construct buildings",
+    decimals: 1,
+    image: "src/assets/tools/hammer.png",
+  },
+  Rod: {
+    description: "A tool used to fish.",
+    decimals: 1,
+    image: "src/assets/tools/fishing_rod.png",
+  },
 
+  // NFTs
+  "Chicken Coop": {
+    description:
+      "A chicken coop that can be used to raise chickens. Chickens produce 3x more eggs with this.",
+    decimals: 1,
+    image: "src/assets/nfts/chicken_coop.png",
+  },
+  "Christmas Tree": {
+    description:
+      "A christmas tree that shows during christmas. It helps santa find your farm and airdrop presents.",
+    decimals: 1,
+    image: "src/assets/nfts/christmas_tree.png",
+  },
+  "Farm Cat": {
+    description: "Victoria the cat. She helps protect your farm from rats.",
+    decimals: 1,
+    image: "src/assets/nfts/farm_cat.png",
+  },
+  "Farm Dog": {
+    description: "Bella the dog. Move sheep faster with bella on your farm.",
+    decimals: 1,
+    image: "src/assets/nfts/farm_dog.png",
+  },
+  Gnome: {
+    description: "A lucky gnome",
+    decimals: 1,
+    image: "src/assets/nfts/gnome.png",
+  },
+  "Gold Egg": {
+    description:
+      "A golden egg. What lays inside is a mystery but it looks like it will hatch soon.",
+    decimals: 1,
+    image: "src/assets/nfts/gold_egg.png",
+  },
+  "Potato Statue": {
+    description:
+      "A statue of a potato. It's a potato statue. Potato blood is in my veins",
+    decimals: 1,
+    image: "src/assets/nfts/potato_statue.png",
+  },
+  Scarecrow: {
+    description:
+      "A scary looking decoration that keeps crows away. Grow wheat 3x faster with a scarecrow on your farm",
+    decimals: 1,
+    image: "src/assets/nfts/scarecrow.png",
+  },
+  "Sunflower Statue": {
+    description: "The holy statue",
+    decimals: 1,
+    image: "src/assets/nfts/sunflower_statue.png",
+  },
+  "Sunflower Rock": {
+    description:
+      "A rare homage to the game that broke Polygon. Owned by the 100+ designers, developers and contributors of the project.",
+    decimals: 1,
+    image: "src/assets/nfts/sunflower_statue.png",
+  },
+  "Sunflower Tombstone": {
+    description: "RIP SFF",
+    decimals: 1,
+    image: "src/assets/nfts/sunflower_statue.png",
+  },
+  "Golden Cauliflower": {
+    description:
+      "A lucky cauliflower that gives you 2x rewards when selling your cauliflowers",
+    decimals: 1,
+    image: "src/assets/nfts/golden_cauliflower.png",
+  },
+  "Goblin Crown": {
+    description: "Summon the leader of the goblins with this unique crown",
+    decimals: 1,
+    image: "src/assets/nfts/goblin_crown.png",
+  },
+  Fountain: {
+    description: "A relaxing fountain for your farm",
+    decimals: 1,
+    image: "src/assets/nfts/fountain.gif",
+  },
+
+  // FOODS
+  "Pumpkin Soup": {
+    description: "A creamy soup which Goblins love",
+    decimals: 1,
+    image: "src/assets/nfts/pumpkin_soup.png",
+  },
+  Sauerkraut: {
+    description: "A delicatese made from harvested cabbage",
+    decimals: 1,
+    image: "src/assets/nfts/saurrerkrat.png",
+  },
+  "Roasted Cauliflower": {
+    description:
+      "A delicious cauliflower that is roasted. Helps keep Goblins off your farm",
+    decimals: 1,
+    image: "src/assets/nfts/roasted_cauliflower.png",
+  },
+  Flour: {
+    description: "A flour that is used to make bread",
+    decimals: 1,
+    image: "src/assets/crops/wheat/flour.png",
+  },
+
+  // Resources
+  Stone: {
+    description: "A resource that can be used to craft items",
+    decimals: 18,
+    image: "src/assets/resources/stone.png",
+  },
+  Wood: {
+    description: "A resource that can be used to craft items",
+    decimals: 18,
+    image: "src/assets/resources/wood.png",
+  },
+  Iron: {
+    description: "A resource that can be used to craft items",
+    decimals: 18,
+    image: "src/assets/resources/iron_ore.png",
+  },
+  Gold: {
+    description: "A resource that can be used to craft items",
+    decimals: 18,
+    image: "src/assets/resources/gold_ore.png",
+  },
+  Chicken: {
+    description: "A resource that can be used to earn eggs",
+    decimals: 18,
+    image: "src/assets/resources/chicken.png",
+  },
+  Egg: {
+    description: "A resource that can be used in cooking recipes",
+    decimals: 18,
+    image: "src/assets/resources/egg.png",
+  },
+};
+
+/**
+ * For each item generate the JSON representation
+ * Also scale the pixel art to an acceptable size for OpenSea
+ */
 async function convert() {
   const names = Object.keys(items) as InventoryItemName[];
 
@@ -89,27 +308,25 @@ async function convert() {
         decimals: item.decimals,
       };
 
-      const inputBuffer = fs.readFileSync(item.image);
-      console.log({ inputBuffer });
-
       const fileName = `${id}.json`;
       const filePath = path.join(__dirname, "../public/erc1155", fileName);
-      fs.writeFile(filePath, JSON.stringify(json), console.log);
+      fs.writeFile(filePath, JSON.stringify(json), () =>
+        console.log(`Wrote file: ${id}`)
+      );
 
       const oldImagePath = path.join(__dirname, "../", item.image);
       const buffer = fs.readFileSync(oldImagePath);
 
-      console.log({ scalePixelArt });
       const scaledImage = await scalePixelArt(buffer, 20);
-      console.log({ scaledImage });
       const fileType = item.image.split(".").pop();
-      console.log({ fileType });
       const imagePath = path.join(
         __dirname,
         "../public/erc1155",
         `${id}.${fileType}`
       );
-      fs.writeFile(imagePath, scaledImage, console.log);
+      fs.writeFile(imagePath, scaledImage, () =>
+        console.log(`Wrote image: ${id}`)
+      );
     })
   );
 }
