0101f67a5d3e3d8e2c01cbc45388ed253c599af4
Spencer <spencer.dezart.smith@gmail.com>
2022-02-18 22:00:58 +1100

    Add animations to team donation

--- src/features/teamDonation/TeamDonation.tsx
+++ src/features/teamDonation/TeamDonation.tsx
@@ -1,23 +1,95 @@
 import React, { useState } from "react";
 import { Modal } from "react-bootstrap";
+import { inspect } from "@xstate/inspect";
+import { assign, createMachine } from "xstate";
+import { useMachine } from "@xstate/react";
 
 import { GRID_WIDTH_PX } from "features/game/lib/constants";
 import { Panel } from "components/ui/Panel";
 import { roundToOneDecimal } from "features/auth/components";
 import { metamask } from "lib/blockchain/metamask";
 import { Button } from "components/ui/Button";
-import { toHex, toWei } from "web3-utils";
 
 import begger from "assets/npcs/begger.gif";
 import richBegger from "assets/npcs/rich_begger.png";
 import upArrow from "assets/icons/arrow_up.png";
 import downArrow from "assets/icons/arrow_down.png";
 import token from "assets/icons/token.png";
+import humanDeath from "assets/npcs/human_death.gif";
+
+type DonateEvent = {
+  type: "DONATE";
+  donation: number;
+};
+
+type Event = DonateEvent | { type: "BEGGER_CLICK" } | { type: "CLOSE" };
+
+interface Context {
+  hasDonated: boolean;
+}
+
+export type State = {
+  value: "idle" | "begging" | "donating" | "donated" | "error";
+  context: Context;
+};
+
+const teamDonationMachine = createMachine<Context, Event, State>({
+  initial: "idle",
+  context: {
+    hasDonated: false,
+  },
+  states: {
+    idle: {
+      on: {
+        BEGGER_CLICK: {
+          target: "begging",
+        },
+      },
+    },
+    begging: {
+      on: {
+        DONATE: {
+          target: "donating",
+        },
+        CLOSE: {
+          target: "idle",
+        },
+      },
+    },
+    donating: {
+      invoke: {
+        src: async (_context: Context, event: any): Promise<void> => {
+          const { donation } = event as DonateEvent;
+
+          await metamask.donateToTheTeam(donation);
+        },
+        onDone: {
+          target: "donated",
+          actions: assign({ hasDonated: (_context, _event) => true }),
+        },
+        onError: "error",
+      },
+    },
+    donated: {
+      after: {
+        1000: {
+          target: "idle",
+        },
+      },
+    },
+    error: {
+      after: {
+        2000: {
+          target: "idle",
+        },
+      },
+    },
+  },
+});
 
 export const TeamDonation: React.FC = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [state, send] = useMachine(teamDonationMachine);
   const [donation, setDonation] = useState(0.1);
-  const [hasDonated, setHadDonated] = useState(false);
 
   const onDonationChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     // If keyboard input "" convert to 0
@@ -35,19 +107,6 @@ export const TeamDonation: React.FC = () => {
     } else setDonation((prevState) => roundToOneDecimal(prevState - 0.1));
   };
 
-  const onDonate = async () => {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    try {
-      await metamask.donateToTheTeam(donation);
-
-      setIsOpen(false);
-      setHadDonated(true);
-    } catch (error) {
-      console.log("error");
-      setIsOpen(false);
-    }
-  };
-
   return (
     <div
       className="z-5 absolute align-items-center w-[72px]"
@@ -57,81 +116,104 @@ export const TeamDonation: React.FC = () => {
         top: `calc(50% - ${GRID_WIDTH_PX * 17.2}px)`,
       }}
     >
-      {!hasDonated ? (
+      {!state.context.hasDonated ? (
         <img
           id="begger"
           src={begger}
           className="absolute hover:cursor-pointer hover:img-highlight z-10"
-          onClick={() => setIsOpen(true)}
+          onClick={() => send("BEGGER_CLICK")}
         />
       ) : (
         <img
           id="rich_begger"
           src={richBegger}
           className="absolute hover:cursor-pointer hover:img-highlight z-10"
-          onClick={() => setIsOpen(true)}
+          onClick={() => send("BEGGER_CLICK")}
         />
       )}
-      <Modal centered show={isOpen} onHide={() => setIsOpen(false)}>
+      <Modal centered show={!state.matches("idle")}>
         <Panel>
-          <div className="flex flex-col items-center mb-1">
-            <img src={token} alt="sunflower token" className="w-12 mb-3" />
-            <div className="flex flex-col text-shadow items-center">
-              <h2 className="text-sm sm:text-base mb-2 text-center pb-2">
-                Buy the team a coffee!
-              </h2>
-              <p className="sm:text-sm mb-3 text-center">
-                Sunflower Land is run by a small group of passionate developers
-                who are 100% sleep deprived.
-              </p>
-              <p className="sm:text-sm mb-3 text-center">
-                {`You can send us a donation of Matic with which we can drink
+          {state.matches("begging") && (
+            <div className="flex flex-col items-center mb-1">
+              <img src={token} alt="sunflower token" className="w-12 mb-3" />
+              <div className="flex flex-col text-shadow items-center">
+                <h2 className="text-sm sm:text-base mb-2 text-center pb-2">
+                  Buy the team a coffee!
+                </h2>
+                <p className="sm:text-sm mb-3 text-center">
+                  Sunflower Land is run by a small group of passionate
+                  developers who are 100% sleep deprived.
+                </p>
+                <p className="sm:text-sm mb-3 text-center">
+                  {`You can send us a donation of Matic with which we can drink
                 more coffee and stay awake longer pumping out awesome new
                 features`}
-              </p>
+                </p>
 
-              <p className="sm:text-sm  mb-3 text-center">
-                Every little bit counts!
-              </p>
+                <p className="sm:text-sm  mb-3 text-center">
+                  Every little bit counts!
+                </p>
+              </div>
+              <div className="relative">
+                <input
+                  type="number"
+                  className="text-shadow shadow-inner shadow-black bg-brown-200 w-24 p-1 text-center"
+                  step="0.1"
+                  min={0.1}
+                  value={donation}
+                  required
+                  onChange={onDonationChange}
+                  onBlur={() => {
+                    if (donation < 0.1) setDonation(0.1);
+                  }}
+                />
+                <img
+                  src={upArrow}
+                  alt="increment donation value"
+                  className="cursor-pointer absolute -right-4 top-0"
+                  onClick={incrementDonation}
+                />
+                <img
+                  src={downArrow}
+                  alt="decrement donation value"
+                  className="cursor-pointer absolute -right-4 bottom-0"
+                  onClick={decrementDonation}
+                />
+              </div>
+              <span className="text-[10px] text-shadow mt-2 mb-3">
+                Amount in MATIC
+              </span>
+              <div className="flex w-full">
+                <Button className="w-full mr-1" onClick={() => send("CLOSE")}>
+                  <span className="text-xs whitespace-nowrap">Close</span>
+                </Button>
+                <Button
+                  className="w-full ml-1"
+                  onClick={() => send("DONATE", { donation })}
+                >
+                  <span className="text-xs whitespace-nowrap">Donate</span>
+                </Button>
+              </div>
             </div>
-            <div className="relative">
-              <input
-                type="number"
-                className="text-shadow shadow-inner shadow-black bg-brown-200 w-24 p-1 text-center"
-                step="0.1"
-                min={0.1}
-                value={donation}
-                required
-                onChange={onDonationChange}
-                onBlur={() => {
-                  if (donation < 0.1) setDonation(0.1);
-                }}
-              />
-              <img
-                src={upArrow}
-                alt="increment donation value"
-                className="cursor-pointer absolute -right-4 top-0"
-                onClick={incrementDonation}
-              />
-              <img
-                src={downArrow}
-                alt="decrement donation value"
-                className="cursor-pointer absolute -right-4 bottom-0"
-                onClick={decrementDonation}
-              />
+          )}
+          {state.matches("donating") && (
+            <div className="flex flex-col items-center">
+              <img id="begger" src={begger} className="w-24" />
+              <p className="loading mb-4">Donating</p>
             </div>
-            <span className="text-[10px] text-shadow mt-2 mb-3">
-              Amount in MATIC
-            </span>
-            <div className="flex w-full">
-              <Button className="w-full mr-1" onClick={() => setIsOpen(false)}>
-                <span className="text-xs whitespace-nowrap">Close</span>
-              </Button>
-              <Button className="w-full ml-1" onClick={onDonate}>
-                <span className="text-xs whitespace-nowrap">Donate</span>
-              </Button>
+          )}
+          {state.matches("donated") && (
+            <div className="flex flex-col items-center">
+              <img id="richBegger" src={richBegger} className="w-24" />
+              <p className="mb-4">Thank you!</p>
             </div>
-          </div>
+          )}
+          {state.matches("error") && (
+            <div className="flex flex-col items-center">
+              <img id="richBegger" src={humanDeath} />
+              <p className="my-4">Oh no! Something went wrong!</p>
+            </div>
+          )}
         </Panel>
       </Modal>
     </div>
