c2c030f5dcc433f2b0b6355ac451f9bd894573fe
j923sneaks <j923sneaks@gmail.com>
2022-02-19 23:37:09 +0800

    boosted sell price

---
+++ src/features/game/lib/pricing.ts
@@ -0,0 +1,15 @@
+import { Inventory } from "../types/game";
+import { Crop } from "../types/crops";
+
+/**
+ * TODO 
+ *  - find better way to check inventory w/o passing it each time
+ *  - add more conditions
+ */
+export const getSellPrice = (crop: Crop, inventory: Inventory) => {
+  if (crop.name === "Cauliflower" && inventory["Golden Cauliflower"]?.greaterThanOrEqualTo(1)) {
+    return crop.sellPrice.mul(2);
+  }
+
+  return crop.sellPrice;
+}
