ecb6e9c4b04ebd671578963831d734e5f20e3cb6
Adam Hannigan <adam@payble.com.au>
2022-02-21 17:55:54 -0300

    [FIX] Styling improvements

--- src/features/bakery/Bakery.tsx
+++ src/features/bakery/Bakery.tsx
@@ -13,43 +13,43 @@ import { Action } from "components/ui/Action";
 import { GRID_WIDTH_PX } from "features/game/lib/constants";
 
 export const Bakery: React.FC = () => {
-    const { gameService } = useContext(Context);
-    const [gameState] = useActor(gameService);
-    const [isOpen, setIsOpen] = React.useState(false);
+  const { gameService } = useContext(Context);
+  const [gameState] = useActor(gameService);
+  const [isOpen, setIsOpen] = React.useState(false);
 
-    const isNotReadOnly = !gameState.matches("readonly");
+  const isNotReadOnly = !gameState.matches("readonly");
 
-    return (
-        <div
-            className="z-10 absolute"
-            // TODO some sort of coordinate system
-            style={{
-                width: `${GRID_WIDTH_PX * 3.7}px`,
-                height: `${GRID_WIDTH_PX * 3.5}px`,
-                left: `calc(50% - ${GRID_WIDTH_PX * -11.8}px)`,
-                top: `calc(50% - ${GRID_WIDTH_PX * 17}px)`,
-            }}
-        >
-            <img
-                src={bakery}
-                alt="market"
-                onClick={isNotReadOnly ? () => setIsOpen(true) : undefined}
-                className={classNames("w-full", {
-                    "cursor-pointer": isNotReadOnly,
-                    "hover:img-highlight": isNotReadOnly,
-                })}
-            />
-            {isNotReadOnly && (
-                <Action
-                    className="absolute -bottom-8 -left-6"
-                    text="Bake"
-                    icon={soup}
-                    onClick={() => setIsOpen(true)}
-                />
-            )}
-            <Modal centered show={isOpen} onHide={() => setIsOpen(false)}>
-                <Crafting onClose={() => setIsOpen(false)} />
-            </Modal>
-        </div>
-    );
+  return (
+    <div
+      className="z-10 absolute"
+      // TODO some sort of coordinate system
+      style={{
+        width: `${GRID_WIDTH_PX * 4}px`,
+        height: `${GRID_WIDTH_PX * 3.5}px`,
+        left: `calc(50% - ${GRID_WIDTH_PX * -11.8}px)`,
+        top: `calc(50% - ${GRID_WIDTH_PX * 17}px)`,
+      }}
+    >
+      <img
+        src={bakery}
+        alt="market"
+        onClick={isNotReadOnly ? () => setIsOpen(true) : undefined}
+        className={classNames("w-full", {
+          "cursor-pointer": isNotReadOnly,
+          "hover:img-highlight": isNotReadOnly,
+        })}
+      />
+      {isNotReadOnly && (
+        <Action
+          className="absolute -bottom-10 left-10"
+          text="Bake"
+          icon={soup}
+          onClick={() => setIsOpen(true)}
+        />
+      )}
+      <Modal centered show={isOpen} onHide={() => setIsOpen(false)}>
+        <Crafting onClose={() => setIsOpen(false)} />
+      </Modal>
+    </div>
+  );
 };
