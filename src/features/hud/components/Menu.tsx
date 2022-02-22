97bc891ecbc40abb76d91ad3c2ea6cb173f89696
Pranav Yadav <pranavyadav3912@gmail.com>
2022-02-23 00:52:32 +0530

    [CHORE]: Layout fix

--- src/features/hud/components/Menu.tsx
+++ src/features/hud/components/Menu.tsx
@@ -19,6 +19,7 @@ import radish from "assets/icons/radish.png";
 import water from "assets/icons/expression_working.png";
 import token from "assets/icons/token.gif";
 import timer from "assets/icons/timer.png";
+import farmImg from "/farms/farm.png";
 import { useTour } from "@reactour/tour";
 import { TourStep } from "features/game/lib/Tour";
 import { canSync } from "features/game/lib/whitelist";
@@ -69,6 +70,7 @@ export const Menu = () => {
   };
 
   const handleCopyFarmURL = (): void => {
+    // copy to clipboard
     navigator.clipboard.writeText(farmURL as string);
     setTooltipMessage("Copied!");
     setShowLabel(true);
@@ -252,18 +254,31 @@ export const Menu = () => {
             <h1>Share Your Farm Link</h1>
           </Modal.Header>
           <Modal.Body>
-            <p className="text-xs">
-              Show off to fellow farmers by sharing your farm link (URL), to
-              directly visit your farm !
-            </p>
-            <InnerPanel className="mt-4 hover:bg-brown-200 !p-0">
+            <div className="row justify-content-center align-items-center">
+              <div className="flex d-none d-sm-block col-sm col justify-content-center align-items-center">
+                <p className="text-sm whitespace-normal">
+                  Show off to fellow farmers by sharing your farm link (URL), to
+                  directly visit your farm !
+                </p>
+              </div>
+              <div className="flex col-sm-12 col justify-content-center md-px-4 lg-px-4 align-items-center">
+                <img
+                  src={farmImg}
+                  className="w-64 md-mt-2"
+                  alt="Sunflower-Land Farm NFT Image"
+                />
+              </div>
+            </div>
+            <InnerPanel className="mt-2 hover:bg-brown-200 !p-0">
               <div
                 className="p-1 cursor-pointer hover:bg-brown-200"
                 onClick={handleCopyFarmURL}
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
               >
-                <span className="text-xs hover:bg-brown-200">{farmURL}</span>
+                <span className="text-xs hover:bg-brown-200 whitespace-normal">
+                  {farmURL}
+                </span>
               </div>
             </InnerPanel>
             <div
