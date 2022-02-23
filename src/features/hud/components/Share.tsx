6edb2d6e7fe39907a6415e274fe0892fe9244fce
Pranav Yadav <pranavyadav3912@gmail.com>
2022-02-23 21:36:25 +0530

    [feat]: Copy BTN Icon & Tweet BTN

--- src/features/hud/components/Share.tsx
+++ src/features/hud/components/Share.tsx
@@ -32,6 +32,15 @@ export const Share: React.FC<Props> = ({ farmURL, isOpen, onClose }) => {
     }, 1500);
   };
 
+  const handleTweetClick = () => {
+    window.open(
+      encodeURI(
+        `https://twitter.com/intent/tweet?text=Visit My Sunflower Land Farm \uD83D\uDC47\n${farmURL}&ref_src=https://sunflower-land.com`
+      ),
+      "_blank"
+    );
+  };
+
   const handleMouseEnter = () => {
     setShowLabel(true);
   };
@@ -67,18 +76,38 @@ export const Share: React.FC<Props> = ({ farmURL, isOpen, onClose }) => {
               />
             </div>
           </div>
-          <InnerPanel className="mt-2 hover:bg-brown-200 !p-0">
-            <div
-              className="p-1 cursor-pointer hover:bg-brown-200"
-              onClick={handleCopyFarmURL}
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
-            >
-              <span className="text-xs hover:bg-brown-200 whitespace-normal">
+          <div className="mt-2 bg-brown-200 p-1">
+            <div className="flex justify-content-between p-1 align-items-center">
+              <span className="text-[0.55rem] sm:text-xs whitespace-normal mr-2">
                 {farmURL}
               </span>
+              <span
+                className="cursor-pointer scale-[1.5]"
+                onClick={handleCopyFarmURL}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              >
+                <svg
+                  className="fill-white hover:fill-brown-300"
+                  aria-hidden="true"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  version="1.1"
+                  width="16"
+                  data-view-component="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 010 1.5h-1.5a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-1.5a.75.75 0 011.5 0v1.5A1.75 1.75 0 019.25 16h-7.5A1.75 1.75 0 010 14.25v-7.5z"
+                  ></path>
+                  <path
+                    fillRule="evenodd"
+                    d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0114.25 11h-7.5A1.75 1.75 0 015 9.25v-7.5zm1.75-.25a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-7.5a.25.25 0 00-.25-.25h-7.5z"
+                  ></path>
+                </svg>
+              </span>
             </div>
-          </InnerPanel>
+          </div>
           <div
             className={`absolute mr-5 transition duration-400 pointer-events-none ${
               showLabel ? "opacity-100" : "opacity-0"
@@ -88,8 +117,8 @@ export const Share: React.FC<Props> = ({ farmURL, isOpen, onClose }) => {
           </div>
         </Modal.Body>
         <Modal.Footer className="justify-content-center">
-          <Button className="text-s w-1/4 px-1" onClick={handleCopyFarmURL}>
-            Copy
+          <Button className="text-s w-1/4 px-1" onClick={handleTweetClick}>
+            Tweet
           </Button>
           <Button
             className="text-s w-1/4 px-1"
