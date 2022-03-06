408926e8eee06e908a53e4c5196e10d382c26023
j923sneaks <j923sneaks@gmail.com>
2022-03-06 19:15:36 +0800

    remove notif counter code and deps

--- src/features/crops/Crops.tsx
+++ src/features/crops/Crops.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import { AppIconProvider } from "features/crops/AppIconProvider";
 import { GRID_WIDTH_PX } from "features/game/lib/constants";
 
 import { Section } from "lib/utils/useScrollIntoView";
@@ -15,49 +14,46 @@ import { CropZoneFour } from "./components/CropZoneFour";
 
 export const Crops: React.FC = () => {
   return (
-    <AppIconProvider>
-      {/*Container*/}
+    <div
+      style={{
+        width: `${GRID_WIDTH_PX * 25}px`,
+        height: `${GRID_WIDTH_PX * 12}px`,
+        left: `calc(50% - ${GRID_WIDTH_PX * 13}px)`,
+        top: `calc(50% - ${GRID_WIDTH_PX * 23}px)`,
+      }}
+      className="absolute"
+    >
+      {/* Navigation Center Point */}
+      <span
+        id={Section.Crops}
+        className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
+      />
+
+      <CropZoneOne />
+      <CropZoneTwo />
+      <CropZoneThree />
+      <CropZoneFour />
+
       <div
         style={{
-          width: `${GRID_WIDTH_PX * 25}px`,
-          height: `${GRID_WIDTH_PX * 12}px`,
-          left: `calc(50% - ${GRID_WIDTH_PX * 13}px)`,
-          top: `calc(50% - ${GRID_WIDTH_PX * 23}px)`,
+          width: `${GRID_WIDTH_PX * 3.2}px`,
+          position: "absolute",
+          right: `${GRID_WIDTH_PX * 0.5}px`,
+          top: `${GRID_WIDTH_PX * 1.8}px`,
         }}
-        className="absolute"
+        className="relative"
       >
-        {/* Navigation Center Point */}
-        <span
-          id={Section.Crops}
-          className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
-        />
-
-        <CropZoneOne />
-        <CropZoneTwo />
-        <CropZoneThree />
-        <CropZoneFour />
-
-        <div
+        <img src={house} alt="house" className="w-full" />
+        <img
+          src={smoke}
           style={{
-            width: `${GRID_WIDTH_PX * 3.2}px`,
+            width: `${GRID_WIDTH_PX * 0.7}px`,
             position: "absolute",
-            right: `${GRID_WIDTH_PX * 0.5}px`,
-            top: `${GRID_WIDTH_PX * 1.8}px`,
+            left: `${GRID_WIDTH_PX * 0.12}px`,
+            top: `${GRID_WIDTH_PX * 0.77}px`,
           }}
-          className="relative"
-        >
-          <img src={house} alt="house" className="w-full" />
-          <img
-            src={smoke}
-            style={{
-              width: `${GRID_WIDTH_PX * 0.7}px`,
-              position: "absolute",
-              left: `${GRID_WIDTH_PX * 0.12}px`,
-              top: `${GRID_WIDTH_PX * 0.77}px`,
-            }}
-          />
-        </div>
+        />
       </div>
-    </AppIconProvider>
+    </div>
   );
 };
