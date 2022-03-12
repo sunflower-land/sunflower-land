f03121148428eff94fdeecad5bec519c25ba7902
Romulo Furtado <rofrtd@gmail.com>
2022-03-12 20:30:54 +1100

    add new song to playlist

--- src/assets/songs/playlist.ts
+++ src/assets/songs/playlist.ts
@@ -1,6 +1,12 @@
 import willow_tree from "./willow_tree.mp3";
+import harvesting from "./harvesting.mp3";
 // Playlist hardcoded, but eventually it will be fetched from the server
 const song_list = [
+  {
+    artist: "Romy & Rick",
+    name: "Harvesting",
+    path: harvesting,
+  },
   {
     artist: "Romy",
     name: "Willow Tree",
