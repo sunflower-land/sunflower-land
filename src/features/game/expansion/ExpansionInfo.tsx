1eebd43b88914e09b775711c4cd5a45c336ee73f
Spencer <spencer.dezart.smith@gmail.com>
2022-03-15 21:30:49 +1100

    Add expansion info

---
+++ src/features/game/expansion/ExpansionInfo.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+
+import curly from "assets/npcs/curly_hair.png";
+import questionMark from "assets/icons/expression_confused.png";
+
+import { GRID_WIDTH_PX } from "features/game/lib/constants";
+
+import { Panel } from "components/ui/Panel";
+
+export const ExpansionInfo: React.FC = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <div
+      style={{
+        width: `${GRID_WIDTH_PX * 2}px`,
+        height: `${GRID_WIDTH_PX * 2.2}px`,
+        right: `calc(50% - ${GRID_WIDTH_PX * 36.7}px)`,
+        bottom: `calc(50% - ${GRID_WIDTH_PX * 40.4}px)`,
+      }}
+      className="absolute"
+    >
+      <div className="relative h-full">
+        <img
+          src={questionMark}
+          className="absolute w-3 left-[42%] z-10 animate-float"
+        />
+        <img
+          src={curly}
+          onClick={() => setShowModal(true)}
+          className="absolute w-20 bottom-0 left-1/2 -translate-x-1/2 cursor-pointer hover:img-highlight"
+        />
+      </div>
+
+      <Modal centered show={showModal} onHide={() => setShowModal(false)}>
+        <Panel>
+          <div className="flex items-start">
+            <img src={questionMark} className="h-10 img-highlight mr-3" />
+            <div className="flex-1">
+              <span className="text-shadow block">
+                {`I can't see anything through these thick clouds!`}
+              </span>
+              <span className="text-shadow block mt-4">
+                I wonder if I will ever get to see what lies beneath them all?
+              </span>
+            </div>
+          </div>
+        </Panel>
+      </Modal>
+    </div>
+  );
+};
