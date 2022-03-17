c93a0017425a48c3ebd993eb3fd9e156f564da8b
Pranav Yadav <pranavyadav3912@gmail.com>
2022-03-18 01:56:36 +0530

    [chore]: Layout touchups :)

--- src/features/game/components/Offline.tsx
+++ src/features/game/components/Offline.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 
-import death from "assets/npcs/skeleton_death.gif";
+// import death from "assets/npcs/skeleton_death.gif";
+import alert from "assets/icons/expression_alerted.png";
 
 export const Offline: React.FC = () => {
   return (
     <div className="flex flex-col items-center p-2">
-      <span className="text-shadow text-center">Offline! :(</span>
-      <img src={death} className="w-1/2" />
-      <span className="text-shadow text-xs text-center">
-        Uh oh, looks like the goblins detected that you are offline.
-      </span>
-      <span className="text-shadow text-xs text-center mt-2">
-        Please connect to the internet and refresh / reload this page :)
-      </span>
+      {/* <img src={death} className="w-1/2" /> */}
+      <img src={alert} className="w-3" />
+      <p className="text-center my-3">OFFLINE! :(</p>
+      <p className="text-shadow text-xs text-center mb-2">
+        Uh oh, looks like you are disconnected, so the goblins detected that you
+        are OFFLINE.
+      </p>
+      <p className="text-shadow text-xs text-center mt-2">
+        Please connect to the internet (or) reload this page :)
+      </p>
     </div>
   );
 };
