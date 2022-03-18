3046dead161e97a254e4f9e82ed48a2021cabb7a
Pranav Yadav <Pranavyadav3912@gmail.com>
2022-03-18 12:12:22 +0530

    [chore]: tidy up - rm extra, commented code
    :)
    
    
    [skip ci]

--- src/features/game/components/Offline.tsx
+++ src/features/game/components/Offline.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 
-// import death from "assets/npcs/skeleton_death.gif";
 import alert from "assets/icons/expression_alerted.png";
 
 export const Offline: React.FC = () => {
   return (
     <div className="flex flex-col items-center p-2">
-      {/* <img src={death} className="w-1/2" /> */}
       <img src={alert} className="w-3" />
       <p className="text-center my-3">OFFLINE! :(</p>
       <p className="text-shadow text-xs text-center mb-2">
