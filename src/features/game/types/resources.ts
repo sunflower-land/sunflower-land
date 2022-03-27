da994386c1d4b79f9330ac4bb1afcf9f7df0de90
Tiffany Domugho <tiffany@domugho.com>
2022-03-28 03:42:20 +0800

    feat: add exp to resources file

--- src/features/game/types/resources.ts
+++ src/features/game/types/resources.ts
@@ -8,24 +8,31 @@ export type ResourceName =
 
 export type Resource = {
   description: string;
+  experience: number;
 };
 export const RESOURCES: Record<ResourceName, Resource> = {
   Wood: {
     description: "Used to craft items",
+    experience: 0.02,
   },
   Stone: {
     description: "Used to craft items",
+    experience: 0.05,
   },
   Iron: {
     description: "Used to craft items",
+    experience: 2,
   },
   Gold: {
     description: "Used to craft items",
+    experience: 5,
   },
   Egg: {
     description: "Used to craft items",
+    experience: 0,
   },
   Chicken: {
     description: "Used to lay eggs",
+    experience: 0,
   },
 };
