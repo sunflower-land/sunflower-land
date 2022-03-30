23ed1e1d6df95a453675872904b68b4b861bdf11
Z1kou <98262339+Z1kou@users.noreply.github.com>
2022-03-30 14:45:23 +0000

    Fix Maj

--- src/features/game/lib/pricing.ts
+++ src/features/game/lib/pricing.ts
@@ -7,7 +7,7 @@ import { Crop } from "../types/crops";
 export const getSellPrice = (crop: Crop, inventory: Inventory) => {
   let price = crop.sellPrice;
 
-    If (inventory["Green Thumb"]?.greaterThanOrEqualTo(1)) {
+    if (inventory["Green Thumb"]?.greaterThanOrEqualTo(1)) {
     price = price.mul(1.05);
   }
 
