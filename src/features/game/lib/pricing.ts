302605fa78902ee74b05795595f5f02cf3cc6aa9
Z1kou <98262339+Z1kou@users.noreply.github.com>
2022-03-30 14:37:20 +0000

    Update scare crow

--- src/features/game/lib/pricing.ts
+++ src/features/game/lib/pricing.ts
@@ -9,10 +9,7 @@ export const getSellPrice = (crop: Crop, inventory: Inventory) => {
 
   if (inventory["Green Thumb"]?.greaterThanOrEqualTo(1)) {
     price = price.mul(1.05);
-  }
-  else if (inventory["Scarecrow"]?.greaterThanOrEqualTo(1)) {
-    price = price.mul(1.20);
-  }
+  
   return price;
 };
   
