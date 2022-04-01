b024243542d8cb0553879b3ef1c06fb60b4768bf
Adam Hannigan <adam@payble.com.au>
2022-04-01 10:24:01 -0300

    Offset chest randomly

--- src/features/crops/components/CropReward.tsx
+++ src/features/crops/components/CropReward.tsx
@@ -1,10 +1,12 @@
 import { Panel } from "components/ui/Panel";
-import React, { useContext, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { Modal } from "react-bootstrap";
 
 import { Reward } from "features/game/types/game";
 
 import secure from "assets/npcs/synced.gif";
+import idle from "assets/npcs/idle.gif";
+
 import { Button } from "components/ui/Button";
 import { ITEM_DETAILS } from "features/game/types/images";
 import { Context } from "features/game/GameProvider";
@@ -15,6 +17,11 @@ interface Props {
   onCollected: () => void;
 }
 
+function randomIntFromInterval(min: number, max: number) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 export const CropReward: React.FC<Props> = ({
   reward,
   onCollected,
@@ -22,6 +29,9 @@ export const CropReward: React.FC<Props> = ({
 }) => {
   const { gameService } = useContext(Context);
   const [opened, setOpened] = useState(false);
+
+  const offset = useRef(randomIntFromInterval(10, 300));
+
   if (!reward) {
     return null;
   }
@@ -56,11 +66,18 @@ export const CropReward: React.FC<Props> = ({
             </>
           ) : (
             <>
-              <img
-                src={secure}
-                className="w-1/2 hover:img-highlight cursor-pointer"
-                onClick={open}
-              />
+              <div className="flex items-center">
+                <img src={idle} className="w-16" />
+                <img
+                  src={secure}
+                  className="w-16 hover:img-highlight cursor-pointer"
+                  onClick={open}
+                  // Random offset to make it unpredictable to click
+                  style={{
+                    marginLeft: `${offset.current}px`,
+                  }}
+                />
+              </div>
 
               <span className="text-sm">Tap the chest to open it</span>
             </>
