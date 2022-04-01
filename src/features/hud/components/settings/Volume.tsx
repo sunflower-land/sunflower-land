752fa252373242f9bbe9add7c5e07ab25af01ac4
Pranav Yadav <pranavyadav3912@gmail.com>
2022-04-01 22:14:59 +0530

    [CHORE]: Volume Settings rfc Init Commit

---
+++ src/features/hud/components/settings/Volume.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useAudioContext } from "lib/utils/hooks/useAudioContext";
+
+export default function Volume() {
+  const audioContext = useAudioContext();
+
+  console.log("Audio Context initiated by useAudioContext hook :)");
+
+  return <div>Volume Controls</div>;
+}
