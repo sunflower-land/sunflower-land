24a230a98533d740dcf1f7f131767532c158881d
Pranav Yadav <pranavyadav3912@gmail.com>
2022-04-06 01:12:15 +0530

    [FEAT]: Abstract Settings :rocket:
    - added toggle switches for bg music and SFX
    - added reset session confirmation
    
    - Note: for OP Settings :flame: checkout to prev commit,
      commit b73cc70ee205b4b98feaeeea9fe2beb513981a7f
      no need to start from scratch :)

--- src/features/hud/components/settings/Settings.tsx
+++ src/features/hud/components/settings/Settings.tsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 
-import { Button } from "components/ui/Button";
+import { metamask } from "lib/blockchain/metamask";
+import { removeSession } from "features/auth/actions/login";
+
+import { useVolumeControls } from "features/hud/lib/volumeControls";
+
 import { Panel } from "components/ui/Panel";
-import { Tab } from "components/ui/Tab";
+import { Button } from "components/ui/Button";
 import { Modal } from "react-bootstrap";
 
-import token from "assets/icons/token.gif";
 import close from "assets/icons/close.png";
-
-import Volume from "./Volume";
-import ResetSession from "./ResetSession";
 
 interface Props {
   isOpen: boolean;
@@ -17,72 +17,112 @@ interface Props {
 }
 
 export const Settings: React.FC<Props> = ({ isOpen, onClose }) => {
-  const [tab, setTab] = useState<"volume" | "session" | "other">("volume");
+  const [isBgMusicMuted, setIsBgMusicMuted] = useState(false);
+  const [isSFXMuted, setIsSFXMuted] = useState(false);
+  const [isResetConfOpen, setIsResetConfOpen] = useState(false);
+  const [toggleAllSFX] = useVolumeControls();
 
-  const handleSaveClick = () => {
-    console.log("Settings Saved :)");
+  const address = metamask.myAccount as string;
+
+  const handleResetSessionClick = () => {
+    removeSession(address);
+    window.location.reload();
+  };
+
+  const confirmResetSession = () => setIsResetConfOpen(true);
+
+  const handleToggleBgMusic = () => {
+    //
+  };
+  const handleToggleSFX = () => {
+    toggleAllSFX(); //incomplete
   };
 
   return (
-    <Modal
-      show={isOpen}
-      onHide={() => {
-        onClose();
-        setTab("volume");
-      }}
-      centered
-    >
-      <Panel className="pt-5 relative max-w-5xl">
-        <div className="flex justify-between absolute top-1.5 left-0.5 right-0 items-center">
-          <div className="flex">
-            <Tab isActive={tab === "volume"} onClick={() => setTab("volume")}>
-              <img src={token} className="h-5 mr-2" />
-              <span className="text-xs text-shadow sm:text-sm">Volume</span>
-            </Tab>
-            <Tab isActive={tab === "session"} onClick={() => setTab("session")}>
-              <img src={token} className="h-5 mr-2" />
-              <span className="text-xs text-shadow sm:text-sm">Session</span>
-            </Tab>
-            <Tab isActive={tab === "other"} onClick={() => setTab("other")}>
-              <img src={token} className="h-5 mr-2" />
-              <span className="text-xs text-shadow sm:text-sm">Other</span>
-            </Tab>
-          </div>
+    <>
+      <Modal show={isOpen} onHide={() => onClose()} centered>
+        <Panel className="relative pt-0">
           <img
             src={close}
-            className="h-6 cursor-pointer mr-2 mb-1"
-            onClick={() => {
-              onClose();
-              setTab("volume");
-            }}
+            className="h-6 cursor-pointer top-3 right-4 absolute"
+            onClick={() => onClose()}
           />
-        </div>
-
-        <div
-          style={{
-            minHeight: "200px",
-          }}
-        >
-          {tab === "volume" && <Volume />}
-          {tab === "session" && <ResetSession />}
-          {tab === "other" && (
-            <div>
-              <p>
-                Other Settings/ Controls/ Preferences are being developed by the
-                SFL team and community developers.
-              </p>
-              <p>Please request/ suggest other settings TBA, on discord :)</p>
+          <div className="flex-col px-2">
+            <div className="row mt-3 items-center">
+              <div className="col-8">Background Music</div>
+              <div className="col-4 flex justify-center items-center">
+                <div className="form-check form-switch">
+                  <input
+                    className="form-check-input form-check-input-md brown-400 bg-brown-200 cursor-pointer checked:bg-brown-400 checked:brown-200 checked:border-brown-400 focus:shadow-[0_0_0_0.25rem_rgb(231,168,115,0.5)] focus:border-brown-400 focus:brown-400 focus:bg-none focus:bg-brown-200"
+                    style={{ width: "3.5rem", height: "1.75rem" }}
+                    type="checkbox"
+                    role="switch"
+                    id="bgMusicSwitch"
+                    defaultChecked={isBgMusicMuted}
+                    onChange={() => setIsBgMusicMuted(!isBgMusicMuted)}
+                  />
+                </div>
+              </div>
             </div>
-          )}
-        </div>
-        <div className="flex justify-center">
-          {tab !== "session" && (
-            <Button className="text-sm w-1/4 px-1" onClick={handleSaveClick}>
-              Save
+            <div className="row mt-3 items-center">
+              <div className="col-8">SFX</div>
+              <div className="col-4 flex justify-center items-center">
+                <div className="form-check form-switch">
+                  <input
+                    className="form-check-input form-check-input-md brown-400 bg-brown-200 cursor-pointer checked:bg-brown-400 checked:brown-200 checked:border-brown-400 focus:shadow-[0_0_0_0.25rem_rgb(231,168,115,0.5)] focus:border-brown-400 focus:brown-400 focus:bg-none focus:bg-brown-200"
+                    style={{ width: "3.5rem", height: "1.75rem" }}
+                    type="checkbox"
+                    role="switch"
+                    id="sfxSwitch"
+                    defaultChecked={isSFXMuted}
+                    onChange={() => setIsSFXMuted(!isSFXMuted)}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-center mt-3 mb-2">
+              <Button
+                className="text-sm w-auto px-1 mt-3"
+                onClick={() => confirmResetSession()}
+              >
+                Reset Session
+              </Button>
+            </div>
+          </div>
+        </Panel>
+      </Modal>
+      <Modal
+        centered
+        show={isResetConfOpen}
+        onHide={() => setIsResetConfOpen(false)}
+      >
+        <Panel className="md:w-4/5 m-auto">
+          <div className="m-auto flex flex-col">
+            <span className="text-sm text-center text-shadow">
+              Are you sure you want to <br className="hidden md:block" />
+              &quot;Reset&quot; the current session?
+            </span>
+            <span className="text-xs mx-1 text-shadow">
+              Note: After reset, you will need to resign the request inorder to
+              enter into the game.
+            </span>
+          </div>
+          <div className="flex justify-content-around p-1">
+            <Button
+              className="text-xs"
+              onClick={() => handleResetSessionClick()}
+            >
+              Yes
             </Button>
-          )}
-        </div>
-      </Panel>
-    </Modal>
+            <Button
+              className="text-xs ml-2"
+              onClick={() => setIsResetConfOpen(false)}
+            >
+              No
+            </Button>
+          </div>
+        </Panel>
+      </Modal>
+    </>
   );
 };
