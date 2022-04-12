f8dbff0e642d866be36b9792a9342b496c1bc261
Adam Hannigan <adam@payble.com.au>
2022-04-12 13:32:16 +1000

    [FIX] Infinite loop

--- src/lib/utils/screen.ts
+++ src/lib/utils/screen.ts
@@ -5,6 +5,19 @@ type Vector = {
   y: number;
 };
 
+// https://stackoverflow.com/questions/6865832/detecting-if-a-point-is-of-a-line-segment
+function calcDistancePointToLine(line1: Vector, line2: Vector, pnt: Vector) {
+  const L2 =
+    (line2.x - line1.x) * (line2.x - line1.x) +
+    (line2.y - line1.y) * (line2.y - line1.y);
+  if (L2 == 0) return false;
+  const s =
+    ((line1.y - pnt.y) * (line2.x - line1.x) -
+      (line1.x - pnt.x) * (line2.y - line1.y)) /
+    L2;
+  return Math.abs(s) * Math.sqrt(L2);
+}
+
 class ScreenTracker {
   private movements: Vector[] = [];
   private tracks = 0;
@@ -21,61 +34,49 @@ class ScreenTracker {
   }
 
   public calculate(): boolean {
-    if (detectMobile()) {
+    try {
+      if (detectMobile()) {
+        return true;
+      }
+
+      let isValid = true;
+
+      const step = Math.floor(this.movements.length / 10) || 1;
+      // Pick random points along the line
+      let points: Vector[] = [];
+      for (let i = 0; i < this.movements.length; i += step) {
+        points = [...points, this.movements[i]];
+      }
+
+      const areCollinear = (vectors: Vector[]) => {
+        return vectors.every((vector) => {
+          const distance = calcDistancePointToLine(
+            vectors[0],
+            vectors[vectors.length - 1],
+            vector
+          );
+          return distance === 0;
+        });
+      };
+
+      const isCollinear = areCollinear(points);
+
+      if (isCollinear) {
+        this.tracks += 3;
+      } else if (this.tracks > 0) {
+        this.tracks = 0;
+      }
+
+      if (this.tracks > 10) {
+        isValid = false;
+      }
+
+      this.movements = [];
+      return isValid;
+    } catch (e) {
+      console.log({ e });
       return true;
     }
-
-    let isValid = true;
-
-    const step = Math.floor(this.movements.length / 10);
-    // Pick random points along the line
-    let points: Vector[] = [];
-    for (let i = 0; i < this.movements.length; i += step) {
-      points = [...points, this.movements[i]];
-    }
-
-    // https://stackoverflow.com/questions/6865832/detecting-if-a-point-is-of-a-line-segment
-    function calcDistancePointToLine(
-      line1: Vector,
-      line2: Vector,
-      pnt: Vector
-    ) {
-      const L2 =
-        (line2.x - line1.x) * (line2.x - line1.x) +
-        (line2.y - line1.y) * (line2.y - line1.y);
-      if (L2 == 0) return false;
-      const s =
-        ((line1.y - pnt.y) * (line2.x - line1.x) -
-          (line1.x - pnt.x) * (line2.y - line1.y)) /
-        L2;
-      return Math.abs(s) * Math.sqrt(L2);
-    }
-
-    const areCollinear = (vectors: Vector[]) => {
-      return vectors.every((vector) => {
-        const distance = calcDistancePointToLine(
-          vectors[0],
-          vectors[vectors.length - 1],
-          vector
-        );
-        return distance === 0;
-      });
-    };
-
-    const isCollinear = areCollinear(points);
-
-    if (isCollinear) {
-      this.tracks += 3;
-    } else if (this.tracks > 0) {
-      this.tracks = 0;
-    }
-
-    if (this.tracks > 10) {
-      isValid = false;
-    }
-
-    this.movements = [];
-    return isValid;
   }
 
   public start() {
