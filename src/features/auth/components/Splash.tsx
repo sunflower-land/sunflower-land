7189ec0ac89ee88c31cb116d52f7d911d521dbfe
Ricardo Furtado <ricardofurtado2609@gmail.com>
2022-04-13 21:55:01 +1000

    add banner with new token launch date

--- src/features/auth/components/Splash.tsx
+++ src/features/auth/components/Splash.tsx
@@ -4,10 +4,36 @@ import logo from "assets/brand/logo.png";
 import clouds from "assets/brand/clouds.png";
 import sunflowers from "assets/brand/sunflower_border.png";
 import pumpkin from "assets/crops/pumpkin/crop.png";
+import { InnerPanel } from "components/ui/Panel";
 
 export const Splash: React.FC = () => {
   return (
     <div className="bg-blue-600 w-full h-full flex relative items-center justify-center">
+      <InnerPanel
+        className="absolute 
+      z-50 
+      top-0 
+      left-1/2 
+      -translate-x-1/2
+      flex 
+      align-items-center
+      justify-center
+      text-center
+      mt-2
+      w-[calc(100%-10px)]
+      sm:w-[calc(100%-100px)]
+      md:w-fit
+      md:whitespace-nowrap
+      md:p-8
+      p-6
+      sm:h-fit
+      text-xs
+      sm:text-sm 
+      h-16
+      text-white"
+      >
+        SFL is not tradable until May 9th. Beware of scams
+      </InnerPanel>
       <div className="relative mb-96 animate-float z-10">
         <img
           src={pumpkin}
