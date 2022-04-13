08d1385301f14992214283fb471cc52cceb89478
Tiffany Domugho <tiffany@domugho.com>
2022-04-14 01:29:36 +0800

    feat: time left panel component

---
+++ src/components/ui/TimeLeftPanel.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { InnerPanel } from "components/ui/Panel";
+import { secondsToMidString } from "lib/utils/time";
+import classNames from "classnames";
+
+interface Props {
+  text?: string;
+  timeLeft: number;
+  showTimeLeft?: Boolean;
+}
+
+export const TimeLeftPanel: React.FC<Props> = ({ text = "", showTimeLeft = false, timeLeft }) => {
+  return (
+    <InnerPanel
+      className={classNames(
+        "ml-10 transition-opacity absolute whitespace-nowrap sm:opacity-0 bottom-5 w-fit left-9 z-20 pointer-events-none",
+        {
+          "opacity-100": showTimeLeft,
+          "opacity-0": !showTimeLeft,
+        }
+      )}
+    >
+      <div className="flex flex-col text-xxs text-white text-shadow ml-2 mr-2">
+        <span className="flex-1">{text}</span>
+        <span className="flex-1">{secondsToMidString(timeLeft)}</span>
+      </div>
+    </InnerPanel>
+  );
+};
