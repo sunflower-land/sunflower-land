34e535a6f6b2d65f6c46b29a2cafa48401829777
Sascha Becker <s.becker@wertarbyte.com>
2022-04-24 00:45:10 +0200

    Add react hooks eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier",
     "plugin:prettier/recommended",
