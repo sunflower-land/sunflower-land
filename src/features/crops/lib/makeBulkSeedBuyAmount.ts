425bd976f53491fb2d17df01dfb7b11e246ad54a
Simon Angell <simon.angell307@gmail.com>
2022-04-25 10:51:45 +1000

    [FEAT] Improve make fn readability

--- src/features/crops/lib/makeBulkSeedBuyAmount.ts
+++ src/features/crops/lib/makeBulkSeedBuyAmount.ts
@@ -3,9 +3,9 @@ import { Decimal } from "decimal.js-light";
 export const MAX_BULK_SEED_BUY_AMOUNT = 10;
 
 export function makeBulkSeedBuyAmount(stock: Decimal) {
-  const amount = stock.lessThan(MAX_BULK_SEED_BUY_AMOUNT)
-    ? stock.toDecimalPlaces(0, Decimal.ROUND_DOWN)
-    : new Decimal(MAX_BULK_SEED_BUY_AMOUNT);
+  if (stock.lessThan(MAX_BULK_SEED_BUY_AMOUNT)) {
+    return stock.toDecimalPlaces(0, Decimal.ROUND_DOWN).toNumber();
+  }
 
-  return amount.toNumber();
+  return MAX_BULK_SEED_BUY_AMOUNT;
 }
