65c90895752f36e0fcfb89ef2b3cafa4d3fded64
Raphael-ui <picotraphael11@gmail.com>
2022-04-25 19:59:14 +0200

    suppressed non-event checking for the keyup event --> back to normal

--- src/features/game/lib/mapMovement.ts
+++ src/features/game/lib/mapMovement.ts
@@ -47,9 +47,10 @@ const keyDownListener = (event: KeyboardEvent) => {
   }
 };
 
+//no fix for keyup, as a movement began before the opening of the modal needs to be stopped when the key is released
 const keyUpListener = (event: KeyboardEvent) => {
   const key = event.key.toLowerCase();
-  if (container && event.target === document.body) {
+  if (container) {
     if (key === "w" || key === "arrowup") {
       if (movementIntervals.moveUp !== undefined) {
         clearInterval(movementIntervals.moveUp);
