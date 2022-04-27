6eb6881ab60ff72a1d578c2e90cbc008f7bcd0bf
Adam Hannigan <adamhannigan81@gmail.com>
2022-04-27 12:29:08 +1000

    Set up basic machines

--- src/features/game/Goblins.tsx
+++ src/features/game/Goblins.tsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import ScrollContainer from "react-indiana-drag-scroll";
+
+import background from "assets/land/background.png";
+
+import { Game } from "./Game";
+import { GameProvider } from "./GameProvider";
+import { ToastProvider } from "./toast/ToastQueueProvider";
+import mapMovement from "./lib/mapMovement";
+import { ExpansionInfo } from "./expansion/ExpansionInfo";
+import { GoblinProvider } from "./GoblinProvider";
 
 export const Goblins: React.FC = () => {
+  // catching and passing scroll container to keyboard listeners
+  const container = useRef(null);
+
+  useEffect(() => {
+    mapMovement.addListeners(container.current);
+    return () => {
+      mapMovement.removeListeners();
+    };
+  }, [container]);
+
   // Load data
-  return <span>TODO</span>;
+  return (
+    <GoblinProvider>
+      <span>Goblin Village</span>
+    </GoblinProvider>
+  );
 };
