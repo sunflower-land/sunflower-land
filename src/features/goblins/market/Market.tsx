c6d72b0113b775015e5fb3a2964e4890f33cabe8
Romy Furtado <rofrtd@gmail.com>
2022-04-28 13:03:05 +1000

    add market

---
+++ src/features/goblins/market/Market.tsx
@@ -0,0 +1,54 @@
+import React, { useContext } from "react";
+import { useActor } from "@xstate/react";
+import { Modal } from "react-bootstrap";
+
+import { Context } from "features/game/GoblinProvider";
+
+import market from "assets/buildings/market_building.png";
+import plant from "assets/icons/plant.png";
+
+import { GRID_WIDTH_PX } from "features/game/lib/constants";
+import { Action } from "components/ui/Action";
+
+import { MarketItems } from "./MarketItems";
+import { Section } from "lib/utils/hooks/useScrollIntoView";
+import { marketAudio } from "lib/utils/sfx";
+
+export const Market: React.FC = () => {
+  const { goblinService } = useContext(Context);
+  const [goblinState] = useActor(goblinService);
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const handleMarketClick = () => {
+    setIsOpen(true);
+    marketAudio.play();
+  };
+
+  return (
+    <div
+      id={Section.Shop}
+      className="absolute cursor-pointer hover:img-highlight"
+      style={{
+        width: `${GRID_WIDTH_PX * 3}px`,
+        right: `${GRID_WIDTH_PX * 30}px`,
+        top: `${GRID_WIDTH_PX * 25}px`,
+      }}
+    >
+      <img
+        src={market}
+        alt="market"
+        onClick={handleMarketClick}
+        className="w-full"
+      />
+      <Action
+        className="absolute top-5 left-4"
+        text="Shop"
+        icon={plant}
+        onClick={handleMarketClick}
+      />
+      <Modal centered show={isOpen} onHide={() => setIsOpen(false)}>
+        <MarketItems onClose={() => setIsOpen(false)} />
+      </Modal>
+    </div>
+  );
+};
