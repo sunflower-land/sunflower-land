0bfeda7920c03bf38d352fec6d5c2681527301e8
Adam Hannigan <adamhannigan81@gmail.com>
2022-05-05 17:04:37 +1000

    Fix conflict

--- src/features/goblins/wishingWell/WishingWell.tsx
+++ src/features/goblins/wishingWell/WishingWell.tsx
@@ -1,12 +1,6 @@
 import React from "react";
-<<<<<<< HEAD
-
-import wishingWell from "assets/buildings/goblin_wishing_well.png";
-=======
-import { Modal } from "react-bootstrap";
 
 import wishingWell from "assets/buildings/wishing_well.png";
->>>>>>> 84f9a4b (Wishing well upgrades)
 import icon from "assets/brand/icon.png";
 
 import { WishingWellModal } from "./WishingWellModal";
@@ -26,15 +20,9 @@ export const WishingWell: React.FC = () => {
       className="absolute"
       // TODO some sort of coordinate system
       style={{
-<<<<<<< HEAD
         width: `${GRID_WIDTH_PX * 2.1}px`,
         right: `${GRID_WIDTH_PX * 12.1}px`,
         top: `${GRID_WIDTH_PX * 8.8}px`,
-=======
-        width: `${GRID_WIDTH_PX * 2}px`,
-        right: `${GRID_WIDTH_PX * 12.2}px`,
-        top: `${GRID_WIDTH_PX * -1}px`,
->>>>>>> 84f9a4b (Wishing well upgrades)
       }}
     >
       <div className="cursor-pointer hover:img-highlight">
@@ -46,11 +34,7 @@ export const WishingWell: React.FC = () => {
         />
         {
           <Action
-<<<<<<< HEAD
             className="absolute -bottom-[36px] -left-[5px]"
-=======
-            className="absolute -bottom-6 -left-2"
->>>>>>> 84f9a4b (Wishing well upgrades)
             text="Wish"
             icon={icon}
             onClick={openWell}
