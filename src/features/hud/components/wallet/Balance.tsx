4fe59353a114f8eb3fd41e132bdca62a3775d76b
Pranav Yadav <pranavyadav3912@gmail.com>
2022-05-05 12:00:10 +0530

    [CHORE] onClick show Wallet Modal :rocket:

--- src/features/hud/components/wallet/Balance.tsx
+++ src/features/hud/components/wallet/Balance.tsx
@@ -7,28 +7,28 @@ import token from "assets/icons/token.gif";
 
 import { Context } from "features/game/GameProvider";
 import Decimal from "decimal.js-light";
+import { Wallet } from "./Wallet";
 
 export const Balance: React.FC = () => {
   const { gameService } = useContext(Context);
   const [game] = useActor(gameService);
-  const [isShown, setIsShown] = useState(false);
+  const [showWallet, setShowWallet] = useState(false);
+
+  const handleClick = () => setShowWallet(true);
 
   return (
-    <InnerPanel className="fixed top-2 right-2 z-50 flex items-center shadow-lg cursor-pointer">
-      <img src={token} className="w-8 img-highlight" />
-      <span
-        className="text-white text-sm text-shadow ml-2"
-        onMouseEnter={() => setIsShown(true)}
-        onMouseLeave={() => setIsShown(false)}
-      >
-        {isShown === false ? (
-          game.context.state.balance
-            .toDecimalPlaces(3, Decimal.ROUND_DOWN)
-            .toString()
-        ) : (
-          <small>{game.context.state.balance.toString()}</small>
-        )}
-      </span>
-    </InnerPanel>
+    <>
+      <InnerPanel className="fixed top-2 right-2 z-50 flex items-center shadow-lg cursor-pointer">
+        <div className="flex justify-center items-center" onClick={handleClick}>
+          <img src={token} className="w-8 col img-highlight" />
+          <span className="col text-white text-sm text-shadow ml-2">
+            {game.context.state.balance
+              .toDecimalPlaces(3, Decimal.ROUND_DOWN)
+              .toString()}
+          </span>
+        </div>
+      </InnerPanel>
+      <Wallet isOpen={showWallet} onClose={() => setShowWallet(false)} />
+    </>
   );
 };
