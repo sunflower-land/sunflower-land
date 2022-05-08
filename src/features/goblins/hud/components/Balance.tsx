a0b5b765c0e3bebc9f9ad85f0bddbc41ec32ccbb
Pranav Yadav <pranavyadav3912@gmail.com>
2022-05-08 15:56:39 +0530

    [CHORE] organize goblin Balance.tsx

--- src/features/goblins/hud/components/Balance.tsx
+++ src/features/goblins/hud/components/Balance.tsx
@@ -7,28 +7,28 @@ import token from "assets/icons/token.gif";
 
 import { Context } from "features/game/GoblinProvider";
 import Decimal from "decimal.js-light";
+import { Wallet } from "./wallet/Wallet";
 
 export const Balance: React.FC = () => {
   const { goblinService } = useContext(Context);
   const [goblinState] = useActor(goblinService);
-  const [isShown, setIsShown] = useState(false);
+  const [showWallet, setShowWallet] = useState(false);
+
+  const handleClick = () => setShowWallet(true);
 
   return (
-    <InnerPanel className="fixed top-2 right-2 z-50 flex items-center shadow-lg cursor-pointer">
-      <img src={token} className="w-8 img-highlight" />
-      <span
-        className="text-white text-sm text-shadow ml-2"
-        onMouseEnter={() => setIsShown(true)}
-        onMouseLeave={() => setIsShown(false)}
-      >
-        {isShown === false ? (
-          goblinState.context.state.balance
-            .toDecimalPlaces(3, Decimal.ROUND_DOWN)
-            .toString()
-        ) : (
-          <small>{goblinState.context.state.balance.toString()}</small>
-        )}
-      </span>
-    </InnerPanel>
+    <>
+      <InnerPanel className="fixed top-2 right-2 z-50 flex items-center shadow-lg cursor-pointer">
+        <div className="flex justify-center items-center" onClick={handleClick}>
+          <img src={token} className="w-8 col img-highlight" />
+          <span className="col text-white text-sm text-shadow ml-2">
+            {goblinState.context.state.balance
+              .toDecimalPlaces(3, Decimal.ROUND_DOWN)
+              .toString()}
+          </span>
+        </div>
+      </InnerPanel>
+      <Wallet isOpen={showWallet} onClose={() => setShowWallet(false)} />
+    </>
   );
 };
