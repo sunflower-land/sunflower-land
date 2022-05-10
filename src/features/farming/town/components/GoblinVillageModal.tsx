c41b7a4982e52feb2643bcc678d06a2e3d8248c5
newb23 <newb23@gmail.com>
2022-05-09 21:45:17 -0600

    Adds sync check to Goblin Village Modal prior to travel.

--- src/features/farming/town/components/GoblinVillageModal.tsx
+++ src/features/farming/town/components/GoblinVillageModal.tsx
@@ -1,7 +1,16 @@
-import React from "react";
-import alert from "assets/icons/expression_alerted.png";
-import { Panel } from "components/ui/Panel";
+import React, { useContext, useState, useEffect } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+import Decimal from "decimal.js-light";
+
+import { Context } from "features/game/GameProvider";
+import { getOnChainState } from "features/game/actions/onchain";
+import { Inventory } from "features/game/types/game";
+
 import { Button } from "components/ui/Button";
+
+import alert from "assets/icons/expression_alerted.png";
+import { useActor } from "@xstate/react";
+import { Panel } from "components/ui/Panel";
 import goblinFence from "assets/land/goblin_fence.png";
 
 export const GoblinVillageModal: React.FC<{ onClose: () => void }> = ({
@@ -12,6 +21,82 @@ export const GoblinVillageModal: React.FC<{ onClose: () => void }> = ({
   const goToGoblinTown = () => {
     window.location.href = `${window.location.pathname}#/goblins`;
   };
+
+  const { gameService } = useContext(Context);
+  const [game] = useActor(gameService);
+  const [isLoading, setIsLoading] = useState(true);
+  const [inventory, setInventory] = useState<Inventory>({});
+  const [balance, setBalance] = useState<Decimal>(new Decimal(0));
+  const [showSyncCaptcha, setShowCaptcha] = useState(false);
+  const localInventory = JSON.stringify(
+    game.context.state.inventory,
+    Object.keys(game.context.state.inventory).sort()
+  );
+  const chainInventory = JSON.stringify(
+    inventory,
+    Object.keys(inventory).sort()
+  );
+  let inventoriesMatch = false;
+
+  const localBalance = game.context.state.balance;
+  const chainBalance = balance;
+  let balancesMatch = false;
+
+  if (localInventory == chainInventory) inventoriesMatch = true;
+  if (localBalance.equals(chainBalance)) balancesMatch = true;
+
+  const farmSynced = inventoriesMatch && balancesMatch;
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const load = async () => {
+      const { game: state } = await getOnChainState({
+        id: game.context.state.id as number,
+        farmAddress: game.context.state.farmAddress as string,
+      });
+
+      setInventory(state.inventory);
+      setBalance(state.balance);
+      setIsLoading(false);
+    };
+
+    load();
+  }, []);
+
+  const onCaptchaSolved = async (captcha: string | null) => {
+    await new Promise((res) => setTimeout(res, 1000));
+    gameService.send("SYNC", { captcha });
+    onClose();
+  };
+
+  const preGoblinTownSync = () => {
+    setShowCaptcha(true);
+  };
+
+  if (isLoading) {
+    return (
+      <Panel>
+        <span className="text-shadow loading">Loading</span>
+      </Panel>
+    );
+  }
+
+  if (showSyncCaptcha) {
+    return (
+      <Panel>
+        <ReCAPTCHA
+          sitekey="6Lfqm6MeAAAAAFS5a0vwAfTGUwnlNoHziyIlOl1s"
+          onChange={onCaptchaSolved}
+          onExpired={() => setShowCaptcha(false)}
+          className="w-full m-4 flex items-center justify-center"
+        />
+        <p className="text-xxs p-1 m-1 text-center">
+          Any unsaved progress will be lost.
+        </p>
+      </Panel>
+    );
+  }
 
   return (
     <Panel>
@@ -36,12 +121,18 @@ export const GoblinVillageModal: React.FC<{ onClose: () => void }> = ({
           <span className="underline">sync on chain</span> before entering.
         </p>
       </div>
-      <div className="flex items-center border-2 rounded-md border-black p-2 mb-2 bg-error">
+      <div
+        className="flex items-center border-2 rounded-md border-black p-2 mb-2 bg-error"
+        hidden={farmSynced}
+      >
         <img src={alert} alt="alert" className="mr-2 w-6" />
         <span className="text-xs">
           You may lose SFL or resources from your farm if they have not been
           synced to the blockchain
         </span>
+        <Button className="mr-1" onClick={preGoblinTownSync}>
+          SYNC
+        </Button>
       </div>
       <div className="flex">
         <Button className="mr-1" onClick={onClose}>
