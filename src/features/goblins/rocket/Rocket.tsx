b1dc5f72c710e1dd6de3ddce7df313b80bfd4d1f
John Rafael Cadorna <rafaelcadorna@pm.me>
2022-05-21 17:45:02 +0800

    [FIX] Add modal and init lore

--- src/features/goblins/rocket/Rocket.tsx
+++ src/features/goblins/rocket/Rocket.tsx
@@ -2,12 +2,51 @@ import React from "react";
 
 import brokenRocket from "assets/buildings/mom_broken_rocket.gif";
 import momNpc from "assets/npcs/mom_npc.gif";
-import icon from "assets/brand/icon.png";
+import close from "assets/icons/close.png";
+import questionMark from "assets/icons/expression_confused.png";
 
 import { GRID_WIDTH_PX } from "features/game/lib/constants";
-import { Action } from "components/ui/Action";
+import { Modal } from "react-bootstrap";
+import { Panel } from "components/ui/Panel";
+import { Button } from "components/ui/Button";
+import { ItemsModal } from "./ItemsModal";
 
 export const Rocket: React.FC = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [isItemsOpen, setIsItemsOpen] = React.useState(false);
+
+  const openRocket = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+    setIsItemsOpen(false);
+  };
+
+  const content = () => {
+    if (isItemsOpen) {
+      return <ItemsModal isOpen={isOpen} onClose={() => setIsOpen(false)} />;
+    }
+
+    return (
+      <Panel>
+        <div className="flex items-start">
+          <img src={questionMark} className="w-12 img-highlight mr-2" />
+          <div className="flex-1">
+            <span className="text-shadow mr-4 block">
+              A rocket has crash landed in Sunflower Land and Melon Dusk needs
+              help to go back to Mars.
+            </span>
+            <Button className="text-sm" onClick={() => setIsItemsOpen(true)}>
+              Fix rocket
+            </Button>
+          </div>
+        </div>
+      </Panel>
+    );
+  };
+
   return (
     <div
       className="absolute"
@@ -17,8 +56,7 @@ export const Rocket: React.FC = () => {
         top: `${GRID_WIDTH_PX * 20}px`,
       }}
     >
-      <div className="cursor-pointer hover:img-highlight">
-        <img src={brokenRocket} className="w-full" />
+      <div className="absolute cursor-pointer hover:img-highlight w-full">
         <img
           src={momNpc}
           style={{
@@ -27,15 +65,22 @@ export const Rocket: React.FC = () => {
             top: `${GRID_WIDTH_PX * 2.5}px`,
             right: `${GRID_WIDTH_PX * 3.75}px`,
           }}
+          // className="absolute cursor-pointer hover:img-highlight"
+          onClick={openRocket}
         />
-        {
-          <Action
-            className="absolute -bottom-7 left-8"
-            text="M.O.M"
-            icon={icon}
-          />
-        }
+        <img src={brokenRocket} className="w-56" onClick={openRocket} />
       </div>
+
+      {isOpen && (
+        <Modal centered show={isOpen} onHide={closeModal}>
+          <img
+            src={close}
+            className="h-6 top-4 right-4 absolute cursor-pointer"
+            onClick={() => setIsOpen(false)}
+          />
+          {content()}
+        </Modal>
+      )}
     </div>
   );
 };
