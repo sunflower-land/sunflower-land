ae1d825180bbd402432653b6d011962ecafe7398
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2022-05-21 23:29:08 +1000

    Remove dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,6 @@ import { Navigation } from "./Navigation";
 
 import "./styles.css";
 
-import { inspect } from "@xstate/inspect";
-
-inspect({
-  // options
-  // url: 'https://stately.ai/viz?inspect', // (default)
-  iframe: false, // open in new window
-});
-import { inspect } from "@xstate/inspect";
-
-inspect({
-  // options
-  // url: 'https://stately.ai/viz?inspect', // (default)
-  iframe: false, // open in new window
-});
-
 import Decimal from "decimal.js-light";
 
 /**
