2d54256fa7e5c33dbe74122cf211a41de1550fc0
pab3507 <pab3507@rit.edu>
2022-05-22 20:09:26 -0400

    Fix useEffect dependency array

--- src/components/ui/Zoom.tsx
+++ src/components/ui/Zoom.tsx
@@ -6,41 +6,44 @@ import { Button } from "components/ui/Button";
 import { useIsMobile } from "lib/utils/hooks/useIsMobile";
 
 export const Zoom: React.FC = () => {
+  const maxZoomIn = 1.0;
+  const maxZoomOut = 0.8;
   const [zoomLevel, setZoomedLevel] = useState<number>(1.0);
   const [isMobile] = useIsMobile();
 
   const zoomOut = () => {
-    if (zoomLevel > 0.8) {
+    if (zoomLevel > maxZoomOut) {
       setZoomedLevel(zoomLevel - 0.1);
       console.log(zoomLevel);
     }
   };
 
   const zoomIn = () => {
-    if (zoomLevel < 1.0) {
+    if (zoomLevel < maxZoomIn) {
       setZoomedLevel(zoomLevel + 0.1);
     }
   };
 
   useEffect(() => {
-    const changeZoom = async () => {
+    const changeZoom = () => {
+      // Not supported on FireFox mobile
       document.body.style.zoom = zoomLevel;
     };
-    changeZoom(), [zoomLevel];
-  });
+    changeZoom();
+  }, [zoomLevel]);
 
   return (
     <>
       {isMobile && (
         <>
-          <Button onClick={() => zoomIn()} disabled={zoomLevel >= 1.0}>
+          <Button onClick={() => zoomIn()} disabled={zoomLevel >= maxZoomIn}>
             <img
               src={zoom_in}
               alt="zoom in on the map"
               className="w-4 h-4 sm:w-6 sm:h-5"
             />
           </Button>
-          <Button onClick={() => zoomOut()} disabled={zoomLevel <= 0.8}>
+          <Button onClick={() => zoomOut()} disabled={zoomLevel <= maxZoomOut}>
             <img
               src={zoom_out}
               alt="zoom out of the map"
