5763d3cb4fc09278ce85300585e7b8c1a219e647
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2022-05-24 12:54:04 +0930

    Fix margins on minting modal

--- src/features/game/components/Minting.tsx
+++ src/features/game/components/Minting.tsx
@@ -6,7 +6,7 @@ export const Minting: React.FC = () => {
   return (
     <div className="flex flex-col items-center p-2">
       <span className="text-shadow text-center loading">Minting</span>
-      <img src={mintingAnimation} className="w-1/2 mt-2 mb-3" />
+      <img src={mintingAnimation} className="w-1/2" />
       <span className="text-sm">
         Please be patient while our minions mint something special for you.
       </span>
