df58c3dc9b9ea2a8aa243e2bf58cb8d693f008d2
Spencer Dezart-Smith <spencer.dezart.smith@gmail.com>
2022-05-25 14:14:10 +0930

    [FIX] Allow default visit state for development (#1003)
    * Add initial state check in visit machine
    
    * Add comment

--- src/features/game/lib/visitingMachine.ts
+++ src/features/game/lib/visitingMachine.ts
@@ -1,4 +1,5 @@
 import { metamask } from "lib/blockchain/metamask";
+import { CONFIG } from "lib/config";
 import { ERRORS } from "lib/errors";
 import { createMachine, assign, Interpreter } from "xstate";
 import { getOnChainState, isFarmBlacklisted } from "../actions/onchain";
@@ -47,12 +48,15 @@ const setFarmDetails = assign<Context, any>({
   state: (_, event) => event.data.state,
 });
 
+const API_URL = CONFIG.API_URL;
+
 export function startGame({ farmToVisitID }: { farmToVisitID: number }) {
   return createMachine<Context, Event, State>(
     {
       id: "visitingMachine",
       context: { state: EMPTY, isBlacklisted: undefined },
-      initial: "loading",
+      // Allow a development playground if there is now API_URL env variable
+      initial: API_URL ? "loading" : "visiting",
       states: {
         loading: {
           invoke: {
