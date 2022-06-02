f4503b805e3c8057ec4c908f80edd3e8cf0b4cb1
Romy Furtado <rofrtd@gmail.com>
2022-06-02 15:47:54 -0300

    reposition stopwatch

--- src/features/farming/bakery/components/CraftingItems.tsx
+++ src/features/farming/bakery/components/CraftingItems.tsx
@@ -150,8 +150,8 @@ export const CraftingItems: React.FC<Props> = ({ items }) => {
         <div className="flex flex-col justify-center items-center p-2 relative">
           {expiryTime && (
             <>
-              <img src={stopwatch} className="w-4  absolute left-0 -top-4" />
-              <span className="bg-blue-600 border  text-xxs absolute left-5 -top-4 p-1 rounded-md">
+              <span className="bg-blue-600 border flex text-xxs items-center absolute left-5 -top-4 p-1 rounded-md">
+                <img src={stopwatch} className="w-4 left-0 -top-4 mr-1" />
                 {`${secondsToString(secondsLeft as number, {
                   separator: " ",
                 })} left`}
