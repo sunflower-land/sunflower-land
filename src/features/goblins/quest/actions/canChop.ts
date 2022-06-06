8b38eda5f43014c7dcadfbe77a5a43202d13daf4
Craig <41215134+c-py@users.noreply.github.com>
2022-06-06 22:24:40 +1000

    [CHORE] Fix linting issues

--- src/features/goblins/quest/actions/canChop.ts
+++ src/features/goblins/quest/actions/canChop.ts
@@ -1,13 +1,18 @@
 import { Context } from "features/game/lib/goblinMachine";
 
 export const canChopAncientTree = (context: Context) => {
-  return (
-    context.limitedItems["Goblin Key"]?.ingredients?.every((ingredient) =>
+  const hasIngredients = context.limitedItems["Goblin Key"]?.ingredients?.every(
+    (ingredient) =>
       ingredient.amount.lessThanOrEqualTo(
         context.state.inventory[ingredient.item] || 0
       )
-    ) && !!context.state.inventory["Apprentice Beaver"]
   );
+
+  const hasBeaver =
+    !!context.state.inventory["Apprentice Beaver"] ||
+    !!context.state.inventory["Foreman Beaver"];
+
+  return hasIngredients && hasBeaver;
 };
 
 export const getAncientTreeIngredients = (context: Context) =>
--- src/features/goblins/quest/actions/canMine.ts
+++ src/features/goblins/quest/actions/canMine.ts
@@ -1,13 +1,19 @@
 import { Context } from "features/game/lib/goblinMachine";
 
 export const canMineAncientRock = (context: Context) => {
-  return (
-    context.limitedItems["Sunflower Key"]?.ingredients?.every((ingredient) =>
-      ingredient.amount.lessThanOrEqualTo(
-        context.state.inventory[ingredient.item] || 0
-      )
-    ) && !!context.state.inventory["Rocky the Mole"]
+  const hasIngredients = context.limitedItems[
+    "Sunflower Key"
+  ]?.ingredients?.every((ingredient) =>
+    ingredient.amount.lessThanOrEqualTo(
+      context.state.inventory[ingredient.item] || 0
+    )
   );
+
+  const hasMole =
+    !!context.state.inventory["Rocky the Mole"] ||
+    !!context.state.inventory["Nugget"];
+
+  return hasIngredients && hasMole;
 };
 
 export const getAncientRockIngredients = (context: Context) =>
