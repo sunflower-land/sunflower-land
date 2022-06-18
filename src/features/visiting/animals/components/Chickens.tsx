ad98329615267f0a67d50ebef783967ea2d25ae8
j923sneaks <j923sneaks@gmail.com>
2022-06-18 09:34:10 +0800

    show mutant chickens

--- src/features/visiting/animals/components/Chickens.tsx
+++ src/features/visiting/animals/components/Chickens.tsx
@@ -2,6 +2,9 @@ import React, { useContext } from "react";
 import { useActor } from "@xstate/react";
 
 import coop from "assets/nfts/chicken_coop.png";
+import speedChicken from "assets/animals/chickens/speed_chicken.gif";
+import richChicken from "assets/animals/chickens/rich_chicken.gif";
+import fatChicken from "assets/animals/chickens/fat_chicken.gif";
 
 import { GRID_WIDTH_PX } from "features/game/lib/constants";
 import { Context } from "features/game/VisitingProvider";
@@ -26,6 +29,42 @@ export const Chickens: React.FC = () => {
             top: `${GRID_WIDTH_PX * 0}px`,
           }}
           id={Section["Chicken Coop"]}
+          className="absolute"
+        />
+      )}
+      {state.inventory["Speed Chicken"] && (
+        <img
+          src={speedChicken}
+          style={{
+            width: `${GRID_WIDTH_PX * 0.8}px`,
+            right: `${GRID_WIDTH_PX * -0.15}px`,
+            top: `${GRID_WIDTH_PX * -0.57}px`,
+          }}
+          id={Section["Speed Chicken"]}
+          className="absolute"
+        />
+      )}
+      {state.inventory["Rich Chicken"] && (
+        <img
+          src={richChicken}
+          style={{
+            width: `${GRID_WIDTH_PX * 1.07}px`,
+            right: `${GRID_WIDTH_PX * 3.4}px`,
+            top: `${GRID_WIDTH_PX * -0.3}px`,
+          }}
+          id={Section["Rich Chicken"]}
+          className="absolute"
+        />
+      )}
+      {state.inventory["Fat Chicken"] && (
+        <img
+          src={fatChicken}
+          style={{
+            width: `${GRID_WIDTH_PX * 0.8}px`,
+            right: `${GRID_WIDTH_PX * 2.9}px`,
+            top: `${GRID_WIDTH_PX * -2.3}px`,
+          }}
+          id={Section["Fat Chicken"]}
           className="absolute"
         />
       )}
