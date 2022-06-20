71ce5bcbb4b618ef1533a3fa7d45d4f3805d2acc
Craig <41215134+c-py@users.noreply.github.com>
2022-06-20 16:55:24 +1000

    [FEAT] Remove the ability to continue into blacklisted farms (#1106)
    * [FEAT] Remove the ability to continue into blacklisted farms
    
    * [FEAT] clear up terminology
    
    * [FEAT] clear up terminology

--- src/features/game/components/Blacklisted.tsx
+++ src/features/game/components/Blacklisted.tsx
@@ -1,36 +1,22 @@
-import React, { useContext } from "react";
-
-import * as AuthProvider from "features/auth/lib/Provider";
+import React from "react";
 
 import suspiciousGoblin from "assets/npcs/suspicious_goblin.gif";
-import { Button } from "components/ui/Button";
 
-export const Blacklisted: React.FC = () => {
-  const { authService } = useContext(AuthProvider.Context);
-
-  const continuePlaying = () => {
-    authService.send("CONTINUE");
-  };
-
-  return (
-    <>
-      <div className="flex flex-col items-center p-2">
-        <span className="text-center">Something strange!</span>
-        <img src={suspiciousGoblin} className="w-16 mt-2" />
-        <span className="text-sm mt-2 mb-2">
-          The anti-bot detection system is relatively new and has picked up some
-          strange behaviour. Some actions may be temporarily restricted.
-        </span>
-        <a
-          href={`https://forms.gle/ajhNS6kr3c6U3YLT9`}
-          className="underline text-center text-sm hover:text-blue-500 mt-1 mb-2 block"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Share details to help us improve our system
-        </a>
-      </div>
-      <Button onClick={continuePlaying}>Continue Playing</Button>
-    </>
-  );
-};
+export const Blacklisted: React.FC = () => (
+  <div className="flex flex-col items-center p-2">
+    <span className="text-center">This farm is banned!</span>
+    <img src={suspiciousGoblin} className="w-16 mt-2" />
+    <span className="text-sm mt-2 mb-2">
+      The anti-bot and multi-account detection system has picked up strange
+      behaviour. Actions have been restricted.
+    </span>
+    <a
+      href={`https://forms.gle/ajhNS6kr3c6U3YLT9`}
+      className="underline text-center text-sm hover:text-blue-500 mt-1 mb-2 block"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Share details to help us improve our system
+    </a>
+  </div>
+);
