9294699f946bf52c8bfa628aa4cd0ca302cadd40
DekV_infinity <79071868+veenhdek@users.noreply.github.com>
2022-07-18 19:38:39 -0500

    add blue box/stopwatch, fix words/postion (#1240)

--- src/features/farming/salesman/Salesman.tsx
+++ src/features/farming/salesman/Salesman.tsx
@@ -14,6 +14,7 @@ import { Offer } from "./component/Offer";
 import { TradeOffer } from "features/game/types/game";
 import { ITEM_DETAILS } from "features/game/types/images";
 import { secondsToString } from "lib/utils/time";
+import stopwatch from "assets/icons/stopwatch.png";
 
 const Content: React.FC<{ title: string }> = ({ title, children }) => {
   return (
@@ -121,13 +122,17 @@ export const Salesman: React.FC = () => {
             I travel all over these lands collecting items to trade.
           </p>
           <p className="sm:text-sm p-2">
-            What I have to offer you today will only be available until{" "}
-            {endDateLocale} (
-            {secondsToString(secondsLeft as number, {
-              separator: " ",
-            })}{" "}
-            left).
+            What I have to offer you today is available for a limited time.
           </p>
+          <span className="bg-blue-600 border flex text-[8px] sm:text-xxs items-center p-[3px] rounded-md whitespace-nowrap  mb-2">
+            <img src={stopwatch} className="w-3 left-0 -top-4 mr-1" />
+            <span className="mt-[2px]">{`${secondsToString(
+              secondsLeft as number,
+              {
+                separator: " ",
+              }
+            )} left`}</span>
+          </span>
           <Button onClick={() => setModalState("showOffer")}>
             {`Let's trade!`}
           </Button>
