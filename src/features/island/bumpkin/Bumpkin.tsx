520d951f7301db6b121bf0552b904b02bd262b7e
Adam Hannigan <adamhannigan81@gmail.com>
2022-07-20 14:17:20 +1000

    Remove modal

--- src/features/island/bumpkin/Bumpkin.tsx
+++ src/features/island/bumpkin/Bumpkin.tsx
@@ -1,26 +1,15 @@
-import React, { useState } from "react";
-import bumpkin from "assets/npcs/bumpkin.gif";
-import { Modal } from "react-bootstrap";
-import { Panel } from "components/ui/Panel";
-import { BumpkinBuilder } from "./components/BumpkinBuilder";
+import React from "react";
 import { BumpkinNPC } from "./components/BumpkinNPC";
 
 export const Bumpkin: React.FC = () => {
-  const [showBuilder, setShowBuilder] = useState(false);
   return (
     <div className="w-full">
       <BumpkinNPC
-        body="Farmer Potion 2"
-        wig="Rancher Wig"
+        body="Farmer Potion"
+        wig="Explorer Wig"
         pants="Lumberjack Overalls"
-        //shirt="Farmer Shirt"
-        onClick={() => setShowBuilder(true)}
+        onClick={() => {}}
       />
-      <Modal show={showBuilder} centered onHide={() => setShowBuilder(false)}>
-        <Panel>
-          <BumpkinBuilder />
-        </Panel>
-      </Modal>
     </div>
   );
 };
