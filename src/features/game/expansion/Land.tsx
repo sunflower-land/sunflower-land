69ea7c80a479e871eb644f28caef72c0fa2a379c
spencer <spencer@sunflower-land.com>
2022-07-27 09:44:22 +1000

    Change index type to number.

--- src/features/game/expansion/Land.tsx
+++ src/features/game/expansion/Land.tsx
@@ -110,7 +110,7 @@ const Expansion: React.FC<ExpansionProps & { expansionIndex: number }> = ({
               height={height}
               width={width}
             >
-              <Tree treeIndex={index} expansionIndex={expansionIndex} />
+              <Tree treeIndex={Number(index)} expansionIndex={expansionIndex} />
             </MapPlacement>
           );
         })}
