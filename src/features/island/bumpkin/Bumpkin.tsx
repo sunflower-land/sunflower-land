175fe74131efb1afd58bda0de046d7b611df0fcd
Adam Hannigan <adamhannigan81@gmail.com>
2022-07-26 16:43:10 +1000

    Bumpkin UI

--- src/features/island/bumpkin/Bumpkin.tsx
+++ src/features/island/bumpkin/Bumpkin.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
+import { BumpkinModal } from "./components/BumpkinModal";
 import { Character } from "./components/Character";
 
 export const Bumpkin: React.FC = () => {
+  const [showModal, setShowModal] = useState(true);
+
   return (
     <div>
       <Character
         body="Farmer Potion"
         wig="Rancher Wig"
         pants="Lumberjack Overalls"
-        onClick={console.log}
+        onClick={() => setShowModal(true)}
       />
+      <Modal show={showModal} centered>
+        <BumpkinModal onClose={() => setShowModal(false)} />
+      </Modal>
     </div>
   );
 };
---
+++ src/features/island/bumpkin/components/BumpkinModal.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+import close from "assets/icons/close.png";
+import house from "assets/buildings/house.png";
+import player from "assets/icons/player.png";
+
+import { Panel } from "components/ui/Panel";
+import { Tab } from "components/ui/Tab";
+import { BumpkinBuilder } from "./BumpkinBuilder";
+
+interface Props {
+  onClose: () => void;
+}
+
+export const BumpkinModal: React.FC<Props> = ({ onClose }) => {
+  const [tab, setTab] = useState<"buildings" | "bumpkin">("bumpkin");
+
+  return (
+    <Panel className="pt-5 relative max-w-5xl">
+      <div className="flex justify-between absolute top-1.5 left-0.5 right-0 items-center">
+        <div className="flex">
+          <Tab
+            isActive={tab === "buildings"}
+            onClick={() => setTab("buildings")}
+          >
+            <img src={house} className="h-5 mr-2" />
+            <span className="text-sm text-shadow">Buildings</span>
+          </Tab>
+          <Tab isActive={tab === "bumpkin"} onClick={() => setTab("bumpkin")}>
+            <img src={player} className="h-5 mr-2" />
+            <span className="text-sm text-shadow">Bumpkin</span>
+          </Tab>
+        </div>
+        <img
+          src={close}
+          className="h-6 cursor-pointer mr-2 mb-1"
+          onClick={onClose}
+        />
+      </div>
+      {tab === "buildings" && <span>Buildings</span>}
+      {tab === "bumpkin" && <BumpkinBuilder />}
+    </Panel>
+  );
+};
