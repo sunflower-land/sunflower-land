8d67febfd23284dbc3c5588377d2caf194199cf7
spencer <spencer@sunflower-land.com>
2022-08-22 09:05:32 +1000

    Add images

--- src/features/auth/useImagePreloader.tsx
+++ src/features/auth/useImagePreloader.tsx
@@ -24,6 +24,24 @@ import egg from "assets/resources/egg.png";
 import richChicken from "assets/animals/chickens/rich_chicken.png";
 import fatChicken from "assets/animals/chickens/fat_chicken.png";
 import speedChicken from "assets/animals/chickens/speed_chicken.png";
+import hair1 from "assets/bumpkins/large/hair/basic.png";
+import hair2 from "assets/bumpkins/large/hair/explorer.png";
+import eyes1 from "assets/bumpkins/large/eyes/rosy_wide.png";
+import eyes2 from "assets/bumpkins/large/eyes/rosy_squint.png";
+import body1 from "assets/bumpkins/large/body/light_farmer.png";
+import body2 from "assets/bumpkins/large/body/dark_farmer.png";
+import shirt1 from "assets/bumpkins/large/shirts/farmer_shirt.png";
+import shirt2 from "assets/bumpkins/large/shirts/lumberjack_shirt.png";
+// Default
+import farmerPantsLg from "assets/bumpkins/large/pants/farmer_pants.png";
+import blackShoesLg from "assets/bumpkins/large/shoes/black_shoes.png";
+import smileLg from "assets/bumpkins/large/mouths/smile.png";
+import hairIcon from "assets/bumpkins/icons/hair_icon.png";
+import eyesIcon from "assets/bumpkins/icons/eyes_icon.png";
+import bodyIcon from "assets/bumpkins/icons/body_icon.png";
+import shirtIcon from "assets/bumpkins/icons/shirt_icon.png";
+import leftArrow from "assets/icons/arrow_left.png";
+import rightArrow from "assets/icons/arrow_right.png";
 
 const IMAGE_LIST: string[] = [
   goblinDonation,
@@ -50,6 +68,23 @@ const IMAGE_LIST: string[] = [
   fatChicken,
   speedChicken,
   waterBackground,
+  hair1,
+  hair2,
+  eyes1,
+  eyes2,
+  body1,
+  body2,
+  shirt1,
+  shirt2,
+  farmerPantsLg,
+  blackShoesLg,
+  smileLg,
+  hairIcon,
+  eyesIcon,
+  bodyIcon,
+  shirtIcon,
+  leftArrow,
+  rightArrow,
 ];
 
 function preloadImage(src: string) {
