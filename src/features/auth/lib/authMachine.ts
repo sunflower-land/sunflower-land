8fa2215c6a853c8e82a1b281a275903fbc69f9e2
Adam Hannigan <adamhannigan81@gmail.com>
2022-08-25 14:24:01 +1000

    Jigger unban

--- src/features/auth/lib/authMachine.ts
+++ src/features/auth/lib/authMachine.ts
@@ -1,3 +1,4 @@
+import { loadBanDetails } from "features/game/actions/bans";
 import { isFarmBlacklisted } from "features/game/actions/onchain";
 import { CONFIG } from "lib/config";
 import { ERRORS } from "lib/errors";
@@ -29,6 +30,7 @@ type Farm = {
   address: string;
   createdAt: number;
   isBlacklisted: boolean;
+  verificationUrl?: string;
 };
 
 export interface Context {
@@ -40,6 +42,7 @@ export interface Context {
   rawToken?: string;
   captcha?: string;
   isBlacklisted?: boolean;
+  verificationUrl?: string;
 }
 
 export type Screen = "land" | "farm" | "viewer";
@@ -494,7 +497,7 @@ export const authMachine = createMachine<
       initMetamask: async (context, event): Promise<void> => {
         await metamask.initialise();
       },
-      loadFarm: async (): Promise<Farm | undefined> => {
+      loadFarm: async (context): Promise<Farm | undefined> => {
         const farmAccounts = await metamask.getFarm()?.getFarms();
 
         if (farmAccounts?.length === 0) {
@@ -508,13 +511,18 @@ export const authMachine = createMachine<
         // V1 just support 1 farm per account - in future let them choose between the NFTs they hold
         const farmAccount = farmAccounts[0];
 
-        const isBlacklisted = await isFarmBlacklisted(farmAccount.tokenId);
+        const { isBanned, verificationUrl } = await loadBanDetails(
+          farmAccount.tokenId,
+          context.rawToken as string
+        );
+        console.log({ isBanned });
 
         return {
           farmId: farmAccount.tokenId,
           address: farmAccount.account,
           createdAt,
-          isBlacklisted,
+          isBlacklisted: isBanned,
+          verificationUrl,
         };
       },
       createFarm: async (context: Context, event: any): Promise<Context> => {
@@ -567,6 +575,7 @@ export const authMachine = createMachine<
         farmId: (_context, event) => event.data.farmId,
         address: (_context, event) => event.data.address,
         isBlacklisted: (_context, event) => event.data.isBlacklisted,
+        verificationUrl: (_context, event) => event.data.verificationUrl,
       }),
       assignToken: assign<Context, any>({
         token: (_context, event) => decodeToken(event.data.token),
--- src/features/game/components/Blacklisted.tsx
+++ src/features/game/components/Blacklisted.tsx
@@ -1,22 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
+import alert from "assets/icons/expression_alerted.png";
 import suspiciousGoblin from "assets/npcs/suspicious_goblin.gif";
+import { Button } from "components/ui/Button";
 
-export const Blacklisted: React.FC = () => (
-  <div className="flex flex-col items-center p-2">
-    <span className="text-center">This farm is banned</span>
-    <img src={suspiciousGoblin} className="w-16 mt-2" />
-    <span className="text-sm mt-2 mb-2">
-      The anti-bot and multi-account detection system has picked up strange
-      behaviour. Actions have been restricted.
-    </span>
-    <a
-      href={`https://sunflowerland.freshdesk.com/support/tickets/new`}
-      className="underline text-center text-sm hover:text-blue-500 mt-1 mb-2 block"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Please submit a ticket with details and we will get back to you.
-    </a>
-  </div>
-);
+interface Props {
+  verificationUrl?: string;
+}
+export const Blacklisted: React.FC<Props> = ({ verificationUrl }) => {
+  const [showWarning, setShowWarning] = useState(false);
+
+  if (showWarning) {
+    return (
+      <div className="flex flex-col items-center p-2">
+        <span className="text-center">Proof of Humanity</span>
+        <img src={alert} className="w-6 mt-2" />
+        <span className="text-sm mt-2 mb-2">
+          You will be redirected to a 3rd party service to take a quick selfie.
+          Never share any personal information or crypto data.
+        </span>
+
+        <Button
+          onClick={() => {
+            window.location.href = verificationUrl as string;
+          }}
+        >
+          Verify
+        </Button>
+      </div>
+    );
+  }
+
+  if (verificationUrl) {
+    return (
+      <div className="flex flex-col items-center p-2">
+        <span className="text-center">Uh oh!</span>
+        <img src={suspiciousGoblin} className="w-16 mt-2" />
+        <span className="text-sm mt-2 mb-2">
+          The anti-bot and multi-account detection system has picked up strange
+          behaviour.
+        </span>
+        <Button onClick={() => setShowWarning(true)}>Continue</Button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col items-center p-2">
+      <span className="text-center">This farm is banned</span>
+      <img src={suspiciousGoblin} className="w-16 mt-2" />
+      <span className="text-sm mt-2 mb-2">
+        The anti-bot and multi-account detection system has picked up strange
+        behaviour. Actions have been restricted.
+      </span>
+      <a
+        href={`https://sunflowerland.freshdesk.com/support/tickets/new`}
+        className="underline text-center text-sm hover:text-blue-500 mt-1 mb-2 block"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Please submit a ticket with details and we will get back to you.
+      </a>
+    </div>
+  );
+};
