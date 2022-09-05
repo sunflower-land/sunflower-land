2086425103dd38c388597b1e3de08a16ea5d4057
Azaro <qppazaro@gmail.com>
2022-09-05 12:32:06 +0800

    [FIX]: Fixed navigation back to town.

--- src/features/goblins/components/TownEntryModal.tsx
+++ src/features/goblins/components/TownEntryModal.tsx
@@ -1,8 +1,10 @@
-import React from "react";
-import { Panel } from "components/ui/Panel";
-import { Button } from "components/ui/Button";
+import React, { useContext } from "react";
+import { useActor } from "@xstate/react";
 import { useNavigate } from "react-router-dom";
 
+import * as AuthProvider from "features/auth/lib/Provider";
+import { Panel } from "components/ui/Panel";
+import { Button } from "components/ui/Button";
 import bumpkin from "assets/npcs/bumpkin.png";
 
 export const TownEntryModal: React.FC<{ onClose: () => void }> = ({
@@ -11,6 +13,9 @@ export const TownEntryModal: React.FC<{ onClose: () => void }> = ({
   onClose: () => void;
 }) => {
   const navigate = useNavigate();
+  const { authService } = useContext(AuthProvider.Context);
+  const [authState] = useActor(authService);
+  const farmId = authState.context.farmId;
 
   return (
     <Panel>
@@ -33,7 +38,7 @@ export const TownEntryModal: React.FC<{ onClose: () => void }> = ({
         <Button className="mr-1" onClick={onClose}>
           Close
         </Button>
-        <Button className="ml-1" onClick={() => navigate(-1)}>
+        <Button className="ml-1" onClick={() => navigate(`/farm/${farmId}`)}>
           Continue
         </Button>
       </div>
