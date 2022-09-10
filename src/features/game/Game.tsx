ff16e917f90ff9a7fd881f474315124f2350e842
Tiffany Domugho <tiffany@domugho.com>
2022-09-10 16:46:06 +0800

    fix: remove unused imports

--- src/features/game/Game.tsx
+++ src/features/game/Game.tsx
@@ -37,7 +37,6 @@ import { Notifications } from "./components/Notifications";
 import { Hoarding } from "./components/Hoarding";
 import { Airdrop } from "./components/Airdrop";
 import { GoblinWar } from "features/war/GoblinWar";
-import { CONFIG } from "lib/config";
 import { CommunityGardenEntry } from "features/farming/town/components/CommunityGardenEntry";
 
 const AUTO_SAVE_INTERVAL = 1000 * 30; // autosave every 30 seconds
