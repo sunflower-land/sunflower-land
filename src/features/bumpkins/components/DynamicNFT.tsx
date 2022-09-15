33b3b5214ba322010e5b6c4ee46479a65089f45b
Adam Hannigan <adamhannigan81@gmail.com>
2022-09-15 16:19:09 +1000

    Fix on chain items

--- src/features/bumpkins/components/DynamicNFT.tsx
+++ src/features/bumpkins/components/DynamicNFT.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import classNames from "classnames";
 import { getKeys } from "features/game/types/craftables";
 
-import farmBackground from "src/assets/bumpkins/shop/background/farm_background.png";
 import dropShadow from "assets/bumpkins/shop/body_dropshadow.png";
 
 import {
   BumpkinItems,
   BumpkinPart,
   BumpkinParts,
+  BumpkinWallpaper,
 } from "features/game/types/bumpkin";
 import { BUMPKIN_ITEMS } from "features/bumpkins/types/BumpkinDetails";
 
@@ -21,7 +21,35 @@ export const DynamicNFT: React.FC<Props> = ({
   bumpkinParts,
   showBackground,
 }) => {
-  console.log({ bumpkinParts });
+  const {
+    background,
+    body,
+    eyes,
+    hair,
+    mouth,
+    pants,
+    shirt,
+    shoes,
+    hat,
+    necklace,
+    tool,
+  } = bumpkinParts;
+
+  // Need to render layers in specific order
+  const orderedParts = {
+    body,
+    eyes,
+    hair,
+    mouth,
+    pants,
+    shirt,
+    shoes,
+    hat,
+    necklace,
+    tool,
+  };
+
+  console.log({ orderedParts });
   return (
     <div className="relative w-full">
       <img
@@ -30,15 +58,24 @@ export const DynamicNFT: React.FC<Props> = ({
         className="absolute bottom-0 z-0 opacity-30"
       />
       {showBackground && (
-        <img src={farmBackground} alt="background" className="z-0 w-full" />
-      )}
-      {getKeys(bumpkinParts).map((part: BumpkinPart, index) => (
         <img
-          key={part}
-          src={BUMPKIN_ITEMS[bumpkinParts[part] as BumpkinItems].layerImage}
-          className={classNames(`inset-0 z-${index * 10} w-full absolute`, {})}
+          src={BUMPKIN_ITEMS[background as BumpkinWallpaper].layerImage}
+          alt="background"
+          className="z-0 w-full"
         />
-      ))}
+      )}
+      {getKeys(orderedParts)
+        .filter((part) => !!bumpkinParts[part])
+        .map((part: BumpkinPart, index) => (
+          <img
+            key={part}
+            src={BUMPKIN_ITEMS[bumpkinParts[part] as BumpkinItems].layerImage}
+            className={classNames(
+              `inset-0 z-${index * 10} w-full absolute`,
+              {}
+            )}
+          />
+        ))}
     </div>
   );
 };
