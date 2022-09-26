80153c1af318835a07502de8809968495c1771b6
Adam Hannigan <adamhannigan81@gmail.com>
2022-09-26 14:05:23 +1000

    [FEAT] Update Goblin Leaderboard

--- src/features/war/components/Leaderboard.tsx
+++ src/features/war/components/Leaderboard.tsx
@@ -28,8 +28,8 @@ export const Leaderboard: React.FC = () => {
     const startOfWeekPoints =
       CONFIG.NETWORK === "mainnet"
         ? {
-            humans: 344445 + 317926,
-            goblins: 354890 + 278205,
+            humans: 344445 + 317926 + 887742,
+            goblins: 354890 + 278205 + 913198,
           }
         : { humans: 20, goblins: 40 };
 
