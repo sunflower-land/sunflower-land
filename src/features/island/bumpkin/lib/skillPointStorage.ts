22dedc2fe0268242b3b792b73ccef89ad0501ee1
spencer <spencer@sunflower-land.com>
2022-09-29 09:55:25 +1000

    Add skillStorage fns

---
+++ src/features/island/bumpkin/lib/skillPointStorage.ts
@@ -0,0 +1,29 @@
+import { getAvailableBumpkinSkillPoints } from "features/game/events/landExpansion/pickSkill";
+import { Bumpkin } from "features/game/types/game";
+
+export function getAcknowledgedSkillPoints() {
+  return localStorage.getItem("acknowledgedSkillPoints");
+}
+
+export function hasUnacknowledgedSkillPoints(bumpkin?: Bumpkin) {
+  if (!bumpkin) return false;
+
+  const availableSkillPoints = getAvailableBumpkinSkillPoints(bumpkin);
+
+  console.log({ availableSkillPoints });
+
+  const acknowledgedSkillPoints = getAcknowledgedSkillPoints() ?? 0;
+
+  return availableSkillPoints > Number(acknowledgedSkillPoints);
+}
+
+export function acknowledgeSkillPoints(bumpkin?: Bumpkin) {
+  if (!bumpkin) return;
+
+  const availableSkillPoints = getAvailableBumpkinSkillPoints(bumpkin);
+
+  return localStorage.setItem(
+    "acknowledgedSkillPoints",
+    `${availableSkillPoints}`
+  );
+}
