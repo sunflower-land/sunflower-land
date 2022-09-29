e55f022711c8d4fc2b5a090111ae6137ba6e0f62
spencer <spencer@sunflower-land.com>
2022-09-29 17:20:12 +1000

    Fix conflicts

--- src/features/bumpkins/components/AchievementDetails.tsx
+++ src/features/bumpkins/components/AchievementDetails.tsx
@@ -16,6 +16,7 @@ import progressBar from "assets/ui/progress/transparent_bar_long.png";
 import classNames from "classnames";
 import { Button } from "components/ui/Button";
 import { GameState } from "features/game/types/game";
+import { Panel } from "components/ui/Panel";
 
 interface Props {
   onClose: () => void;
@@ -38,98 +39,100 @@ export const AchievementDetails: React.FC<Props> = ({
   const isAlreadyClaimed = !!bumpkinAchievements[name];
 
   return (
-    <div className="flex flex-col items-center">
-      <div
-        className="flex justify-center mb-2 w-full relative text-center px-10"
-        style={{
-          minHeight: "2rem",
-        }}
-      >
-        <img
-          src={arrowLeft}
-          className="w-8 mr-7 cursor-pointer absolute left-2 top-0"
-          onClick={onClose}
-        />
-        <span className="text-center text-lg mt-0.5">{name}</span>
-      </div>
-      <div className="flex flex-col items-center justify-center">
+    <Panel>
+      <div className="flex flex-col items-center">
         <div
-          className={classNames(
-            "w-20 h-20 flex justify-center items-center mb-2 p-2.5 rounded-md mr-2 relative cursor-pointer hover:img-highlight",
-            {
-              "opacity-50": !isComplete,
-            }
-          )}
+          className="flex justify-center mb-2 w-full relative text-center px-10"
+          style={{
+            minHeight: "2rem",
+          }}
         >
-          <img src={ITEM_DETAILS[name].image} className="h-full z-10" />
-          <img src={disc} className="absolute inset-0 w-full" />
+          <img
+            src={arrowLeft}
+            className="w-8 mr-7 cursor-pointer absolute left-2 top-0"
+            onClick={onClose}
+          />
+          <span className="text-center text-lg mt-0.5">{name}</span>
         </div>
-
         <div className="flex flex-col items-center justify-center">
-          {/* <p className="text-lg underline">{name}</p> */}
-          <div className="flex-1 mt-1.5 text-xs flex-wrap justify-center items-center text-center">
-            <p className="text-sm mb-1">{achievement.description}</p>
-
-            <div className="flex items-center justify-center  mt-1 mb-1">
-              <div className="flex items-center justify-center relative w-40 h-4 z-10">
-                <img src={progressBar} className="absolute w-full h-full" />
-                <img
-                  src={progressBarEdge}
-                  className="absolute top-0 left-[-2px] h-full"
-                />
-                <img
-                  src={progressBarEdge}
-                  className="absolute top-0 right-[-2px] h-full"
-                  style={{
-                    transform: "scaleX(-1)",
-                  }}
-                />
-                <div className="w-full h-full bg-[#193c3e] absolute -z-20" />
-                <div
-                  className="h-full bg-[#63c74d] absolute -z-10 left-0"
-                  style={{
-                    width: `${(progress / achievement.requirement) * 100}%`,
-                    maxWidth: "100%",
-                    borderRight: "3px solid #418848",
-                  }}
-                />
-              </div>
-            </div>
-            <span className="text-xs text-white ml-2 mt-1">{`${progress}/${achievement.requirement}`}</span>
-          </div>
-        </div>
-      </div>
-
-      {!isAlreadyClaimed && (
-        <div
-          className={classNames("mt-4", {
-            "opacity-50": !isComplete,
-          })}
-        >
-          <div className="flex">
-            {!!achievement.sflReward && (
-              <div className="flex items-center mt-2 mr-3">
-                <img src={sflIcon} className="h-6 z-10 mr-2" />
-                <span className="text-xs">{`${achievement.sflReward} SFL`}</span>
-              </div>
+          <div
+            className={classNames(
+              "w-20 h-20 flex justify-center items-center mb-2 p-2.5 rounded-md mr-2 relative cursor-pointer hover:img-highlight",
+              {
+                "opacity-50": !isComplete,
+              }
             )}
-            {!!achievement.experienceReward && (
-              <div className="flex items-center mt-2">
-                <img src={experience} className="h-6 z-10 mr-2" />
-                <span className="text-xs">{`${achievement.experienceReward} Exp`}</span>
-              </div>
-            )}
-          </div>
-          <Button
-            className="text-sm mt-2"
-            onClick={onClaim}
-            disabled={!isComplete}
           >
-            <span>Claim</span>
-            <img src={chest} className="w-6 ml-2" />
-          </Button>
+            <img src={ITEM_DETAILS[name].image} className="h-full z-10" />
+            <img src={disc} className="absolute inset-0 w-full" />
+          </div>
+
+          <div className="flex flex-col items-center justify-center">
+            {/* <p className="text-lg underline">{name}</p> */}
+            <div className="flex-1 mt-1.5 text-xs flex-wrap justify-center items-center text-center">
+              <p className="text-sm mb-1">{achievement.description}</p>
+
+              <div className="flex items-center justify-center  mt-1 mb-1">
+                <div className="flex items-center justify-center relative w-40 h-4 z-10">
+                  <img src={progressBar} className="absolute w-full h-full" />
+                  <img
+                    src={progressBarEdge}
+                    className="absolute top-0 left-[-2px] h-full"
+                  />
+                  <img
+                    src={progressBarEdge}
+                    className="absolute top-0 right-[-2px] h-full"
+                    style={{
+                      transform: "scaleX(-1)",
+                    }}
+                  />
+                  <div className="w-full h-full bg-[#193c3e] absolute -z-20" />
+                  <div
+                    className="h-full bg-[#63c74d] absolute -z-10 left-0"
+                    style={{
+                      width: `${(progress / achievement.requirement) * 100}%`,
+                      maxWidth: "100%",
+                      borderRight: "3px solid #418848",
+                    }}
+                  />
+                </div>
+              </div>
+              <span className="text-xs text-white ml-2 mt-1">{`${progress}/${achievement.requirement}`}</span>
+            </div>
+          </div>
         </div>
-      )}
-    </div>
+
+        {!isAlreadyClaimed && (
+          <div
+            className={classNames("mt-4", {
+              "opacity-50": !isComplete,
+            })}
+          >
+            <div className="flex">
+              {!!achievement.sflReward && (
+                <div className="flex items-center mt-2 mr-3">
+                  <img src={sflIcon} className="h-6 z-10 mr-2" />
+                  <span className="text-xs">{`${achievement.sflReward} SFL`}</span>
+                </div>
+              )}
+              {!!achievement.experienceReward && (
+                <div className="flex items-center mt-2">
+                  <img src={experience} className="h-6 z-10 mr-2" />
+                  <span className="text-xs">{`${achievement.experienceReward} Exp`}</span>
+                </div>
+              )}
+            </div>
+            <Button
+              className="text-sm mt-2"
+              onClick={onClaim}
+              disabled={!isComplete}
+            >
+              <span>Claim</span>
+              <img src={chest} className="w-6 ml-2" />
+            </Button>
+          </div>
+        )}
+      </div>
+    </Panel>
   );
 };
