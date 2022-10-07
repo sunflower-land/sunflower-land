7e4d54207abf087336a5044ad2cd657cdecc0f91
HBandesh <35486909+HBandesh@users.noreply.github.com>
2022-10-07 15:51:53 +1030

    [FEAT] Adding test for mine gold

---
+++ src/features/game/events/landExpansion/mineGold.ts
@@ -0,0 +1,45 @@
+import { EVENT_ERRORS } from "features/game/expansion/lib/errorMessages";
+import { GOLD_MINE_STAMINA_COST } from "features/game/lib/constants";
+import { GameState } from "features/game/types/game";
+import cloneDeep from "lodash.clonedeep";
+import { replenishStamina } from "./replenishStamina";
+
+export type LandExpansionMineGoldAction = {
+  type: "goldRock.mined";
+  expansionIndex: number;
+  index: number;
+};
+
+type Options = {
+  state: Readonly<GameState>;
+  action: LandExpansionMineGoldAction;
+  createdAt?: number;
+};
+
+export function mineGold({
+  state,
+  action,
+  createdAt = Date.now(),
+}: Options): GameState {
+  const replenishedState = replenishStamina({
+    state,
+    action: { type: "bumpkin.replenishStamina" },
+    createdAt,
+  });
+  const stateCopy = cloneDeep(replenishedState);
+  const { expansions, bumpkin } = stateCopy;
+  const expansion = expansions[action.expansionIndex];
+
+  if (!bumpkin) {
+    throw new Error(EVENT_ERRORS.NO_BUMPKIN);
+  }
+
+  if (bumpkin.stamina.value < GOLD_MINE_STAMINA_COST) {
+    throw new Error(EVENT_ERRORS.NO_STAMINA);
+  }
+
+  if (!expansion) {
+    throw new Error(EVENT_ERRORS.NO_EXPANSION);
+  }
+  return stateCopy;
+}
---
+++ src/features/game/expansion/lib/errorMessages.ts
@@ -0,0 +1,9 @@
+export enum EVENT_ERRORS {
+  NO_PICKAXES = "No pickaxes left",
+  NO_IRON = "No iron",
+  STILL_RECOVERING = "Iron is still recovering",
+  EXPANSION_HAS_NO_IRON = "Expansion has no iron",
+  NO_EXPANSION = "Expansion does not exist",
+  NO_STAMINA = "You do not have enough stamina",
+  NO_BUMPKIN = "You do not have a Bumpkin",
+}
