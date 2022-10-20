3ee6d86fdb2e1b6e880b45ccd0120bd91f0b9288
Marcos Ricardo <94913189+mGranTY@users.noreply.github.com>
2022-10-20 11:45:59 -0400

    Nyon Statue hallowen themed 🙄

--- src/features/game/components/decorations/NyonStatue.tsx
+++ src/features/game/components/decorations/NyonStatue.tsx
@@ -5,7 +5,8 @@ import { GRID_WIDTH_PX } from "../../lib/constants";
 import { Panel } from "components/ui/Panel";
 
 import close from "assets/icons/close.png";
-import nyonStatue from "assets/nfts/nyon_statue.png";
+// nyon statue path edited to match event theme
+import nyonStatue from "assets/nfts/halloween/nyon_statue.png";
 
 // Only show 1 Nyon statue at a time
 export const NyonStatue: React.FC = () => {
