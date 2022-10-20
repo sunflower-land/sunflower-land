62c74624b764b7aed790185ac6e78b9d431a192e
Romy Furtado <rofrtd@gmail.com>
2022-10-20 16:06:34 +1100

    [FEAT] Sunflower Tombstone sizing

--- src/features/game/types/craftables.ts
+++ src/features/game/types/craftables.ts
@@ -1104,7 +1104,7 @@ export const COLLECTIBLES_DIMENSIONS: Record<CollectibleName, Dimensions> = {
   "Potato Statue": { width: 2, height: 2 },
   "Christmas Tree": { width: 2, height: 2 },
   Gnome: { width: 1, height: 1 },
-  "Sunflower Tombstone": { width: 1, height: 1 },
+  "Sunflower Tombstone": { width: 2, height: 2 },
   "Sunflower Rock": { width: 5, height: 4 },
   "Goblin Crown": { width: 1, height: 1 },
   Fountain: { width: 2, height: 2 },
