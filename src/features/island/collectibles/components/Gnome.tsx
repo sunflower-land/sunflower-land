3fd1c8f3ca6549d28053069fd63f3e3fcca4c472
Romy Furtado <rofrtd@gmail.com>
2022-10-21 11:43:29 +1100

    [FEAT] Gnome sizing

--- src/features/island/collectibles/components/Gnome.tsx
+++ src/features/island/collectibles/components/Gnome.tsx
@@ -2,14 +2,23 @@ import React from "react";
 
 import gnome from "assets/nfts/gnome.gif";
 import { PIXEL_SCALE } from "features/game/lib/constants";
+
 export const Gnome: React.FC = () => {
   return (
-    <img
-      src={gnome}
+    <div
+      className="absolute"
       style={{
-        width: `${PIXEL_SCALE * 19}px`,
+        width: `${PIXEL_SCALE * 17}px`,
       }}
-      alt="Gnome"
-    />
+    >
+      <img
+        src={gnome}
+        style={{
+          width: `${PIXEL_SCALE * 17}px`,
+        }}
+        alt="Gnome"
+        className="absolute"
+      />
+    </div>
   );
 };
