232002fb42a2bae3946a68788262efc66380cc0f
Marcos Ricardo <94913189+mGranTY@users.noreply.github.com>
2022-10-21 10:19:16 -0400

    Water updates!

--- src/features/halloween/water/Water.tsx
+++ src/features/halloween/water/Water.tsx
@@ -9,8 +9,8 @@ import chick from "assets/events/halloween/assets/animals/chick.gif";
 import dragonfly from "assets/decorations/dragonfly.gif";
 import Shark from "../../../components/ui/Shark";
 
-import goblinSwimming from "assets/npcs/goblin_swimming.gif";
-import goblinSnorkling from "assets/npcs/goblin_snorkling.gif";
+import goblinSwimming from "assets/events/halloween/assets/npcs/goblin_swimming.gif";
+import goblinSnorkling from "assets/events/halloween/assets/npcs/goblin_snorkling.gif";
 import waterBoat from "assets/npcs/water_boat.png";
 import { Frog } from "./Frog";
 import { FrogNft } from "components/ui/FrogNft";
