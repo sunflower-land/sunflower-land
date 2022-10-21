2ca5a636abd9b1b5aec244aa5fb62e0f58d57784
Marcos Ricardo <94913189+mGranTY@users.noreply.github.com>
2022-10-21 11:25:43 -0400

    Change soil color

--- src/features/halloween/crops/components/Field.tsx
+++ src/features/halloween/crops/components/Field.tsx
@@ -242,7 +242,7 @@ export const Field: React.FC<Props> = ({
     <div
       onMouseEnter={handleMouseHover}
       onMouseLeave={handleMouseLeave}
-      className={classNames("relative group", className)}
+      className={classNames("brightness-75 relative group", className)}
       style={{
         width: `${GRID_WIDTH_PX}px`,
         height: `${GRID_WIDTH_PX}px`,
