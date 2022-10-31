c445b700a5b2ed3e8b1575bb83a36a5e9ef2c267
Azaro <qppazaro@gmail.com>
2022-10-31 12:32:22 +0800

    [FEAT]: Display frogs in community garden.

--- src/features/community/project-dignity/components/FrogContainer.tsx
+++ src/features/community/project-dignity/components/FrogContainer.tsx
@@ -1,22 +1,29 @@
 import React from "react";
 
 import frogStatue from "assets/nfts/frogs/frog_statue.png";
-import { GRID_WIDTH_PX } from "features/game/lib/constants";
+import { GRID_WIDTH_PX, PIXEL_SCALE } from "features/game/lib/constants";
 import { Frog } from "../models/frog";
 import { FrogComponent } from "./FrogComponent";
 
 interface Props {
   frogs: Frog[];
   farmId: number | undefined;
+  position?: { left: string; top: string };
 }
 
-export const FrogContainer: React.FC<Props> = ({ frogs, farmId }) => {
+export const FrogContainer: React.FC<Props> = ({ frogs, farmId, position }) => {
+  if (!position) {
+    position = {
+      left: `${GRID_WIDTH_PX * 12}px`,
+      top: `${GRID_WIDTH_PX * 2.2}px`,
+    };
+  }
+
   return (
     <div
       style={{
-        width: `${GRID_WIDTH_PX * 12}px`,
-        left: `${GRID_WIDTH_PX * 14}px`,
-        top: `${GRID_WIDTH_PX * 2.2}px`,
+        width: `${GRID_WIDTH_PX * 8 * 2}px`, // Max 8(7+1 statue) frogs, each frog takes 2x grid width
+        ...position,
       }}
       className="flex absolute justify-center"
     >
@@ -25,7 +32,7 @@ export const FrogContainer: React.FC<Props> = ({ frogs, farmId }) => {
           src={frogStatue}
           className="z-10"
           style={{
-            width: `${GRID_WIDTH_PX * 2}px`,
+            width: `${32 * PIXEL_SCALE}px`,
           }}
         />
       )}
