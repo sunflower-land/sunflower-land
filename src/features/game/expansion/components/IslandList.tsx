8664463b8fda992ae59dc54aa0ae39c75cba2f2e
Adam Hannigan <adamhannigan81@gmail.com>
2022-11-01 13:54:29 +1100

    [FEAT] Missing required levels

--- src/features/game/expansion/components/IslandList.tsx
+++ src/features/game/expansion/components/IslandList.tsx
@@ -5,8 +5,11 @@ import { OuterPanel } from "components/ui/Panel";
 import { getBumpkinLevel } from "features/game/lib/level";
 import { Bumpkin } from "features/game/types/game";
 import goblin from "assets/buildings/goblin_sign.png";
-import human from "assets/npcs/bumpkin.png";
-import merchant from "assets/npcs/merchant.png";
+import human from "assets/icons/player.png";
+import helios from "assets/land/islands/helios_icon.png";
+import treasureIsland from "assets/land/islands/treasure_icon.png";
+import stoneHaven from "assets/land/islands/stone_haven.png";
+import sunflorea from "assets/land/islands/sunflorea.png";
 import snowman from "assets/npcs/snowman.png";
 
 const CONTENT_HEIGHT = 380;
@@ -16,16 +19,26 @@ interface Island {
   levelRequired: number;
   path: string;
   image?: string;
+  comingSoon?: boolean;
 }
 
 interface Props extends Island {
   bumpkin: Bumpkin | undefined;
 }
 
-const Island = ({ name, levelRequired, path, bumpkin, image }: Props) => {
+const Island = ({
+  name,
+  levelRequired,
+  path,
+  bumpkin,
+  image,
+  comingSoon,
+}: Props) => {
   const navigate = useNavigate();
   const cannotNavigate =
-    !bumpkin || getBumpkinLevel(bumpkin.experience) < levelRequired;
+    !bumpkin ||
+    getBumpkinLevel(bumpkin.experience) < levelRequired ||
+    comingSoon;
 
   if (cannotNavigate) {
     return (
@@ -47,6 +60,9 @@ const Island = ({ name, levelRequired, path, bumpkin, image }: Props) => {
               </span>
 
               <img src={lock} className="h-4 ml-1" />
+              {comingSoon && (
+                <span className="text-xxs ml-2 italic">Coming soon</span>
+              )}
             </div>
           </div>
         </OuterPanel>
@@ -74,28 +90,50 @@ export const IslandList = ({ bumpkin }: { bumpkin: Bumpkin | undefined }) => {
   const location = useLocation();
   const islands: Island[] = [
     {
-      name: "Helios",
-      levelRequired: 0,
-      image: merchant,
-      path: `/land/${id}/helios`,
-    },
-    {
-      name: "Snow Kingdom",
-      levelRequired: 0,
-      image: snowman,
-      path: `/snow/${id}`,
-    },
-    {
-      name: "Goblin Retreat",
-      levelRequired: 0,
-      image: goblin,
-      path: `/retreat/${id}`,
-    },
-    {
       name: "Farm",
       image: human,
       levelRequired: 0,
       path: `/land/${id}`,
+    },
+    {
+      name: "Helios",
+      levelRequired: 3,
+      image: helios,
+      path: `/land/${id}/helios`,
+    },
+    {
+      name: "Goblin Retreat",
+      levelRequired: 5,
+      image: goblin,
+      path: `/retreat/${id}`,
+    },
+    {
+      name: "Treasure Island",
+      levelRequired: 10,
+      image: treasureIsland,
+      path: `/treasure/${id}`,
+      comingSoon: true,
+    },
+    {
+      name: "Stone Haven",
+      levelRequired: 20,
+      image: stoneHaven,
+      path: `/treasure/${id}`,
+      comingSoon: true,
+    },
+    {
+      name: "Sunflorea",
+      levelRequired: 30,
+      image: sunflorea,
+      path: `/treasure/${id}`,
+      comingSoon: true,
+    },
+    {
+      name: "Snow Kingdom",
+      levelRequired: 50,
+      image: snowman,
+      path: `/snow/${id}`,
+      comingSoon: true,
     },
   ];
 
