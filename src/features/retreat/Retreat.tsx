5fb545cc625755b38ec5972eafa17230dce59650
ShiningMinions <github@shiningminions.com>
2022-11-01 09:40:52 -0400

    [CHORE] Remove IslandTravelWrapper and hoist context to parent.

--- src/features/retreat/Retreat.tsx
+++ src/features/retreat/Retreat.tsx
@@ -1,12 +1,12 @@
-import { GRID_WIDTH_PX } from "features/game/lib/constants";
-import { Section, useScrollIntoView } from "lib/utils/hooks/useScrollIntoView";
-import React, { useEffect, useRef } from "react";
+import React, { useContext, useEffect, useRef } from "react";
+import { useActor } from "@xstate/react";
 import ScrollContainer from "react-indiana-drag-scroll";
-
-import background from "assets/land/retreat.png";
+import { Section, useScrollIntoView } from "lib/utils/hooks/useScrollIntoView";
+import { GRID_WIDTH_PX, PIXEL_SCALE } from "features/game/lib/constants";
 import { ToastProvider } from "features/game/toast/ToastQueueProvider";
-import waterMovement from "assets/decorations/water_movement.png";
-import { GoblinProvider } from "features/game/GoblinProvider";
+import { Context, GoblinProvider } from "features/game/GoblinProvider";
+import { MapPlacement } from "features/game/expansion/components/MapPlacement";
+import { IslandTravel } from "features/game/expansion/components/IslandTravel";
 import { RetreatBank } from "./components/bank/RetreatBank";
 import { RetreatStorageHouse } from "./components/storageHouse/RetreatStorageHouse";
 import { RetreatHotAirBalloon } from "./components/hotAirBalloon/RetreatHotAirBalloon";
@@ -15,9 +15,25 @@ import { RetreatBlacksmith } from "./components/blacksmith/RetreatBlacksmith";
 import { Auctioneer } from "./components/auctioneer/Auctioneer";
 import { Resale } from "./components/resale/Resale";
 import { RetreatWishingWell } from "./components/wishingWell/RetreatWishingWell";
-import { IslandTravelWrapper } from "./components/islandTravel/IslandTravelWrapper";
+
+import background from "assets/land/retreat.png";
+import waterMovement from "assets/decorations/water_movement.png";
+import pirateGoblin from "assets/npcs/pirate_goblin.gif";
 
 export const Retreat: React.FC = () => {
+  return (
+    <GoblinProvider>
+      <ToastProvider>
+        <RetreatView />
+      </ToastProvider>
+    </GoblinProvider>
+  );
+};
+
+const RetreatView: React.FC = () => {
+  const { goblinService } = useContext(Context);
+  const [gameState] = useActor(goblinService);
+  const { bumpkin } = gameState.context.state;
   const container = useRef(null);
 
   const [scrollIntoView] = useScrollIntoView();
@@ -29,45 +45,46 @@ export const Retreat: React.FC = () => {
 
   // Load data
   return (
-    <GoblinProvider>
-      <ToastProvider>
-        <ScrollContainer
-          className="relative w-full h-full bg-[#0099db] overflow-scroll"
-          innerRef={container}
-        >
-          <div
-            className="relative flex"
+    <ScrollContainer
+      className="relative w-full h-full bg-[#0099db] overflow-scroll"
+      innerRef={container}
+    >
+      <div
+        className="relative flex"
+        style={{
+          width: `${40 * GRID_WIDTH_PX}px`,
+          height: `${40 * GRID_WIDTH_PX}px`,
+        }}
+      >
+        <img src={background} className="absolute inset-0 w-full h-full z-10" />
+        <RetreatBank />
+        <RetreatStorageHouse />
+        <RetreatHotAirBalloon />
+        <RetreatTailor />
+        <RetreatBlacksmith />
+        <Auctioneer />
+        <Resale />
+        <RetreatWishingWell />
+        <MapPlacement x={-1.4} y={-12}>
+          <img
+            src={pirateGoblin}
             style={{
-              width: `${40 * GRID_WIDTH_PX}px`,
-              height: `${40 * GRID_WIDTH_PX}px`,
-            }}
-          >
-            <img
-              src={background}
-              className="absolute inset-0 w-full h-full z-10"
-            />
-            <RetreatBank />
-            <RetreatStorageHouse />
-            <RetreatHotAirBalloon />
-            <RetreatTailor />
-            <RetreatBlacksmith />
-            <Auctioneer />
-            <Resale />
-            <RetreatWishingWell />
-            <IslandTravelWrapper />
-          </div>
-          <div
-            className="absolute inset-0 bg-repeat"
-            style={{
-              width: `${80 * GRID_WIDTH_PX}px`,
-              height: `${80 * GRID_WIDTH_PX}px`,
-              backgroundImage: `url(${waterMovement})`,
-              backgroundSize: "400px",
-              imageRendering: "pixelated",
+              width: `${25 * PIXEL_SCALE}px`,
             }}
           />
-        </ScrollContainer>
-      </ToastProvider>
-    </GoblinProvider>
+        </MapPlacement>
+        <IslandTravel bumpkin={bumpkin} x={-2} y={-15} />
+      </div>
+      <div
+        className="absolute inset-0 bg-repeat"
+        style={{
+          width: `${80 * GRID_WIDTH_PX}px`,
+          height: `${80 * GRID_WIDTH_PX}px`,
+          backgroundImage: `url(${waterMovement})`,
+          backgroundSize: "400px",
+          imageRendering: "pixelated",
+        }}
+      />
+    </ScrollContainer>
   );
 };
--- src/features/snowKingdom/SnowKingdom.tsx
+++ src/features/snowKingdom/SnowKingdom.tsx
@@ -1,15 +1,32 @@
-import { GRID_WIDTH_PX } from "features/game/lib/constants";
-import { Section, useScrollIntoView } from "lib/utils/hooks/useScrollIntoView";
-import React, { useEffect, useRef } from "react";
+import React, { useContext, useEffect, useRef } from "react";
+import { useActor } from "@xstate/react";
 import ScrollContainer from "react-indiana-drag-scroll";
+import { Section, useScrollIntoView } from "lib/utils/hooks/useScrollIntoView";
+import { GRID_WIDTH_PX, PIXEL_SCALE } from "features/game/lib/constants";
+import { Context, GameProvider } from "features/game/GameProvider";
+import { ToastProvider } from "features/game/toast/ToastQueueProvider";
+import { MapPlacement } from "features/game/expansion/components/MapPlacement";
+import { IslandTravel } from "features/game/expansion/components/IslandTravel";
 
 import background from "assets/land/snow_kingdom.png";
-import { GameProvider } from "features/game/GameProvider";
-import { ToastProvider } from "features/game/toast/ToastQueueProvider";
 import waterMovement from "assets/decorations/water_movement.png";
-import { IslandTravelWrapper } from "./IslandTravelWrapper";
+import pirateGoblin from "assets/npcs/pirate_goblin.gif";
 
 export const SnowKingdom: React.FC = () => {
+  return (
+    <GameProvider>
+      <ToastProvider>
+        <SnowKingdomView />
+      </ToastProvider>
+    </GameProvider>
+  );
+};
+
+const SnowKingdomView: React.FC = () => {
+  const { gameService } = useContext(Context);
+  const [gameState] = useActor(gameService);
+  const { bumpkin } = gameState.context.state;
+
   const container = useRef(null);
 
   const [scrollIntoView] = useScrollIntoView();
@@ -21,37 +38,38 @@ export const SnowKingdom: React.FC = () => {
 
   // Load data
   return (
-    <GameProvider>
-      <ToastProvider>
-        <ScrollContainer
-          className="relative w-full h-full bg-[#0099db] overflow-scroll"
-          innerRef={container}
-        >
-          <div
-            className="relative flex"
+    <ScrollContainer
+      className="relative w-full h-full bg-[#0099db] overflow-scroll"
+      innerRef={container}
+    >
+      <div
+        className="relative flex"
+        style={{
+          width: `${40 * GRID_WIDTH_PX}px`,
+          height: `${40 * GRID_WIDTH_PX}px`,
+        }}
+      >
+        <img src={background} className="absolute inset-0 w-full h-full z-10" />
+        <MapPlacement x={-1.3} y={-12}>
+          <img
+            src={pirateGoblin}
             style={{
-              width: `${40 * GRID_WIDTH_PX}px`,
-              height: `${40 * GRID_WIDTH_PX}px`,
-            }}
-          >
-            <img
-              src={background}
-              className="absolute inset-0 w-full h-full z-10"
-            />
-            <IslandTravelWrapper />
-          </div>
-          <div
-            className="absolute inset-0 bg-repeat"
-            style={{
-              width: `${80 * GRID_WIDTH_PX}px`,
-              height: `${80 * GRID_WIDTH_PX}px`,
-              backgroundImage: `url(${waterMovement})`,
-              backgroundSize: "400px",
-              imageRendering: "pixelated",
+              width: `${25 * PIXEL_SCALE}px`,
             }}
           />
-        </ScrollContainer>
-      </ToastProvider>
-    </GameProvider>
+        </MapPlacement>
+        <IslandTravel bumpkin={bumpkin} x={-2} y={-14} />
+      </div>
+      <div
+        className="absolute inset-0 bg-repeat"
+        style={{
+          width: `${80 * GRID_WIDTH_PX}px`,
+          height: `${80 * GRID_WIDTH_PX}px`,
+          backgroundImage: `url(${waterMovement})`,
+          backgroundSize: "400px",
+          imageRendering: "pixelated",
+        }}
+      />
+    </ScrollContainer>
   );
 };
