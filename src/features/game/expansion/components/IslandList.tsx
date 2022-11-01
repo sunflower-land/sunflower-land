5fdb0b4c0016112bfaff9dca5eef0dad0cba3320
Adam Hannigan <adamhannigan81@gmail.com>
2022-11-01 14:02:47 +1100

    [FIX] Travel level icon

--- src/features/game/expansion/components/IslandList.tsx
+++ src/features/game/expansion/components/IslandList.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import lock from "assets/skills/lock.png";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { OuterPanel } from "components/ui/Panel";
 import { getBumpkinLevel } from "features/game/lib/level";
 import { Bumpkin } from "features/game/types/game";
+
+import lock from "assets/skills/lock.png";
+import heart from "assets/icons/heart.png";
+
 import goblin from "assets/buildings/goblin_sign.png";
-import human from "assets/icons/player.png";
+import farm from "assets/crops/sunflower/planted.png";
 import helios from "assets/land/islands/helios_icon.png";
 import treasureIsland from "assets/land/islands/treasure_icon.png";
 import stoneHaven from "assets/land/islands/stone_haven.png";
@@ -43,7 +46,7 @@ const Island = ({
   if (cannotNavigate) {
     return (
       <div>
-        <OuterPanel className="flex relative items-center py-2 mb-1 opacity-50">
+        <OuterPanel className="flex relative items-center py-2 mb-1 opacity-70">
           {image && (
             <div className="w-16 justify-center flex mr-2">
               <img src={image} className="h-9" />
@@ -52,6 +55,8 @@ const Island = ({
           <div className="flex-1 flex flex-col justify-center">
             <span className="text-sm">{name}</span>
             <div className="flex items-center">
+              <img src={heart} className="h-4 mr-1" />
+
               <span
                 className="bg-error border text-xxs p-1 rounded-md"
                 style={{ lineHeight: "10px" }}
@@ -91,7 +96,7 @@ export const IslandList = ({ bumpkin }: { bumpkin: Bumpkin | undefined }) => {
   const islands: Island[] = [
     {
       name: "Farm",
-      image: human,
+      image: farm,
       levelRequired: 0,
       path: `/land/${id}`,
     },
