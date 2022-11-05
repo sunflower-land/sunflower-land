c679363af1c9886a4fa544b72f2e84e457a67dcf
HBandesh <35486909+HBandesh@users.noreply.github.com>
2022-11-06 00:13:53 +1030

    putting assertions back

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,7 +5,13 @@ module.exports = {
     },
     assert: {
       // preset: "lighthouse:recommended",
-      budgetsFile: "./budget.json",
+      // budgetsFile: "./budget.json",
+      "first-contentful-paint": ["error", { maxNumericValue: 8600 }],
+      interactive: ["error", { maxNumericValue: 18900 }],
+      "total-blocking-time": ["error", { maxNumericValue: 2200 }],
+      "categories:performance": ["error", { minScore: 0.17 }],
+      "categories:accessibility": ["error", { minScore: 0.7 }],
+      "categories:best-practices": ["error", { minScore: 0.85 }],
     },
   },
 };
