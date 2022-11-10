af5f74e65d88584d63cd103d5ea590ebe924571f
Polysemouse <sunflowermousewax@gmail.com>
2022-11-10 02:04:08 -0800

    added toast for placeable

--- src/features/farming/hud/components/PlaceableController.tsx
+++ src/features/farming/hud/components/PlaceableController.tsx
@@ -7,9 +7,15 @@ import { Context } from "features/game/GameProvider";
 
 import confirm from "assets/icons/confirm.png";
 import cancel from "assets/icons/cancel.png";
+import { ToastContext } from "features/game/toast/ToastQueueProvider";
+import { BUILDINGS } from "features/game/types/buildings";
+import { ITEM_DETAILS } from "features/game/types/images";
+import token from "src/assets/icons/token_2.png";
+import { ANIMALS } from "features/game/types/craftables";
 
 export const PlaceableController: React.FC = () => {
   const { gameService } = useContext(Context);
+  const { setToast } = useContext(ToastContext);
   const child = gameService.state.children.editing as MachineInterpreter;
 
   const [
@@ -20,7 +26,45 @@ export const PlaceableController: React.FC = () => {
   ] = useActor(child);
 
   const handleConfirmPlacement = () => {
+    // prevents multiple toasts while spam clicking place button
+    if (!child.state.matches("idle")) {
+      return;
+    }
+
     send("PLACE");
+
+    if (gameService.state.event.type === "building.constructed") {
+      const buildingName = gameService.state.event.name;
+      const building = BUILDINGS()[buildingName];
+      building.ingredients?.forEach((ingredient) => {
+        const item = ITEM_DETAILS[ingredient.item];
+        setToast({
+          icon: item.image,
+          content: `-${ingredient.amount}`,
+        });
+      });
+
+      if (building.sfl.greaterThan(0)) {
+        setToast({
+          icon: token,
+          content: `-${building.sfl}`,
+        });
+      }
+
+      setToast({
+        icon: ITEM_DETAILS[buildingName].image,
+        content: "+1",
+      });
+    } else if (gameService.state.event.type === "chicken.bought") {
+      setToast({
+        icon: token,
+        content: `-${ANIMALS().Chicken.tokenAmount}`,
+      });
+      setToast({
+        icon: ITEM_DETAILS["Chicken"].image,
+        content: "+1",
+      });
+    }
   };
 
   const handleCancelPlacement = () => {
