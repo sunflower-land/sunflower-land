2648a291163c086c6a9bfb121a40bec78554decb
spencer <spencer@sunflower-land.com>
2022-11-11 14:31:10 +1100

    [FEAT] Add shadow to prized potato

--- src/features/island/collectibles/components/PrizedPotato.tsx
+++ src/features/island/collectibles/components/PrizedPotato.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 
 import prizedPotato from "assets/sfts/prized_potato.gif";
+import shadow from "assets/npcs/shadow.png";
 import { PIXEL_SCALE } from "features/game/lib/constants";
 
 export const PrizedPotato: React.FC = () => {
   return (
     <>
+      <img
+        src={shadow}
+        style={{
+          width: `${PIXEL_SCALE * 15}px`,
+          bottom: `${PIXEL_SCALE * 0.1}px`,
+          left: `${PIXEL_SCALE * 0}px`,
+        }}
+        className="absolute w-full left z-29"
+      />
       <img
         src={prizedPotato}
         style={{
