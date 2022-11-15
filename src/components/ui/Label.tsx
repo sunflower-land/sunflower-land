45b08242b65ba1f6c428b88e5b15372298d199f2
Adam Hannigan <adamhannigan81@gmail.com>
2022-11-16 08:41:20 +1100

    [FEAT] Resize defaults

--- src/components/ui/Label.tsx
+++ src/components/ui/Label.tsx
@@ -14,7 +14,7 @@ export const Label: React.FC<Props> = ({ children, className }) => {
       )}
       style={pixelWhiteBorderStyle}
     >
-      {children}
+      <span>{children}</span>
     </div>
   );
 };
