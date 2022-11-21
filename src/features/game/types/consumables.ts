7dafcd66723fcfec138c138a285f76104ac7f037
Adam Hannigan <adamhannigan81@gmail.com>
2022-11-21 19:54:39 +1100

    [FIX] Sunflower Cake values

--- src/features/game/types/consumables.ts
+++ src/features/game/types/consumables.ts
@@ -184,14 +184,14 @@ export const CONSUMABLES: Record<ConsumableName, Consumable> = {
     name: "Sunflower Cake",
     description: "Sunflower Cake",
     building: "Bakery",
-    experience: 425,
+    experience: 525,
     cookingSeconds: 60 * 60 * 6.5,
     ingredients: {
       Sunflower: new Decimal(1000),
       Wheat: new Decimal(10),
       Egg: new Decimal(15),
     },
-    marketRate: 360,
+    marketRate: 440,
   },
   "Potato Cake": {
     name: "Potato Cake",
