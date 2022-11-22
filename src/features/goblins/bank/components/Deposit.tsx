e49d2f5541c0b0d140ad8e627da2d442dd581a87
AncientHorse <97365256+AncientHorse@users.noreply.github.com>
2022-11-22 05:27:21 -0800

    Not sure what is causing prettier/prettier error

--- src/features/goblins/bank/components/Deposit.tsx
+++ src/features/goblins/bank/components/Deposit.tsx
@@ -100,8 +100,7 @@ const SFLItemsInstructions = () => (
 
     <li className="flex text-xs mb-3">
       <span className="mr-1">1.</span>
-      <span>{'Go to your OpenSea profile and click the "Transfer" button on the item'}
-      </span>
+      <span>{'Go to your OpenSea profile and click the "Transfer" button on the item'}</span>
     </li>
     <li className="flex text-xs mb-3">
       <span className="mr-1">2.</span>
