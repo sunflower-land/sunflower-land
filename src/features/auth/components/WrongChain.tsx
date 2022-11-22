1103a45146aa1a3d8f004df394a016eea76fb73c
Polysemouse <sunflowermousewax@gmail.com>
2022-11-21 18:59:43 -0800

    [FIX] Menu button text alignments

--- src/features/auth/components/WrongChain.tsx
+++ src/features/auth/components/WrongChain.tsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from "react";
 import { Button } from "components/ui/Button";
 
 import alert from "assets/icons/expression_alerted.png";
-import metamaskIcon from "assets/icons/metamask-icon.png";
 import { metamask } from "lib/blockchain/metamask";
 import { useIsMobile } from "lib/utils/hooks/useIsMobile";
+import { metamaskIcon } from "./WalletIcons";
+import { PIXEL_SCALE } from "features/game/lib/constants";
 
 export const WrongChain: React.FC = () => {
   const [isDefaultNetwork, setIsDefaultNetwork] = useState(false);
@@ -31,22 +32,36 @@ export const WrongChain: React.FC = () => {
 
   return (
     <div className="flex flex-col text-center text-shadow items-center p-1">
-      <div className="flex mb-3 items-center">
-        <img src={alert} alt="Warning" className="w-3 mr-3" />
+      <div className="flex m-2 items-center">
+        <img
+          src={alert}
+          alt="Warning"
+          style={{
+            width: `${PIXEL_SCALE * 4}px`,
+          }}
+        />
       </div>
       <p className="text-center mb-3">{`You're not connected to Polygon`}</p>
 
       <p className="text-center mb-4 text-xs">
         Check out this guide to help you get connected.
       </p>
-      <Button onClick={goToPolygonSetupDocs} className="overflow-hidden mb-2">
+      <Button
+        onClick={goToPolygonSetupDocs}
+        className="mb-2 py-2 text-sm relative"
+      >
         <span>Go to guide</span>
       </Button>
       {/* This doesn't work on metamask browser so we won't show if on mobile */}
       {(!isDefaultNetwork || !isMobile) && (
-        <Button onClick={initialiseNetwork} className="overflow-hidden mb-2">
-          <img src={metamaskIcon} alt="Metamask Icon" className="mr-2" />
-          <span>Add or Switch Network</span>
+        <Button
+          onClick={initialiseNetwork}
+          className="mb-2 py-2 text-sm relative"
+        >
+          <div className="px-8">
+            {metamaskIcon}
+            Add or Switch Network
+          </div>
         </Button>
       )}
     </div>
