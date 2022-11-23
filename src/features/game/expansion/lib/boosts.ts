0528b25c62bc72d777a1bdaa759feef90f7c35dd
Romy Furtado <rofrtd@gmail.com>
2022-11-24 10:00:39 +1100

    [FEAT] remove tool discount

--- src/features/game/expansion/lib/boosts.ts
+++ src/features/game/expansion/lib/boosts.ts
@@ -11,7 +11,6 @@ import { CAKES } from "../../types/craftables";
 import Decimal from "decimal.js-light";
 import { isCollectibleBuilt } from "features/game/lib/collectibleBuilt";
 import { CRAFTABLE_TOOLS } from "features/game/events/landExpansion/craftTool";
-import { WorkbenchToolName } from "features/game/types/tools";
 
 const crops = CROPS();
 const cakes = CAKES();
@@ -147,22 +146,4 @@ export const getFoodExpBoost = (foodExp: number, bumpkin: Bumpkin): number => {
   }
 
   return boostedExp.toNumber();
-};
-export const getToolBuyPrice = (
-  item: WorkbenchToolName,
-  inventory: Inventory
-) => {
-  const toolObject = tools[item];
-  let price = toolObject.sfl;
-
-  if (!price) {
-    return new Decimal(0);
-  }
-
-  //apply 10% discount to tools
-  if (price && inventory.Artist?.gte(1)) {
-    price = price.mul(0.9);
-  }
-
-  return price;
 };
--- src/features/island/buildings/components/building/workBench/components/WorkbenchModal.tsx
+++ src/features/island/buildings/components/building/workBench/components/WorkbenchModal.tsx
@@ -21,7 +21,6 @@ import { Tab } from "components/ui/Tab";
 import { WorkbenchToolName, WORKBENCH_TOOLS } from "features/game/types/tools";
 import { getKeys } from "features/game/types/craftables";
 import { PIXEL_SCALE } from "features/game/lib/constants";
-import { getToolBuyPrice } from "features/game/expansion/lib/boosts";
 
 interface Props {
   isOpen: boolean;
@@ -278,7 +277,7 @@ export const WorkbenchModal: React.FC<Props> = ({ isOpen, onClose }) => {
                           "text-red-500": lessFunds(),
                         })}
                       >
-                        {`$${getToolBuyPrice(selectedName, inventory)}`}
+                        {`$${price?.toNumber()}`}
                       </span>
                     </div>
                   )}
