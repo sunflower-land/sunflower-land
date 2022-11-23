01df5d878d5252c676ee462d11a9663d591bae55
Marcos Ricardo <94913189+mGranTY@users.noreply.github.com>
2022-11-22 23:45:12 -0400

    [FIX] TEST LOCATION

--- src/features/game/events/landExpansion/chop.test.ts
+++ src/features/game/events/landExpansion/chop.test.ts
@@ -192,6 +192,34 @@ describe("chop", () => {
     expect(tree2.wood.amount).toBeGreaterThan(2);
   });
 
+  it("chops trees with the logger Skill", () => {
+    const game = chop({
+      state: {
+        ...GAME_STATE,
+        inventory: {
+          Logger: new Decimal(1),
+          Axe: new Decimal(1),
+        },
+        bumpkin: INITIAL_BUMPKIN,
+        collectibles: {},
+      },
+      createdAt: Date.now(),
+      action: {
+        type: "timber.chopped",
+        item: "Axe",
+        index: 0,
+        expansionIndex: 0,
+      } as LandExpansionChopAction,
+    });
+
+    const { expansions } = game;
+    const trees = expansions[0].trees;
+    const tree = (trees as Record<number, LandExpansionTree>)[0];
+
+    expect(game.inventory.Wood).toEqual(new Decimal(3));
+    expect(tree.wood.amount).toBeGreaterThan(2);
+    expect(game.inventory.Axe).toEqual(new Decimal(0.5));
+  });
   it("tree replenishes normally", () => {
     const dateNow = Date.now();
     const game = chop({
@@ -445,33 +473,5 @@ describe("getChoppedAt", () => {
     const treeTimeWithBeaver = TREE_RECOVERY_TIME / 2; // 3600
     const treeTimeStacked = (treeTimeWithBeaver + treeTimeWithSkill) * 1000;
     expect(time).toEqual(now - treeTimeStacked);
-  });
-  it("chops trees with the logger Skill", () => {
-    const game = chop({
-      state: {
-        ...GAME_STATE,
-        inventory: {
-          Logger: new Decimal(1),
-          Axe: new Decimal(1),
-        },
-        bumpkin: INITIAL_BUMPKIN,
-        collectibles: {},
-      },
-      createdAt: Date.now(),
-      action: {
-        type: "timber.chopped",
-        item: "Axe",
-        index: 0,
-        expansionIndex: 0,
-      } as LandExpansionChopAction,
-    });
-
-    const { expansions } = game;
-    const trees = expansions[0].trees;
-    const tree = (trees as Record<number, LandExpansionTree>)[0];
-
-    expect(game.inventory.Wood).toEqual(new Decimal(3));
-    expect(tree.wood.amount).toBeGreaterThan(2);
-    expect(game.inventory.Axe).toEqual(new Decimal(0.5));
   });
 });
