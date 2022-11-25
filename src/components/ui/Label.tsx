1125034009b2cdcb8cf00202174c001306205c2e
hujikol <nikolasnanda@gmail.com>
2022-11-26 02:17:03 +0700

    fix error TS2322

--- src/components/ui/Label.tsx
+++ src/components/ui/Label.tsx
@@ -7,61 +7,58 @@ interface Props {
   type?: string;
 }
 export const Label: React.FC<Props> = ({ children, className, type }) => {
-  if (type === "default")
-    return (
-      <div
-        className={classnames(
-          "bg-silver-300 text-white text-xxs object-contain justify-center items-center flex px-1",
-          className
-        )}
-        style={pixelWhiteBorderStyle}
-      >
+  return (
+    <>
+      {type === "default" && (
+        <div
+          className={classnames(
+            "bg-silver-300 text-white text-xxs object-contain justify-center items-center flex px-1",
+            className
+          )}
+          style={pixelWhiteBorderStyle}
+        >
+          <span
+            className={classnames(" text-white text-xxs")}
+            style={{
+              lineHeight: "12px",
+              height: "14px",
+            }}
+          >
+            {children}
+          </span>
+        </div>
+      )}
+
+      {type === "success" && (
         <span
-          className={classnames(" text-white text-xxs")}
-          style={{
-            lineHeight: "12px",
-            height: "14px",
-          }}
+          className={classnames(
+            "bg-green-600 border text-xxs px-2 py-1 rounded-md",
+            className
+          )}
         >
           {children}
         </span>
-      </div>
-    );
-
-  if (type === "success") {
-    return (
-      <span
-        className={classnames(
-          "bg-green-600 border text-xxs px-2 py-1 rounded-md",
-          className
-        )}
-      >
-        {children}
-      </span>
-    );
-  }
-  if (type === "info") {
-    return (
-      <span
-        className={classnames(
-          "bg-blue-600 border text-xxs px-2 py-1 rounded-md",
-          className
-        )}
-      >
-        {children}
-      </span>
-    );
-  }
-  if (type === "danger") {
-    return (
-      <span
-        className={classnames(
-          "bg-error border text-xxs px-2 py-1 rounded-md",
-          className
-        )}
-      >
-        {children}
-      </span>
-    );
-  }
+      )}
+      {type === "info" && (
+        <span
+          className={classnames(
+            "bg-blue-600 border text-xxs px-2 py-1 rounded-md",
+            className
+          )}
+        >
+          {children}
+        </span>
+      )}
+      {type === "danger" && (
+        <span
+          className={classnames(
+            "bg-error border text-xxs px-2 py-1 rounded-md",
+            className
+          )}
+        >
+          {children}
+        </span>
+      )}
+    </>
+  );
 };
