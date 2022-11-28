87b16727402488ed6dc68226c76de60121e38234
j923sneaks <89294757+j923sneaks@users.noreply.github.com>
2022-11-28 10:22:54 +0800

    [FEAT] Background Color on Placeables (#1764)
    * [FEAT] Background Color on Placeables
    
    * feat: add bg color to currently placed items

--- src/features/game/expansion/components/MapPlacement.tsx
+++ src/features/game/expansion/components/MapPlacement.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import classNames from "classnames";
 import { GRID_WIDTH_PX } from "../../lib/constants";
 
 export type Coordinates = {
@@ -11,21 +12,28 @@ export type Position = {
   width?: number;
 } & Coordinates;
 
+type Props = {
+  isEditing?: boolean;
+} & Position;
+
 /**
  * This component is used to place items on the map. It uses the cartesian place coordinates
  * as the basis for its positioning. If the coordinates are 1,1 then the item will be placed one
  * grid size up and one grid right. The item will be placed at 0,0 of this coordinate.
  */
-export const MapPlacement: React.FC<Position> = ({
+export const MapPlacement: React.FC<Props> = ({
   x,
   y,
   height,
   width,
+  isEditing = false,
   children,
 }) => {
   return (
     <div
-      className="absolute"
+      className={classNames("absolute", {
+        "bg-red-background/80": isEditing,
+      })}
       style={{
         top: `calc(50% - ${GRID_WIDTH_PX * y}px)`,
         left: `calc(50% + ${GRID_WIDTH_PX * x}px)`,
