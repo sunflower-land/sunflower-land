7bc004408fd8114c0d2881cf9dc5e993b849c51b
ShiningMinions <github@shiningminions.com>
2022-12-01 23:17:03 -0500

    [CHORE] Re-enable easter bunny withdraws.

--- src/features/game/types/withdrawables.ts
+++ src/features/game/types/withdrawables.ts
@@ -31,7 +31,23 @@ const globalDefaults = Object.keys(KNOWN_IDS).reduce(
 // Group withdraw conditions for common items
 const cropDefaults = buildDefaults(Object.keys(CROPS()), true);
 
+// Helper functions
+type CanWithdrawArgs = {
+  item: InventoryItemName;
+  game: GoblinState;
+};
+
+function cropIsPlanted({ item, game }: CanWithdrawArgs): boolean {
+  if (!game.fields) return false;
+
+  const isPlanted = Object.values(game.fields).some(
+    (field) => field.name === item
+  );
+  return isPlanted;
+}
+
 export const WITHDRAWABLES: Record<InventoryItemName, WithdrawCondition> = {
   ...globalDefaults,
   ...cropDefaults,
+  "Easter Bunny": (game) => !cropIsPlanted({ item: "Carrot", game }),
 };
