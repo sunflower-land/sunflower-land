da0644b1fd6e1b00621722b41c29459c4aeaa2fb
Spencer Dezart-Smith <17863697+spencerdezartsmith@users.noreply.github.com>
2022-12-05 10:54:02 +1100

    [CHORE] Update HUD layout (#1809)
    * [CHORE] Update HUD layout
    
    * [CHORE] Update image
    
    * [CHORE] Move toast back to the left
    
    * [FIX] type
    
    * [FIX] Modal

--- src/features/island/hud/components/inventory/Inventory.tsx
+++ src/features/island/hud/components/inventory/Inventory.tsx
@@ -41,10 +41,10 @@ export const Inventory: React.FC<Props> = ({
 
   return (
     <div
-      className="flex flex-row-reverse items-center fixed z-50"
+      className="flex flex-col items-center fixed z-50"
       style={{
         right: `${PIXEL_SCALE * 3}px`,
-        bottom: `${PIXEL_SCALE * 3}px`,
+        top: `${PIXEL_SCALE * 50.2}px`,
       }}
     >
       <div
@@ -52,7 +52,7 @@ export const Inventory: React.FC<Props> = ({
         className="relative flex z-50 cursor-pointer hover:img-highlight"
         style={{
           marginLeft: `${PIXEL_SCALE * 2}px`,
-          marginBottom: `${PIXEL_SCALE * 22.5}px`,
+          marginBottom: `${PIXEL_SCALE * 24}px`,
           width: `${PIXEL_SCALE * 22}px`,
         }}
       >
@@ -79,7 +79,7 @@ export const Inventory: React.FC<Props> = ({
       </Modal>
 
       {isFarming && (
-        <div className="flex items-center">
+        <div className="flex flex-col-reverse items-center">
           {shortcuts.map((item, index) => (
             <Box
               key={index}
