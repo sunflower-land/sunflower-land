99fe49503c8b73a3b871421b45ab8d1d8705a721
Polysemouse <107602352+Polysemouse@users.noreply.github.com>
2022-12-06 15:16:23 -0800

    [FIX] Improve save button UI (#1820)
    * [FIX] Improve save button UI
    
    * [FIX] Minor fix
    
    * [FIX] Disable save button instead of hiding it when save is not needed
    
    * [FIX] Reduce save icon size
    
    * [FIX] Updated save icon
    
    * [CHORE] Update save image
    
    Co-authored-by: spencer <spencer@sunflower-land.com>

--- src/components/ui/Panel.tsx
+++ src/components/ui/Panel.tsx
@@ -51,13 +51,14 @@ export const Panel: React.FC<Props> = ({
 /**
  * Light panel with border effect
  */
-export const InnerPanel: React.FC<Props> = ({ children, className }) => {
+export const InnerPanel: React.FC<Props> = ({ children, className, style }) => {
   return (
     <div
       className={classNames("bg-brown-300", className)}
       style={{
         ...pixelLightBorderStyle,
         padding: `${PIXEL_SCALE * 1}px`,
+        ...style,
       }}
     >
       {children}
