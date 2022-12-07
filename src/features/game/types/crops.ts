7af0305209638fd73a0e7f6165314c14c764f10b
HBandesh <35486909+HBandesh@users.noreply.github.com>
2022-12-07 10:53:02 +1030

    [FIX] Correcting kale seed level issue (#1827)
    * [FIX] Correcting kale seed level issue
    
    * [CHORE] Update final Kale values
    
    * [CHORE] Add sell price
    
    * [CHORE] Kale numbers
    
    Co-authored-by: spencer <spencer@sunflower-land.com>

--- src/features/game/types/crops.ts
+++ src/features/game/types/crops.ts
@@ -25,7 +25,6 @@ export type Crop = {
 
 /**
  * Crops and their original prices
- * TODO - use crop name from GraphQL API
  */
 export const CROPS: () => Record<CropName, Crop> = () => ({
   Sunflower: {
@@ -100,7 +99,8 @@ export const CROPS: () => Record<CropName, Crop> = () => ({
   },
   Kale: {
     buyPrice: marketRate(8),
-    harvestSeconds: 24 * 60 * 60,
+    sellPrice: marketRate(10),
+    harvestSeconds: 36 * 60 * 60,
     name: "Kale",
     description: "Bumpkin Power Food",
   },
@@ -179,10 +179,9 @@ export const CROP_SEEDS: () => Record<CropSeedName, CraftableItem> = () => ({
   },
   "Kale Seed": {
     name: "Kale Seed",
-    tokenAmount: marketRate(5),
+    tokenAmount: marketRate(7),
     ingredients: [],
     description: "A Bumpkin Power Food!",
-    plantSeconds: 24 * 60 * 60,
-    disabled: true,
+    bumpkinLevel: 7,
   },
 });
