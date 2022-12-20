b06377d3a0aeaf13c222d4f343a999b1586fb07c
Marcos Ricardo <94913189+mGranTY@users.noreply.github.com>
2022-12-20 00:18:24 -0400

    [FIX] potion building art

--- src/features/helios/components/potions/Potions.tsx
+++ src/features/helios/components/potions/Potions.tsx
@@ -2,8 +2,9 @@ import React from "react";
 
 import { PIXEL_SCALE } from "features/game/lib/constants";
 
-import building from "assets/buildings/fertilisers.png";
+// import building from "assets/buildings/fertilisers.png";
 // import potionMaster from "assets/npcs/potion_master.gif";
+import building from "assets/events/christmas/buildings/fertilisers.png";
 import potionMaster from "assets/events/christmas/npcs/potion_master.gif";
 import shadow from "assets/npcs/shadow.png";
 import close from "assets/icons/close.png";
