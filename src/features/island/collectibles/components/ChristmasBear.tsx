fe9fbd97d3d1856af232d62579cabf9d0cc0e9af
Romy Furtado <rofrtd@gmail.com>
2022-12-22 10:12:29 +1100

    [FIX] xmas bear sizing (#1917)

--- src/features/island/collectibles/components/ChristmasBear.tsx
+++ src/features/island/collectibles/components/ChristmasBear.tsx
@@ -5,7 +5,14 @@ import { PIXEL_SCALE } from "features/game/lib/constants";
 
 export const ChristmasBear: React.FC = () => {
   return (
-    <>
+    <div
+      className="absolute"
+      style={{
+        width: `${PIXEL_SCALE * 20}px`,
+        left: `${PIXEL_SCALE * 0}px`,
+        bottom: `${PIXEL_SCALE * 0}px`,
+      }}
+    >
       <img
         src={bear}
         style={{
@@ -16,6 +23,6 @@ export const ChristmasBear: React.FC = () => {
         className="absolute"
         alt="Christmas Bear"
       />
-    </>
+    </div>
   );
 };
