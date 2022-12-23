36eeec4afc5719b77bfe6a88d936af2590d1ae4d
Nicholas NS <nikolasnanda@gmail.com>
2022-12-23 17:07:04 +0700

    [FIX] Grub shop timer on mobile

--- src/features/helios/components/grubShop/components/GrubShopModal.tsx
+++ src/features/helios/components/grubShop/components/GrubShopModal.tsx
@@ -128,7 +128,19 @@ export const GrubShopModal: React.FC<Props> = ({ onClose }) => {
           }}
         >
           <div className="flex flex-col-reverse sm:flex-row">
-            <div className="flex flex-col-reverse md:flex-row flex-wrap w-full sm:w-3/5 h-fit max-h-48 sm:max-h-96 overflow-y-auto scrollable overflow-x-hidden p-1 mt-1 sm:mt-0 sm:mr-1">
+            <div className="flex md:flex-row flex-wrap w-full sm:w-3/5 h-fit max-h-48 sm:max-h-96 overflow-y-auto scrollable overflow-x-hidden p-1 mt-1 sm:mt-0 sm:mr-1">
+              {isAllFullFilled && (
+                <div className="flex items-start my-2 mb-2 md:mt-2">
+                  <p className="text-xs mb-1 mr-2">More orders in</p>
+                  <Label type="info" className="flex flex-row items-center">
+                    <img src={stopwatch} className="w-3 left-0 mr-1" />
+                    {`${secondsToString(secondsLeft as number, {
+                      length: "medium",
+                      isShortFormat: true,
+                    })}`}
+                  </Label>
+                </div>
+              )}
               <div className="flex flex-wrap">
                 {Object.values(grubShop.orders).map((item, index) => {
                   const isFulfilled = !!state.grubOrdersFulfilled?.find(
@@ -166,18 +178,6 @@ export const GrubShopModal: React.FC<Props> = ({ onClose }) => {
                   );
                 })}
               </div>
-              {isAllFullFilled && (
-                <div className="flex items-start sm:flex-col ml-2 mb-2 md:mt-2">
-                  <p className="text-xs my-1 mr-2">More orders in</p>
-                  <Label type="info" className="flex flex-row items-center">
-                    <img src={stopwatch} className="w-3 left-0 mr-1" />
-                    {`${secondsToString(secondsLeft as number, {
-                      length: "medium",
-                      isShortFormat: true,
-                    })}`}
-                  </Label>
-                </div>
-              )}
             </div>
             {selected && (
               <OuterPanel className="w-full flex-1">
