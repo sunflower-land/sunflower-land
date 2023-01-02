654d4d72b458652bf91eee7ac394e83fed680e91
Adam Hannigan <adamhannigan81@gmail.com>
2023-01-03 08:56:41 +1100

    Discord widget test

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,67 @@ import ErrorBoundary from "features/auth/components/ErrorBoundary";
 // Initialise Global Settings
 initialise();
 
+import { Client } from "@widgetbot/embed-api";
+
 /**
  * Top level wrapper for providers
  */
 export const App: React.FC = () => {
+  // useEffect(() => {
+  //   const load = async () => {
+  //     await new Promise((res) => setTimeout(res, 2000));
+
+  //     const iframe = document.getElementsByTagName("iframe")[0];
+  //     console.log({ iframe });
+  //     const api = new Client({
+  //       id: "test",
+  //       iframe,
+  //     });
+
+  //     api.on("message", (message) => {
+  //       console.log("message:", message.id);
+  //     });
+
+  //     api.emit("sendMessage", "hello world");
+  //   };
+
+  //   load();
+  // }, []);
+
+  const onApi = (client: Client) => {
+    console.log({ client });
+    client.on("signIn", (user) => {
+      console.log(`Guest signed in as ${user.username}`, user);
+    });
+
+    // Listen for discord message events
+    client.on("message", ({ message, channel }) => {
+      console.log(`New message in ${channel}`, message);
+    });
+
+    // Listen for discord message delete events
+    client.on("messageDelete", ({ channel, id }) => {
+      console.log(`Message in ${channel} with an ID of ${id}, was deleted`);
+    });
+  };
   return (
     <Auth.Provider>
       <ErrorBoundary>
         <Navigation />
+        {/* <div
+          className="fixed inset-0 z-50"
+          style={{
+            zIndex: 100000000,
+          }}
+        >
+          <WidgetBot
+            height={500}
+            server="880987707214544966"
+            channel="880987707692687381"
+            shard="https://emerald.widgetbot.io"
+            onAPI={onApi}
+          />
+        </div> */}
       </ErrorBoundary>
     </Auth.Provider>
   );
