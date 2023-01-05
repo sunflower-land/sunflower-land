3d69135900c006133567d6496e0e35fb45802dad
Romy Furtado <25412194+Rofrtd@users.noreply.github.com>
2023-01-05 13:38:57 +1100

    [FEAT] Pineapple Shirt (#1971)
    * [FEAT] add party shirt
    
    * [FEAT] rename image
    
    * [FEAT] change NPC

--- src/features/island/buildings/components/building/smoothieShack/SmoothieShack.tsx
+++ src/features/island/buildings/components/building/smoothieShack/SmoothieShack.tsx
@@ -3,8 +3,8 @@ import classNames from "classnames";
 
 import smoothieShack from "assets/buildings/smoothie_shack_background.webp";
 import smoothieShackDesk from "assets/buildings/smoothie_shack_desk.webp";
-import chef from "assets/npcs/chef.gif";
-import chefDoing from "assets/npcs/chef_doing.gif";
+import smoothieChef from "assets/npcs/smoothie.gif";
+import smoothieChefMaking from "assets/npcs/smoothie_making.gif";
 
 import { ConsumableName } from "features/game/types/consumables";
 import { ITEM_DETAILS } from "features/game/types/images";
@@ -92,7 +92,7 @@ export const SmoothieShack: React.FC<Props> = ({
 
         {crafting ? (
           <img
-            src={chefDoing}
+            src={smoothieChefMaking}
             className="absolute pointer-events-none"
             style={{
               width: `${PIXEL_SCALE * 16}px`,
@@ -102,7 +102,7 @@ export const SmoothieShack: React.FC<Props> = ({
           />
         ) : (
           <img
-            src={chef}
+            src={smoothieChef}
             className="absolute pointer-events-none"
             style={{
               width: `${PIXEL_SCALE * 15}px`,
