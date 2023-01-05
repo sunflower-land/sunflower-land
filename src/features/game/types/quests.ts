3cbae954584899ae73c06dea66e289b0069eb76a
HBandesh <35486909+HBandesh@users.noreply.github.com>
2023-01-05 16:30:03 +1030

    [FEAT] Adding fruit quest UI

--- src/features/game/types/quests.ts
+++ src/features/game/types/quests.ts
@@ -1,11 +1,12 @@
-import { GameState } from "../types/game";
 import { BumpkinItem } from "./bumpkin";
+import { GameState } from "./game";
 
 export type QuestName =
   | "Farmer Quest 1"
-  | "Reindeer Quest 1"
-  | "Reindeer Quest 2"
-  | "Reindeer Quest 3";
+  | "Fruit Quest 1"
+  | "Fruit Quest 2"
+  | "Fruit Quest 3"
+  | "Fruit Quest 4";
 
 export type Quest = {
   description: string;
@@ -22,32 +23,49 @@ export const QUESTS: Record<QuestName, Quest> = {
     requirement: 1000,
     wearable: "Red Farmer Shirt",
   },
-  "Reindeer Quest 1": {
-    description: "Eat 20 Reindeer Carrots",
+  "Fruit Quest 1": {
+    description: "Harvest 10 Blueberries",
     progress: (gameState: GameState) =>
-      gameState.bumpkin?.activity?.["Reindeer Carrot Fed"] || 0,
-    requirement: 20,
-    wearable: "Reindeer Suit",
+      gameState.bumpkin?.activity?.["Blueberry Harvested"] || 0,
+    requirement: 10,
+    wearable: "Red Farmer Shirt",
   },
-  "Reindeer Quest 2": {
-    description: "Eat 120 Reindeer Carrots",
+  "Fruit Quest 2": {
+    description: "Harvest 100 Oranges",
     progress: (gameState: GameState) =>
-      gameState.bumpkin?.activity?.["Reindeer Carrot Fed"] || 0,
-    requirement: 120,
-    wearable: "Reindeer Antlers",
+      gameState.bumpkin?.activity?.["Orange Harvested"] || 0,
+    requirement: 100,
+    wearable: "Farmer Overalls",
   },
-  "Reindeer Quest 3": {
-    description: "Eat 500 Reindeer Carrots",
+  "Fruit Quest 3": {
+    description: "Harvest 200 Apples",
     progress: (gameState: GameState) =>
-      gameState.bumpkin?.activity?.["Reindeer Carrot Fed"] || 0,
-    requirement: 500,
-    wearable: "Christmas Background",
+      gameState.bumpkin?.activity?.["Apple Harvested"] || 0,
+    requirement: 200,
+    wearable: "Farmer Hat",
+  },
+  "Fruit Quest 4": {
+    description: "Harvest 1200 Blueberries",
+    progress: (gameState: GameState) =>
+      gameState.bumpkin?.activity?.["Blueberry Harvested"] || 0,
+    requirement: 1200,
+    wearable: "Farmer Pitchfork",
   },
 };
 
-export const BUMPKIN_QUEST_IDS: Record<QuestName, number> = {
-  "Farmer Quest 1": 100001,
-  "Reindeer Quest 1": 100002,
-  "Reindeer Quest 2": 100003,
-  "Reindeer Quest 3": 100004,
-};
+type CompletedQuestName =
+  | "Reindeer Quest 1"
+  | "Reindeer Quest 2"
+  | "Reindeer Quest 3";
+
+export const BUMPKIN_QUEST_IDS: Record<QuestName | CompletedQuestName, number> =
+  {
+    "Farmer Quest 1": 100001,
+    "Reindeer Quest 1": 100002,
+    "Reindeer Quest 2": 100003,
+    "Reindeer Quest 3": 100004,
+    "Fruit Quest 1": 100005,
+    "Fruit Quest 2": 100006,
+    "Fruit Quest 3": 100007,
+    "Fruit Quest 4": 100008,
+  };
