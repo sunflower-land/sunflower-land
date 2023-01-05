b63c744a3c63740349d41b2b2237b992fda09365
HBandesh <35486909+HBandesh@users.noreply.github.com>
2023-01-05 12:07:53 +1030

    [FEAT] pre loading harvest tree image

--- src/features/island/fruit/FruitTree.tsx
+++ src/features/island/fruit/FruitTree.tsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { getTimeLeft } from "lib/utils/time";
 import { setImageWidth } from "lib/images";
 import { PlantedFruit } from "features/game/types/game";
 import useUiRefresher from "lib/utils/hooks/useUiRefresher";
-import { FRUIT } from "features/game/types/fruits";
+import { FRUIT, FruitName } from "features/game/types/fruits";
 import { FRUIT_LIFECYCLE } from "./fruits";
 import { Soil } from "./Soil";
 
 import { Seedling } from "./Seedling";
 import { ReplenishingTree } from "./ReplenishingTree";
+
+import apple from "/src/assets/resources/apple.png";
+import orange from "/src/assets/resources/orange.png";
+import blueberry from "/src/assets/resources/blueberry.png";
+import { FruitDropAnimator } from "components/animation/FruitDropAnimator";
+
+export const getFruitImage = (fruitName: FruitName): string => {
+  switch (fruitName) {
+    case "Apple":
+      return apple;
+    case "Orange":
+      return orange;
+    case "Blueberry":
+      return blueberry;
+  }
+};
 
 interface Props {
   plantedFruit?: PlantedFruit;
@@ -18,6 +34,7 @@ interface Props {
   harvestFruit: () => void;
   removeTree: () => void;
   onError: () => void;
+  playAnimation: boolean;
 }
 
 export const FruitTree: React.FC<Props> = ({
@@ -27,46 +44,68 @@ export const FruitTree: React.FC<Props> = ({
   removeTree,
   onError,
   playing,
+  playAnimation,
 }) => {
   useUiRefresher({ active: !!plantedFruit });
+  //UI Refresher reloads this component after a regular time intervals.
+  //which results to pre loading of the image again and again.
+  const [alreadyPreloaded, setAlreadyPreloaded] = useState(false);
+
+  const preloadImage = (url: string) => {
+    const img = new Image();
+    img.src = url;
+    setAlreadyPreloaded(true);
+  };
 
   if (!plantedFruit) {
     return <Soil playing={playing} onClick={plantTree} />;
   }
 
-  const { harvestSeconds, isBush } = FRUIT()[plantedFruit.name];
-  const lifecycle = FRUIT_LIFECYCLE[plantedFruit.name];
+  const { name, amount, harvestsLeft, harvestedAt, plantedAt } = plantedFruit;
+  const { harvestSeconds, isBush } = FRUIT()[name];
+  const lifecycle = FRUIT_LIFECYCLE[name];
 
   // Dead tree
-  if (!plantedFruit.harvestsLeft) {
+  if (!harvestsLeft) {
     return (
-      <img
-        className="relative cursor-pointer hover:img-highlight"
-        style={{
-          bottom: "-10px",
-          zIndex: "1",
+      <FruitDropAnimator
+        mainImageProps={{
+          src: lifecycle.dead,
+          className: "relative cursor-pointer hover:img-highlight",
+          style: {
+            bottom: "-9px",
+            zIndex: "1",
+          },
+          onLoad: (e) => setImageWidth(e.currentTarget),
+          onClick: removeTree,
         }}
-        src={lifecycle.dead}
-        onLoad={(e) => setImageWidth(e.currentTarget)}
-        onClick={removeTree}
+        dropImageProps={{
+          src: getFruitImage(name),
+        }}
+        dropCount={amount}
+        playDropAnimation={playAnimation}
+        playShakeAnimation={false}
       />
     );
   }
 
   // Replenishing tree
-  if (plantedFruit.harvestedAt) {
-    const replenishingTimeLeft = getTimeLeft(
-      plantedFruit.harvestedAt,
-      harvestSeconds
-    );
+  if (harvestedAt) {
+    const replenishingTimeLeft = getTimeLeft(harvestedAt, harvestSeconds);
 
     if (replenishingTimeLeft > 0) {
-      return <ReplenishingTree onClick={onError} plantedFruit={plantedFruit} />;
+      return (
+        <ReplenishingTree
+          onClick={onError}
+          plantedFruit={plantedFruit}
+          playAnimation={playAnimation}
+        />
+      );
     }
   }
 
   // Seedling
-  const growingTimeLeft = getTimeLeft(plantedFruit.plantedAt, harvestSeconds);
+  const growingTimeLeft = getTimeLeft(plantedAt, harvestSeconds);
 
   if (growingTimeLeft > 0) {
     return (
@@ -76,6 +115,13 @@ export const FruitTree: React.FC<Props> = ({
         plantedFruit={plantedFruit}
       />
     );
+  }
+
+  //Pre loading the harvested tree image so that we get a smooth transition
+  // when the user clicks on the ready tree and it transitions to the harvest state.
+  if (!alreadyPreloaded) {
+    preloadImage(lifecycle.harvested);
+    preloadImage(getFruitImage(name));
   }
 
   // Ready tree
