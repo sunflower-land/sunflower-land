71282573c4c6aa331882f7501688925d24392a17
Craig <41215134+c-py@users.noreply.github.com>
2023-01-06 18:49:41 +1300

    [FEAT] Fruit announcement (#1992)
    * [FEAT] Fruit announcement
    
    * [FEAT] add docs link

--- src/features/announcements/index.ts
+++ src/features/announcements/index.ts
@@ -1,4 +1,5 @@
 import reindeer from "assets/announcements/christmas_banner.gif";
+import fruit from "assets/announcements/fruit.gif";
 
 export interface Announcement {
   date: Date;
@@ -18,5 +19,16 @@ export const ANNOUNCEMENTS: Announcement[] = [
     title: "Reinder Quest",
     notes: ["A mysterious Reindeer has appeared to the South."],
     image: reindeer,
+  },
+  {
+    date: new Date("2023-01-10T00:00:00"),
+    title: "Fruit Trees are here!",
+    notes: [
+      "Apple, orange, and blueberry seeds are now available! Seeds grow into fruit trees, which can be harvested a random amount of times to collect fruit!",
+      "But don't get too attached, once you're done harvesting, you gotta cut them trees down to make room for new ones.",
+      "Once you've collected enough fruit, you can sell it or turn it into them fancy smoothies at the smoothie shack!",
+    ],
+    image: fruit,
+    link: "https://docs.sunflower-land.com/player-guides/planting-and-harvesting/fruit",
   },
 ];
