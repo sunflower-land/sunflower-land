2a6ec5ba985ed56dde4b2a463496b5bffb10cf5b
Craig <41215134+c-py@users.noreply.github.com>
2023-01-11 12:54:00 +1100

    [FEAT] Add Foreman Beaver to Fruit Trees

--- src/features/game/events/landExpansion/fruitTreeRemoved.ts
+++ src/features/game/events/landExpansion/fruitTreeRemoved.ts
@@ -1,6 +1,18 @@
 import Decimal from "decimal.js-light";
-import { GameState, InventoryItemName } from "features/game/types/game";
+import { isCollectibleBuilt } from "features/game/lib/collectibleBuilt";
+import { FruitName } from "features/game/types/fruits";
+import {
+  Collectibles,
+  GameState,
+  Inventory,
+  InventoryItemName,
+} from "features/game/types/game";
 import cloneDeep from "lodash.clonedeep";
+
+export enum FRUIT_TREE_REMOVED_ERRORS {
+  MISSING_AXE = "No axe",
+  NO_AXES = "No axes left",
+}
 
 export type RemoveFruitTreeAction = {
   type: "fruitTree.removed";
@@ -15,13 +27,32 @@ type Options = {
   createdAt?: number;
 };
 
+export function getRequiredAxeAmount(
+  fruitName: FruitName,
+  inventory: Inventory,
+  collectibles: Collectibles
+) {
+  // Apply boost for Trees
+  if (fruitName === "Apple" || fruitName === "Orange") {
+    if (isCollectibleBuilt("Foreman Beaver", collectibles)) {
+      return new Decimal(0);
+    }
+
+    if (inventory.Logger?.gte(1)) {
+      return new Decimal(0.5);
+    }
+  }
+
+  return new Decimal(1);
+}
+
 export function removeFruitTree({
   state,
   action,
   createdAt = Date.now(),
 }: Options): GameState {
   const stateCopy = cloneDeep(state);
-  const { expansions, bumpkin, inventory } = stateCopy;
+  const { expansions, bumpkin, inventory, collectibles } = stateCopy;
   const expansion = expansions[action.expansionIndex];
 
   if (!bumpkin) {
@@ -36,16 +67,6 @@ export function removeFruitTree({
     throw new Error("Expansion does not have any fruit patches");
   }
 
-  if (action.selectedItem !== "Axe") {
-    throw new Error("No axe selected");
-  }
-
-  const axeAmount = inventory.Axe || new Decimal(0);
-
-  if (axeAmount.lessThan(1)) {
-    throw new Error("No axes left");
-  }
-
   const { fruitPatches } = expansion;
 
   const patch = fruitPatches[action.index];
@@ -58,6 +79,22 @@ export function removeFruitTree({
     throw new Error("Nothing was planted");
   }
 
+  const requiredAxes = getRequiredAxeAmount(
+    patch.fruit.name,
+    inventory,
+    collectibles
+  );
+
+  if (action.selectedItem !== "Axe" && requiredAxes.gt(0)) {
+    throw new Error(FRUIT_TREE_REMOVED_ERRORS.MISSING_AXE);
+  }
+
+  const axeAmount = inventory.Axe || new Decimal(0);
+
+  if (axeAmount.lessThan(requiredAxes)) {
+    throw new Error(FRUIT_TREE_REMOVED_ERRORS.NO_AXES);
+  }
+
   const { harvestsLeft } = patch.fruit;
 
   if (harvestsLeft) {
@@ -66,7 +103,7 @@ export function removeFruitTree({
 
   delete patch.fruit;
 
-  inventory.Axe = axeAmount.sub(1);
+  inventory.Axe = axeAmount.sub(requiredAxes);
   stateCopy.inventory.Wood = stateCopy.inventory.Wood?.add(1) || new Decimal(1);
 
   return stateCopy;
