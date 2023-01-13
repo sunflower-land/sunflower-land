f971d784b46a513c4623fa76170005a451037934
spencer <spencer@sunflower-land.com>
2023-01-13 15:54:24 +1100

    [FEAT] Add secondary image placeholder for gold egg

--- src/features/island/collectibles/components/GoldEgg.tsx
+++ src/features/island/collectibles/components/GoldEgg.tsx
@@ -1,19 +1,41 @@
 import React from "react";
 
 import goldEgg from "assets/sfts/gold_egg.png";
-import { PIXEL_SCALE } from "features/game/lib/constants";
+import questionMark from "assets/icons/expression_confused.png";
 
-export const GoldEgg: React.FC = () => {
+import { PIXEL_SCALE } from "features/game/lib/constants";
+import { CollectibleProps } from "../Collectible";
+
+export const GoldEgg: React.FC<CollectibleProps> = ({ readyAt }) => {
+  const isReady = readyAt <= Date.now();
+
   return (
-    <img
-      src={goldEgg}
-      style={{
-        width: `${PIXEL_SCALE * 10}px`,
-        bottom: `${PIXEL_SCALE * 2}px`,
-        left: `${PIXEL_SCALE * 3}px`,
-      }}
-      className="absolute"
-      alt="Gold Egg"
-    />
+    <>
+      {!isReady && (
+        <img
+          src={goldEgg}
+          style={{
+            width: `${PIXEL_SCALE * 10}px`,
+            bottom: `${PIXEL_SCALE * 2}px`,
+            left: `${PIXEL_SCALE * 3}px`,
+          }}
+          className="absolute"
+          alt="Gold Egg"
+        />
+      )}
+      {isReady && (
+        <img
+          src={questionMark}
+          style={{
+            width: `${PIXEL_SCALE * 7}px`,
+            bottom: `${PIXEL_SCALE * 2}px`,
+            left: `50%`,
+            transform: "translatex(-50%)",
+          }}
+          className="absolute"
+          alt="Gold Egg"
+        />
+      )}
+    </>
   );
 };
