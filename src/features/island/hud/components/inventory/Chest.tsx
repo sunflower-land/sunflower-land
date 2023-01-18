00f461d92225eb8c1addd04d851cfaa3d05e3b8e
Polysemouse <sunflowermousewax@gmail.com>
2023-01-17 20:02:22 -0800

    [CHORE] Apply component to inventory

--- src/features/island/hud/components/inventory/Chest.tsx
+++ src/features/island/hud/components/inventory/Chest.tsx
@@ -19,9 +19,10 @@ import {
   GOBLIN_BLACKSMITH_ITEMS,
   HELIOS_BLACKSMITH_ITEMS,
 } from "features/game/types/collectibles";
-import { SplitScreenView } from "features/game/components/SplitScreenView";
-import { SquareIcon } from "components/ui/SquareIcon";
+import { SplitScreenView } from "components/ui/SplitScreenView";
 import { AUCTIONEER_ITEMS } from "features/game/types/auctioneer";
+import { PIXEL_SCALE } from "features/game/lib/constants";
+import { CraftingRequirementsView } from "components/ui/CraftingRequirementsView";
 
 interface Props {
   state: GameState;
@@ -70,6 +71,24 @@ export const Chest: React.FC<Props> = ({
     getKeys(collectibles)[0]
   );
 
+  const chestIsEmpty = getKeys(collectibles).length === 0;
+  if (chestIsEmpty) {
+    return (
+      <div className="flex flex-col justify-evenly items-center p-2">
+        <img
+          src={chest}
+          alt="Empty Chest"
+          style={{
+            width: `${PIXEL_SCALE * 17}px`,
+          }}
+        />
+        <span className="text-xs text-center mt-2">
+          Your chest is empty, discover rare items today!
+        </span>
+      </div>
+    );
+  }
+
   const handlePlace = () => {
     onPlace && onPlace(selected);
 
@@ -80,76 +99,50 @@ export const Chest: React.FC<Props> = ({
     setSelected(item);
   };
 
-  const chestIsEmpty = getKeys(collectibles).length === 0;
-
-  if (chestIsEmpty) {
-    return (
-      <div className="flex flex-col justify-evenly items-center p-2">
-        <img src={chest} className="h-12" alt="Empty Chest" />
-        <span className="text-xs text-center mt-2 w-80">
-          Your chest is empty, discover rare items today!
-        </span>
-      </div>
-    );
-  }
-
   return (
     <SplitScreenView
       divRef={divRef}
       tallMobileContent={true}
       wideModal={true}
-      showHeader={!chestIsEmpty && !!selected}
+      showHeader={!!selected}
       header={
         selected && (
-          <>
-            <div className="flex flex-col justify-center p-2 pb-0">
-              <div className="flex space-x-2 justify-start mb-1 items-center sm:flex-col-reverse md:space-x-0">
-                <div className="sm:mt-2">
-                  <SquareIcon icon={ITEM_DETAILS[selected].image} width={14} />
-                </div>
-                <span className="sm:text-center">{selected}</span>
-              </div>
-              <span className="text-xs sm:text-center">
-                {ITEM_DETAILS[selected].description}
-              </span>
-              <div className="border-t border-white w-full my-2 pt-1 flex justify-between sm:flex-col sm:items-center">
-                <a
-                  href={`https://opensea.io/assets/matic/0x22d5f9b75c524fec1d6619787e582644cd4d7422/${KNOWN_IDS[selected]}`}
-                  className="underline text-xxs hover:text-blue-500 p-2"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  OpenSea
-                </a>
-              </div>
-            </div>
-            {onPlace && (
-              <Button onClick={handlePlace} disabled={isSaving}>
-                {isSaving ? "Saving..." : "Place on map"}
-              </Button>
-            )}
-          </>
+          <CraftingRequirementsView
+            gameState={state}
+            details={{
+              type: "item",
+              item: selected,
+            }}
+            requirements={{
+              showOpenSeaLink: true,
+            }}
+            actionView={
+              onPlace && (
+                <Button onClick={handlePlace} disabled={isSaving}>
+                  {isSaving ? "Saving..." : "Place on map"}
+                </Button>
+              )
+            }
+          />
         )
       }
       content={
-        <>
-          {Object.values(collectibles) && (
-            <div className="flex flex-col pl-2" key={"Collectibles"}>
-              <div className="flex mb-2 flex-wrap -ml-1.5 pt-1">
-                {getKeys(collectibles).map((item) => (
-                  <Box
-                    count={getItemCount(item)}
-                    isSelected={selected === item}
-                    key={item}
-                    onClick={() => handleItemClick(item)}
-                    image={ITEM_DETAILS[item].image}
-                    parentDivRef={divRef}
-                  />
-                ))}
-              </div>
+        Object.values(collectibles) && (
+          <div className="flex flex-col pl-2">
+            <div className="flex mb-2 flex-wrap -ml-1.5 pt-1">
+              {getKeys(collectibles).map((item) => (
+                <Box
+                  count={getItemCount(item)}
+                  isSelected={selected === item}
+                  key={item}
+                  onClick={() => handleItemClick(item)}
+                  image={ITEM_DETAILS[item].image}
+                  parentDivRef={divRef}
+                />
+              ))}
             </div>
-          )}
-        </>
+          </div>
+        )
       }
     />
   );
