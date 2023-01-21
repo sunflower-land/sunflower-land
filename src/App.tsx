f7d229899370c7a638a24da1574865ad6bfaab73
HBandesh <35486909+HBandesh@users.noreply.github.com>
2023-01-22 04:41:18 +1030

    correcting syntax error

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import "./styles.css";
 import ErrorBoundary from "features/auth/components/ErrorBoundary";
 
 //Import PWA icons so that they are included in vite build
+import pwaIcon from "./src/assets/brand/icon_pwa_1.png";
+import icon192 from "./src/assets/brand/logo_with_sunflower_pwa_192.png";
+import icon512 from "./src/assets/brand/logo_with_sunflower_pwa_512.png";
 
 // Initialise Global Settings
 initialise();
@@ -22,9 +25,9 @@ export const App: React.FC = () => {
         <Navigation />
       </ErrorBoundary>
       <div>
-        <img src="pwaIcon" className="hidden" />
-        <img src="icon192" className="hidden" />
-        <img src="icon512" className="hidden" />
+        <img src={pwaIcon} className="hidden" />
+        <img src={icon192} className="hidden" />
+        <img src={icon512} className="hidden" />
       </div>
     </Auth.Provider>
   );
