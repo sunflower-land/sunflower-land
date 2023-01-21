31b37753249bec0f05c8e74045db9b45ba475560
HBandesh <35486909+HBandesh@users.noreply.github.com>
2023-01-22 03:17:28 +1030

    service worker support of vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { minifyHtml, injectHtml } from "vite-plugin-html";
 import inject from "@rollup/plugin-inject";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,6 +15,7 @@ export default defineConfig({
       // TODO with API environment variables
       injectData: {},
     }),
+    VitePWA({ registerType: "autoUpdate" }),
   ],
   // Addresses web3 issue
   resolve: {
