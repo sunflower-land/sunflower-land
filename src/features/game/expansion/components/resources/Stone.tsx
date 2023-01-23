dd4daeb92669020df1e13f7ecbb89843b218c806
Polysemouse <sunflowermousewax@gmail.com>
2023-01-23 13:52:18 -0800

    [CHORE] Unify resource node touch UX

--- src/features/game/expansion/components/resources/Stone.tsx
+++ src/features/game/expansion/components/resources/Stone.tsx
@@ -98,9 +98,11 @@ export const Stone: React.FC<Props> = ({ rockIndex, expansionIndex }) => {
 
     if (!hasPickaxes) return;
 
-    const isPlaying = sparkGif.current?.getInfo("isPlaying");
+    const spriteFrame = sparkGif.current?.getInfo("frame");
 
-    if (isPlaying) return;
+    if (spriteFrame && spriteFrame < 6) {
+      return;
+    }
 
     miningAudio.play();
     sparkGif.current?.goToAndPlay(0);
