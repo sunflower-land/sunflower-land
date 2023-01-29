65c5f5fe4afb4fcaa4c4160d5b0ba49c2382421b
Polysemouse <107602352+Polysemouse@users.noreply.github.com>
2023-01-29 14:30:45 -0800

    [FIX] Observatory audio play before gif is loaded (#2033)
    * [FIX] Observatory audio play before gif is loaded
    
    * [CHORE] Remove unnessary classname
    
    * [FIX] Make placeables clickable only in their respective grids
    
    * [FIX] Limit clickable area to grid

--- src/features/island/collectibles/components/Fountain.tsx
+++ src/features/island/collectibles/components/Fountain.tsx
@@ -6,20 +6,29 @@ import { fountainAudio } from "lib/utils/sfx";
 
 export const Fountain: React.FC = () => {
   return (
-    <img
-      style={{
-        width: `${PIXEL_SCALE * 26}px`,
-        bottom: `${PIXEL_SCALE * 1}px`,
-        left: `${PIXEL_SCALE * 3}px`,
-      }}
+    <div
+      className="absolute w-full h-full hover:img-highlight cursor-pointer"
       onClick={() => {
         if (!fountainAudio.playing()) {
           fountainAudio.play();
         }
       }}
-      className="absolute"
-      src={fountain}
-      alt="Fountain"
-    />
+    >
+      <img
+        style={{
+          width: `${PIXEL_SCALE * 26}px`,
+          bottom: `${PIXEL_SCALE * 1}px`,
+          left: `${PIXEL_SCALE * 3}px`,
+        }}
+        onClick={() => {
+          if (!fountainAudio.playing()) {
+            fountainAudio.play();
+          }
+        }}
+        className="absolute pointer-events-none"
+        src={fountain}
+        alt="Fountain"
+      />
+    </div>
   );
 };
