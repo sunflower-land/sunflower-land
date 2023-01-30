2759438cdffaf2e80873e771b6c0dc254bb18ddf
Polysemouse <107602352+Polysemouse@users.noreply.github.com>
2023-01-29 16:56:37 -0800

    [FIX] Plaza chat improvements (#2103)
    * [FIX] Pumpkin plaza improvements 1
    
    * [FIX] Error on sending spaces in chat
    
    * [FIX] Increase text box width
    
    * [FIX] Warning label getting pushed out of panel

--- src/features/pumpkinPlaza/components/ChatUI.tsx
+++ src/features/pumpkinPlaza/components/ChatUI.tsx
@@ -1,4 +1,3 @@
-import { Panel } from "components/ui/Panel";
 import React, { useState } from "react";
 
 import { ChatText } from "./ChatText";
@@ -6,68 +5,39 @@ import { ChatReactions } from "./ChatReactions";
 import { GameState } from "features/game/types/game";
 import { ReactionName } from "../lib/reactions";
 import { SUNNYSIDE } from "assets/sunnyside";
+import { CloseButtonPanel } from "features/game/components/CloseablePanel";
 
 interface Props {
   game: GameState;
   onMessage: (content: { text?: string; reaction?: ReactionName }) => void;
 }
 export const ChatUI: React.FC<Props> = ({ onMessage, game }) => {
-  const [tab, setTab] = useState<"text" | "reactions">("text");
+  const [tab, setTab] = useState(0);
 
   return (
     <div
-      className="w-full flex justify-center fixed bottom-4 pl-2 md:pr-2 pr-20"
+      className="w-full flex justify-center fixed bottom-4 pl-2 pr-[73.5px] md:pr-2"
       style={{ zIndex: 999 }}
       onClick={() => console.log("parent clicked")}
     >
-      <Panel className="md:w-1/2 w-full h-full">
-        <div className="flex justify-between">
-          {tab === "text" && (
-            <>
-              <div className="flex items-center">
-                <img
-                  src={SUNNYSIDE.icons.expression_chat}
-                  className="h-6 mr-2"
-                />
-                <span className="text-sm">Chat</span>
-              </div>
-              <div
-                className="flex items-center cursor-pointer"
-                onClick={() => setTab("reactions")}
-              >
-                <span className="text-xs underline">Reactions</span>
-                <img src={SUNNYSIDE.icons.heart} className="h-4 ml-2" />
-              </div>
-            </>
-          )}
-
-          {tab === "reactions" && (
-            <>
-              <div className="flex items-center">
-                <img src={SUNNYSIDE.icons.heart} className="h-6 mr-2" />
-                <span className="text-sm">Reactions</span>
-              </div>
-              <div
-                className="flex items-center cursor-pointer"
-                onClick={() => setTab("text")}
-              >
-                <img src={SUNNYSIDE.icons.arrow_left} className="h-4 mr-2" />
-                <span className="text-xs underline">Back</span>
-              </div>
-            </>
-          )}
-        </div>
-
-        {tab === "text" && (
-          <ChatText onMessage={(text) => onMessage({ text })} />
-        )}
-        {tab === "reactions" && (
+      <CloseButtonPanel
+        className="w-full sm:w-[30rem]"
+        showCloseButton={false}
+        tabs={[
+          { icon: SUNNYSIDE.icons.expression_chat, name: "Chat" },
+          { icon: SUNNYSIDE.icons.heart, name: "Reactions" },
+        ]}
+        currentTab={tab}
+        setCurrentTab={setTab}
+      >
+        {tab === 0 && <ChatText onMessage={(text) => onMessage({ text })} />}
+        {tab === 1 && (
           <ChatReactions
             onReact={(reaction) => onMessage({ reaction })}
             game={game}
           />
         )}
-      </Panel>
+      </CloseButtonPanel>
     </div>
   );
 };
