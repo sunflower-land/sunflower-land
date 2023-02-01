36e5379c54e9fab5c2d37965b9c14610f96cc69d
Craig <41215134+c-py@users.noreply.github.com>
2023-02-01 15:03:29 +1100

    [FEAT] Add treasure trove

--- src/features/treasureIsland/TreasureIsland.tsx
+++ src/features/treasureIsland/TreasureIsland.tsx
@@ -14,6 +14,7 @@ import {
 import { SandPlot } from "./components/SandPlot";
 import { BeachConstruction } from "./components/BeachConstruction";
 import { PirateQuest } from "features/game/expansion/components/PirateQuest";
+import { TreasureDropRates } from "./components/TreasureDropRates";
 
 export const CLICKABLE_COORDINATES: Coordinates[] = [];
 
@@ -70,6 +71,7 @@ export const TreasureIsland: React.FC = () => {
         <TreasureShop />
         <BeachConstruction />
         <PirateQuest />
+        <TreasureDropRates />
       </div>
     </>
   );
