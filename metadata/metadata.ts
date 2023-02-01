403b0401b03a1eb03357a7954aa68022d65ba4a9
spencer <spencer@sunflower-land.com>
2023-02-01 15:24:02 +1100

    [FIX] Fix metadata

--- metadata/metadata.ts
+++ metadata/metadata.ts
@@ -2978,11 +2978,7 @@ export const OPEN_SEA_ITEMS: Record<InventoryItemName, Metadata> = {
     attributes: [],
   },
   "Lunar Calendar": {
-<<<<<<< HEAD
-    description: "A charm made from crops.",
-=======
     description: "?",
->>>>>>> 6c15ff6a ([FEAT] Update boost names)
     decimals: 0,
     external_url: "https://docs.sunflower-land.com/getting-started/about",
     image_url: "../public/erc1155/images/question_mark.png",
