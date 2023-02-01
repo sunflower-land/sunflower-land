51424ec3f7ace574636aa83750fae6e38acec732
Polysemouse <107602352+Polysemouse@users.noreply.github.com>
2023-01-31 18:44:06 -0800

    [FIX] Show revealing and revealed prompt in pumpkin plaza (#2140)

--- src/features/game/components/Revealing.tsx
+++ src/features/game/components/Revealing.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 
-import maneki from "assets/sfts/maneki_neko.gif";
+import { setImageWidth } from "lib/images";
 
-import { PIXEL_SCALE } from "../lib/constants";
+interface Props {
+  icon: string;
+}
 
-export const Revealing: React.FC = () => {
+export const Revealing: React.FC<Props> = ({ icon }) => {
   return (
     <div className="flex flex-col items-center p-2">
-      <span className="text-center">What could it be?</span>
+      <span className="text-center mb-2">What could it be?</span>
       <img
-        src={maneki}
+        src={icon}
         alt="digging"
         className="my-2"
-        style={{
-          width: `${PIXEL_SCALE * 33}px`,
-        }}
+        onLoad={(e) => setImageWidth(e.currentTarget)}
       />
       <span
         className="text-center text-xs loading mb-2"
