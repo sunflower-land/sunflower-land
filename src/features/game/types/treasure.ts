11e2d10cff7405a35c8ddf73229be197b65eaa79
spencer <spencer@sunflower-land.com>
2023-02-06 15:07:51 +1100

    [CHORE] Update bounty sell prices.

--- src/features/game/types/treasure.ts
+++ src/features/game/types/treasure.ts
@@ -70,52 +70,52 @@ export type BeachBounty = {
 
 export const BEACH_BOUNTY_TREASURE: Record<BeachBountyTreasure, BeachBounty> = {
   "Clam Shell": {
-    sellPrice: marketRate(500),
+    sellPrice: marketRate(375),
     description: "A clam shell.",
     type: "good",
   },
   Coral: {
-    sellPrice: marketRate(2000),
+    sellPrice: marketRate(1500),
     description: "A piece of coral, it's pretty",
     type: "good",
   },
   Crab: {
-    sellPrice: marketRate(20),
+    sellPrice: marketRate(18.75),
     description: "A crab, watch out for it's claws!",
     type: "average",
   },
   Pearl: {
-    sellPrice: marketRate(5000),
+    sellPrice: marketRate(3750),
     description: "Shimmers in the sun.",
     type: "rare",
   },
   Pipi: {
-    sellPrice: marketRate(250),
+    sellPrice: marketRate(187.5),
     description: "Plebidonax deltoides, found in the Pacific Ocean.",
     type: "good",
   },
   "Pirate Bounty": {
-    sellPrice: marketRate(10000),
+    sellPrice: marketRate(7500),
     description: "A bounty for a pirate. It's worth a lot of money.",
     type: "rare",
   },
   "Sea Cucumber": {
-    sellPrice: marketRate(30),
+    sellPrice: marketRate(18.75),
     description: "A sea cucumber.",
     type: "average",
   },
   Seaweed: {
-    sellPrice: marketRate(100),
+    sellPrice: marketRate(75),
     description: "Seaweed.",
     type: "average",
   },
   Starfish: {
-    sellPrice: marketRate(150),
+    sellPrice: marketRate(112.5),
     description: "The star of the sea.",
     type: "average",
   },
   "Wooden Compass": {
-    sellPrice: marketRate(175),
+    sellPrice: marketRate(131.25),
     description: "?",
     type: "good",
   },
