bb11f2d1b75a80c26f29f21d9a5951a1c404f82d
Marcos Ricardo <94913189+mGranTY@users.noreply.github.com>
2023-02-05 20:53:03 -0400

    [FIX] ValentinFarmer

--- src/features/valentineIsland/components/ValentinFarmer.tsx
+++ src/features/valentineIsland/components/ValentinFarmer.tsx
@@ -8,15 +8,13 @@ import { MapPlacement } from "features/game/expansion/components/MapPlacement";
 
 export const ValentinFarmer: React.FC = () => {
   return (
-    <MapPlacement x={3.7} y={1.4} height={1} width={1}>
+    <MapPlacement x={3.2} y={2} width={3}>
       <div className="relative w-full h-full">
         <img
           src={npc}
           className="absolute"
           style={{
-            width: `${PIXEL_SCALE * 112}px`,
-            right: `${PIXEL_SCALE * 8}px`,
-            bottom: `${PIXEL_SCALE * 6}px`,
+            width: `${PIXEL_SCALE * 16}px`,
           }}
         />
       </div>
