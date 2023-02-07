b6ecfdd28fd2c7fd910794088f38d78713bdf6cc
Adam Hannigan <adamhannigan81@gmail.com>
2023-02-08 10:49:00 +1100

    [FIX] Sand Drill metadata

--- metadata/metadata.ts
+++ metadata/metadata.ts
@@ -3166,7 +3166,7 @@ export const OPEN_SEA_ITEMS: Record<InventoryItemName, Metadata> = {
   },
   "Sand Drill": {
     description: "Drill deep for rare treasure",
-    decimals: 0,
+    decimals: 18,
     external_url: "https://docs.sunflower-land.com/getting-started/about",
     image_url: "../public/erc1155/images/311.png",
     attributes: [],
