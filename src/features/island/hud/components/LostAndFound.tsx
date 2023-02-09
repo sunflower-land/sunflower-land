0724876c82c5006e080699f496bba3d98f574e25
spencer <spencer@sunflower-land.com>
2023-02-10 09:01:53 +1100

    [FEAT] WIP

--- src/features/island/hud/components/LostAndFound.tsx
+++ src/features/island/hud/components/LostAndFound.tsx
@@ -1,5 +1,9 @@
+import { Loading } from "features/auth/components";
 import { CloseButtonPanel } from "features/game/components/CloseablePanel";
-import React from "react";
+import { Context } from "features/game/GameProvider";
+import { balanceOf, loadBalanceBatch } from "lib/blockchain/BumpkinItems";
+import { wallet } from "lib/blockchain/wallet";
+import React, { useContext, useEffect, useState } from "react";
 import { Modal } from "react-bootstrap";
 
 interface Props {
@@ -8,6 +12,34 @@ interface Props {
 }
 
 export const LostAndFound: React.FC<Props> = ({ isOpen, onClose }) => {
+  const { gameService } = useContext(Context);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!isOpen) return;
+    // Load balances
+    const loadBalances = async () => {
+      const balance = await balanceOf(
+        wallet.web3Provider,
+        gameService.state.context.state.farmAddress as string,
+        81
+      );
+
+      console.log({ balance });
+      const balances = await loadBalanceBatch(
+        wallet.web3Provider,
+        gameService.state.context.state.farmAddress as string
+      );
+
+      console.log(balances);
+    };
+
+    loadBalances();
+    setLoading(false);
+  }, [isOpen]);
+
+  if (loading) return <Loading />;
+
   return (
     <Modal show={isOpen} centered onHide={onClose}>
       <CloseButtonPanel
--- src/lib/blockchain/BumpkinItems.ts
+++ src/lib/blockchain/BumpkinItems.ts
@@ -4,8 +4,9 @@ import BumpkinItemsJSON from "./abis/BumpkinItems.json";
 import { CONFIG } from "lib/config";
 import { parseMetamaskError } from "./utils";
 import { BumpkinItems as IBumpkinItems } from "./types/BumpkinItems";
+import { IDS } from "features/game/types/bumpkin";
 
-const address = CONFIG.INVENTORY_CONTRACT;
+const address = CONFIG.BUMPKIN_ITEMS_CONTRACT;
 
 export async function loadSupplyBatch(
   web3: Web3,
@@ -53,13 +54,50 @@ export async function balanceOf(
     ).methods
       .balanceOf(account, id)
       .call({ from: account });
-
-    console.log({ balance });
     return Number(balance);
   } catch (e) {
     const error = parseMetamaskError(e);
     if (attempts < 3) {
       return balanceOf(web3, account, id, attempts + 1);
+    }
+
+    throw error;
+  }
+}
+
+export async function loadBalanceBatch(
+  web3: Web3,
+  account: string,
+  attempts = 0
+): Promise<Record<number, number>> {
+  console.log("load balance batch");
+  console.log(new Array(IDS.length).fill(account));
+  try {
+    const balances = await (
+      new web3.eth.Contract(
+        BumpkinItemsJSON as AbiItem[],
+        address as string
+      ) as unknown as IBumpkinItems
+    ).methods
+      .balanceOfBatch(
+        new Array(IDS.length).fill(account),
+        IDS.map((id) => `${id}`)
+      )
+      .call({ from: account });
+
+    console.log({ balances });
+
+    return balances.reduce((amounts, itemBalance, index) => {
+      if (Number(itemBalance) > 0) {
+        amounts[IDS[index]] = Number(itemBalance);
+      }
+
+      return amounts;
+    }, {} as Record<number, number>);
+  } catch (e) {
+    const error = parseMetamaskError(e);
+    if (attempts < 3) {
+      return loadBalanceBatch(web3, account, attempts + 1);
     }
 
     throw error;
