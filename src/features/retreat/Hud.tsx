1b0260745f85ead4432dd811166d81e915fdf2c2
spencer <spencer@sunflower-land.com>
2023-02-16 14:36:42 +1100

    [FEAT] Add deposit modal to retreat balance

--- src/features/retreat/Hud.tsx
+++ src/features/retreat/Hud.tsx
@@ -8,23 +8,34 @@ import { LandId } from "features/island/hud/components/LandId";
 import { GameState } from "features/game/types/game";
 import { GoblinInventory } from "./components/hud/GoblinInventory";
 import { Settings } from "features/island/hud/components/Settings";
+import { DepositArgs } from "lib/blockchain/Deposit";
 
 /**
  * Heads up display - a concept used in games for the small overlaid display of information.
  * Balances, Inventory, actions etc.
  */
 export const Hud: React.FC = () => {
-  const goblinContext = useContext(Context);
-  const [gameState] = useActor(goblinContext.goblinService);
+  const { goblinService } = useContext(Context);
+  const [goblinState] = useActor(goblinService);
 
-  const { state } = gameState.context;
+  const { state } = goblinState.context;
   const landId = state.id;
+
+  const handleDeposit = (
+    args: Pick<DepositArgs, "sfl" | "itemIds" | "itemAmounts">
+  ) => {
+    goblinService.send("DEPOSIT", args);
+  };
 
   return (
     <div data-html2canvas-ignore="true" aria-label="Hud">
       <>
-        <Balance balance={gameState.context.state.balance} />
-        <GoblinInventory state={gameState.context.state as GameState} />
+        <Balance
+          onDeposit={handleDeposit}
+          farmAddress={goblinState.context.state.farmAddress}
+          balance={goblinState.context.state.balance}
+        />
+        <GoblinInventory state={goblinState.context.state as GameState} />
         {landId && <LandId landId={landId} />}
         <BumpkinAvatar bumpkin={state.bumpkin} />
         <Settings isFarming={false} />
