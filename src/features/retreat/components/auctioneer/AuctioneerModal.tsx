c2477a9283208b9cf5c9e716da1e288ed31f13e7
Adam Hannigan <adamhannigan81@gmail.com>
2023-02-27 15:08:59 +1100

    [FEAT] Auctioneer Coming Soon

--- src/features/retreat/components/auctioneer/AuctioneerModal.tsx
+++ src/features/retreat/components/auctioneer/AuctioneerModal.tsx
@@ -39,6 +39,26 @@ export const AuctioneerModal: React.FC<Props> = ({ isOpen, onClose }) => {
 
   return (
     <Modal centered show={isOpen} onHide={onClose} scrollable>
+      <Panel className="relative">
+        <div className="p-2 flex flex-col items-center">
+          <p>Under construction!</p>
+          <img src={SUNNYSIDE.npcs.goblin_hammering} className="w-1/3" />
+          <p className="my-2 text-sm">This feature is coming soon.</p>
+          <a
+            href="https://docs.sunflower-land.com/player-guides/islands/goblin-retreat/goblin-auctioneer"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-xs underline"
+          >
+            Read more
+          </a>
+        </div>
+      </Panel>
+    </Modal>
+  );
+
+  return (
+    <Modal centered show={isOpen} onHide={onClose} scrollable>
       {isMinting && (
         <Panel className="relative">
           <div className="flex flex-col items-center p-2">
