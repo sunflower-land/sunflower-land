81e43a829cf5fd9f5d0f5c3c4de204cce9bcdb4c
Romy Furtado <25412194+Rofrtd@users.noreply.github.com>
2023-03-28 10:24:26 +1100

    [FEAT] moveCollectible

---
+++ src/features/game/events/landExpansion/moveCollectible.test.ts
@@ -0,0 +1,126 @@
+import Decimal from "decimal.js-light";
+import { TEST_FARM } from "features/game/lib/constants";
+import { GameState } from "features/game/types/game";
+import { moveCollectible, MOVE_COLLECTIBLE_ERRORS } from "./moveCollectible";
+
+const GAME_STATE: GameState = {
+  ...TEST_FARM,
+  balance: new Decimal(0),
+  inventory: {
+    "Rusty Shovel": new Decimal(1),
+  },
+};
+
+describe("moveCollectible", () => {
+  it("does not move non-existent building ", () => {
+    expect(() =>
+      moveCollectible({
+        state: {
+          ...GAME_STATE,
+          collectibles: {
+            "Abandoned Bear": [
+              {
+                id: "123",
+                coordinates: { x: 1, y: 1 },
+                createdAt: 0,
+                readyAt: 0,
+              },
+            ],
+          },
+        },
+        action: {
+          type: "collectible.moved",
+          name: "Algerian Flag",
+          id: "1",
+          coordinates: { x: 2, y: 2 },
+        },
+      })
+    ).toThrow(MOVE_COLLECTIBLE_ERRORS.NO_COLLECTIBLES);
+  });
+
+  it("does not move building with invalid id", () => {
+    expect(() =>
+      moveCollectible({
+        state: {
+          ...GAME_STATE,
+          collectibles: {
+            "Algerian Flag": [
+              {
+                id: "123",
+                coordinates: { x: 1, y: 1 },
+                createdAt: 0,
+                readyAt: 0,
+              },
+            ],
+          },
+        },
+        action: {
+          type: "collectible.moved",
+          name: "Algerian Flag",
+          id: "1",
+          coordinates: { x: 2, y: 2 },
+        },
+      })
+    ).toThrow(MOVE_COLLECTIBLE_ERRORS.COLLECTIBLE_NOT_PLACED);
+  });
+
+  it("moves a building", () => {
+    const gameState = moveCollectible({
+      state: {
+        ...GAME_STATE,
+        collectibles: {
+          "Abandoned Bear": [
+            {
+              id: "123",
+              coordinates: { x: 1, y: 1 },
+              createdAt: 0,
+              readyAt: 0,
+            },
+            {
+              id: "456",
+              coordinates: { x: 4, y: 4 },
+              createdAt: 0,
+              readyAt: 0,
+            },
+            {
+              id: "789",
+              coordinates: { x: 8, y: 8 },
+              createdAt: 0,
+              readyAt: 0,
+            },
+          ],
+        },
+      },
+      action: {
+        type: "collectible.moved",
+        name: "Abandoned Bear",
+        id: "123",
+        coordinates: { x: 2, y: 2 },
+      },
+    });
+
+    expect(gameState.collectibles["Abandoned Bear"]).toEqual([
+      {
+        id: "123",
+        coordinates: { x: 2, y: 2 },
+        createdAt: 0,
+        readyAt: 0,
+      },
+      {
+        id: "456",
+        coordinates: { x: 4, y: 4 },
+        createdAt: 0,
+        readyAt: 0,
+      },
+      {
+        id: "789",
+        coordinates: {
+          x: 8,
+          y: 8,
+        },
+        createdAt: 0,
+        readyAt: 0,
+      },
+    ]);
+  });
+});
