c5feebcac16b9e893674c01c4cea44cc23693ccd
Marcos Ricardo <94913189+mGranTY@users.noreply.github.com>
2023-03-28 19:44:33 -0400

    water decorations

--- src/features/bunnyTrove/components/Water.tsx
+++ src/features/bunnyTrove/components/Water.tsx
@@ -4,6 +4,8 @@ import { PIXEL_SCALE } from "features/game/lib/constants";
 
 import swimmer from "assets/events/easter/2023/npcs/swimmer.gif";
 import goblin from "assets/events/easter/2023/npcs/farting_goblin.gif";
+import dragonfly from "assets/events/easter/2023/npcs/dragonfly.gif";
+import snorkling from "assets/events/easter/2023/npcs/goblin_snorkling.gif";
 
 import { MapPlacement } from "features/game/expansion/components/MapPlacement";
 
@@ -28,6 +30,28 @@ export const Water: React.FC = () => {
             className="absolute"
             style={{
               width: `${PIXEL_SCALE * 29}px`,
+            }}
+          />
+        </div>
+      </MapPlacement>
+      <MapPlacement x={14} y={0} height={1} width={1}>
+        <div className="relative w-full h-full">
+          <img
+            src={dragonfly}
+            className="absolute"
+            style={{
+              width: `${PIXEL_SCALE * 13}px`,
+            }}
+          />
+        </div>
+      </MapPlacement>
+      <MapPlacement x={14} y={4.5} height={1} width={2}>
+        <div className="relative w-full h-full">
+          <img
+            src={snorkling}
+            className="absolute"
+            style={{
+              width: `${PIXEL_SCALE * 24}px`,
             }}
           />
         </div>
