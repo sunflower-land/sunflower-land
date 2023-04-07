c5cbd7d496b1a38994dca85afc5510b03d184e30
Polysemouse <sunflowermousewax@gmail.com>
2023-04-07 12:13:48 -0700

    [CHORE] Improve performance for crop plots

--- src/features/island/plots/Plot.tsx
+++ src/features/island/plots/Plot.tsx
@@ -1,12 +1,6 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
-import { useActor } from "@xstate/react";
-import classNames from "classnames";
+import React, { useContext, useRef, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-import selectBox from "assets/ui/select/select_box.png";
-import well from "assets/buildings/well1.png";
-
-import { Context } from "features/game/GameProvider";
 import {
   Reward,
   FERTILISERS,
@@ -15,47 +9,34 @@ import {
 } from "features/game/types/game";
 import { CropName, CROPS } from "features/game/types/crops";
 import { ITEM_DETAILS } from "features/game/types/images";
-import { PIXEL_SCALE, POPOVER_TIME_MS } from "features/game/lib/constants";
+import { PIXEL_SCALE } from "features/game/lib/constants";
 import { ToastContext } from "features/game/toast/ToastQueueProvider";
-import { Soil } from "features/island/plots/components/Soil";
 import { harvestAudio, plantAudio } from "lib/utils/sfx";
 import { isPlotFertile } from "features/game/events/landExpansion/plant";
-import { Modal } from "react-bootstrap";
-import { Panel } from "components/ui/Panel";
 import Spritesheet from "components/animation/SpriteAnimator";
 import { HARVEST_PROC_ANIMATION } from "features/island/plots/lib/plant";
 import { isReadyToHarvest } from "features/game/events/landExpansion/harvest";
-import { useIsMobile } from "lib/utils/hooks/useIsMobile";
-import { Bar } from "components/ui/ProgressBar";
-import { ChestReward } from "features/island/common/chest-reward/ChestReward";
-import { GoldenCropModal } from "features/island/plots/components/GoldenCropModal";
-import golden_crop_sheet from "assets/events/golden_crop/golden_crop_sheet.png";
-import { SUNNYSIDE } from "assets/sunnyside";
+import { NonFertilePlot } from "./components/NonFertilePlot";
+import { FertilePlot } from "./components/FertilePlot";
+import { ChestReward } from "../common/chest-reward/ChestReward";
+import { Context } from "features/game/GameProvider";
+import { useActor } from "@xstate/react";
+
 interface Props {
   id: string;
-  onboarding?: boolean;
 }
 
-const REMOVE_CROP_TIMEOUT = 5000; // 5 seconds
-
 export const Plot: React.FC<Props> = ({ id }) => {
+  console.log("render plots!");
   const { gameService, selectedItem, showTimers } = useContext(Context);
   const [game] = useActor(gameService);
-  const [showPopover, setShowPopover] = useState(false);
-  const [showSelectBox, setShowSelectBox] = useState(false);
-  const [showCropDetails, setShowCropDetails] = useState(false);
-  const [isFertileModalOpen, setIsFertileModalOpen] = useState(false);
-  const [procAnimation, setProcAnimation] = useState<JSX.Element | null>();
+  const [procAnimation, setProcAnimation] = useState<JSX.Element>();
   const { setToast } = useContext(ToastContext);
   const [touchCount, setTouchCount] = useState(0);
-  const [reward, setReward] = useState<Reward | null>(null);
+  const [reward, setReward] = useState<Reward>();
   const clickedAt = useRef<number>(0);
-  const [isRemoving, setIsRemoving] = useState(false);
-  const [isMobile] = useIsMobile();
-  //Golden Crop event
-  const [showGoldenCropModal, setShowGoldenCropModal] = useState(false);
-  const plot = game.context.state.crops?.[id];
 
+  const plot = game.context.state.crops?.[id];
   const crop = plot && plot.crop;
 
   const isFertile = isPlotFertile({
@@ -63,141 +44,44 @@ export const Plot: React.FC<Props> = ({ id }) => {
     gameState: game.context.state,
   });
 
-  const playing = game.matches("playing") || game.matches("autosaving");
-
-  // If selected item changes, then stop removing crops
-  useEffect(() => setIsRemoving(false), [selectedItem]);
-
-  // If enough time has passed, then stop removing crops
-  useEffect(() => {
-    if (!isRemoving) return;
-    const timer = setTimeout(() => setIsRemoving(false), REMOVE_CROP_TIMEOUT);
-    return () => clearTimeout(timer);
-  }, [isRemoving]);
-
-  const displayPopover = async () => {
-    setShowPopover(true);
-    await new Promise((resolve) => setTimeout(resolve, POPOVER_TIME_MS));
-    setShowPopover(false);
-  };
+  if (!isFertile) return <NonFertilePlot />;
 
   const harvestCrop = (crop: PlantedCrop) => {
-    try {
-      const newState = gameService.send("crop.harvested", {
-        index: id,
-      });
+    const newState = gameService.send("crop.harvested", {
+      index: id,
+    });
+    if (newState.matches("hoarding")) return;
 
-      if (!newState.matches("hoarding")) {
-        harvestAudio.play();
+    harvestAudio.play();
 
-        // firework animation
-        if (crop.amount && crop.amount >= 10) {
-          setProcAnimation(
-            <Spritesheet
-              className="absolute pointer-events-none"
-              style={{
-                top: `${PIXEL_SCALE * -23}px`,
-                left: `${PIXEL_SCALE * -10}px`,
-                width: `${PIXEL_SCALE * HARVEST_PROC_ANIMATION.size}px`,
-                imageRendering: "pixelated",
-              }}
-              image={HARVEST_PROC_ANIMATION.sprites[crop.name]}
-              widthFrame={HARVEST_PROC_ANIMATION.size}
-              heightFrame={HARVEST_PROC_ANIMATION.size}
-              fps={HARVEST_PROC_ANIMATION.fps}
-              steps={HARVEST_PROC_ANIMATION.steps}
-              onPause={() => setProcAnimation(<></>)}
-            />
-          );
-        }
-        // golden crop event animation
-        const rewardName = crop?.reward?.items?.[0].name;
-        if (rewardName === "Golden Crop") {
-          setProcAnimation(
-            <Spritesheet
-              className="absolute pointer-events-none"
-              style={{
-                top: `${PIXEL_SCALE * -23}px`,
-                left: `${PIXEL_SCALE * -10}px`,
-                width: `${PIXEL_SCALE * HARVEST_PROC_ANIMATION.size}px`,
-                imageRendering: "pixelated",
-              }}
-              image={golden_crop_sheet}
-              widthFrame={HARVEST_PROC_ANIMATION.size}
-              heightFrame={HARVEST_PROC_ANIMATION.size}
-              fps={6}
-              steps={HARVEST_PROC_ANIMATION.steps}
-              onPause={() => setProcAnimation(<></>)}
-            />
-          );
-        }
-
-        setToast({
-          icon: ITEM_DETAILS[crop.name].image,
-          content: `+${crop.amount || 1}`,
-        });
-      }
-    } catch (e) {
-      // TODO - catch more elaborate errors
-      displayPopover();
-    }
-  };
-
-  // Temp shovel disable
-  // const removeCrop = () => {
-  //   if (!isRemoving) {
-  //     setIsRemoving(true);
-  //     return;
-  //   }
-  //   gameService.send("crop.removed", {
-  //     item: selectedItem,
-  //     index: plotIndex,
-  //     expansionIndex,
-  //   });
-  //   setIsRemoving(false);
-  // };
-
-  const onCollectReward = (success: boolean) => {
-    setReward(null);
-    setTouchCount(0);
-    setShowGoldenCropModal(false);
-
-    if (success && crop) {
-      const rewardItemName = reward?.items?.[0].name;
-      const rewardItemAmount = reward?.items?.[0].amount;
-      setToast({
-        icon: ITEM_DETAILS[rewardItemName as InventoryItemName].image,
-        content: `+${rewardItemAmount}`,
-      });
-      harvestCrop(crop);
-    }
-  };
-
-  const handleMouseHover = () => {
-    setShowSelectBox(!isMobile);
-
-    if (!crop) {
-      return;
+    // firework animation
+    if (crop.amount && crop.amount >= 10) {
+      setProcAnimation(
+        <Spritesheet
+          className="absolute pointer-events-none"
+          style={{
+            top: `${PIXEL_SCALE * -23}px`,
+            left: `${PIXEL_SCALE * -10}px`,
+            width: `${PIXEL_SCALE * HARVEST_PROC_ANIMATION.size}px`,
+            imageRendering: "pixelated",
+          }}
+          image={HARVEST_PROC_ANIMATION.sprites[crop.name]}
+          widthFrame={HARVEST_PROC_ANIMATION.size}
+          heightFrame={HARVEST_PROC_ANIMATION.size}
+          fps={HARVEST_PROC_ANIMATION.fps}
+          steps={HARVEST_PROC_ANIMATION.steps}
+          onPause={() => setProcAnimation(<></>)}
+        />
+      );
     }
 
-    const now = Date.now();
-    const isReady = isReadyToHarvest(now, crop, CROPS()[crop.name]);
-    const isJustPlanted = now - crop.plantedAt < 1000;
-
-    // show details if field is NOT ready and NOT just planted
-    if (!isReady && !isJustPlanted) {
-      // set state to show details
-      setShowCropDetails(true);
-    }
+    setToast({
+      icon: ITEM_DETAILS[crop.name].image,
+      content: `+${crop.amount || 1}`,
+    });
   };
 
-  const handleMouseLeave = () => {
-    // set state to hide details
-    setShowCropDetails(false);
-    setShowSelectBox(false);
-  };
-
-  const onClick = (analytics: boolean | undefined = undefined) => {
+  const onClick = () => {
     // small buffer to prevent accidental double clicks
     const now = Date.now();
     if (now - clickedAt.current < 100) {
@@ -219,10 +103,6 @@ export const Plot: React.FC<Props> = ({ id }) => {
         return;
       }
 
-      if (crop?.reward?.items?.[0].name === "Golden Crop") {
-        console.log("golden crop");
-        setShowGoldenCropModal(true);
-      }
       // They have touched enough!
       setReward(crop.reward);
 
@@ -231,205 +111,74 @@ export const Plot: React.FC<Props> = ({ id }) => {
 
     // plant
     if (!crop) {
-      try {
-        gameService.send("seed.planted", {
-          index: id,
-          item: selectedItem,
-          analytics,
-          cropId: uuidv4().slice(0, 8),
-        });
+      gameService.send("seed.planted", {
+        index: id,
+        item: selectedItem,
+        cropId: uuidv4().slice(0, 8),
+      });
 
-        plantAudio.play();
+      plantAudio.play();
 
-        setToast({
-          icon: ITEM_DETAILS[selectedItem as CropName].image,
-          content: `-1`,
-        });
-      } catch (e) {
-        // TODO - catch more elaborate errors
-        displayPopover();
-      }
+      setToast({
+        icon: ITEM_DETAILS[selectedItem as CropName].image,
+        content: `-1`,
+      });
 
       return;
     }
 
-    // Temp shovel disable
-    // remove crop
-    // if (
-    //   selectedItem === "Shovel" &&
-    //   !isReadyToHarvest(now, crop, CROPS()[crop.name])
-    // ) {
-    //   removeCrop();
-    //   return;
-    // }
-
     // apply fertilisers
     if (selectedItem && selectedItem in FERTILISERS) {
-      try {
-        gameService.send("crop.fertilised", {
-          plotIndex: id,
-          fertiliser: selectedItem,
-        });
+      gameService.send("crop.fertilised", {
+        plotIndex: id,
+        fertiliser: selectedItem,
+      });
 
-        return;
-        // TODO - sound effects
-      } catch {
-        displayPopover();
-      }
+      return;
     }
 
     // harvest crop
-    try {
-      harvestCrop(crop);
-    } catch (e) {
-      // TODO - catch more elaborate errors
-      displayPopover();
-    }
+    harvestCrop(crop);
 
     setTouchCount(0);
   };
 
-  const notFertileCallback = () => setIsFertileModalOpen(!isFertileModalOpen);
+  const onCollectReward = (success: boolean) => {
+    setReward(undefined);
+    setTouchCount(0);
 
-  if (!isFertile) {
-    return (
-      <>
-        <Modal centered show={isFertileModalOpen} onHide={notFertileCallback}>
-          <Panel>
-            <img
-              src={SUNNYSIDE.icons.close}
-              className="absolute cursor-pointer z-20"
-              onClick={notFertileCallback}
-              style={{
-                top: `${PIXEL_SCALE * 6}px`,
-                right: `${PIXEL_SCALE * 6}px`,
-                width: `${PIXEL_SCALE * 11}px`,
-              }}
-            />
-            <p className="text-center">These crops need water!</p>
-            <p className="text-center mt-4">
-              In order to support more crops, build a well.
-              <span>
-                <img
-                  src={well}
-                  alt="well image"
-                  className="mx-auto w-16 my-4"
-                />
-              </span>
-            </p>
-          </Panel>
-        </Modal>
-        <div className="w-full h-full relative cursor-pointer hover:img-highlight">
-          <img
-            src={SUNNYSIDE.soil.soil_dry}
-            alt="soil image"
-            className="absolute"
-            style={{
-              top: `${PIXEL_SCALE * 2}px`,
-              width: `${PIXEL_SCALE * 16}px`,
-            }}
-            onClick={notFertileCallback}
-          />
-        </div>
-      </>
-    );
-  }
-
-  // onMouseUp is needed for PC to prevent lingering issues if clicking the crop results in showing the crop reward modal
-  const onMouseUpProps = isMobile
-    ? {}
-    : { onMouseUp: () => handleMouseLeave() };
+    if (success && crop) {
+      const rewardItemName = reward?.items?.[0].name;
+      const rewardItemAmount = reward?.items?.[0].amount;
+      setToast({
+        icon: ITEM_DETAILS[rewardItemName as InventoryItemName].image,
+        content: `+${rewardItemAmount}`,
+      });
+      harvestCrop(crop);
+    }
+  };
 
   return (
-    <div
-      onMouseEnter={handleMouseHover}
-      onMouseLeave={handleMouseLeave}
-      {...onMouseUpProps}
-      className="w-full h-full relative"
-    >
-      {/* Crop base image */}
-      <div
-        className="absolute pointer-events-none"
-        style={{
-          width: `${PIXEL_SCALE * 16}px`,
-        }}
-      >
-        <Soil
+    <>
+      <div onClick={onClick} className="w-full h-full relative">
+        <FertilePlot
+          cropName={crop?.name}
+          plantedAt={crop?.plantedAt}
+          fertilisers={crop?.fertilisers}
+          procAnimation={procAnimation}
+          touchCount={touchCount}
           showTimers={showTimers}
-          plantedCrop={crop}
-          showCropDetails={showCropDetails}
-          isRemoving={isRemoving}
         />
       </div>
-
-      {/* Health bar for collecting rewards */}
-      <div
-        className={classNames(
-          "transition-opacity pointer-events-none absolute",
-          {
-            "opacity-100": touchCount > 0,
-            "opacity-0": touchCount === 0,
-          }
-        )}
-        style={{
-          top: `${PIXEL_SCALE * 9}px`,
-          width: `${PIXEL_SCALE * 15}px`,
-        }}
-      >
-        <Bar percentage={100 - touchCount * 50} type="health" />
-      </div>
-
-      {/* Popover */}
-      <div
-        className={classNames(
-          "transition-opacity absolute top-6 w-full z-40 pointer-events-none flex justify-center",
-          {
-            "opacity-100": showPopover,
-            "opacity-0": !showPopover,
-          }
-        )}
-      >
-        <img className="w-5" src={SUNNYSIDE.icons.cancel} />
-      </div>
-
-      {/* Firework animation */}
-      {procAnimation}
-
-      {/* Select box */}
-      {playing && (
-        <img
-          src={selectBox}
-          className={classNames("absolute z-40 cursor-pointer", {
-            "opacity-100": showSelectBox,
-            "opacity-0": !showSelectBox,
-          })}
-          style={{
-            width: `${PIXEL_SCALE * 16}px`,
-          }}
-          onClick={() => onClick()}
-        />
-      )}
-
-      {/* Golden Crop Event */}
-      {showGoldenCropModal && (
-        <GoldenCropModal
-          show={showGoldenCropModal}
-          onContinue={() => onCollectReward(true)}
-        />
-      )}
-
-      {/* Crop reward */}
-      {!showGoldenCropModal && (
-        <ChestReward
-          reward={reward}
-          onCollected={onCollectReward}
-          onOpen={() =>
-            gameService.send("cropReward.collected", {
-              plotIndex: id,
-            })
-          }
-        />
-      )}
-    </div>
+      <ChestReward
+        reward={reward}
+        onCollected={onCollectReward}
+        onOpen={() =>
+          gameService.send("cropReward.collected", {
+            plotIndex: id,
+          })
+        }
+      />
+    </>
   );
 };
