cab474fe213ca2a699ec89d57180bf6238d8c3a6
Romy Furtado <25412194+Rofrtd@users.noreply.github.com>
2023-04-11 10:37:23 +1000

    [FEAT] Update Assets (#2424)
    * [FEAT] update assets
    
    * [FEAT] remove unuse asset

--- src/features/island/collectibles/components/CabbageGirl.tsx
+++ src/features/island/collectibles/components/CabbageGirl.tsx
@@ -1,19 +1,34 @@
 import React from "react";
 
-import cabbageGirl from "assets/sfts/cabbage_girl.png";
+import cabbageGirl from "assets/sfts/cabbage_girl.gif";
+import shadow from "assets/npcs/shadow.png";
 import { PIXEL_SCALE } from "features/game/lib/constants";
 
 export const CabbageGirl: React.FC = () => {
   return (
-    <img
-      src={cabbageGirl}
+    <div
+      className="absolute top-0 left-0 h-full"
       style={{
         width: `${PIXEL_SCALE * 16}px`,
-        bottom: `${PIXEL_SCALE * 0}px`,
-        left: `${PIXEL_SCALE * 0}px`,
       }}
-      className="absolute pointer-events-none"
-      alt="Cabbage Girl"
-    />
+    >
+      <img
+        src={shadow}
+        style={{
+          width: `${PIXEL_SCALE * 15}px`,
+        }}
+        className="absolute bottom-0 left-0 pointer-events-none"
+      />
+      <img
+        src={cabbageGirl}
+        style={{
+          width: `${PIXEL_SCALE * 16}px`,
+          bottom: `${PIXEL_SCALE * 2}px`,
+          left: `${PIXEL_SCALE * 0}px`,
+        }}
+        className="absolute left-0 pointer-events-none"
+        alt="Cabbage Girl"
+      />
+    </div>
   );
 };
