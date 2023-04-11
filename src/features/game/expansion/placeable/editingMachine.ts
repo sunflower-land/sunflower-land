36953da93413fdf5168be4075822dc439f96d912
Adam Hannigan <adamhannigan81@gmail.com>
2023-04-11 11:37:46 +1000

    editing machine

--- src/features/game/expansion/placeable/editingMachine.ts
+++ src/features/game/expansion/placeable/editingMachine.ts
@@ -4,15 +4,19 @@ import { BuildingName } from "features/game/types/buildings";
 import { CollectibleName } from "features/game/types/craftables";
 import { assign, createMachine, Interpreter, sendParent } from "xstate";
 import { Coordinates } from "../components/MapPlacement";
-import { Inventory } from "features/game/types/game";
 import Decimal from "decimal.js-light";
-import { detectCollision } from "./lib/collisionDetection";
+import { Inventory } from "features/game/types/game";
+import {
+  Context as GameMachineContext,
+  saveGame,
+} from "features/game/lib/gameMachine";
 
 export interface Context {
   placeable: BuildingName | CollectibleName;
   action: GameEventName<PlacementEvent>;
   coordinates: Coordinates;
   collisionDetected: boolean;
+  origin?: Coordinates;
   requirements: {
     sfl: Decimal;
     ingredients: Inventory;
@@ -27,13 +31,20 @@ type UpdateEvent = {
 
 type PlaceEvent = {
   type: "PLACE";
-  nextPosition?: Coordinates;
-  willCollide?: boolean;
+  nextOrigin?: Coordinates;
+  nextWillCollide?: boolean;
 };
 
 type ConstructEvent = {
   type: "CONSTRUCT";
   actionName: PlacementEvent;
+};
+
+export type SaveEvent = {
+  type: "SAVE";
+  gameMachineContext: GameMachineContext;
+  rawToken: string;
+  farmId: number;
 };
 
 export type BlockchainEvent =
@@ -42,10 +53,21 @@ export type BlockchainEvent =
   | ConstructEvent
   | PlaceEvent
   | UpdateEvent
+  | SaveEvent
   | { type: "CANCEL" };
 
 export type BlockchainState = {
-  value: "idle" | "dragging" | "placed" | "close";
+  value:
+    | "saving"
+    | "editing"
+    | "close"
+    | { saving: "idle" }
+    | { saving: "autosaving" }
+    | { saving: "close" }
+    | { editing: "idle" }
+    | { editing: "dragging" }
+    | { editing: "close" }
+    | { editing: "resetting" };
   context: Context;
 };
 
@@ -62,87 +84,143 @@ export const editingMachine = createMachine<
   BlockchainState
 >({
   id: "placeableMachine",
-  initial: "idle",
+  type: "parallel",
   preserveActionOrder: true,
   on: {
     CANCEL: {
-      target: "close",
+      target: ["saving.done", "editing.done"],
     },
   },
   states: {
-    idle: {
-      on: {
-        UPDATE: {
-          actions: assign({
-            coordinates: (_, event) => event.coordinates,
-            collisionDetected: (_, event) => event.collisionDetected,
-          }),
-        },
-        DRAG: {
-          target: "dragging",
-        },
-        PLACE: [
-          {
-            target: "idle",
-            cond: (_, e) => {
-              return !!e.nextPosition;
+    saving: {
+      id: "saving",
+      initial: "idle",
+      states: {
+        idle: {
+          on: {
+            SAVE: {
+              target: "autosaving",
             },
-            actions: [
-              sendParent(
-                ({ placeable, action, coordinates: { x, y } }) =>
-                  ({
-                    type: action,
-                    name: placeable,
-                    coordinates: { x, y },
-                    id: uuidv4(),
-                  } as PlacementEvent)
-              ),
-              assign({
-                coordinates: (_, e) => e.nextPosition as Coordinates,
-                collisionDetected: (_, e) => !!e.willCollide,
+          },
+        },
+        autosaving: {
+          invoke: {
+            src: async (_: Context, event: any) => {
+              const saveEvent = event as SaveEvent;
+
+              const result = await saveGame(
+                saveEvent.gameMachineContext,
+                undefined,
+                saveEvent.farmId,
+                saveEvent.rawToken
+              );
+
+              return result;
+            },
+            onDone: {
+              target: "idle",
+              actions: sendParent((_, event) => ({
+                type: "SAVE_SUCCESS",
+                data: event.data,
+              })),
+            },
+            onError: {
+              actions: (_, event) => {
+                console.error(event);
+              },
+            },
+          },
+        },
+        done: {
+          type: "final",
+        },
+      },
+    },
+    editing: {
+      initial: "idle",
+      states: {
+        idle: {
+          on: {
+            UPDATE: {
+              actions: assign({
+                coordinates: (_, event) => event.coordinates,
+                collisionDetected: (_, event) => event.collisionDetected,
               }),
+            },
+            DRAG: {
+              target: "dragging",
+            },
+            PLACE: [
+              {
+                target: "idle",
+                // They have more to place
+                cond: (_, e) => {
+                  return !!e.nextOrigin;
+                },
+                actions: [
+                  sendParent(
+                    ({ placeable, action, coordinates: { x, y } }) =>
+                      ({
+                        type: action,
+                        name: placeable,
+                        coordinates: { x, y },
+                        id: uuidv4().slice(0, 8),
+                      } as PlacementEvent)
+                  ),
+                  assign({
+                    collisionDetected: (_, event) => !!event.nextWillCollide,
+                    origin: (_, event) => event.nextOrigin ?? { x: 0, y: 0 },
+                    coordinates: (_, event) =>
+                      event.nextOrigin ?? { x: 0, y: 0 },
+                  }),
+                ],
+              },
+              {
+                target: ["#saving.done", "done"],
+                actions: sendParent(
+                  ({ placeable, action, coordinates: { x, y } }) =>
+                    ({
+                      type: action,
+                      name: placeable,
+                      coordinates: { x, y },
+                      id: uuidv4().slice(0, 8),
+                    } as PlacementEvent)
+                ),
+              },
             ],
           },
-          {
-            target: "placed",
-            actions: [
-              sendParent(
-                ({ placeable, action, coordinates: { x, y } }) =>
-                  ({
-                    type: action,
-                    name: placeable,
-                    coordinates: { x, y },
-                    id: uuidv4(),
-                  } as PlacementEvent)
-              ),
-            ],
+        },
+        resetting: {
+          always: {
+            target: "idle",
+            // Move the next piece
+            actions: assign({
+              coordinates: (context) => {
+                return {
+                  x: context.coordinates.x,
+                  y: context.coordinates.y - 1,
+                };
+              },
+            }),
           },
-        ],
-      },
-    },
-    dragging: {
-      on: {
-        UPDATE: {
-          actions: assign({
-            coordinates: (_, event) => event.coordinates,
-            collisionDetected: (_, event) => event.collisionDetected,
-          }),
         },
-        DROP: {
-          target: "idle",
+        dragging: {
+          on: {
+            UPDATE: {
+              actions: assign({
+                coordinates: (_, event) => event.coordinates,
+                collisionDetected: (_, event) => event.collisionDetected,
+              }),
+            },
+            DROP: {
+              target: "idle",
+            },
+          },
         },
-      },
-    },
-    placed: {
-      after: {
-        // 300ms allows time for the .bulge animation
-        300: {
-          target: "close",
+        done: {
+          type: "final",
         },
       },
-    },
-    close: {
-      type: "final",
     },
   },
 });
