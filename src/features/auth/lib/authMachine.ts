dd72b37d7cb980df246aa92cf9cdae05492f28d4
Craig <41215134+c-py@users.noreply.github.com>
2023-04-13 16:27:22 +1000

    [FEAT] Feature Flag Guest Login

--- src/features/auth/lib/authMachine.ts
+++ src/features/auth/lib/authMachine.ts
@@ -34,6 +34,7 @@ import {
   getGuestModeComplete,
   setGuestKey,
 } from "../actions/createGuestAccount";
+import { hasFeatureAccess } from "lib/flags";
 
 export const ART_MODE = !CONFIG.API_URL;
 
@@ -218,7 +219,8 @@ export const authMachine = createMachine<
         id: "idle",
         always: {
           target: "signIn",
-          cond: () => !!getGuestModeComplete(),
+          cond: () =>
+            !!getGuestModeComplete() || !hasFeatureAccess({}, "GUEST_GAME"),
         },
         on: {
           SIGN_IN: {
