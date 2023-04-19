060794755fb531f50ede38cbf6d20bafe1ed995b
Craig <41215134+c-py@users.noreply.github.com>
2023-04-19 10:06:55 +1000

    [FEAT] Remove zoom button

--- src/components/ZoomProvider.tsx
+++ src/components/ZoomProvider.tsx
@@ -10,9 +10,6 @@ const getScaleLimits = () => {
   // Todo base this off the count of expansions
   const maxZoomWidth = gameboardWidth / 4;
   const maxZoomHeight = gameboardHeight / 4;
-  // TODO delete when finished testing
-  // const maxZoomWidth = gameboardWidth;
-  // const maxZoomHeight = gameboardHeight;
 
   const vw = Math.max(
     document.documentElement.clientWidth || 0,
@@ -32,55 +29,22 @@ const getScaleLimits = () => {
   return { minScale, maxScale };
 };
 
-const calculateCanZoom = () => {
-  const { minScale, maxScale } = getScaleLimits();
-  return maxScale !== minScale;
-};
-
 interface Context {
   scale: SpringValue;
-  setMax: () => void;
-  setMin: () => void;
-  canZoom: boolean;
   isZoomedIn: boolean;
 }
 
 export const ZoomContext = createContext<Context>({
   scale: new SpringValue(1),
-  setMax: () => {
-    throw new Error("setMax not implement");
-  },
-  setMin: () => {
-    throw new Error("setMin not implement");
-  },
-  canZoom: false,
   isZoomedIn: false,
 });
 
 export const ZoomProvider: React.FC = ({ children }) => {
-  const [canZoom, setCanZoom] = useState(calculateCanZoom());
   const [isZoomedIn, setIsZoomedIn] = useState(true);
   const scale = useSpringValue(1);
 
   window.addEventListener("resize", () => updateScale(0));
 
-  const setMax = () => {
-    const { maxScale } = getScaleLimits();
-    setScale(maxScale);
-  };
-
-  const setMin = () => {
-    const { minScale } = getScaleLimits();
-    setScale(minScale);
-  };
-
-  const setScale = (newScale: number) => {
-    const { maxScale, minScale } = getScaleLimits();
-
-    scale.start(newScale);
-    setIsZoomedIn(newScale > (maxScale + minScale) / 2);
-    setCanZoom(calculateCanZoom());
-  };
   const updateScale = (delta: number) => {
     const { minScale, maxScale } = getScaleLimits();
 
@@ -88,7 +52,8 @@ export const ZoomProvider: React.FC = ({ children }) => {
     if (newScale < minScale) newScale = minScale;
     if (newScale > maxScale) newScale = maxScale;
 
-    setScale(newScale);
+    scale.start(newScale);
+    setIsZoomedIn(newScale > (maxScale + minScale) / 2);
   };
 
   usePinch(
@@ -99,9 +64,7 @@ export const ZoomProvider: React.FC = ({ children }) => {
   );
 
   return (
-    <ZoomContext.Provider
-      value={{ scale, setMax, setMin, canZoom, isZoomedIn }}
-    >
+    <ZoomContext.Provider value={{ scale, isZoomedIn }}>
       {children}
     </ZoomContext.Provider>
   );
--- src/features/island/hud/components/Recenter.tsx
+++ src/features/island/hud/components/Recenter.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { SUNNYSIDE } from "assets/sunnyside";
 import { PIXEL_SCALE } from "features/game/lib/constants";
 import { Section, useScrollIntoView } from "lib/utils/hooks/useScrollIntoView";
+import recenter from "assets/icons/recenter.png";
 
 export const Recenter: React.FC = () => {
   const [scrollIntoView] = useScrollIntoView();
@@ -21,6 +22,15 @@ export const Recenter: React.FC = () => {
         className="absolute"
         style={{
           width: `${PIXEL_SCALE * 22}px`,
+        }}
+      />
+      <img
+        src={recenter}
+        className="absolute"
+        style={{
+          top: `${PIXEL_SCALE * 3.2}px`,
+          left: `${PIXEL_SCALE * 3.4}px`,
+          width: `${PIXEL_SCALE * 15}px`,
         }}
       />
     </div>
