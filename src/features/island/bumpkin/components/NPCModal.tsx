133a12d9c95081e951bcdc757944a9b230d86226
Adam Hannigan <adamhannigan81@gmail.com>
2023-05-02 13:37:54 +1000

    [FEAT] Level up Modal (#2521)
    * Basic modal
    
    * Analytics
    
    * Add analytics comment:
    
    * Fix TSC

--- src/features/island/bumpkin/components/NPCModal.tsx
+++ src/features/island/bumpkin/components/NPCModal.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 
 import { getEntries } from "features/game/types/craftables";
 
@@ -9,6 +9,9 @@ import { Feed } from "./Feed";
 import { Modal } from "react-bootstrap";
 import foodIcon from "src/assets/food/chicken_drumstick.png";
 import { CloseButtonPanel } from "features/game/components/CloseablePanel";
+import { getBumpkinLevel } from "features/game/lib/level";
+import { LevelUp } from "./LevelUp";
+import { Equipped } from "features/game/types/bumpkin";
 
 interface Props {
   isOpen: boolean;
@@ -27,15 +30,42 @@ export const NPCModal: React.FC<Props> = ({ isOpen, onFeed, onClose }) => {
     .filter(([name, _]) => !!state.inventory[name]?.gt(0))
     .map(([_, consumable]) => consumable);
 
+  const [showLevelUp, setShowLevelUp] = useState(false);
+
+  const bumpkinLevel = useRef(getBumpkinLevel(state.bumpkin?.experience ?? 0));
+
+  useEffect(() => {
+    const newLevel = getBumpkinLevel(state.bumpkin?.experience ?? 0);
+
+    if (newLevel !== bumpkinLevel.current) {
+      setShowLevelUp(true);
+      bumpkinLevel.current = newLevel;
+    }
+  }, [state.bumpkin?.experience]);
+
   return (
     <Modal show={isOpen} onHide={onClose} centered>
-      <CloseButtonPanel
-        onClose={onClose}
-        tabs={[{ icon: foodIcon, name: "Feed Bumpkin" }]}
-        bumpkinParts={state.bumpkin?.equipped}
-      >
-        <Feed food={availableFood} onFeed={onFeed} />
-      </CloseButtonPanel>
+      {showLevelUp ? (
+        <CloseButtonPanel
+          onClose={() => setShowLevelUp(false)}
+          title="Level up!"
+          bumpkinParts={state.bumpkin?.equipped}
+        >
+          <LevelUp
+            level={bumpkinLevel.current}
+            onClose={() => setShowLevelUp(false)}
+            wearables={state.bumpkin?.equipped as Equipped}
+          />
+        </CloseButtonPanel>
+      ) : (
+        <CloseButtonPanel
+          onClose={onClose}
+          tabs={[{ icon: foodIcon, name: "Feed Bumpkin" }]}
+          bumpkinParts={state.bumpkin?.equipped}
+        >
+          <Feed food={availableFood} onFeed={onFeed} />
+        </CloseButtonPanel>
+      )}
     </Modal>
   );
 };
