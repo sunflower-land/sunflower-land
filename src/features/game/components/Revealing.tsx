16b14054a0817fcaec32001a5d83d9cf382390ff
Craig <41215134+c-py@users.noreply.github.com>
2023-05-03 16:20:07 +1000

    [FEAT] Genie Lamp

--- src/features/game/components/Revealing.tsx
+++ src/features/game/components/Revealing.tsx
@@ -3,13 +3,14 @@ import React from "react";
 import { setImageWidth } from "lib/images";
 
 interface Props {
+  text?: string;
   icon: string;
 }
 
-export const Revealing: React.FC<Props> = ({ icon }) => {
+export const Revealing: React.FC<Props> = ({ text, icon }) => {
   return (
     <div className="flex flex-col items-center p-2">
-      <span className="text-center mb-2">What could it be?</span>
+      <span className="text-center mb-2">{text || "What could it be?"}</span>
       <img
         src={icon}
         alt="digging"
--- src/features/island/collectibles/components/GenieLamp.tsx
+++ src/features/island/collectibles/components/GenieLamp.tsx
@@ -1,19 +1,66 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { Context } from "features/game/GameProvider";
 
 import genieLamp from "assets/sfts/genie_lamp.png";
 import { PIXEL_SCALE } from "features/game/lib/constants";
+import { Modal } from "react-bootstrap";
+import { Panel } from "components/ui/Panel";
+import { Revealing } from "features/game/components/Revealing";
+import { Revealed } from "features/game/components/Revealed";
+import { useActor } from "@xstate/react";
 
-export const GenieLamp: React.FC = () => {
+interface Props {
+  id: string;
+}
+
+export const GenieLamp: React.FC<Props> = ({ id }) => {
+  const { gameService } = useContext(Context);
+  const [gameState] = useActor(gameService);
+
+  const wish = () => {
+    // setIsShaking(true);
+    // setIsRevealing(true);
+
+    // // Can only shake a Maneki every 24 hours (even if you have multiple)
+    // if (hasShakenRecently) {
+    //   return;
+    // }
+
+    gameService.send("REVEAL", {
+      event: {
+        type: "genieLamp.rubbed",
+        id,
+        createdAt: new Date(),
+      },
+    });
+  };
+
   return (
     <>
       <img
+        onClick={wish}
         src={genieLamp}
         style={{
           width: `${PIXEL_SCALE * 22}px`,
         }}
-        className="absolute"
+        className="absolute cursor-pointer hover:img-highlight"
         alt="Genie Lamp"
       />
+      {gameState.matches("revealing") && (
+        <Modal show centered>
+          <Panel>
+            <Revealing icon={genieLamp} />
+          </Panel>
+        </Modal>
+      )}
+      {gameState.matches("revealed") && (
+        <Modal show centered>
+          <Panel>
+            <Revealed />
+          </Panel>
+        </Modal>
+      )}
     </>
   );
 };
