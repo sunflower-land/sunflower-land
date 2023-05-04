49d2821e5a5c53f4a85098e1e5f936a5a609d140
Romy Furtado <25412194+Rofrtd@users.noreply.github.com>
2023-05-04 16:28:00 +1000

    [FAET] os image (#2532)

--- metadata/metadata.ts
+++ metadata/metadata.ts
@@ -3391,7 +3391,7 @@ export const OPEN_SEA_ITEMS: Record<InventoryItemName, Metadata> = {
     description: "The magical bunny that increases your carrot harvests",
     decimals: 0,
     external_url: "https://docs.sunflower-land.com/getting-started/about",
-    image_url: "../public/erc1155/images/question_mark.png",
+    image_url: "../public/erc1155/images/926.gif",
     attributes: [{ trait_type: "Purpose", value: "Boost" }],
   },
   "Crop Plot": {
