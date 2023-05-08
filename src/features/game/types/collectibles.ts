792f1a53b969085cc9dabfff6481fbb6b82ee511
Adam Hannigan <adamhannigan81@gmail.com>
2023-05-08 15:05:46 +1000

    [CHORE] Bump giveaway numbers

--- src/features/game/types/collectibles.ts
+++ src/features/game/types/collectibles.ts
@@ -137,7 +137,8 @@ export const GOBLIN_BLACKSMITH_ITEMS: (
         "Wild Mushroom": new Decimal(50),
         Gold: new Decimal(10),
       },
-      supply: 2000,
+      // 50 Team supply + giveaways
+      supply: 2000 + 50,
       sfl: SFLDiscount(state, new Decimal(50)),
       boost: "+0.2 Wild Mushroom",
       // only available when SEASONS["DAWN_BREAKER"] starts
@@ -151,7 +152,8 @@ export const GOBLIN_BLACKSMITH_ITEMS: (
         "Dawn Breaker Ticket": new Decimal(3200),
       },
       sfl: SFLDiscount(state, marketRate(20000)),
-      supply: 350,
+      // 50 Team Supply + giveaways
+      supply: 350 + 50,
       boost: "+1 Eggplant",
       disabled: SEASONS["Dawn Breaker"].startDate.getTime() > Date.now(),
     },
@@ -163,7 +165,8 @@ export const GOBLIN_BLACKSMITH_ITEMS: (
         "Dawn Breaker Ticket": new Decimal(1200),
       },
       sfl: SFLDiscount(state, marketRate(2000)),
-      supply: 2500,
+      // 100 Team Supply + Giveaways
+      supply: 2500 + 100,
       boost: "25% faster eggplants",
       disabled: SEASONS["Dawn Breaker"].startDate.getTime() > Date.now(),
     },
