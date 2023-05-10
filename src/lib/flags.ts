bc676693113e91024e68ca98e79e5cd64bff0ee5
spencer <spencer@sunflower-land.com>
2023-05-10 11:29:52 +1000

    [FEAT] Feature flag phantom

--- src/lib/flags.ts
+++ src/lib/flags.ts
@@ -13,7 +13,7 @@ const testnetFeatureFlag = (inventory: GameState["inventory"]) =>
  *
  * Do not delete JEST_TEST.
  */
-type FeatureName = "JEST_TEST" | "FRUIT_QUEST" | "PUMPKIN_PLAZA";
+type FeatureName = "JEST_TEST" | "FRUIT_QUEST" | "PUMPKIN_PLAZA" | "PHANTOM";
 
 type FeatureFlag = (inventory: GameState["inventory"]) => boolean;
 
@@ -21,6 +21,7 @@ const featureFlags: Record<FeatureName, FeatureFlag> = {
   JEST_TEST: defaultFeatureFlag,
   FRUIT_QUEST: testnetFeatureFlag,
   PUMPKIN_PLAZA: defaultFeatureFlag,
+  PHANTOM: testnetFeatureFlag,
 };
 
 export const hasFeatureAccess = (
