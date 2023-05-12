046951960611647ada24e15b102d6021631df7a0
Polysemouse <sunflowermousewax@gmail.com>
2023-05-11 17:35:53 -0700

    [CHORE] Remove limit for min and max scale

--- src/components/ZoomProvider.tsx
+++ src/components/ZoomProvider.tsx
@@ -1,29 +1,9 @@
 import { SpringValue, useSpringValue, config } from "@react-spring/web";
 import { usePinch } from "@use-gesture/react";
-import { GRID_WIDTH_PX } from "features/game/lib/constants";
 import React, { createContext, useEffect } from "react";
 
 const getScaleLimits = () => {
-  const gameboardWidth = 84 * GRID_WIDTH_PX;
-  const gameboardHeight = 56 * GRID_WIDTH_PX;
-
-  // For now, you can only zoom if your screen is less than 1/4 of the game board size
-  const maxZoomWidth = gameboardWidth / 4;
-  const maxZoomHeight = gameboardHeight / 4;
-
-  const vw = Math.max(
-    document.documentElement.clientWidth || 0,
-    window.innerWidth || 0
-  );
-  const vh = Math.max(
-    document.documentElement.clientHeight || 0,
-    window.innerHeight || 0
-  );
-
-  const minScale = Math.max(
-    Math.min(vw / maxZoomWidth, vh / maxZoomHeight, 1),
-    0.5
-  );
+  const minScale = 0.5;
   const maxScale = 1;
 
   return { minScale, maxScale };
