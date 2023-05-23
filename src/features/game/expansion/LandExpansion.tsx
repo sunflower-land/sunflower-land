0ed8236a6655dcc770dab40ac27a52282f80d896
Adam Hannigan <adamhannigan81@gmail.com>
2023-05-23 16:09:35 +1000

    Fix lints

--- src/features/game/expansion/LandExpansion.tsx
+++ src/features/game/expansion/LandExpansion.tsx
@@ -1,7 +1,6 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef } from "react";
 import ScrollContainer from "react-indiana-drag-scroll";
 
-import mapMovement from "../lib/mapMovement";
 import { useLocation, useParams } from "react-router-dom";
 import { GameProvider } from "../GameProvider";
 import { Game } from "./Game";
