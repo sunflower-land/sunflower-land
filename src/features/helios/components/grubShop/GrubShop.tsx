99beab46100709a336c3c498b332f61d6f7f26f3
Adam Hannigan <adamhannigan81@gmail.com>
2023-05-30 07:55:26 +1000

    [FEAT] Prepare Grub Shop Changeover

--- src/features/helios/components/grubShop/GrubShop.tsx
+++ src/features/helios/components/grubShop/GrubShop.tsx
@@ -2,11 +2,10 @@ import React, { useContext } from "react";
 import { Modal } from "react-bootstrap";
 
 import stall from "assets/buildings/grub_shop.png";
-import closeSign from "assets/buildings/close_sign_2.png";
 import goblinChef from "assets/npcs/goblin_chef.gif";
 import shadow from "assets/npcs/shadow.png";
 
-import { PIXEL_SCALE } from "features/game/lib/constants";
+import { GRID_WIDTH_PX, PIXEL_SCALE } from "features/game/lib/constants";
 import { Context } from "features/game/GameProvider";
 import { useActor } from "@xstate/react";
 import { GrubShopModal } from "./components/GrubShopModal";
@@ -15,6 +14,9 @@ import { ConsumableName } from "features/game/types/consumables";
 import { MapPlacement } from "features/game/expansion/components/MapPlacement";
 import { setImageWidth } from "lib/images";
 import { SUNNYSIDE } from "assets/sunnyside";
+import { DeliveryModal } from "features/island/delivery/Delivery";
+import { NPC } from "features/island/bumpkin/components/NPC";
+import { NPC_WEARABLES } from "lib/npcs";
 
 export const GrubShop: React.FC = () => {
   const { gameService } = useContext(Context);
@@ -44,77 +46,91 @@ export const GrubShop: React.FC = () => {
     )?.name;
   }
 
+  const showDeliveries = Date.now() > new Date("2023-06-07").getTime();
+
   return (
     <>
       <Modal centered show={showModal} onHide={closeModal}>
-        <GrubShopModal onClose={closeModal} />
+        {showDeliveries ? (
+          <DeliveryModal isOpen={showModal} onClose={closeModal} />
+        ) : (
+          <GrubShopModal onClose={closeModal} />
+        )}
       </Modal>
 
-      {/* <DeliveryModal isOpen={showModal} onClose={closeModal} />
-      <div
-        className="flex absolute z-10"
-        style={{
-          left: `${GRID_WIDTH_PX * 22}px`,
-          bottom: `${GRID_WIDTH_PX * 17.5}px`,
-        }}
-      >
-        {state.delivery.orders
-          .filter((o) => o.readyAt < Date.now())
-          .slice(0, 1)
-          .map((order) => (
-            <div
-              className="mr-2"
-              style={{
-                width: `${PIXEL_SCALE * 16}px`,
-              }}
-            >
-              <NPC parts={NPC_WEARABLES[order.from]} />
-            </div>
-          ))}
-      </div> */}
       <MapPlacement x={2} y={1} height={5} width={5}>
         <div
           className="relative w-full h-full cursor-pointer hover:img-highlight"
           onClick={openModal}
         >
-          <img
-            src={shadow}
-            className="absolute"
-            style={{
-              width: `${PIXEL_SCALE * 15}px`,
-              left: `${PIXEL_SCALE * 52}px`,
-              bottom: `${PIXEL_SCALE * 0}px`,
-            }}
-          />
-          <img
-            src={SUNNYSIDE.npcs.goblin}
-            className="absolute z-10"
-            style={{
-              width: `${PIXEL_SCALE * 18}px`,
-              left: `${PIXEL_SCALE * 51}px`,
-              bottom: `${PIXEL_SCALE * 2}px`,
-              transform: "scaleX(-1)",
-            }}
-          />
+          {showDeliveries ? (
+            <>
+              <div
+                className="flex absolute z-10"
+                style={{
+                  left: `${GRID_WIDTH_PX * 0}px`,
+                  bottom: `${GRID_WIDTH_PX * 1.5}px`,
+                }}
+              >
+                {state.delivery.orders
+                  .filter((o) => o.readyAt < Date.now())
+                  .slice(0, 1)
+                  .map((order) => (
+                    <div
+                      key={order.id}
+                      className="mr-2"
+                      style={{
+                        width: `${PIXEL_SCALE * 16}px`,
+                      }}
+                    >
+                      <NPC parts={NPC_WEARABLES[order.from]} />
+                    </div>
+                  ))}
+              </div>
+            </>
+          ) : (
+            <>
+              <img
+                src={shadow}
+                className="absolute"
+                style={{
+                  width: `${PIXEL_SCALE * 15}px`,
+                  left: `${PIXEL_SCALE * 52}px`,
+                  bottom: `${PIXEL_SCALE * 0}px`,
+                }}
+              />
+              <img
+                src={SUNNYSIDE.npcs.goblin}
+                className="absolute z-10"
+                style={{
+                  width: `${PIXEL_SCALE * 18}px`,
+                  left: `${PIXEL_SCALE * 51}px`,
+                  bottom: `${PIXEL_SCALE * 2}px`,
+                  transform: "scaleX(-1)",
+                }}
+              />
 
-          <img
-            src={shadow}
-            className="absolute"
-            style={{
-              width: `${PIXEL_SCALE * 15}px`,
-              left: `${PIXEL_SCALE * 5.5}px`,
-              bottom: `${PIXEL_SCALE * 2}px`,
-            }}
-          />
-          <img
-            src={SUNNYSIDE.npcs.goblin}
-            className="absolute z-10"
-            style={{
-              width: `${PIXEL_SCALE * 18}px`,
-              left: `${PIXEL_SCALE * 4}px`,
-              bottom: `${PIXEL_SCALE * 4}px`,
-            }}
-          />
+              <img
+                src={shadow}
+                className="absolute"
+                style={{
+                  width: `${PIXEL_SCALE * 15}px`,
+                  left: `${PIXEL_SCALE * 5.5}px`,
+                  bottom: `${PIXEL_SCALE * 2}px`,
+                }}
+              />
+              <img
+                src={SUNNYSIDE.npcs.goblin}
+                className="absolute z-10"
+                style={{
+                  width: `${PIXEL_SCALE * 18}px`,
+                  left: `${PIXEL_SCALE * 4}px`,
+                  bottom: `${PIXEL_SCALE * 4}px`,
+                }}
+              />
+            </>
+          )}
+
           <img
             src={stall}
             style={{
@@ -125,18 +141,6 @@ export const GrubShop: React.FC = () => {
             alt="bakery"
             className="absolute"
           />
-          {isClosed && (
-            <img
-              src={closeSign}
-              style={{
-                width: `${PIXEL_SCALE * 29}px`,
-                bottom: `${PIXEL_SCALE * 40}px`,
-                left: `${PIXEL_SCALE * 23}px`,
-              }}
-              alt="closed"
-              className="absolute"
-            />
-          )}
 
           <img
             src={shadow}
