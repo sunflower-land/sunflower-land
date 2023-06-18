48e7b39935ff81934f9433a522dba148b5b05fcc
Adam Hannigan <adamhannigan81@gmail.com>
2023-06-19 08:59:20 +1000

    [CHORE] Already minted

--- src/features/retreat/components/auctioneer/AuctionDetails.tsx
+++ src/features/retreat/components/auctioneer/AuctionDetails.tsx
@@ -16,6 +16,7 @@ import { Auction } from "features/game/lib/auctionMachine";
 import { BUMPKIN_ITEM_BUFF, ITEM_IDS } from "features/game/types/bumpkin";
 import { getImageUrl } from "features/goblins/tailor/TabContent";
 import { COLLECTIBLE_BUFF } from "features/game/types/collectibles";
+import { CONFIG } from "lib/config";
 
 type Props = {
   item: Auction;
@@ -85,10 +86,10 @@ export const AuctionDetails: React.FC<Props> = ({
       item.type === "collectible"
         ? game.inventory[item.collectible]
         : game.wardrobe[item.wearable];
-    // Turned off for testing
-    // if (CONFIG.NETWORK !== "mumbai" && hasMinted) {
-    //   return <span className="text-sm">Already minted</span>;
-    // }
+
+    if (CONFIG.NETWORK !== "mumbai" && hasMinted) {
+      return <span className="text-sm">Already minted</span>;
+    }
 
     return (
       <Button
