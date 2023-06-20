d04f3503039b1093599c72101fdc73bdcc5ca931
Romy Furtado <25412194+Rofrtd@users.noreply.github.com>
2023-06-20 21:56:06 +1000

    [FEAT] remove pointer-events

--- src/features/island/collectibles/Collectible.tsx
+++ src/features/island/collectibles/Collectible.tsx
@@ -446,7 +446,7 @@ export const READONLY_COLLECTIBLES: Record<CollectibleName, React.FC<any>> = {
           width: `${PIXEL_SCALE * 16 * 3}px`,
           height: `${PIXEL_SCALE * 16 * 3}px`,
           left: `${PIXEL_SCALE * -12.8}px`,
-          top: `${PIXEL_SCALE * 16 * 2 - 2}px`,
+          top: `${PIXEL_SCALE * 16 * 2 - 7.5}px`,
         }}
       >
         <img
@@ -469,7 +469,7 @@ export const READONLY_COLLECTIBLES: Record<CollectibleName, React.FC<any>> = {
     >
       <img
         src={ITEM_DETAILS["Laurie the Chuckle Crow"].image}
-        className="w-full pointer-events-auto"
+        className="w-full"
       />
       <div
         className="absolute bottom-0 bg-blue-300 bg-opacity-50 animate-pulse z-50 pointer-events-none"
