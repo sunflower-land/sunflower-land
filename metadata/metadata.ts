2fb09337a0d5d4f84f15a3f126e9973b4b526b0a
Romy Furtado <25412194+Rofrtd@users.noreply.github.com>
2023-07-04 12:30:39 +1000

    [FEAT] non-withdrawable

--- metadata/metadata.ts
+++ metadata/metadata.ts
@@ -3826,6 +3826,6 @@ export const OPEN_SEA_ITEMS: Record<InventoryItemName, Metadata> = {
     decimals: 0,
     external_url: "https://docs.sunflower-land.com/getting-started/about",
     image_url: "../public/erc1155/images/469.png",
-    attributes: [],
+    attributes: [{ trait_type: "Purpose", value: "Boost" }],
   },
 };
