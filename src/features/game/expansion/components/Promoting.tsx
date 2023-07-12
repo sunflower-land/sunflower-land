21f7edb582ee659a5c79cfae2055f45b942a7b07
spencer <spencer@sunflower-land.com>
2023-07-12 14:57:45 +1000

    [FEAT] Update link

--- src/features/game/expansion/components/Promoting.tsx
+++ src/features/game/expansion/components/Promoting.tsx
@@ -199,7 +199,7 @@ export const PromotingModal: React.FC<Props> = ({
           )}
 
           <a
-            href="https://docs.sunflower-land.com/player-guides/seasons/dawn-breaker"
+            href="https://docs.sunflower-land.com/player-guides/seasons/witches-eve#witches-eve-banner"
             target="_blank"
             rel="noopener noreferrer"
             className="underline text-xxs pb-1 pt-0.5 hover:text-blue-500"
