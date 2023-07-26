d5966e3549cf68cddde877e5a24c3ed7245a4532
Adam Hannigan <adamhannigan81@gmail.com>
2023-07-26 11:06:06 +1000

    [FIX] TSC

--- src/features/pumpkinPlaza/Room.tsx
+++ src/features/pumpkinPlaza/Room.tsx
@@ -255,7 +255,6 @@ export const Room: React.FC<Props> = ({
               reaction,
             });
           }}
-          onChatStarted={console.log}
           messages={[]}
         />
       )}
