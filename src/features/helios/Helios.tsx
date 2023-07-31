81d244e1ed9c496c2472db1739e66869b884c874
Craig <41215134+c-py@users.noreply.github.com>
2023-07-31 16:26:22 +1000

    [FEAT] Hide Hank

--- src/features/helios/Helios.tsx
+++ src/features/helios/Helios.tsx
@@ -82,7 +82,7 @@ export const Helios: React.FC = () => {
           onTravelDialogOpened={() => gameService.send("SAVE")}
           travelAllowed={!autosaving}
         />
-        {Date.now() < new Date("2023-08-01").getTime() && <HayseedHank />}
+        {gameState.context.state.hayseedHank && <HayseedHank />}
       </div>
       <Hud isFarming={false} />
       <AuctionCountdown />
