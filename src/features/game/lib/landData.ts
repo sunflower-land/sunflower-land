fedb4ee8cf072ab32b2cf4bafd3b2b65ff38b3d6
Adam Hannigan <adamhannigan81@gmail.com>
2023-07-31 16:13:29 +1000

    [FIX] TSC

--- src/features/game/lib/landData.ts
+++ src/features/game/lib/landData.ts
@@ -1,6 +1,6 @@
 import Decimal from "decimal.js-light";
 import { CHORES } from "../types/chores";
-import { Bumpkin, ChoreV2Name, GameState, Inventory } from "../types/game";
+import { Bumpkin, GameState, Inventory } from "../types/game";
 import { getKeys } from "../types/craftables";
 
 const INITIAL_STOCK: Inventory = {
@@ -483,48 +483,7 @@ export const OFFLINE_FARM: GameState = {
   chores: {
     choresCompleted: 0,
     choresSkipped: 2,
-    chores: {
-      [ChoreV2Name.EASY_1]: {
-        activity: "Corn Harvested",
-        bumpkinId: 1,
-        createdAt: 0,
-        description: "Harvest 2 Corn",
-        requirement: 5,
-        tickets: 2,
-        completedAt: 100,
-        startCount: -200,
-      },
-      [ChoreV2Name.EASY_2]: {
-        activity: "Sunflower Harvested",
-        bumpkinId: 1,
-        createdAt: Date.now() - 25 * 60 * 60 * 1000,
-        description: "Harvest 3 Sunflowers in long text",
-        requirement: 5,
-        tickets: 2,
-
-        startCount: -50,
-      },
-      [ChoreV2Name.MEDIUM_1]: {
-        activity: "Sunflower Harvested",
-        bumpkinId: 1,
-        createdAt: Date.now() - 100,
-        description: "Harvest 3 Sunflowers in long text",
-        requirement: 5,
-        tickets: 2,
-
-        startCount: 0,
-      },
-      [ChoreV2Name.MEDIUM_2]: {
-        activity: "Sunflower Harvested",
-        bumpkinId: 1,
-        createdAt: 0,
-        description: "Harvest 3 Sunflowers in long text",
-        requirement: 5,
-        tickets: 2,
-
-        startCount: 0,
-      },
-    },
+    chores: {},
   },
 
   buildings: {
