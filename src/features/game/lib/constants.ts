2af86104f39c8b3d21e6b09c1f4f7af6022c3526
spencer <spencer@sunflower-land.com>
2023-07-31 12:35:02 +1000

    [CHORE] Remove code

--- src/features/game/lib/constants.ts
+++ src/features/game/lib/constants.ts
@@ -377,10 +377,6 @@ export const TEST_FARM: GameState = {
     spawnedAt: 0,
     mushrooms: {},
   },
-  witchesEve: {
-    weeklyLostCrowCount: 25,
-    maze: {},
-  },
 };
 
 export const EMPTY: GameState = {
