cad11eff8fd63058eea9ac615122187b70d8ae1b
Adam Hannigan <adamhannigan81@gmail.com>
2023-07-31 17:43:19 +1000

    [FIX] Corn render layer

--- src/features/world/scenes/CornScene.ts
+++ src/features/world/scenes/CornScene.ts
@@ -273,6 +273,7 @@ export class CornScene extends BaseScene {
             const spriteY = y * tileHeight + tileHeight / 2;
 
             const crow = this.physics.add.sprite(spriteX, spriteY, "crow");
+            crow.setDepth(100000);
             // on collision with player, collect crow
             if (this.currentPlayer) {
               this.physics.add.overlap(this.currentPlayer, crow, () => {
