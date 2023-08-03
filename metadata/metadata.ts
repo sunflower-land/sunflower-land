6882da1373258debf4f69fc3217256176f4b91cb
Brian Manthos <manthos@msn.com>
2023-08-03 00:40:35 -0700

    [FIX] image_url in El Pollo Veloz metadata

--- metadata/metadata.ts
+++ metadata/metadata.ts
@@ -3876,7 +3876,7 @@ export const OPEN_SEA_ITEMS: Record<InventoryItemName, Metadata> = {
     description: "Give me those eggs, fast! 4 hour speed boost on egg laying.",
     decimals: 0,
     external_url: "https://docs.sunflower-land.com/getting-started/about",
-    image_url: "../public/erc1155/images/445.gif",
+    image_url: "../public/erc1155/images/470.png",
     attributes: [
       { trait_type: "Purpose", value: "Boost" },
       {
