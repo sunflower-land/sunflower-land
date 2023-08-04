5a28415dc0cbd2a10f5dbb7dbdd5b04f71e8a6eb
Craig <41215134+c-py@users.noreply.github.com>
2023-08-04 13:59:24 +1000

    [FIX] Close Stylist (#2774)

--- src/features/world/ui/stylist/Stylist.tsx
+++ src/features/world/ui/stylist/Stylist.tsx
@@ -1,14 +1,6 @@
-import { SUNNYSIDE } from "assets/sunnyside";
 import { CloseButtonPanel } from "features/game/components/CloseablePanel";
 import { NPC_WEARABLES } from "lib/npcs";
 import React, { useState } from "react";
-import { StylistWearables } from "./StylistWearables";
-import {
-  BASIC_WEARABLES,
-  LIMITED_WEARABLES,
-} from "features/game/types/stylist";
-import { TEST_FARM } from "features/game/lib/constants";
-import { Merch } from "./Merch";
 
 interface Props {
   onClose: () => void;
@@ -19,20 +11,26 @@ export const Stylist: React.FC<Props> = ({ onClose }) => {
   return (
     <CloseButtonPanel
       bumpkinParts={NPC_WEARABLES.stella}
-      tabs={[
-        { icon: SUNNYSIDE.icons.wardrobe, name: "Wearables" },
-        { icon: SUNNYSIDE.icons.timer, name: "Limited" },
-        { icon: SUNNYSIDE.icons.heart, name: "Merch" },
-      ]}
+      // Comment this back in when Stella is ready to sell wearables
+      // tabs={[
+      //   { icon: SUNNYSIDE.icons.wardrobe, name: "Wearables" },
+      //   { icon: SUNNYSIDE.icons.timer, name: "Limited" },
+      //   { icon: SUNNYSIDE.icons.heart, name: "Merch" },
+      // ]}
       onClose={onClose}
       currentTab={tab}
       setCurrentTab={setTab}
     >
-      {tab === 0 && <StylistWearables wearables={BASIC_WEARABLES} />}
+      {/*Delete this back in when Stella is ready to sell wearables*/}
+      <div className="p-2 pb-3">
+        {`Sorry, I'm temporarily closed for business! Please come back later!`}
+      </div>
+      {/*Comment this back in when Stella is ready to sell wearables*/}
+      {/* {tab === 0 && <StylistWearables wearables={BASIC_WEARABLES} />}
       {tab === 1 && (
         <StylistWearables wearables={LIMITED_WEARABLES(TEST_FARM)} />
       )}
-      {tab === 2 && <Merch />}
+      {tab === 2 && <Merch />} */}
     </CloseButtonPanel>
   );
 };
