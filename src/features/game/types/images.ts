e2eeaa3467fc0aa6fa4c226a4c328a9ca18276ab
Brian Manthos <manthos@msn.com>
2023-08-11 21:54:23 -0700

    [FIX] Spelling of 'ganoush' in PNG filename

--- src/features/game/types/images.ts
+++ src/features/game/types/images.ts
@@ -269,7 +269,7 @@ import fruitSalad from "assets/food/fruit_salad.png";
 import kaleOmelette from "assets/food/kale_omelette.png";
 import cabbersNMash from "assets/food/cabbers_n_mash.png";
 import fancyFries from "assets/food/fancy_fries.png";
-import bumpkinGanoush from "assets/food/bumpkin_ghanoush.png";
+import bumpkinGanoush from "assets/food/bumpkin_ganoush.png";
 import eggplantCake from "assets/food/eggplant_cake.png";
 import cornBread from "assets/food/corn_bread.png";
 import popcorn from "assets/food/popcorn.png";
