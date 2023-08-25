9e492f5e1733818ce8bbfc8e5f9478bf2c7977fc
Craig <41215134+c-py@users.noreply.github.com>
2023-08-25 10:14:24 +1000

    [FEAT] Potion hoarding limit

--- src/features/game/lib/processEvent.ts
+++ src/features/game/lib/processEvent.ts
@@ -97,8 +97,7 @@ export const maxItems: Inventory = {
   "Crow Feather": new Decimal(750),
 
   // Potion House
-  // "Potion Ticket": new Decimal(750),
-  "Potion Ticket": new Decimal(1000000),
+  "Potion Ticket": new Decimal(750),
   "Giant Cabbage": new Decimal(50),
   "Giant Potato": new Decimal(50),
   "Giant Pumpkin": new Decimal(50),
