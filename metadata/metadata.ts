1de547f2c085a3e929428eb8bf2f129c1353ba3f
elias7905 <elias.chew1999@gmail.com>
2023-08-25 16:24:51 +0800

    Change purpose for Magic Beans to Seed

--- metadata/metadata.ts
+++ metadata/metadata.ts
@@ -3001,7 +3001,7 @@ export const OPEN_SEA_ITEMS: Record<InventoryItemName, Metadata> = {
     decimals: 0,
     external_url: "https://docs.sunflower-land.com/getting-started/about",
     image_url: "../public/erc1155/images/115.png",
-    attributes: [{ trait_type: "Purpose", value: "Boost" }],
+    attributes: [{ trait_type: "Purpose", value: "Seed" }],
   },
   "Christmas Snow Globe": {
     description:
