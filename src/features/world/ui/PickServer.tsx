c0b029bc238b1970dee6e75a1af65099266b7b77
0xSacul <63975424+0xSacul@users.noreply.github.com>
2023-08-29 02:23:24 +0200

    Fix FULL Message Position

--- src/features/world/ui/PickServer.tsx
+++ src/features/world/ui/PickServer.tsx
@@ -114,15 +114,17 @@ export const PickServer: React.FC<Props> = ({ mmoService }) => {
                     <img src={ICONS[index]} className="w-6 mr-2" />
                     <div>
                       <p className="text-sm break-words">{server.name}</p>
-                      <p className="text-xs break-words">{server.purpose}</p>
-                      {isServerFull(servers, server.id) && (
-                        <Label
-                          type="danger"
-                          className="flex gap-2 items-center"
-                        >
-                          FULL
-                        </Label>
-                      )}
+                      <div className="flex items-center gap-2">
+                        {isServerFull(servers, server.id) && (
+                          <Label
+                            type="danger"
+                            className="flex gap-2 items-center"
+                          >
+                            FULL
+                          </Label>
+                        )}
+                        <p className="text-xs break-words">{server.purpose}</p>
+                      </div>
                     </div>
                   </div>
                   <div className="flex-1 flex items-center justify-end">
