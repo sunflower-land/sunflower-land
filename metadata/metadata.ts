4e58416547d1297fa432d59abb61d72d882eddef
elias7905 <elias.chew1999@gmail.com>
2023-09-15 13:33:53 +0800

    Minor Changes to Town Sign Metadata

--- metadata/metadata.ts
+++ metadata/metadata.ts
@@ -5258,7 +5258,7 @@ export const OPEN_SEA_ITEMS: Record<InventoryItemName, Metadata> = {
     external_url: "https://docs.sunflower-land.com/getting-started/about",
     image_url: "../public/erc1155/images/1284.png",
     attributes: [
-      { trait_type: "Purpose", value: "Decoration" },
+      { trait_type: "Decoration", value: "Landscape" },
       { trait_type: "Tradable", value: "No" },
     ],
   },
