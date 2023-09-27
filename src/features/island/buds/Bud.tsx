aa975291b3f546dbfe830f180f641e22ac2c8453
spencer <spencer@sunflower-land.com>
2023-09-27 15:56:13 +1000

    [CHORE] Add small offset for retreat bud position

--- src/features/island/buds/Bud.tsx
+++ src/features/island/buds/Bud.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 
 import { Context } from "features/game/GameProvider";
-import { useSelector } from "@xstate/react";
+import { useActor, useSelector } from "@xstate/react";
 import { MoveableComponent } from "../collectibles/MovableComponent";
 import { Bud as _Bud } from "../collectibles/components/Bud";
 import { MachineState } from "../buildings/lib/craftingMachine";
@@ -16,16 +16,19 @@ const isLandscaping = (state: MachineState) => state.matches("landscaping");
 
 const BudComponent: React.FC<BudProps> = ({ id, x, y }) => {
   const { gameService } = useContext(Context);
+  const [gameState] = useActor(gameService);
+  const buds = gameState.context.state.buds ?? {};
   const landscaping = useSelector(gameService, isLandscaping);
+  const bud = buds[Number(id)];
 
   if (landscaping)
     return (
       <MoveableComponent name="Bud" id={String(id)} x={x} y={y}>
-        <_Bud id={String(id)} />
+        <_Bud id={String(id)} type={bud.type} />
       </MoveableComponent>
     );
 
-  return <_Bud id={String(id)} />;
+  return <_Bud id={String(id)} type={bud.type} />;
 };
 
 export const Bud = React.memo(BudComponent);
