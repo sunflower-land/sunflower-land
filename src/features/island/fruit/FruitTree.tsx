15ba61de4329be27b98ebf88f4046792eeb346e1
Romy Furtado <25412194+Rofrtd@users.noreply.github.com>
2023-10-05 16:37:05 +1100

    [FEAT] fertilise fruit

--- src/features/island/fruit/FruitTree.tsx
+++ src/features/island/fruit/FruitTree.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import { getTimeLeft } from "lib/utils/time";
 import { setImageWidth } from "lib/images";
-import { PlantedFruit } from "features/game/types/game";
+import { FruitFertiliser, PlantedFruit } from "features/game/types/game";
 import useUiRefresher from "lib/utils/hooks/useUiRefresher";
 import { FRUIT, FruitName, FRUIT_SEEDS } from "features/game/types/fruits";
 import { FRUIT_LIFECYCLE } from "./fruits";
@@ -35,11 +35,13 @@ interface Props {
   harvestFruit: () => void;
   removeTree: () => void;
   onError: () => void;
+  fertilise: () => void;
   playAnimation: boolean;
   /**
    * Handles showing "hover" information on mobile or "error" on click action information
    */
   showOnClickInfo: boolean;
+  fertiliser?: FruitFertiliser;
 }
 
 export const FruitTree: React.FC<Props> = ({
@@ -48,9 +50,11 @@ export const FruitTree: React.FC<Props> = ({
   harvestFruit,
   removeTree,
   onError,
+  fertilise,
   playing,
   playAnimation,
   showOnClickInfo,
+  fertiliser,
 }) => {
   useUiRefresher({ active: !!plantedFruit });
   //UI Refresher reloads this component after a regular time intervals.
@@ -97,12 +101,14 @@ export const FruitTree: React.FC<Props> = ({
 
     if (replenishingTimeLeft > 0) {
       return (
-        <ReplenishingTree
-          onClick={onError}
-          plantedFruit={plantedFruit}
-          playAnimation={playAnimation}
-          showOnClickInfo={showOnClickInfo}
-        />
+        <>
+          <ReplenishingTree
+            onClick={fertilise}
+            plantedFruit={plantedFruit}
+            playAnimation={playAnimation}
+            showOnClickInfo={showOnClickInfo}
+          />
+        </>
       );
     }
   }
@@ -112,12 +118,14 @@ export const FruitTree: React.FC<Props> = ({
 
   if (growingTimeLeft > 0) {
     return (
-      <Seedling
-        onClick={onError}
-        playing={playing}
-        plantedFruit={plantedFruit}
-        showOnClickInfo={showOnClickInfo}
-      />
+      <>
+        <Seedling
+          onClick={fertilise}
+          playing={playing}
+          plantedFruit={plantedFruit}
+          showOnClickInfo={showOnClickInfo}
+        />
+      </>
     );
   }
 
@@ -130,19 +138,21 @@ export const FruitTree: React.FC<Props> = ({
 
   // Ready tree
   return (
-    <div
-      className="absolute h-full w-full hover:img-highlight cursor-pointer"
-      onClick={harvestFruit}
-    >
-      <img
-        className="absolute pointer-events-none"
-        style={{
-          bottom: `${PIXEL_SCALE * 5}px`,
-          left: `${PIXEL_SCALE * (isBush ? 4 : 3)}px`,
-        }}
-        src={lifecycle.ready}
-        onLoad={(e) => setImageWidth(e.currentTarget)}
-      />
-    </div>
+    <>
+      <div
+        className="absolute h-full w-full hover:img-highlight cursor-pointer"
+        onClick={harvestFruit}
+      >
+        <img
+          className="absolute pointer-events-none"
+          style={{
+            bottom: `${PIXEL_SCALE * 5}px`,
+            left: `${PIXEL_SCALE * (isBush ? 4 : 3)}px`,
+          }}
+          src={lifecycle.ready}
+          onLoad={(e) => setImageWidth(e.currentTarget)}
+        />
+      </div>
+    </>
   );
 };
