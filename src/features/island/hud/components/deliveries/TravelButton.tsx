5061d1934cb4f2e6099128a0e256b6831ff436e7
Polysemouse <sunflowermousewax@gmail.com>
2023-10-05 20:46:17 -0700

    [FIX] Travel button pixel scale

--- src/features/island/hud/components/deliveries/TravelButton.tsx
+++ src/features/island/hud/components/deliveries/TravelButton.tsx
@@ -4,7 +4,6 @@ import { PIXEL_SCALE } from "features/game/lib/constants";
 import { useSelector } from "@xstate/react";
 import { Context } from "features/game/GameProvider";
 import world from "assets/icons/world.png";
-import classNames from "classnames";
 import { hasNewOrders } from "features/island/delivery/lib/delivery";
 import { MachineState } from "features/game/lib/gameMachine";
 import { TravelModal } from "./TravelModal";
@@ -17,57 +16,52 @@ export const Travel: React.FC = () => {
   const delivery = useSelector(gameService, _delivery);
 
   return (
-    <div>
-      <div
-        id="deliveries"
-        className="flex relative z-50 justify-center cursor-pointer hover:img-highlight"
-        style={{
-          width: `${PIXEL_SCALE * 22}px`,
-          height: `${PIXEL_SCALE * 22}px`,
-        }}
-        onClick={(e) => {
-          e.stopPropagation();
-          e.preventDefault();
-          setShowModal(true);
-        }}
-      >
-        <img
-          src={SUNNYSIDE.ui.round_button}
-          className="absolute"
+    <>
+      <div>
+        <div
+          id="deliveries"
+          className="flex relative z-50 justify-center cursor-pointer hover:img-highlight"
           style={{
             width: `${PIXEL_SCALE * 22}px`,
-            height: `${PIXEL_SCALE * 22}px`,
+            height: `${PIXEL_SCALE * 23}px`,
           }}
-        />
-        <img
-          src={world}
-          style={{
-            width: `${PIXEL_SCALE * 12}px`,
-            left: `${PIXEL_SCALE * 5}px`,
-            top: `${PIXEL_SCALE * 3.5}px`,
-          }}
-          className={classNames("absolute cursor-pointer hover:img-highlight")}
-        />
-        <div
-          className="absolute"
-          style={{
-            width: `${PIXEL_SCALE * 10.5}px`,
-            top: `${PIXEL_SCALE * 3.3}px`,
+          onClick={(e) => {
+            e.stopPropagation();
+            e.preventDefault();
+            setShowModal(true);
           }}
         >
-          {hasNewOrders(delivery) && (
-            <img
-              src={SUNNYSIDE.icons.expression_alerted}
-              className="absolute -top-2 -right-2 pointer-events-none"
-              style={{
-                width: `${PIXEL_SCALE * 4}px`,
-              }}
-            />
-          )}
+          <img
+            src={SUNNYSIDE.ui.round_button}
+            className="absolute"
+            style={{
+              width: `${PIXEL_SCALE * 22}px`,
+            }}
+          />
+          <img
+            src={world}
+            style={{
+              width: `${PIXEL_SCALE * 10}px`,
+              left: `${PIXEL_SCALE * 6}px`,
+              top: `${PIXEL_SCALE * 5}px`,
+            }}
+            className="absolute"
+          />
         </div>
+        {hasNewOrders(delivery) && (
+          <img
+            src={SUNNYSIDE.icons.expression_alerted}
+            className="absolute animate-float pointer-events-none"
+            style={{
+              width: `${PIXEL_SCALE * 4}px`,
+              top: `${PIXEL_SCALE * -13}px`,
+              left: `${PIXEL_SCALE * 9}px`,
+            }}
+          />
+        )}
       </div>
       <TravelModal isOpen={showModal} onClose={() => setShowModal(false)} />
-    </div>
+    </>
   );
 };
 
