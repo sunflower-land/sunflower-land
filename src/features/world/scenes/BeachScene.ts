3f121fef7c3763c84bf99b4eab502204542ed2bd
Adam Hannigan <adamhannigan81@gmail.com>
2023-10-15 13:46:49 +1100

    Beach NPC placeholders

--- src/features/world/scenes/BeachScene.ts
+++ src/features/world/scenes/BeachScene.ts
@@ -3,13 +3,48 @@ import mapJSON from "assets/map/beach.json";
 import { SceneId } from "../mmoMachine";
 import { BaseScene, NPCBumpkin } from "./BaseScene";
 
-const BUMPKINS: NPCBumpkin[] = [];
+const BUMPKINS: NPCBumpkin[] = [
+  {
+    npc: "shelly",
+    x: 326,
+    y: 691,
+  },
+  {
+    npc: "finn",
+    x: 84,
+    y: 518,
+  },
+  {
+    npc: "tango",
+    x: 416,
+    y: 351,
+  },
+  {
+    npc: "goldtooth",
+    x: 304,
+    y: 255,
+  },
+  {
+    npc: "mystara",
+    x: 149,
+    y: 670,
+  },
+];
 
 export class BeachScene extends BaseScene {
   sceneId: SceneId = "beach";
 
   constructor() {
     super({ name: "beach", map: { json: mapJSON } });
+  }
+
+  preload() {
+    super.preload();
+
+    this.load.spritesheet("turtle_bud", "world/turtle.png", {
+      frameWidth: 15,
+      frameHeight: 17,
+    });
   }
 
   async create() {
@@ -20,5 +55,18 @@ export class BeachScene extends BaseScene {
     super.create();
 
     this.initialiseNPCs(BUMPKINS);
+
+    const turtle = this.add.sprite(328, 520, "turtle_bud");
+    turtle.setScale(-1, 1);
+    this.anims.create({
+      key: "turtle_bud_anim",
+      frames: this.anims.generateFrameNumbers("turtle_bud", {
+        start: 0,
+        end: 8,
+      }),
+      repeat: -1,
+      frameRate: 10,
+    });
+    turtle.play("turtle_bud_anim", true);
   }
 }
