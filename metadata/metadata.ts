1d39c0e865d9a880b6c36c77827fb11a5f112b11
EliasGC <101262042+elias7905@users.noreply.github.com>
2023-10-18 01:48:29 +0000

    Fix Typo

--- metadata/metadata.ts
+++ metadata/metadata.ts
@@ -5432,7 +5432,7 @@ export const OPEN_SEA_ITEMS: Record<InventoryItemName, Metadata> = {
     image_url: "../public/erc1155/images/1501.png",
     attributes: [],
   },
-  Butteryflyfish: {
+  Butterflyfish: {
     description: "?",
     decimals: 0,
     external_url: "https://docs.sunflower-land.com/getting-started/about",
