3b190cb9a2492598725fb93eec1c3b06748fc7bd
EliasGC <elias.chew1999@gmail.com>
2023-10-18 09:34:31 +0800

    Remove typo again

--- src/features/game/types/images.ts
+++ src/features/game/types/images.ts
@@ -458,7 +458,7 @@ import anchovy from "assets/fish/anchovy.png";
 import barredKnifejaw from "assets/fish/barred_knifejaw.png";
 import blowfish from "assets/fish/blowfish.png";
 import blueMarlin from "assets/fish/blue_marlin.png";
-import butteryflyfish from "assets/fish/butterfly_fish.png";
+import butterflyfish from "assets/fish/butterfly_fish.png";
 import clownfish from "assets/fish/clownfish.png";
 import coelacanth from "assets/fish/coelacanth.png";
 import footballFish from "assets/fish/football_fish.png";
@@ -2558,7 +2558,7 @@ export const ITEM_DETAILS: Items = {
 
   Anchovy: { image: anchovy, description: "?" },
   Butteryflyfish: {
-    image: butteryflyfish,
+    image: butterflyfish,
     description: "?",
   },
   Blowfish: { image: blowfish, description: "?" },
