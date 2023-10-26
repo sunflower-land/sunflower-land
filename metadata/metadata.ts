32a9129e093c882fc03c55d8ede2fafa97f9b1f3
Adam Hannigan <adamhannigan81@gmail.com>
2023-10-26 11:25:32 +1100

    [FEAT] fix wearable

--- metadata/metadata.ts
+++ metadata/metadata.ts
@@ -5851,7 +5851,7 @@ export const OPEN_SEA_ITEMS: Record<InventoryItemName, Metadata> = {
     ],
   },
   "Sapo Travessuras": {
-    description: "Oh oh....someone was naught!",
+    description: "Oh oh....someone was naughty!",
     decimals: 0,
     external_url: "https://docs.sunflower-land.com/getting-started/about",
     image_url: "../public/erc1155/images/1290.png",
