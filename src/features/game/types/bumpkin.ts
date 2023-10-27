7730ede3eb8d3f1dbc1493a260ec6de1ae1997c1
Spencer Dezart-Smith <17863697+spencerdezartsmith@users.noreply.github.com>
2023-10-27 14:30:07 +1100

    [FEAT] Stylist Wearables (#3043)
    * [FEAT] Stylist Wearables
    
    * [FEAT] Fix dates

--- src/features/game/types/bumpkin.ts
+++ src/features/game/types/bumpkin.ts
@@ -1,9 +1,3 @@
-import powerup from "assets/icons/level_up.png";
-import lightning from "assets/icons/lightning.png";
-import { CROP_LIFECYCLE } from "features/island/plots/lib/plant";
-import { SUNNYSIDE } from "assets/sunnyside";
-import { BuffLabel } from ".";
-
 export type BumpkinBackground =
   | "Farm Background"
   | "Seashore Background"
@@ -99,7 +93,8 @@ export type BumpkinShirt =
   | "Pumpkin Shirt"
   | "Grey Merch Hoodie"
   | "Clown Shirt"
-  | "Seaside Tank Top";
+  | "Seaside Tank Top"
+  | "Tiki Armor";
 
 export type BumpkinCoat =
   | "Chef Apron"
@@ -200,7 +195,10 @@ export type BumpkinHat =
   | "Luminous Anglerfish Topper"
   | "Abyssal Angler Hat"
   | "Fishing Hat"
-  | "Coconut Mask";
+  | "Coconut Mask"
+  | "Koi Fish Hat"
+  | "Normal Fish Hat"
+  | "Tiki Mask";
 
 export type BumpkinPant =
   | "Farmer Overalls"
@@ -224,7 +222,8 @@ export type BumpkinPant =
   | "Crimson Skirt"
   | "Tattered Slacks"
   | "Fishing Pants"
-  | "Angler Waders";
+  | "Angler Waders"
+  | "Tiki Pants";
 
 export type BumpkinDress =
   | "Cupid Dress"
@@ -255,7 +254,8 @@ export type BumpkinOnesie =
   | "Frog Onesie"
   | "Bunny Onesie"
   | "Eggplant Onesie"
-  | "Corn Onesie";
+  | "Corn Onesie"
+  | "Stockeye Salmon Onesie";
 
 // Goes over clothes
 export type BumpkinSuit =
@@ -531,6 +531,12 @@ export const ITEM_IDS: Record<BumpkinItem, number> = {
   "Tackle Box": 241,
   "Mermaid Potion": 242,
   "Squirrel Monkey Potion": 243,
+  "Koi Fish Hat": 244,
+  "Normal Fish Hat": 245,
+  "Stockeye Salmon Onesie": 246,
+  "Tiki Armor": 247,
+  "Tiki Mask": 248,
+  "Tiki Pants": 249,
 };
 
 // The reverse of above
@@ -833,124 +839,10 @@ export const BUMPKIN_ITEM_PART: Record<BumpkinItem, keyof Wallet> = {
   "Infernal Rod": "tool",
   "Mermaid Potion": "body",
   "Squirrel Monkey Potion": "body",
+  "Koi Fish Hat": "hat",
+  "Normal Fish Hat": "hat",
+  "Stockeye Salmon Onesie": "onesie",
+  "Tiki Armor": "shirt",
+  "Tiki Mask": "hat",
+  "Tiki Pants": "pants",
 };
-
-export const BUMPKIN_ITEM_BUFF_LABELS: Partial<Record<BumpkinItem, BuffLabel>> =
-  {
-    "Chef Apron": {
-      shortDescription: "+20% Cake Profit",
-      labelType: "success",
-      boostTypeIcon: powerup,
-    },
-    "Fruit Picker Apron": {
-      shortDescription: "+0.1 Fruit",
-      labelType: "success",
-      boostTypeIcon: powerup,
-    },
-    "Angel Wings": {
-      shortDescription: "Instant Crops",
-      labelType: "vibrant",
-      boostTypeIcon: lightning,
-    },
-    "Devil Wings": {
-      shortDescription: "Instant Crops",
-      labelType: "vibrant",
-      boostTypeIcon: lightning,
-    },
-    "Eggplant Onesie": {
-      shortDescription: "+0.1 Eggplant",
-      labelType: "success",
-      boostTypeIcon: powerup,
-      boostedItemIcon: CROP_LIFECYCLE.Eggplant.crop,
-    },
-    "Golden Spatula": {
-      shortDescription: "+10% XP",
-      labelType: "success",
-      boostTypeIcon: powerup,
-    },
-    "Mushroom Hat": {
-      shortDescription: "+0.1 Mushrooms",
-      boostTypeIcon: powerup,
-      labelType: "success",
-      boostedItemIcon: SUNNYSIDE.resource.wild_mushroom,
-    },
-    Parsnip: {
-      shortDescription: "+20% Parsnip",
-      labelType: "success",
-      boostTypeIcon: powerup,
-      boostedItemIcon: CROP_LIFECYCLE.Parsnip.crop,
-    },
-    "Sunflower Amulet": {
-      shortDescription: "+10% Sunflower",
-      labelType: "success",
-      boostTypeIcon: powerup,
-      boostedItemIcon: CROP_LIFECYCLE.Sunflower.crop,
-    },
-    "Carrot Amulet": {
-      shortDescription: "-20% Carrot growth time",
-      labelType: "info",
-      boostTypeIcon: SUNNYSIDE.icons.stopwatch,
-      boostedItemIcon: CROP_LIFECYCLE.Carrot.crop,
-    },
-    "Beetroot Amulet": {
-      shortDescription: "+20% Beetroot",
-      labelType: "success",
-      boostTypeIcon: powerup,
-      boostedItemIcon: CROP_LIFECYCLE.Beetroot.crop,
-    },
-    "Green Amulet": {
-      shortDescription: "Chance 10x Crops",
-      labelType: "vibrant",
-      boostTypeIcon: lightning,
-    },
-    "Luna's Hat": {
-      shortDescription: "-50% Cooking Time",
-      labelType: "info",
-      boostTypeIcon: SUNNYSIDE.icons.stopwatch,
-    },
-    "Infernal Pitchfork": {
-      shortDescription: "+3 Crops",
-      labelType: "success",
-      boostTypeIcon: powerup,
-    },
-    Cattlegrim: {
-      shortDescription: "+0.25 Animal Produce",
-      labelType: "success",
-      boostTypeIcon: powerup,
-    },
-    "Corn Onesie": {
-      shortDescription: "+0.1 Corn",
-      labelType: "success",
-      boostTypeIcon: powerup,
-      boostedItemIcon: CROP_LIFECYCLE.Corn.crop,
-    },
-    "Sunflower Rod": {
-      shortDescription: "Chance +1 Fish",
-      labelType: "vibrant",
-      boostTypeIcon: lightning,
-      boostedItemIcon: SUNNYSIDE.icons.fish,
-    },
-    Trident: {
-      shortDescription: "Chance +1 Fish",
-      labelType: "vibrant",
-      boostTypeIcon: lightning,
-      boostedItemIcon: SUNNYSIDE.icons.fish,
-    },
-    "Bucket O' Worms": {
-      shortDescription: "+1 Worm",
-      labelType: "success",
-      boostTypeIcon: powerup,
-    },
-    "Luminous Anglerfish Topper": {
-      shortDescription: "+50% Fish XP",
-      labelType: "success",
-      boostTypeIcon: powerup,
-      boostedItemIcon: SUNNYSIDE.icons.fish,
-    },
-    "Angler Waders": {
-      shortDescription: "+10 Fish Limit",
-      labelType: "success",
-      boostTypeIcon: powerup,
-      boostedItemIcon: SUNNYSIDE.icons.fish,
-    },
-  };
