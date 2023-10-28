8ecd055bc53b4213513726b1d3e93fc38f5bb08a
Adam Hannigan <adamhannigan81@gmail.com>
2023-10-28 11:39:01 +1100

    Ring bar improvements

---
+++ src/components/ui/SensitiveButton.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import classnames from "classnames";
+import { pixelLightBorderStyle } from "features/game/lib/style";
+
+interface Props {
+  onClick?: () => void;
+  disabled?: boolean;
+  className?: string;
+  type?: "button" | "submit" | undefined;
+}
+
+/**
+ * A button which is trigger on touch, not release
+ */
+export const SensitiveButton: React.FC<Props> = ({
+  children,
+  onClick,
+  disabled,
+  className,
+  type,
+}) => {
+  return (
+    <button
+      className={classnames(
+        "bg-brown-200 w-full p-1 text-xs object-contain justify-center items-center hover:bg-brown-300 cursor-pointer flex disabled:opacity-50 ",
+        className
+      )}
+      type={type}
+      disabled={disabled}
+      style={pixelLightBorderStyle}
+      // TODO only use one
+      onMouseDown={onClick}
+      onTouchStart={onClick}
+    >
+      <div className="mb-1">{children}</div>
+    </button>
+  );
+};
--- src/features/island/fisherman/KrakenMechanic.tsx
+++ src/features/island/fisherman/KrakenMechanic.tsx
@@ -1,16 +1,14 @@
-import tentacle from "assets/ui/tentacle.png";
 import kraken from "assets/ui/kraken.png";
+import ringDot from "assets/icons/fish_dot.png";
 
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { SUNNYSIDE } from "assets/sunnyside";
 import { FishingBait } from "features/game/types/fishing";
-import { Box } from "components/ui/Box";
-import { ITEM_DETAILS } from "features/game/types/images";
-import Decimal from "decimal.js-light";
-import { getKeys } from "features/game/types/craftables";
-import { CloseButtonPanel } from "features/game/components/CloseablePanel";
 import { Button } from "components/ui/Button";
-import { Bar } from "components/ui/ProgressBar";
+import { ResizableBar } from "components/ui/ProgressBar";
+import { Panel } from "components/ui/Panel";
+import { useSpring, animated } from "react-spring";
+import { SensitiveButton } from "components/ui/SensitiveButton";
 
 // Function to generate a random angle at least 150 degrees away from a given angle
 function getRandomAngle(minDistance: number, existingAngle: number) {
@@ -49,48 +47,110 @@ function getRotationAngle(element: HTMLElement): number | undefined {
   return angle;
 }
 
+const extractAngle = (transformString: string) => {
+  const match = transformString.match(/[-+]?\d*\.\d+|\d+/);
+  if (match && match[0]) {
+    return parseFloat(match[0]);
+  }
+  return 0; // Default value if the match fails
+};
+
+// Converts 354 -> 6
+const normaliseAngle = (angle: number) => {
+  return ((angle % 360) + 360) % 360;
+};
+
+type Attempt = "hit" | "miss";
+
+function getSpeed(attempts: number) {
+  if (attempts > 10) {
+    return 1500;
+  }
+
+  if (attempts > 7) {
+    return 2000;
+  }
+
+  if (attempts > 3) {
+    return 2500;
+  }
+
+  return 3000;
+}
 export const FishingBar: React.FC = () => {
   const [state, setState] = useState<"idle" | "playing" | "caught" | "escaped">(
     "idle"
   );
-  const [bait, setBait] = useState<FishingBait>("Earthworm");
 
   const [tentacleAngle, setTentacleAngle] = useState(getRandomAngle(150, 0));
-  const [misses, setMisses] = useState(0);
-  const [hits, setHits] = useState(0);
+  const [attempts, setAttempts] = useState<Attempt[]>([]);
 
-  const barHeight = BAIT_SIZE[bait];
+  const direction = useRef<"clockwise" | "anticlockwise">("clockwise");
+  const [castFrom, setCastFrom] = useState(0);
+  const [castTo, setCastTo] = useState(360);
+
+  const changeDirection = (fromDegrees: number) => {
+    const to =
+      direction.current === "clockwise" ? fromDegrees - 360 : fromDegrees + 360;
+    setCastTo(to);
+    setCastFrom(fromDegrees);
+    direction.current =
+      direction.current === "anticlockwise" ? "clockwise" : "anticlockwise";
+  };
+
+  const greenBarProps = useSpring({
+    from: { transform: `rotate(${castFrom}deg)` },
+    to: { transform: `rotate(${castTo}deg)` },
+    onRest: () => changeDirection(castTo),
+    config: {
+      duration: getSpeed(attempts.length),
+    },
+    pause: state === "idle",
+  });
+
+  const misses = attempts.filter((a) => a === "miss").length;
+  const hits = attempts.filter((a) => a === "hit").length;
+
+  const tentacleProps = useSpring({
+    from: { transform: `rotate(${tentacleAngle}deg)` },
+    to: { transform: `rotate(${tentacleAngle}deg)` },
+    config: {
+      duration: 3000,
+    },
+    pause: true,
+  });
+
+  const barHeight = 10;
 
   const reel = () => {
-    // Usage example:
-    const element = document.getElementById("fishing-bar") as HTMLElement;
-    const rotationAngle = getRotationAngle(element) as number;
+    const greenBarAngle = extractAngle(greenBarProps.transform.get());
+    const tentacleAngle = extractAngle(tentacleProps.transform.get());
 
-    const tentacleEl = document.getElementById("tentacle") as HTMLElement;
-    const tentacleAngle = getRotationAngle(tentacleEl) as number;
+    const degreeDifference = Math.abs(
+      normaliseAngle(greenBarAngle) - normaliseAngle(tentacleAngle)
+    );
 
-    const degreeDifference = Math.abs(rotationAngle - tentacleAngle);
+    console.log({ greenBarAngle, tentacleAngle, degreeDifference, barHeight });
 
-    const hit = degreeDifference < barHeight; //barStart < tentaclePosition && barEnd > tentaclePosition;
+    const fishBuffer = 5;
+    const hit = degreeDifference < barHeight + fishBuffer;
+
+    setAttempts((prev) => [...prev, hit ? "hit" : "miss"]);
 
     if (!hit) {
-      setMisses((prev) => prev + 1);
-
       if (misses === 2) {
         setState("escaped");
       }
     } else {
-      setHits((prev) => prev + 1);
-
       if (hits === 9) {
         setState("caught");
       }
     }
 
     setTentacleAngle((prev) => getRandomAngle(150, prev));
-  };
 
-  const speed = 10 - hits / 2;
+    changeDirection(greenBarAngle);
+  };
 
   if (state === "caught") {
     return <p>Caught</p>;
@@ -100,21 +160,25 @@ export const FishingBar: React.FC = () => {
     return <p>Escaped</p>;
   }
 
+  let castIndicatorColour = "#ffffff";
+  if (attempts.length > 0 && attempts[attempts.length - 1] === "hit") {
+    castIndicatorColour = "#63c74d";
+  } else if (attempts.length > 0 && attempts[attempts.length - 1] === "miss") {
+    castIndicatorColour = "#f6757a";
+  }
+
   return (
     <div className="flex flex-col">
-      <div className="flex flex-wrap mx-auto">
-        {getKeys(BAIT_SIZE).map((name) => (
-          <Box
-            count={new Decimal(1)}
-            key={name}
-            image={ITEM_DETAILS[name].image}
-            isSelected={name === bait}
-            onClick={() => setBait(name)}
-            disabled={state === "playing"}
-          />
-        ))}
+      <div className="flex w-full justify-center">
+        <ResizableBar
+          percentage={100 - (hits / 10) * 100}
+          type={"error"}
+          outerDimensions={{
+            width: 50,
+            height: 8,
+          }}
+        />
       </div>
-
       {/* Bar */}
       <div
         className="mx-auto my-2 relative pointer-events-none"
@@ -134,16 +198,17 @@ export const FishingBar: React.FC = () => {
             boxShadow: "inset 0 0 0 3px #fff", // Create an inset box shadow
           }}
         />
-        <svg
+        <animated.svg
           width="200"
           height="200"
           viewBox="0 0 200 200"
           id="fishing-bar"
           className="absolute inset-0 "
-          style={{
-            animation:
-              state === "playing" ? `spin ${speed}s linear infinite` : "none",
-          }}
+          style={greenBarProps}
+          // style={{
+          //   animation:
+          //     state === "playing" ? `spin ${speed}s linear infinite` : "none",
+          // }}
         >
           <circle
             cx="100"
@@ -164,9 +229,32 @@ export const FishingBar: React.FC = () => {
             strokeWidth="12"
             pathLength="100"
           />
+        </animated.svg>
+
+        <svg
+          width="200"
+          height="200"
+          viewBox="0 0 200 200"
+          className="absolute inset-0 "
+          style={{
+            transform: `rotate(${castFrom}deg)`,
+          }}
+        >
+          <circle
+            strokeDasharray={`${0.5} ${100 - 0.5}`}
+            strokeDashoffset={25 + 0.5 / 2}
+            cx="100"
+            cy="100"
+            r="95"
+            fill="none"
+            stroke={castIndicatorColour}
+            strokeWidth="12"
+            pathLength="100"
+            strokeOpacity={castIndicatorColour === "#ffffff" ? 0.6 : 1}
+          />
         </svg>
 
-        <div
+        <animated.div
           id="tentacle"
           style={{
             position: "absolute",
@@ -175,23 +263,24 @@ export const FishingBar: React.FC = () => {
             top: "0px",
             left: "90px",
             transformOrigin: "bottom center",
-            transform: `rotate(${tentacleAngle}deg)`,
+            // transform: `rotate(${tentacleAngle}deg)`,
+            ...tentacleProps,
           }}
         >
           <img
-            src={tentacle}
+            src={ringDot}
             style={{
               position: "absolute",
-              width: "16px",
-              top: "-2px",
+              width: "24px",
+              top: "1px",
               left: "2px",
+              rotate: "137deg",
             }}
           />
-        </div>
+        </animated.div>
 
         <div className="absolute inset-0 w-full h-full flex flex-col items-center justify-center z-50">
           <img src={kraken} width="80px" />
-          <Bar percentage={100 - (hits / 10) * 100} type={"error"} />
 
           <div className="flex justify-center items-center space-x-1 mt-1">
             {new Array(3).fill(null)?.map((_, index) => {
@@ -212,7 +301,9 @@ export const FishingBar: React.FC = () => {
         <Button onClick={() => setState("playing")}>Cast</Button>
       )}
 
-      {state === "playing" && <Button onClick={reel}>Reel</Button>}
+      {state === "playing" && (
+        <SensitiveButton onClick={reel}>Reel</SensitiveButton>
+      )}
     </div>
   );
 };
@@ -228,22 +319,9 @@ interface Props {
   onClose: () => void;
 }
 export const KrakenMechanic: React.FC<Props> = ({ onClose }) => {
-  const [tab, setTab] = useState(0);
-
   return (
-    <CloseButtonPanel
-      onClose={onClose}
-      tabs={[
-        { icon: SUNNYSIDE.icons.fish, name: "Kraken" },
-        {
-          icon: SUNNYSIDE.tools.fishing_rod,
-          name: "Guide",
-        },
-      ]}
-      currentTab={tab}
-      setCurrentTab={setTab}
-    >
+    <Panel>
       <FishingBar />
-    </CloseButtonPanel>
+    </Panel>
   );
 };
