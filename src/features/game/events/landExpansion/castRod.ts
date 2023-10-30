b17869d322ba9ad8faa647dede3a1db2af367951
Romy Furtado <25412194+Rofrtd@users.noreply.github.com>
2023-10-30 14:14:17 +1100

    [FEAT] Ancient Rod boost

--- src/features/game/events/landExpansion/castRod.ts
+++ src/features/game/events/landExpansion/castRod.ts
@@ -81,7 +81,9 @@ export function castRod({
   }
 
   // Subtracts Rod
-  game.inventory.Rod = rodCount.sub(1);
+  if (game.bumpkin.equipped.tool === "Ancient Rod") {
+    game.inventory.Rod = rodCount.sub(0);
+  } else game.inventory.Rod = rodCount.sub(1);
 
   // Subtracts Bait
   game.inventory[action.bait] = baitCount.sub(1);
