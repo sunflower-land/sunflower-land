2846914385e5683779a0ab5b92cd4c38a4f2a618
brian-man <36871683+brian-man@users.noreply.github.com>
2023-10-31 15:44:15 -0700

    [FIX] Add missing bait to wharf (#3053)
    * [FIX] Add missing bait to wharf
    
    * [FIX] Update Bait type
    
    ---------
    
    Co-authored-by: Craig <41215134+c-py@users.noreply.github.com>

--- src/features/game/types/game.ts
+++ src/features/game/types/game.ts
@@ -702,6 +702,7 @@ export type Fishing = {
   weather: FishingConditions;
   wharf: {
     castedAt?: number;
+    bait?: FishingBait;
     chum?: InventoryItemName;
     caught?: Partial<Record<InventoryItemName, number>>;
   };
