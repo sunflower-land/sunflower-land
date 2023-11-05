dd8ba371437fa77f8fa9d26dc33eac615df592af
brian-man <36871683+brian-man@users.noreply.github.com>
2023-11-05 14:02:08 -0800

    [FIX] Sort order of items for plaza trading (#3094)
    Co-authored-by: Adam Hannigan <adamhannigan81@gmail.com>

--- src/features/game/events/landExpansion/listTrade.ts
+++ src/features/game/events/landExpansion/listTrade.ts
@@ -17,11 +17,6 @@ type Options = {
 };
 
 export const TRADE_LIMITS: Partial<Record<InventoryItemName, number>> = {
-  Wood: 100,
-  Gold: 50,
-  Stone: 100,
-  Iron: 100,
-  Egg: 100,
   Sunflower: 1000,
   Potato: 1000,
   Pumpkin: 1000,
@@ -29,15 +24,20 @@ export const TRADE_LIMITS: Partial<Record<InventoryItemName, number>> = {
   Carrot: 500,
   Beetroot: 500,
   Cauliflower: 500,
-  Radish: 200,
-  Eggplant: 200,
-  Corn: 200,
   Parsnip: 200,
+  Eggplant: 200,
+  Radish: 200,
   Wheat: 200,
   Kale: 200,
-  Blueberry: 100,
-  Orange: 100,
+  Corn: 200,
   Apple: 100,
+  Orange: 100,
+  Blueberry: 100,
+  Wood: 100,
+  Stone: 100,
+  Iron: 100,
+  Gold: 50,
+  Egg: 100,
 };
 
 /**
