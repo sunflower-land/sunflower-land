0d5667d0357253f94e3b95294c95a775f84333c0
0xSacul <63975424+0xSacul@users.noreply.github.com>
2023-11-07 13:28:03 +0100

    Mayor, Text and UI

--- src/features/game/components/PlazaMayor.tsx
+++ src/features/game/components/PlazaMayor.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { GameState } from "../types/game";
 
+import { Button } from "components/ui/Button";
 import { Modal } from "react-bootstrap";
 import { CloseButtonPanel } from "./CloseablePanel";
 import { SpeakingModal } from "./SpeakingModal";
@@ -45,10 +46,27 @@ export const PlazaMayor: React.FC<PlazaMayorProps> = ({
                 bumpkinParts={NPC_WEARABLES.mayor}
                 message={[
                   {
-                    text: `Howdy ${gameState?.username}! Seems like we've already met. In case you forgot, I'm the Mayor of this town!`,
+                    text: "Howdy fellow Bumpkin I.. Don't seem to know yet? Let's meet!",
                   },
                   {
-                    text: "You might want to ask other people around to call you with another name? Unfortunately, I can't do that for you now, the paperwork is too much for me to handle..",
+                    text: "I'm the Mayor of this town! I'm in charge of making sure everyone is happy and has a place to live. I also make sure that everyone has a name!",
+                  },
+                  {
+                    text: "You don't have a name yet? Well, we can fix that! Do you want me to get the papers ready?",
+                    actions: [
+                      {
+                        text: "Yes please!",
+                        cb: () => {
+                          setTab(1);
+                        },
+                      },
+                      {
+                        text: "No thanks.",
+                        cb: () => {
+                          onClose();
+                        },
+                      },
+                    ],
                   },
                 ]}
               />
@@ -58,12 +76,23 @@ export const PlazaMayor: React.FC<PlazaMayorProps> = ({
 
         {tab === 1 && (
           <CloseButtonPanel onClose={onClose}>
-            {/* <div className="text-center">
-            <h1>Plaza Mayor</h1>
-            <Label type="danger" icon={ITEM_DETAILS["Beta Pass"].image}>
-              Beta Pass Required
-            </Label>
-          </div> */}
+            <div className="flex flex-col items-center">
+              <span>Enter your name:</span>
+              <input
+                type="string"
+                name="Username"
+                className="text-shadow shadow-inner shadow-black bg-brown-200 w-full p-2 m-2 text-center"
+              />
+              <Button
+                className="overflow-hidden"
+                type="submit"
+                onClick={() => {
+                  setTab(2);
+                }}
+              >
+                Submit
+              </Button>
+            </div>
           </CloseButtonPanel>
         )}
       </Modal>
