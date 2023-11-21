5a581223ab1ad0aa940514ba3efabb9ff8ed60bc
EliasGC <elias.chew1999@gmail.com>
2023-11-21 13:55:14 +0800

    Update Refreshing

--- src/features/auth/components/Refreshing.tsx
+++ src/features/auth/components/Refreshing.tsx
@@ -1,5 +1,7 @@
+import { useAppTranslation } from "lib/i18n/useAppTranslations";
 import React from "react";
 
 export const Refreshing: React.FC = () => {
-  return <span className="text-shadow loading">Refreshing</span>;
+  const { t } = useAppTranslation();
+  return <span className="text-shadow loading">{t("refreshing")}</span>;
 };
