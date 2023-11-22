8c34a54f2629c0d640a3c15dbdf0f8c6792733cc
EliasGC <elias.chew1999@gmail.com>
2023-11-22 12:32:11 +0800

    Temporarily Delete Portuguese Dictionary while working on terms

--- src/lib/i18n/dictionaries/dictionary.ts
+++ src/lib/i18n/dictionaries/dictionary.ts
@@ -1,8 +1,7 @@
 import { ENGLISH_TERMS } from "./englishDictionary";
-import { PORTUGUESE_TERMS } from "./portugueseDictionary";
 import { TranslationKeys } from "./types";
 
-export type LanguageCode = "en" | "pt";
+export type LanguageCode = "en";
 
 export type TranslationResource = Record<TranslationKeys, string>;
 
@@ -12,8 +11,5 @@ export const resources: Record<
 > = {
   en: {
     translation: ENGLISH_TERMS,
-  },
-  pt: {
-    translation: PORTUGUESE_TERMS,
   },
 };
