670d017bd21b3b8c79c7505819236256a6ad39db
EliasGC <elias.chew1999@gmail.com>
2023-11-22 22:29:32 +0800

    Add collectible name terms

--- src/features/auth/components/Offer.tsx
+++ src/features/auth/components/Offer.tsx
@@ -18,7 +18,7 @@ export const OfferItems: React.FC = () => {
           <div className="w-8">
             <img src={ITEM_DETAILS["Block Buck"].image} className="h-5 mr-2" />
           </div>
-          <p className="text-sm">10 {t("blockBucks")}</p>
+          <p className="text-sm">10 {t("block.buck")}</p>
         </div>
         <div className="flex mb-2 items-center w-1/2">
           <div className="w-8">
@@ -53,7 +53,7 @@ export const OfferItems: React.FC = () => {
         <div className="w-8">
           <img src={ITEM_DETAILS["Block Buck"].image} className="h-5 mr-2" />
         </div>
-        <p className="text-sm">5 {t("blockBucks")}</p>
+        <p className="text-sm">5 {t("block.buck")}</p>
       </div>
       <div className="flex mb-2 items-center w-1/2">
         <div className="w-8">
