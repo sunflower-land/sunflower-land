e54c8a6047342f43136bf78bc0dfb7eab693b560
EliasGC <elias.chew1999@gmail.com>
2023-11-23 08:39:25 +0800

    [FIX] Translate.ts error

--- src/lib/i18n/translate.ts
+++ src/lib/i18n/translate.ts
@@ -1,6 +1,7 @@
 import i18n from "lib/i18n";
 import { TOptions } from "i18next";
 import { TranslationKeys } from "./dictionaries/types";
+// added this unused import so that I can draft the PR
 
 export const translate = (key: TranslationKeys, options?: TOptions): string => {
   return i18n.t(key, options);
