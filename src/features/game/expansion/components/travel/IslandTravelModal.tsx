1fd19c619f8656366d9abbf8baa69e9443334d87
EliasGC <101262042+elias7905@users.noreply.github.com>
2023-11-24 04:38:08 +0000

    fix lint (hopefully)

--- src/features/game/expansion/components/travel/IslandTravelModal.tsx
+++ src/features/game/expansion/components/travel/IslandTravelModal.tsx
@@ -55,22 +55,22 @@ export const IslandTravelModal: React.FC<IslandTravelModalProps> = ({
     setShowTutorial(false);
   };
 
-// TEMP: Disable for conference
-//  if (getBumpkinLevel(bumpkin?.experience ?? 0) < 3) {
-//    return (
-//      <CloseButtonPanel onClose={onClose}>
-//        <div className="flex flex-col items-center">
-//          <Label className="mt-2" icon={lockIcon} type="danger">
-//            {t("warning.level.three.required")}
-//          </Label>
-//          <img src={worldIcon} className="w-10 mx-auto my-2" />
-//          <p className="text-sm text-center mb-2">
-//            {t("statements.visit.firePit")}
-//          </p>
-//        </div>
-//      </CloseButtonPanel>
-//    );
-//  }
+  // TEMP: Disable for conference
+  //  if (getBumpkinLevel(bumpkin?.experience ?? 0) < 3) {
+  //    return (
+  //      <CloseButtonPanel onClose={onClose}>
+  //        <div className="flex flex-col items-center">
+  //          <Label className="mt-2" icon={lockIcon} type="danger">
+  //            {t("warning.level.three.required")}
+  //          </Label>
+  //          <img src={worldIcon} className="w-10 mx-auto my-2" />
+  //          <p className="text-sm text-center mb-2">
+  //            {t("statements.visit.firePit")}
+  //          </p>
+  //        </div>
+  //      </CloseButtonPanel>
+  //    );
+  //  }
 
   if (showTutorial) {
     return (
