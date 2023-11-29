a2d0188494a4ef5ad29003838c61713bae66137f
Micsk10 <46845534+Micsk10@users.noreply.github.com>
2023-11-28 19:26:39 -0500

    [CHORE] Code source reorganization for improved readability. (#3198)
    * up
    
    * up
    
    * up
    
    * up
    
    * up
    
    * fix lint
    
    * fix consumables
    
    * remove placeholders comment
    
    * up
    
    * up
    
    * up
    
    * fix lint
    
    * fix consumables
    
    ---------
    
    Co-authored-by: EliasGC <elias.chew1999@gmail.com>

--- src/features/game/types/collectibles.ts
+++ src/features/game/types/collectibles.ts
@@ -106,96 +106,96 @@ export const HELIOS_BLACKSMITH_ITEMS: (
 ) => ({
   "Basic Scarecrow": {
     description: "Choosy defender of your farm's VIP (Very Important Plants)",
+    boost: "20% faster Sunflowers, Potatoes and Pumpkins",
+    sfl: new Decimal(0),
     ingredients: {
       Wood: new Decimal(2),
     },
-    boost: "20% faster Sunflowers, Potatoes and Pumpkins",
-    sfl: new Decimal(0),
   },
   "Scary Mike": {
     description:
       "The veggie whisperer and champion of frightfully good harvests!",
+    boost:
+      "+0.2 yield on Carrots, Cabbages, Beetroots, Cauliflowers and Parsnips",
+    sfl: new Decimal(15),
     ingredients: {
       Wood: new Decimal(30),
       Carrot: new Decimal(50),
       Wheat: new Decimal(10),
       Parsnip: new Decimal(10),
     },
-    sfl: new Decimal(15),
-    boost:
-      "+0.2 yield on Carrots, Cabbages, Beetroots, Cauliflowers and Parsnips",
   },
   "Laurie the Chuckle Crow": {
     description:
       "With her disconcerting chuckle, she shooes peckers away from your crops!",
+    boost: "+0.2 yield on Eggplants, Corn, Radishes, Wheat and Kale",
+    sfl: new Decimal(45),
     ingredients: {
       Wood: new Decimal(100),
       Radish: new Decimal(60),
       Kale: new Decimal(40),
       Wheat: new Decimal(20),
     },
-    sfl: new Decimal(45),
-    boost: "+0.2 yield on Eggplants, Corn, Radishes, Wheat and Kale",
   },
   Bale: {
     description:
       "A poultry's favorite neighbor, providing a cozy retreat for chickens",
+    boost: "Adjacent chickens produce +0.2 Eggs",
+    sfl: new Decimal(5),
     ingredients: {
       Egg: new Decimal(200),
       Wheat: new Decimal(200),
       Wood: new Decimal(100),
       Stone: new Decimal(30),
     },
-    sfl: new Decimal(5),
-    boost: "Adjacent chickens produce +0.2 Eggs",
   },
   "Immortal Pear": {
     description: "A long-lived pear that makes fruit trees last longer.",
+    boost: "+1 Harvest",
     ingredients: {
       Gold: new Decimal(5),
       Apple: new Decimal(10),
       Blueberry: new Decimal(10),
       Orange: new Decimal(10),
     },
-    boost: "+1 Harvest",
   },
   "Treasure Map": {
     description: "X marks the spot!",
+    boost: "+20% SFL on Treasure Bounty",
     ingredients: {
       Gold: new Decimal(5),
       "Wooden Compass": new Decimal(2),
     },
-    boost: "+20% SFL on Treasure Bounty",
   },
   ...(getCurrentSeason(date) === "Witches' Eve" && {
     Poppy: {
       description: "The mystical corn kernel.",
+      boost: "+0.1 Corn",
       ingredients: {
         Gold: new Decimal(5),
         "Crow Feather": new Decimal(250),
       },
-      boost: "+0.1 Corn",
       from: new Date("2023-08-01"),
       to: new Date("2023-09-01"),
     },
     Kernaldo: {
       description: "The magical corn whisperer.",
+      boost: "+25% Corn Speed",
+      sfl: SFLDiscount(state, new Decimal(50)),
       ingredients: {
         "Crow Feather": new Decimal(500),
       },
-      sfl: SFLDiscount(state, new Decimal(50)),
-      boost: "+25% Corn Speed",
       from: new Date("2023-09-01"),
       to: new Date("2023-10-01"),
     },
     "Grain Grinder": {
       description:
         "Grind your grain and experience a delectable surge in Cake XP.",
+      boost: "+20% Cake XP",
+      sfl: SFLDiscount(state, new Decimal(100)),
       ingredients: {
         "Crow Feather": new Decimal(750),
       },
-      sfl: SFLDiscount(state, new Decimal(100)),
-      boost: "+20% Cake XP",
       from: new Date("2023-10-01"),
       to: new Date("2023-11-01"),
     },
@@ -204,33 +204,33 @@ export const HELIOS_BLACKSMITH_ITEMS: (
     Nana: {
       description:
         "This rare beauty is a surefire way to boost your banana harvests.",
+      boost: "+10% Banana Speed",
+      sfl: SFLDiscount(state, new Decimal(50)),
       ingredients: {
         "Mermaid Scale": new Decimal(350),
       },
-      sfl: SFLDiscount(state, new Decimal(50)),
-      boost: "+10% Banana Speed",
       from: new Date("2023-11-01"),
       to: new Date("2023-12-01"),
     },
     "Soil Krabby": {
       description:
         "Speedy sifting with a smile! Enjoy a 10% composter speed boost with this crustaceous champ.",
+      boost: "+10% Composter Speed",
+      sfl: SFLDiscount(state, new Decimal(65)),
       ingredients: {
         "Mermaid Scale": new Decimal(650),
       },
-      sfl: SFLDiscount(state, new Decimal(65)),
-      boost: "+10% Composter Speed",
       from: new Date("2023-12-01"),
       to: new Date("2024-01-01"),
     },
     "Skill Shrimpy": {
       description:
         "Shrimpy's here to help! He'll ensure you get that extra XP from fish.",
+      boost: "+20% Fish XP",
+      sfl: SFLDiscount(state, new Decimal(115)),
       ingredients: {
         "Mermaid Scale": new Decimal(865),
       },
-      sfl: SFLDiscount(state, new Decimal(115)),
-      boost: "+20% Fish XP",
       from: new Date("2024-01-01"),
       to: new Date("2024-02-01"),
     },
@@ -254,76 +254,78 @@ export const GOBLIN_PIRATE_ITEMS: Record<
 > = {
   "Iron Idol": {
     description: "The Idol adds 1 iron every time you mine iron.",
+    boost: "+1 Iron",
+    supply: 200,
     ingredients: {
       Gold: new Decimal(10),
       Starfish: new Decimal(40),
       Pearl: new Decimal(1),
     },
-    supply: 200,
-    boost: "+1 Iron",
   },
   "Emerald Turtle": {
     description:
       "The Emerald Turtle gives +0.5 to any minerals you mine within its Area of Effect.",
+    boost: "+0.5 AoE Any Minerals",
+    sfl: new Decimal(100),
+    supply: 100,
     ingredients: {
       "Iron Compass": new Decimal(30),
       "Old Bottle": new Decimal(80),
       Seaweed: new Decimal(50),
       "Block Buck": new Decimal(1),
     },
-    sfl: new Decimal(100),
-    supply: 100,
   },
   "Tin Turtle": {
     description:
       "The Tin Turtle gives +0.1 to Stones you mine within its Area of Effect.",
+    boost: "+0.1 AoE Stones",
+    sfl: new Decimal(40),
+    supply: 3000,
     ingredients: {
       "Iron Compass": new Decimal(15),
       "Old Bottle": new Decimal(50),
       Seaweed: new Decimal(25),
       "Block Buck": new Decimal(1),
     },
-    sfl: new Decimal(40),
-    supply: 3000,
   },
   "Heart of Davy Jones": {
     description:
       "Whoever possesses it holds immense power over the seven seas, can dig for treasure without tiring",
+    boost: "Dig an extra 20 times per day",
+    supply: 1000,
     ingredients: {
       Gold: new Decimal(10),
       "Wooden Compass": new Decimal(6),
     },
-    supply: 1000,
-    boost: "Dig an extra 20 times per day",
   },
   Karkinos: {
     description:
       "Pinchy but kind, the crabby cabbage-boosting addition to your farm!",
+    boost: "+0.1 Cabbage",
+    supply: 7500,
     ingredients: {
       Crab: new Decimal(5),
       Cabbage: new Decimal(500),
       Gold: new Decimal(3),
       "Solar Flare Ticket": new Decimal(350),
     },
-    supply: 7500,
-    boost: "+0.1 Cabbage",
     disabled: true,
   },
   "Parasaur Skull": {
     description: "A skull from a parasaur!",
+    supply: 1000,
     ingredients: {
       "Emerald Compass": new Decimal(20),
       "Block Buck": new Decimal(1),
     },
-    supply: 1000,
   },
   "Golden Bear Head": {
     description: "Spooky, but cool.",
+    supply: 200,
     ingredients: {
       "Emerald Compass": new Decimal(60),
       "Block Buck": new Decimal(1),
     },
-    supply: 200,
   },
 };
 
@@ -334,54 +336,52 @@ export const GOBLIN_BLACKSMITH_ITEMS: (
     "Mushroom House": {
       description:
         "A whimsical, fungi-abode where the walls sprout with charm and even the furniture has a 'spore-tacular' flair!",
-      ingredients: {
-        "Wild Mushroom": new Decimal(50),
-        Gold: new Decimal(10),
-      },
       // 50 Team supply + giveaways
       supply: 2000 + 50,
       sfl: SFLDiscount(state, new Decimal(50)),
       boost: "+0.2 Wild Mushroom",
+      ingredients: {
+        "Wild Mushroom": new Decimal(50),
+        Gold: new Decimal(10),
+      },
       // only available when SEASONS["DAWN_BREAKER"] starts
       disabled: getCurrentSeason() !== "Dawn Breaker",
     },
     Maximus: {
       description: "Squash the competition with plump Maximus",
-      // Placeholders
+      // 50 Team Supply + giveaways
+      supply: 350 + 50,
+      sfl: SFLDiscount(state, marketRate(20000)),
+      boost: "+1 Eggplant",
       ingredients: {
         Eggplant: new Decimal(100),
         "Dawn Breaker Ticket": new Decimal(3200),
       },
-      sfl: SFLDiscount(state, marketRate(20000)),
-      // 50 Team Supply + giveaways
-      supply: 350 + 50,
-      boost: "+1 Eggplant",
       disabled: getCurrentSeason() !== "Dawn Breaker",
     },
     Obie: {
       description: "A fierce eggplant soldier",
-      // Placeholders
+      // 100 Team Supply + Giveaways
+      supply: 2500 + 100,
+      sfl: SFLDiscount(state, marketRate(2000)),
+      boost: "25% faster eggplants",
       ingredients: {
         Eggplant: new Decimal(150),
         "Dawn Breaker Ticket": new Decimal(1200),
       },
-      sfl: SFLDiscount(state, marketRate(2000)),
-      // 100 Team Supply + Giveaways
-      supply: 2500 + 100,
-      boost: "25% faster eggplants",
       disabled: getCurrentSeason() !== "Dawn Breaker",
     },
     "Purple Trail": {
       description:
         "Leave your opponents in a trail of envy with the mesmerizing and unique Purple Trail",
-      // Placeholders
+
+      sfl: SFLDiscount(state, marketRate(800)),
+      supply: 10000,
+      boost: "+0.2 Eggplant",
       ingredients: {
         Eggplant: new Decimal(25),
         "Dawn Breaker Ticket": new Decimal(500),
       },
-      sfl: SFLDiscount(state, marketRate(800)),
-      supply: 10000,
-      boost: "+0.2 Eggplant",
       disabled: getCurrentSeason() !== "Dawn Breaker",
     },
   };
