4e035479f7fc263bde2179e96b3d7d74549a2e6c
spencer <spencer@sunflower-land.com>
2023-12-18 11:18:38 +1100

    [CHORE] Revert packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
+import { PortalApp } from "features/portal/Portal";
+import { CONFIG } from "lib/config";
 
 // TODO - tree shaking to minimise bundle size
-// if (CONFIG.PORTAL_APP) {
-//   ReactDOM.render(
-//     <React.StrictMode>
-//       <PortalApp />
-//     </React.StrictMode>,
-//     document.getElementById("root")
-//   );
-// } else {
-// Main Game
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-// }
+if (CONFIG.PORTAL_APP) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <PortalApp />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+} else {
+  // Main Game
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
