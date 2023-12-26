269388be0fc841deb4bdfa0c1f53d33015a90625
Adam Hannigan <adamhannigan81@gmail.com>
2023-12-27 09:27:31 +1100

    Home

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 
 import tent from "assets/land/tent_inside.png";
-import { GRID_WIDTH_PX, PIXEL_SCALE } from "features/game/lib/constants";
+import { GRID_WIDTH_PX } from "features/game/lib/constants";
+import { Hud } from "features/island/hud/Hud";
 
 export const Home: React.FC = () => {
   return (
-    <img
-      src={tent}
-      style={{
-        width: `${12 * GRID_WIDTH_PX}px`,
-        height: `${12 * GRID_WIDTH_PX}px`,
-      }}
-    />
+    <>
+      <Hud isFarming />
+      <div className="absolute inset-0 bg-black">
+        <img
+          src={tent}
+          style={{
+            width: `${12 * GRID_WIDTH_PX}px`,
+            height: `${12 * GRID_WIDTH_PX}px`,
+          }}
+        />
+      </div>
+    </>
   );
 };
