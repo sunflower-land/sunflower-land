0043051b13786fceadfccded820fd59897858464
Adam Hannigan <adamhannigan81@gmail.com>
2024-01-01 15:17:38 +1100

    Is wearable active

---
+++ src/features/game/lib/wearables.ts
@@ -0,0 +1,20 @@
+import { BumpkinItem } from "../types/bumpkin";
+import { GameState } from "../types/game";
+
+export function isWearableActive({
+  game,
+  name,
+}: {
+  game: GameState;
+  name: BumpkinItem;
+}) {
+  const equipped = [
+    ...Object.values(game.bumpkin?.equipped ?? {}),
+    ...Object.values(game.farmHands).reduce(
+      (acc, f) => [...acc, ...Object.values(f.equipped)],
+      [] as BumpkinItem[]
+    ),
+  ];
+
+  return equipped.includes(name);
+}
