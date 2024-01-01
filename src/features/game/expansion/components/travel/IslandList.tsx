74eb2bc4b1df9a924825410f46db0c91da5745a3
Adam Hannigan <adamhannigan81@gmail.com>
2024-01-02 09:56:46 +1100

    [FIX] Save while travelling

--- src/features/game/expansion/components/travel/IslandList.tsx
+++ src/features/game/expansion/components/travel/IslandList.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useSelector } from "@xstate/react";
 import classNames from "classnames";
@@ -177,6 +177,10 @@ export const IslandList: React.FC<IslandListProps> = ({
 
   const location = useLocation();
   const [view, setView] = useState<"list" | "visitForm">("list");
+
+  useEffect(() => {
+    gameService.send("SAVE");
+  }, []);
 
   const islands: Island[] = [
     {
