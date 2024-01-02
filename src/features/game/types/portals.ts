ebeee949ae63a89ad05f387c609282a3c8546bea
Adam Hannigan <adamhannigan81@gmail.com>
2024-01-02 12:24:03 +1100

    [CHORE] Bumpkin fight club

--- src/features/game/types/portals.ts
+++ src/features/game/types/portals.ts
@@ -1,9 +1,13 @@
-export type PortalName = "crop-boom";
+export type PortalName = "crop-boom" | "bumpkin-fight-club";
 
-export const SUPPORTED_PORTALS: PortalName[] = ["crop-boom"];
+export const SUPPORTED_PORTALS: PortalName[] = [
+  "crop-boom",
+  "bumpkin-fight-club",
+];
 
 export const MAX_TOTAL_ARCADE_TOKENS = 50;
 
 export const DAILY_ARCADE_TOKENS: Record<PortalName, number> = {
   "crop-boom": 1,
+  "bumpkin-fight-club": 1,
 };
