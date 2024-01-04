5184894c71135899a017accfcd21d290faa01b75
spencer <spencer@sunflower-land.com>
2024-01-04 12:30:53 +1100

    [FEAT] Add offline fallback

--- public/sw.ts
+++ public/sw.ts
@@ -2,27 +2,10 @@ import { CONFIG } from "../src/lib/config";
 
 const OFFLINE_VERSION = CONFIG.RELEASE_VERSION;
 
-import {
-  cleanupOutdatedCaches,
-  createHandlerBoundToURL,
-  precacheAndRoute,
-} from "workbox-precaching";
-import { clientsClaim } from "workbox-core";
-import { NavigationRoute, registerRoute } from "workbox-routing";
+import { offlineFallback } from "workbox-recipes";
+import { setDefaultHandler } from "workbox-routing";
+import { NetworkOnly } from "workbox-strategies";
 
-declare let self: ServiceWorkerGlobalScope;
+setDefaultHandler(new NetworkOnly());
 
-// self.__WB_MANIFEST is default injection point
-precacheAndRoute(self.__WB_MANIFEST);
-
-// clean old assets
-cleanupOutdatedCaches();
-
-registerRoute(
-  new NavigationRoute(createHandlerBoundToURL("offline.html"), {
-    allowlist: [/^\/offline$/],
-  })
-);
-
-(self as any).skipWaiting();
-clientsClaim();
+offlineFallback();
