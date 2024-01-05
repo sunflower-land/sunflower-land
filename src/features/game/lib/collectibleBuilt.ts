420cfacc210b803b96f7fcdb5020d7a3d5e931b6
Adam Hannigan <adamhannigan81@gmail.com>
2024-01-05 13:47:16 +1100

    [FEAT] Collectible built tests

--- src/features/game/lib/collectibleBuilt.ts
+++ src/features/game/lib/collectibleBuilt.ts
@@ -16,9 +16,12 @@ export function isCollectibleBuilt({
     game.home.collectibles[name] &&
     game.home.collectibles[name]?.some((placed) => placed.readyAt < Date.now());
 
-  return placedOnFarm || placedInHome;
+  return !!placedOnFarm || !!placedInHome;
 }
 
+const COOLDOWNS: Partial<Record<CollectibleName, number>> = {
+  "Time Warp Totem": 2 * 60 * 60 * 1000,
+};
 /**
  * Useful for collectibles which expire after X time
  * Currently we only support Time Warp Totem
@@ -30,12 +33,19 @@ export function isCollectibleActive({
   name: CollectibleName;
   game: GameState;
 }) {
-  if (!game.collectibles[name]) {
-    return false;
-  }
+  const cooldown = COOLDOWNS[name] ?? 0;
 
-  // Expires after 2 hours
-  return game.collectibles[name]?.some(
-    (placed) => placed.createdAt + 2 * 60 * 60 * 1000 > Date.now()
-  );
+  const placedOnFarm =
+    game.collectibles[name] &&
+    game.collectibles[name]?.some(
+      (placed) => placed.createdAt + cooldown > Date.now()
+    );
+
+  const placedInHome =
+    game.home.collectibles[name] &&
+    game.home.collectibles[name]?.some(
+      (placed) => placed.createdAt + cooldown > Date.now()
+    );
+
+  return !!placedOnFarm || !!placedInHome;
 }
