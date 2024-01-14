6c2a95e88b3e911bb540e6718e6f083ffe06aa57
spencer <spencer@sunflower-land.com>
2024-01-14 21:21:34 +1100

    [CHORE] Manually add quinn changes

--- vite.config.ts
+++ vite.config.ts
@@ -22,13 +22,16 @@ export default defineConfig({
       },
       srcDir: "src",
       strategies: "injectManifest",
+      includeManifestIcons: false,
+      includeAssets: ["**/*.{ico,gif,jpg,jpeg,png,webp,json,zip,xml}"],
       injectManifest: {
-        injectionPoint: undefined,
+        maximumFileSizeToCacheInBytes: 15000000,
+        globPatterns: ["**/*.{js,css,html}", "assets/*.{jpg,mp3,svg,gif,png}"],
       },
       filename: "sw.ts",
-      workbox: {
-        navigateFallback: "offline.html",
-      },
+      // workbox: {
+      //   navigateFallback: "offline.html",
+      // },
       manifest: {
         name: "Sunflower Land",
         id: "com.sunflower-land",
@@ -42,25 +45,23 @@ export default defineConfig({
         orientation: "portrait",
         icons: [
           {
-            src: "pwa/icons/manifest-icon-192.maskable.png",
-            sizes: "192x192",
+            src: "pwa/icons/pwa-64x64.png",
+            sizes: "64x64",
             type: "image/png",
             purpose: "any",
           },
           {
-            src: "pwa/icons/manifest-icon-192.maskable.png",
+            src: "pwa/icons/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
-            purpose: "maskable",
           },
           {
-            src: "pwa/icons/manifest-icon-512.maskable.png",
+            src: "pwa/icons/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "any",
           },
           {
-            src: "pwa/icons/manifest-icon-512.maskable.png",
+            src: "pwa/icons/maskable-icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable",
@@ -103,12 +104,5 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1000,
     assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          phaser: ["phaser"],
-        },
-      },
-    },
   },
 });
