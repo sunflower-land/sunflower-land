a43e89ca20619e2ae31bd9240e7091005ef9600f
Adam Hannigan <adamhannigan81@gmail.com>
2024-01-17 11:53:12 +1100

    Fix lints

--- src/features/game/lib/landDataStatic.ts
+++ src/features/game/lib/landDataStatic.ts
@@ -2,11 +2,7 @@ import Decimal from "decimal.js-light";
 import { GameState, Inventory } from "../types/game";
 import { BumpkinLevel } from "features/game/lib/level";
 import { getEnabledNodeCount } from "../expansion/lib/expansionNodes";
-import {
-  INITIAL_BUMPKIN,
-  INITIAL_BUMPKIN_LEVEL,
-  INITIAL_EXPANSIONS,
-} from "./bumpkinData";
+import { INITIAL_BUMPKIN, INITIAL_BUMPKIN_LEVEL } from "./bumpkinData";
 
 export const INITIAL_RESOURCES: Pick<
   GameState,
