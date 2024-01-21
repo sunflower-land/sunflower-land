87ccccf977917b0b16813cdca8bd100c5887abf3
Micsk10 <46845534+Micsk10@users.noreply.github.com>
2024-01-21 05:53:28 -0500

    Translate SessionExpired

--- src/features/auth/components/SessionExpired.tsx
+++ src/features/auth/components/SessionExpired.tsx
@@ -18,10 +18,10 @@ export const SessionExpired: React.FC = () => {
         <div className="flex mb-3 items-center ml-8">
           <img src={humanDeath} alt="Warning" className="w-full" />
         </div>
-        <p className="text-center mb-3">Session expired!</p>
+        <p className="text-center mb-3">{t("session.expired")}</p>
 
         <p className="text-center mb-4 text-xs">
-          {`It looks like your session has expired. Please refresh the page to continue playing.`}
+          {`${t("session.expiredMessage")}`}
         </p>
       </div>
       <Button
