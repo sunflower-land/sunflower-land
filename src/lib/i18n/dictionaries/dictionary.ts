51d44e1d4138b31458362a3794bec7e3e2cf3dec
EliasGC <elias.chew1999@gmail.com>
2024-01-22 16:35:58 +0000

    Temporarily remove french and Portuguese dictionaries

--- src/lib/i18n/dictionaries/dictionary.ts
+++ src/lib/i18n/dictionaries/dictionary.ts
@@ -1,9 +1,9 @@
 import { ENGLISH_TERMS } from "./englishDictionary";
-import { PORTUGUESE_TERMS } from "./portugueseDictionary";
-import { FRENCH_TERMS } from "./frenchDictionary";
+// import { PORTUGUESE_TERMS } from "./portugueseDictionary";
+// import { FRENCH_TERMS } from "./frenchDictionary";
 import { TranslationKeys } from "./types";
 
-export type LanguageCode = "en" | "fr" | "pt";
+export type LanguageCode = "en"; //| "fr" | "pt";
 
 export type TranslationResource = Record<TranslationKeys, string>;
 
@@ -14,10 +14,10 @@ export const resources: Record<
   en: {
     translation: ENGLISH_TERMS,
   },
-  pt: {
-    translation: PORTUGUESE_TERMS,
-  },
-  fr: {
-    translation: FRENCH_TERMS,
-  },
+  // pt: {
+  //   translation: PORTUGUESE_TERMS,
+  // },
+  // fr: {
+  //   translation: FRENCH_TERMS,
+  // },
 };
