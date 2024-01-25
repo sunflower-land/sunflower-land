a341a469a562cd9ac25c057c4660f18f58e105b8
Craig <41215134+c-py@users.noreply.github.com>
2024-01-25 11:16:14 +1100

    [FEAT] Clean up

--- src/features/game/types/flowers.ts
+++ src/features/game/types/flowers.ts
@@ -2,9 +2,17 @@ import Decimal from "decimal.js-light";
 import { InventoryItemName } from "./game";
 import { getKeys } from "./craftables";
 
-type SunpetalFlowerName = "Flower 1" | "Flower 2";
-type BloomFlowerName = "Flower 3";
-type LilyFlowerName = "Flower 4";
+type PansyName = "Red Pansy" | "Yellow Pansy" | "Purple Pansy";
+type CosmosName = "Red Cosmos" | "Yellow Cosmos" | "Purple Cosmos";
+type DaffodilName = "Red Daffodil" | "Yellow Daffodil" | "Purple Daffodil";
+type BalloonFlowerName =
+  | "Red Balloon Flower"
+  | "Yellow Balloon Flower"
+  | "Purple Balloon Flower";
+type LotusName = "Red Lotus" | "Yellow Lotus" | "Purple Lotus";
+type SunpetalFlowerName = PansyName | CosmosName;
+type BloomFlowerName = DaffodilName | BalloonFlowerName;
+type LilyFlowerName = LotusName;
 
 export type FlowerSeed = {
   price: Decimal;
@@ -73,14 +81,25 @@ type Flower = {
 
 const SUNPETAL_FLOWERS: Record<SunpetalFlowerName, { seed: "Sunpetal Seed" }> =
   {
-    "Flower 1": { seed: "Sunpetal Seed" },
-    "Flower 2": { seed: "Sunpetal Seed" },
+    "Red Pansy": { seed: "Sunpetal Seed" },
+    "Yellow Pansy": { seed: "Sunpetal Seed" },
+    "Purple Pansy": { seed: "Sunpetal Seed" },
+    "Red Cosmos": { seed: "Sunpetal Seed" },
+    "Yellow Cosmos": { seed: "Sunpetal Seed" },
+    "Purple Cosmos": { seed: "Sunpetal Seed" },
   };
 const BLOOM_FLOWERS: Record<BloomFlowerName, { seed: "Bloom Seed" }> = {
-  "Flower 3": { seed: "Bloom Seed" },
+  "Red Daffodil": { seed: "Bloom Seed" },
+  "Yellow Daffodil": { seed: "Bloom Seed" },
+  "Purple Daffodil": { seed: "Bloom Seed" },
+  "Red Balloon Flower": { seed: "Bloom Seed" },
+  "Yellow Balloon Flower": { seed: "Bloom Seed" },
+  "Purple Balloon Flower": { seed: "Bloom Seed" },
 };
 const LILY_FLOWERS: Record<LilyFlowerName, { seed: "Lily Seed" }> = {
-  "Flower 4": { seed: "Lily Seed" },
+  "Red Lotus": { seed: "Lily Seed" },
+  "Yellow Lotus": { seed: "Lily Seed" },
+  "Purple Lotus": { seed: "Lily Seed" },
 };
 
 export const FLOWERS: Record<FlowerName, Flower> = {
