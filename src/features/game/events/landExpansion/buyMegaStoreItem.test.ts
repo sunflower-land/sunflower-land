32424f243b909b235af2fd080d2b67cdf5b454ed
spencer <spencer@sunflower-land.com>
2024-01-28 15:31:51 +1100

    [FEAT] Add buyMegaStore item event

---
+++ src/features/game/events/landExpansion/buyMegaStoreItem.test.ts
@@ -0,0 +1,301 @@
+import { TEST_FARM } from "features/game/lib/constants";
+import { buyMegaStoreItem } from "./buyMegaStoreItem";
+import Decimal from "decimal.js-light";
+import { getSeasonalTicket } from "features/game/types/seasons";
+
+describe("buyMegaStoreItem", () => {
+  beforeEach(() => jest.useFakeTimers());
+  afterEach(() => jest.clearAllTimers());
+
+  it("throws an error if there is no bumpkin", () => {
+    expect(() =>
+      buyMegaStoreItem({
+        state: {
+          ...TEST_FARM,
+          bumpkin: undefined,
+        },
+        action: {
+          type: "megastoreItem.bought",
+          name: "Devil Wings",
+        },
+      })
+    ).toThrow("Bumpkin not found");
+  });
+
+  it("throws an error if the item is not available", () => {
+    expect(() =>
+      buyMegaStoreItem({
+        state: {
+          ...TEST_FARM,
+          megastore: {
+            ...TEST_FARM.megastore,
+            collectibles: [],
+            wearables: [],
+          },
+        },
+        action: {
+          type: "megastoreItem.bought",
+          name: "Soil Krabby",
+        },
+      })
+    ).toThrow("This item is not available");
+  });
+
+  it("throws an error if the players doesn't have enough SFL for and SFL item", () => {
+    expect(() =>
+      buyMegaStoreItem({
+        state: {
+          ...TEST_FARM,
+          balance: new Decimal(0),
+          megastore: {
+            ...TEST_FARM.megastore,
+            collectibles: [
+              {
+                name: "Soil Krabby",
+                price: new Decimal(100),
+                currency: "SFL",
+                type: "collectible",
+                shortDescription: "Lorem ipsum",
+              },
+            ],
+            wearables: [],
+          },
+        },
+        action: {
+          type: "megastoreItem.bought",
+          name: "Soil Krabby",
+        },
+      })
+    ).toThrow("Not enough SFL");
+  });
+
+  it("throws an error if the players doesn't enough seasonal tickets for a seasonal ticket item", () => {
+    const now = new Date("2024-01-02").getTime();
+    jest.setSystemTime(now);
+
+    const seasonTicket = getSeasonalTicket();
+
+    expect(() =>
+      buyMegaStoreItem({
+        state: {
+          ...TEST_FARM,
+          inventory: {
+            [seasonTicket]: new Decimal(0),
+          },
+          megastore: {
+            ...TEST_FARM.megastore,
+            wearables: [
+              {
+                name: "Tiki Mask",
+                price: new Decimal(100),
+                currency: "Seasonal Ticket",
+                type: "wearable",
+                shortDescription: "Lorem ipsum",
+              },
+            ],
+          },
+        },
+        action: {
+          type: "megastoreItem.bought",
+          name: "Tiki Mask",
+        },
+      })
+    ).toThrow(`Not enough ${seasonTicket}`);
+  });
+
+  it("throws an error if the players doesn't have inventory items for a non SFL item", () => {
+    expect(() =>
+      buyMegaStoreItem({
+        state: {
+          ...TEST_FARM,
+          inventory: {
+            Crimstone: new Decimal(5),
+          },
+          megastore: {
+            ...TEST_FARM.megastore,
+            collectibles: [
+              {
+                name: "Knowledge Crab",
+                price: new Decimal(100),
+                currency: "Crimstone",
+                type: "collectible",
+                shortDescription: "Lorem ipsum",
+              },
+            ],
+          },
+        },
+        action: {
+          type: "megastoreItem.bought",
+          name: "Knowledge Crab",
+        },
+      })
+    ).toThrow(`Not enough Crimstone`);
+  });
+
+  it("[SFL Wearable] subtracts the price from SFL balance and add item to wardrobe", () => {
+    const { balance, wardrobe } = buyMegaStoreItem({
+      state: {
+        ...TEST_FARM,
+        balance: new Decimal(1000),
+        megastore: {
+          ...TEST_FARM.megastore,
+          wearables: [
+            {
+              name: "Fish Pro Vest",
+              price: new Decimal(1000),
+              currency: "SFL",
+              type: "wearable",
+              shortDescription: "Lorem ipsum",
+            },
+          ],
+        },
+      },
+      action: {
+        type: "megastoreItem.bought",
+        name: "Fish Pro Vest",
+      },
+    });
+
+    expect(balance).toEqual(new Decimal(0));
+    expect(wardrobe["Fish Pro Vest"]).toEqual(1);
+  });
+
+  it("[SFL Collectible] subtracts the price from SFL balance and add item to inventory", () => {
+    const { balance, inventory } = buyMegaStoreItem({
+      state: {
+        ...TEST_FARM,
+        balance: new Decimal(1000),
+        megastore: {
+          ...TEST_FARM.megastore,
+          collectibles: [
+            {
+              name: "Soil Krabby",
+              price: new Decimal(1000),
+              currency: "SFL",
+              type: "collectible",
+              shortDescription: "Lorem ipsum",
+            },
+          ],
+        },
+      },
+      action: {
+        type: "megastoreItem.bought",
+        name: "Soil Krabby",
+      },
+    });
+
+    expect(balance).toEqual(new Decimal(0));
+    expect(inventory["Soil Krabby"]).toEqual(new Decimal(1));
+  });
+
+  it("[Non SFL Wearable] subtracts the price from inventory balance and add item to wardrobe", () => {
+    const seasonTicket = getSeasonalTicket();
+
+    const { inventory, wardrobe } = buyMegaStoreItem({
+      state: {
+        ...TEST_FARM,
+        inventory: {
+          [seasonTicket]: new Decimal(1000),
+        },
+        megastore: {
+          ...TEST_FARM.megastore,
+          wearables: [
+            {
+              name: "Fish Pro Vest",
+              price: new Decimal(1000),
+              currency: "Seasonal Ticket",
+              type: "wearable",
+              shortDescription: "Lorem ipsum",
+            },
+          ],
+        },
+      },
+      action: {
+        type: "megastoreItem.bought",
+        name: "Fish Pro Vest",
+      },
+    });
+
+    expect(inventory[seasonTicket]).toEqual(new Decimal(0));
+    expect(wardrobe["Fish Pro Vest"]).toEqual(1);
+  });
+
+  it("[Non SFL Collectible] subtracts the price from inventory balance and add item to inventory", () => {
+    const { inventory } = buyMegaStoreItem({
+      state: {
+        ...TEST_FARM,
+        inventory: {
+          Crimstone: new Decimal(100),
+        },
+        megastore: {
+          ...TEST_FARM.megastore,
+          collectibles: [
+            {
+              name: "Knowledge Crab",
+              price: new Decimal(10),
+              currency: "Crimstone",
+              type: "collectible",
+              shortDescription: "Lorem ipsum",
+            },
+          ],
+        },
+      },
+      action: {
+        type: "megastoreItem.bought",
+        name: "Knowledge Crab",
+      },
+    });
+
+    expect(inventory.Crimstone).toEqual(new Decimal(90));
+    expect(inventory["Knowledge Crab"]).toEqual(new Decimal(1));
+  });
+
+  it("adds SFL Spent to bumpkin activity", () => {
+    const { bumpkin } = buyMegaStoreItem({
+      state: {
+        ...TEST_FARM,
+        balance: new Decimal(1000),
+        megastore: {
+          ...TEST_FARM.megastore,
+          collectibles: [
+            {
+              name: "Knowledge Crab",
+              price: new Decimal(1000),
+              currency: "SFL",
+              type: "wearable",
+              shortDescription: "Lorem ipsum",
+            },
+          ],
+        },
+      },
+      action: {
+        type: "megastoreItem.bought",
+        name: "Knowledge Crab",
+      },
+    });
+
+    expect(bumpkin?.activity?.["SFL Spent"]).toEqual(1000);
+  });
+
+  // TODO: Add items to bought event
+  // it.skip("adds [item] Bought to bumpkin activity", () => {
+  //   const now = new Date("2024-02-01").getTime();
+  //   jest.setSystemTime(now);
+
+  //   const state = {
+  //     ...TEST_FARM,
+  //     inventory: { Crimstone: new Decimal(1000) },
+  //   };
+
+  //   const { bumpkin } = buyMegaStoreItem({
+  //     state,
+  //     action: {
+  //       type: "megastoreItem.bought",
+  //       name: "Knowledge Crab",
+  //     },
+  //     createdAt: now,
+  //   });
+
+  //   expect(bumpkin?.activity?.["Knowledge Crab Bought"]).toEqual(1);
+  // });
+});
