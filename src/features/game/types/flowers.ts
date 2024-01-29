135d1f320b7e9cc528272ade37f27d8f1af33157
Craig <41215134+c-py@users.noreply.github.com>
2024-01-29 14:08:41 +1100

    [FEAT] Update description

--- src/features/game/types/flowers.ts
+++ src/features/game/types/flowers.ts
@@ -77,8 +77,8 @@ export const FLOWER_CROSS_BREED_AMOUNTS: Record<FlowerCrossBreedName, number> =
 
 export const FLOWER_CROSS_BREED_DETAILS: Record<FlowerCrossBreedName, string> =
   {
-    Sunflower: "Bumpkin scientists swear they're not flowers",
-    Cauliflower: "I wouldn't even ask the Bumpkin scientists about this one",
+    Sunflower: "Bumpkin Botanists swear they're not flowers",
+    Cauliflower: "Not so sure what the Bumpkin Botanists say about this one",
   };
 
 type Flower = {
