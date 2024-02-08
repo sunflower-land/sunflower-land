885039f457356bfc8c65b2ce5d187ffaeff98442
spencer <spencer@sunflower-land.com>
2024-02-08 11:19:17 +1100

    [FEAT] Add log

--- src/sw.ts
+++ src/sw.ts
@@ -18,7 +18,7 @@ declare let self: ServiceWorkerGlobalScope;
 
 const OFFLINE_VERSION = CONFIG.RELEASE_VERSION;
 
-console.log("service worker version:", OFFLINE_VERSION);
+console.log("[SW] Service Worker Version:", OFFLINE_VERSION);
 
 // Disable workbox logs => do not delete this static import: import "workbox-core";
 self.__WB_DISABLE_DEV_LOGS = true;
