d882e1dd2aeed77e29888a83781d43e70753a86c
spencer <spencer@sunflower-land.com>
2024-02-08 13:58:02 +1100

    [FEAT] Add updates

--- src/features/island/hud/components/BumpkinProfile.tsx
+++ src/features/island/hud/components/BumpkinProfile.tsx
@@ -187,7 +187,7 @@ export const BumpkinAvatar: React.FC<AvatarProps> = ({
               marginTop: `${DIMENSIONS.username.marginTop}px`,
             }}
           >
-            {`PWA Spencer`}
+            {`PWA Update Guy`}
           </div>
         )}
         {showSkillPointAlert && (
