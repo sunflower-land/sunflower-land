f746b4fb0c99a72be15b9017ea36e76cacaaed06
spencer <spencer@sunflower-land.com>
2024-02-09 21:33:13 +1100

    [CHORE] Remove test code

--- src/features/island/hud/components/BumpkinProfile.tsx
+++ src/features/island/hud/components/BumpkinProfile.tsx
@@ -185,7 +185,7 @@ export const BumpkinAvatar: React.FC<AvatarProps> = ({
               marginTop: `${DIMENSIONS.username.marginTop}px`,
             }}
           >
-            Boss {username}
+            {username}
           </div>
         )}
         {showSkillPointAlert && (
