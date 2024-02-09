286637836f76fe8b904ab81e815aa86fa3a4aa92
spencer <spencer@sunflower-land.com>
2024-02-09 14:03:03 +1100

    [FEAT] Move update logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+/* eslint-disable no-console */
+import React, { useEffect, useRef } from "react";
 
 import { initialise } from "lib/utils/init";
 
@@ -8,15 +9,71 @@ import ErrorBoundary from "features/auth/components/ErrorBoundary";
 import { Navigation } from "./Navigation";
 import "./lib/i18n";
 import { WalletProvider } from "features/wallet/WalletProvider";
-import { ReloadPrompt } from "features/pwa/ReloadPrompt";
+import lifecycle from "page-lifecycle/dist/lifecycle.mjs";
+import { useRegisterSW } from "virtual:pwa-register/react";
+
+const CHECK_FOR_UPDATE_INTERVAL = 1000 * 60 * 5;
 
 // Initialise Global Settings
 initialise();
-
 /**
  * Top level wrapper for providers
  */
 export const App: React.FC = () => {
+  // useServiceWorkerUpdate();
+
+  const {
+    needRefresh: [needRefresh],
+    updateServiceWorker,
+  } = useRegisterSW({
+    onRegisteredSW(swUrl, registration) {
+      if (registration) {
+        setInterval(async () => {
+          console.log("REGISTRATION STATE:: ", registration);
+          if (!(!registration.installing && navigator)) return;
+
+          if ("connection" in navigator && !navigator.onLine) return;
+
+          const resp = await fetch(swUrl, {
+            cache: "no-store",
+            headers: {
+              cache: "no-store",
+              "cache-control": "no-cache",
+            },
+          });
+
+          if (resp?.status === 200) {
+            console.log("CHECKING FOR UPDATE");
+            await registration.update();
+          }
+        }, CHECK_FOR_UPDATE_INTERVAL);
+      }
+    },
+  });
+
+  const needRefreshRef = useRef(needRefresh);
+
+  // Update the ref whenever needRefresh changes
+  useEffect(() => {
+    needRefreshRef.current = needRefresh;
+  }, [needRefresh]);
+
+  const handleStateChange = (evt: any) => {
+    console.log("PAGE LIFECYCLE STATE CHANGE", evt.newState);
+    if (evt.newState === "hidden" && needRefreshRef.current) {
+      console.log("UPDATE NEEDED: Refreshing Service Worker");
+      updateServiceWorker();
+    }
+  };
+
+  useEffect(() => {
+    lifecycle.addEventListener("statechange", handleStateChange);
+
+    return () => {
+      lifecycle.removeEventListener("statechange", handleStateChange);
+    };
+  }, []);
+
   return (
     <>
       <Auth.Provider>
@@ -26,7 +83,6 @@ export const App: React.FC = () => {
           </ErrorBoundary>
         </WalletProvider>
       </Auth.Provider>
-      <ReloadPrompt />
     </>
   );
 };
