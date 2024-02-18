ba6fb27194b991dd27c96a26494fb1d726f2a8bd
Craig <41215134+c-py@users.noreply.github.com>
2024-02-19 10:16:55 +1100

    [FEAT] Size

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 
 interface ModalProps {
-  size?: "lg" | "sm";
+  size?: "lg" | "sm" | "md";
   backdropClassName?: string;
   dialogClassName?: string;
   className?: string;
@@ -28,7 +28,7 @@ export const Modal: React.FC<ModalProps> & {
   Body: React.FC<Props>;
   Footer: React.FC<Props>;
   Header: React.FC<Props>;
-} = ({ children, show, onHide }) => {
+} = ({ children, show, onHide, size = "md" }) => {
   // const ref = React.createRef<HTMLDialogElement>();
 
   // useEffect(() => {
@@ -64,17 +64,29 @@ export const Modal: React.FC<ModalProps> & {
           <div className="fixed inset-0 bg-black/25" />
         </Transition.Child>
 
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          <div className="relative">{children}</div>
-        </Transition.Child>
+        <div className="fixed inset-0 overflow-y-auto" onClick={onHide}>
+          <div className="flex min-h-full items-center justify-center text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div
+                className={classNames("relative", {
+                  "max-w-[300px]": size === "sm",
+                  "max-w-[500px]": size === "md",
+                  "max-w-[800px]": size === "lg",
+                })}
+              >
+                {children}
+              </div>
+            </Transition.Child>
+          </div>
+        </div>
       </Dialog>
     </Transition>
   );
