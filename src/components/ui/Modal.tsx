cb75bc427d39be522e208c6177223362163d7234
Craig <41215134+c-py@users.noreply.github.com>
2024-02-19 11:46:29 +1100

    backdrop

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -27,30 +27,41 @@ export const Modal: React.FC<ModalProps> & {
   Body: React.FC<Props>;
   Footer: React.FC<Props>;
   Header: React.FC<Props>;
-} = ({ children, show, onHide, onExited, onShow, size, fullscreen }) => {
+} = ({
+  children,
+  show,
+  onHide,
+  onExited,
+  onShow,
+  size,
+  fullscreen,
+  backdrop = true,
+}) => {
   return (
     <Transition appear show={show} as={Fragment}>
       <Dialog
         as="div"
         className="fixed inset-0 flex min-h-full items-center justify-center z-50"
-        onClose={() => onHide?.()}
+        onClose={() => (backdrop === "static" ? undefined : onHide?.())}
         // Prevent click through to Phaser
         onMouseDown={(e) => e.stopPropagation()}
         onMouseUp={(e) => e.stopPropagation()}
         onTouchStart={(e) => e.stopPropagation()}
         onTouchEnd={(e) => e.stopPropagation()}
       >
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-black/50" />
-        </Transition.Child>
+        {backdrop && (
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/50" />
+          </Transition.Child>
+        )}
 
         <div className="fixed inset-0 overflow-y-auto flex min-h-full items-center justify-center">
           <Transition.Child
