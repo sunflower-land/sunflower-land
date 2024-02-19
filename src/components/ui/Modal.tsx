cc2083fb98bda560d45dcf0c6119c74395700944
Craig <41215134+c-py@users.noreply.github.com>
2024-02-19 13:52:56 +1100

    [FEAT] Ease in

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -56,11 +56,11 @@ export const Modal: React.FC<ModalProps> = ({
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
-            enterFrom="opacity-0 scale-95"
-            enterTo="opacity-100 scale-100"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
             leave="ease-in duration-200"
-            leaveFrom="opacity-100 scale-100"
-            leaveTo="opacity-0 scale-95"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
             beforeEnter={() => onShow?.()}
             afterLeave={() => onExited?.()}
           >
