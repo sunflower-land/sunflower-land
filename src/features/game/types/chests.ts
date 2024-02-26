fedd1c46f5d0b54410cd16b9db85814ef813b464
Adam Hannigan <adamhannigan81@gmail.com>
2024-02-26 15:07:29 +1100

    Update rewards

--- src/features/game/types/chests.ts
+++ src/features/game/types/chests.ts
@@ -1,133 +1,38 @@
-/**
- * READONLY file
- * Treasure Chest Rewards are calculated on the backend server
- * We have included the weights here for transparency and players who are trying to calculate odds
- * Note: Odds may become out of sync with backend if we change the weights on the server
- */
-
 import { InventoryItemName, Wardrobe } from "./game";
 
-type Reward = {
+export type ChestReward = {
   items?: Partial<Record<InventoryItemName, number>>;
   wearables?: Partial<Wardrobe>;
   sfl?: number;
   weighting: number;
 };
 
-const BASIC_REWARDS: Reward[] = [
-  {
-    sfl: 5,
-    weighting: 100,
-  },
-  {
-    sfl: 10,
-    weighting: 50,
-  },
-  {
-    sfl: 25,
-    weighting: 20,
-  },
-
-  {
-    items: { "Block Buck": 1 },
-    weighting: 100,
-  },
-  {
-    items: { "Block Buck": 2 },
-    weighting: 50,
-  },
-  {
-    items: { "Block Buck": 5 },
-    weighting: 20,
-  },
-  {
-    items: { "Block Buck": 10 },
-    weighting: 5,
-  },
-  {
-    items: {
-      Axe: 5,
-      Pickaxe: 5,
-      "Stone Pickaxe": 5,
-    },
-    weighting: 100,
-  },
-  {
-    items: {
-      "Iron Pickaxe": 10,
-    },
-    weighting: 10,
-  },
-  {
-    items: {
-      Rod: 10,
-    },
-    weighting: 20,
-  },
-  {
-    items: {
-      "Rapid Root": 10,
-      "Sprout Mix": 10,
-    },
-    weighting: 50,
-  },
-  {
-    items: {
-      "Fishing Lure": 10,
-    },
-    weighting: 10,
-  },
-  {
-    items: { "Pirate Cake": 5 },
-    weighting: 5,
-  },
-  {
-    items: { "Wheat Cake": 3 },
-    weighting: 20,
-  },
-  {
-    items: { "Goblin Brunch": 3 },
-    weighting: 30,
-  },
-  {
-    items: { "Bumpkin Roast": 3 },
-    weighting: 40,
-  },
-  {
-    items: { "Fermented Carrots": 5 },
-    weighting: 50,
-  },
-  {
-    items: { "Blueberry Jam": 3 },
-    weighting: 100,
-  },
-  {
-    wearables: {
-      "Fox Hat": 1,
-    },
-    weighting: 50,
-  },
-  {
-    items: {
-      "Time Warp Totem": 1,
-    },
-    weighting: 25,
-  },
-  {
-    items: {
-      Rug: 1,
-    },
-    weighting: 25,
-  },
-  {
-    items: {
-      "Prize Ticket": 1,
-    },
-    weighting: 5,
-  },
+export const BASIC_REWARDS: ChestReward[] = [
+  { sfl: 5, weighting: 100 },
+  { sfl: 10, weighting: 50 },
+  { sfl: 25, weighting: 20 },
+  { items: { "Block Buck": 1 }, weighting: 100 },
+  { items: { "Block Buck": 2 }, weighting: 50 },
+  { items: { "Block Buck": 5 }, weighting: 20 },
+  { items: { "Block Buck": 10 }, weighting: 5 },
+  { items: { Axe: 5, Pickaxe: 5, "Stone Pickaxe": 5 }, weighting: 100 },
+  { items: { "Iron Pickaxe": 10 }, weighting: 10 },
+  { items: { Rod: 10 }, weighting: 20 },
+  { items: { "Rapid Root": 10, "Sprout Mix": 10 }, weighting: 50 },
+  { items: { "Fishing Lure": 10 }, weighting: 10 },
+  { items: { "Pirate Cake": 5 }, weighting: 5 },
+  { items: { "Wheat Cake": 3 }, weighting: 20 },
+  { items: { "Goblin Brunch": 3 }, weighting: 30 },
+  { items: { "Bumpkin Roast": 3 }, weighting: 40 },
+  { items: { "Fermented Carrots": 5 }, weighting: 50 },
+  { items: { "Blueberry Jam": 3 }, weighting: 100 },
+  { wearables: { "Fox Hat": 1 }, weighting: 50 },
+  { items: { "Time Warp Totem": 1 }, weighting: 25 },
+  { items: { Rug: 1 }, weighting: 25 },
+  { items: { "Prize Ticket": 1 }, weighting: 5 },
 ];
 
-const RARE_REWARDS: Reward[] = [
+export const RARE_REWARDS: ChestReward[] = [
   { sfl: 5, weighting: 50 },
   { sfl: 10, weighting: 100 },
   { sfl: 25, weighting: 50 },
@@ -138,9 +43,9 @@ const RARE_REWARDS: Reward[] = [
   { items: { "Block Buck": 10 }, weighting: 20 },
   { items: { "Block Buck": 25 }, weighting: 10 },
   { items: { "Block Buck": 50 }, weighting: 5 },
-  { items: { Axe: 5, Pickaxe: 5, "Stone Pickaxe": 5 }, weighting: 50 },
+  { items: { Axe: 15, Pickaxe: 15, "Stone Pickaxe": 15 }, weighting: 50 },
   { items: { "Iron Pickaxe": 10 }, weighting: 50 },
-  { items: { Rod: 10 }, weighting: 50 },
+  { items: { Rod: 5, Earthworm: 5, "Red Wiggler": 5, Grub: 5 }, weighting: 50 },
   { items: { "Fishing Lure": 25 }, weighting: 25 },
   { items: { "Pirate Cake": 5 }, weighting: 30 },
   { items: { "Wheat Cake": 3 }, weighting: 20 },
@@ -150,9 +55,13 @@ const RARE_REWARDS: Reward[] = [
   { wearables: { "Pale Potion": 1 }, weighting: 50 },
   { items: { "Time Warp Totem": 1 }, weighting: 25 },
   { items: { "Prize Ticket": 1 }, weighting: 20 },
+  { wearables: { "Bee Wings": 1 }, weighting: 5 },
+  { wearables: { "Beekeeper Hat": 1 }, weighting: 5 },
+  { items: { Capybara: 1 }, weighting: 25 },
+  { items: { "Flower Rug": 1 }, weighting: 25 },
 ];
 
-const LUXURY_REWARDS: Reward[] = [
+export const LUXURY_REWARDS: ChestReward[] = [
   { sfl: 10, weighting: 50 },
   { sfl: 25, weighting: 100 },
   { sfl: 50, weighting: 50 },
@@ -160,14 +69,21 @@ const LUXURY_REWARDS: Reward[] = [
   { items: { "Block Buck": 10 }, weighting: 100 },
   { items: { "Block Buck": 25 }, weighting: 25 },
   { items: { "Block Buck": 50 }, weighting: 10 },
-  { items: { "Iron Pickaxe": 10 }, weighting: 75 },
-  { items: { Rod: 10 }, weighting: 50 },
+  { items: { "Gold Pickaxe": 10 }, weighting: 75 },
+  {
+    items: { Rod: 10, Earthworm: 10, "Red Wiggler": 10, Grub: 10 },
+    weighting: 50,
+  },
   { items: { "Fishing Lure": 25 }, weighting: 25 },
-  { items: { "Pirate Cake": 5 }, weighting: 50 },
-  { items: { "Goblin Brunch": 3 }, weighting: 25 },
-  { items: { "Bumpkin Roast": 3 }, weighting: 25 },
+  { items: { "Pirate Cake": 10 }, weighting: 50 },
+  { items: { "Goblin Brunch": 10 }, weighting: 25 },
+  { items: { "Bumpkin Roast": 10 }, weighting: 25 },
   { wearables: { "Pale Potion": 1 }, weighting: 25 },
   { wearables: { "Chicken Hat": 1 }, weighting: 25 },
   { items: { "Time Warp Totem": 1 }, weighting: 25 },
   { items: { "Prize Ticket": 1 }, weighting: 50 },
+  { wearables: { "Bee Wings": 1 }, weighting: 10 },
+  { wearables: { "Beekeeper Hat": 1 }, weighting: 10 },
+  { items: { Capybara: 1 }, weighting: 50 },
+  { items: { "Flower Fox": 1 }, weighting: 25 },
 ];
