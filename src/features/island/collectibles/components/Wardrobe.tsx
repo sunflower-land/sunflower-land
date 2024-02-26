66e6b2c4f39e7646d1a8c352f68603c81bcf9e9c
Adam Hannigan <adamhannigan81@gmail.com>
2024-02-27 09:13:33 +1100

    Add wardrobe wearables

--- src/features/island/collectibles/components/Wardrobe.tsx
+++ src/features/island/collectibles/components/Wardrobe.tsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { PIXEL_SCALE } from "features/game/lib/constants";
 import { SUNNYSIDE } from "assets/sunnyside";
+import { Modal } from "components/ui/Modal";
+import { CloseButtonPanel } from "features/game/components/CloseablePanel";
+import { StylistWearables } from "features/world/ui/stylist/StylistWearables";
+import { BASIC_WEARABLES } from "features/game/types/stylist";
 
+function hasOpened() {
+  return !!localStorage.getItem("hasOpenedWardrobe");
+}
+
+function acknowledge() {
+  localStorage.setItem("hasOpenedWardrobe", "true");
+}
 export const Wardrobe: React.FC = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const open = () => {
+    setShowModal(true);
+    acknowledge();
+  };
+
   return (
     <>
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <CloseButtonPanel>
+          <StylistWearables wearables={BASIC_WEARABLES} />
+        </CloseButtonPanel>
+      </Modal>
+
+      {!hasOpened() && (
+        <img
+          src={SUNNYSIDE.icons.click_icon}
+          className="absolute bottom-0 right-0 z-20 cursor-pointer"
+          style={{
+            width: `${PIXEL_SCALE * 18}px`,
+            right: `${PIXEL_SCALE * -8}px`,
+          }}
+          onClick={open}
+        />
+      )}
+
       <img
         src={SUNNYSIDE.decorations.wardrobe}
         style={{
@@ -13,8 +49,9 @@ export const Wardrobe: React.FC = () => {
           bottom: `${PIXEL_SCALE * 6}px`,
           left: `${PIXEL_SCALE * 1.5}px`,
         }}
-        className="absolute"
+        className="absolute cursor-pointer hover:img-highlight"
         alt="Wardrobe"
+        onClick={open}
       />
     </>
   );
