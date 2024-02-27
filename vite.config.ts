24a8eb95e6009891fc36fdd51cdbf74455752423
spencer <spencer@sunflower-land.com>
2024-02-27 19:30:28 +1100

    [CHORE] Testing

--- vite.config.ts
+++ vite.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
         description:
           "Plant, Chop, Mine, Craft & Collect at Sunflower Land. The MetaVerse game with endless resources.",
         short_name: "Sunflower Land",
-        start_url: "/play/",
+        start_url: "/pwa/",
         theme_color: "#303443",
         display: "standalone",
         background_color: "#0099dc",
