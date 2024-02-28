ed2a7647e5ad5330397a4f07f7b9f9f9b7b86ef5
Romy Furtado <25412194+Rofrtd@users.noreply.github.com>
2024-02-29 09:36:05 +1100

    [FIX] Rich Bear (#3465)

--- src/features/island/collectibles/components/RichBear.tsx
+++ src/features/island/collectibles/components/RichBear.tsx
@@ -5,17 +5,23 @@ import { PIXEL_SCALE } from "features/game/lib/constants";
 
 export const RichBear: React.FC = () => {
   return (
-    <>
+    <div
+      className="absolute flex justify-center w-full h-ful"
+      style={{
+        width: `${PIXEL_SCALE * 24}px`,
+        bottom: `${PIXEL_SCALE * 0}px`,
+        right: `${PIXEL_SCALE * -4}px`,
+      }}
+    >
       <img
         src={bear}
         style={{
           width: `${PIXEL_SCALE * 24}px`,
           bottom: `${PIXEL_SCALE * 0}px`,
-          left: `${PIXEL_SCALE * 4}px`,
         }}
         className="absolute"
         alt="Rich Bear"
       />
-    </>
+    </div>
   );
 };
