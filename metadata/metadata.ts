2f89135599ceb293274585a5daa3c79cf7e71707
Elias <elias.chew1999@gmail.com>
2024-03-06 14:39:41 +0800

    [FEAT] Update Flower Fox Metadata

--- metadata/metadata.ts
+++ metadata/metadata.ts
@@ -6746,7 +6746,12 @@ export const OPEN_SEA_ITEMS: Record<InventoryItemName, Metadata> = {
     external_url: "https://docs.sunflower-land.com/getting-started/about",
     image: "../public/erc1155/images/492.png",
     attributes: [
-      { trait_type: "Purpose", value: "Decoration" },
+      { trait_type: "Purpose", value: "Boost" },
+      {
+        display_type: "boost_percentage",
+        trait_type: "Flower Growth Time",
+        value: -10,
+      },
       { trait_type: "Tradable", value: "Yes" },
     ],
   },
