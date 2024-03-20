b1f8a2d95844ab7dde355acdd441d6364cd44180
spencer <spencer@sunflower-land.com>
2024-03-20 11:56:34 +1100

    [CHORE] Remove old chores

--- src/features/game/types/chores.ts
+++ src/features/game/types/chores.ts
@@ -1,13 +1,6 @@
 import { BumpkinActivityName } from "./bumpkinActivity";
 import { InventoryItemName } from "./game";
 
-import clickToHarvest from "assets/tutorials/click_to_harvest.png";
-import betty from "assets/tutorials/betty.png";
-import bruce from "assets/tutorials/bruce_intro.png";
-import workbench from "assets/tutorials/workbench_chat.png";
-import { translate } from "lib/i18n/translate";
-
-import { CROPS } from "./crops";
 export type Chore = {
   // Challenges
   activity?: BumpkinActivityName;
@@ -27,76 +20,4 @@ export type Chore = {
 
 // TODO Goals should be means - not do X task - reach level 2, not eat 2 potatos
 
-export const CHORES: Chore[] = [
-  {
-    activity: "Sunflower Harvested",
-    description: translate("chores.harvestFields"),
-    image: clickToHarvest,
-    introduction: translate("chores.harvestFieldsIntro"),
-    requirement: 3,
-    reward: {
-      items: {},
-    },
-  },
-  {
-    description: `${translate("earn")} ${CROPS().Sunflower.sellPrice.mul(
-      5
-    )} SFL`,
-    sfl: CROPS().Sunflower.sellPrice.mul(5).toNumber(),
-    image: betty,
-    introduction: translate("chores.earnSflIntro"),
-    requirement: CROPS().Sunflower.sellPrice.mul(5).toNumber(),
-    reward: {
-      items: {},
-    },
-  },
-  {
-    description: translate("chores.reachLevel"),
-    bumpkinLevel: 2,
-    image: bruce,
-    introduction: translate("chores.reachLevelIntro"),
-    requirement: 2,
-    reward: {
-      items: {},
-    },
-  },
-  {
-    activity: "Tree Chopped",
-    description: translate("chores.chopTrees"),
-    requirement: 3,
-    image: workbench,
-    introduction: translate("chores.helpWithTrees"),
-    reward: {
-      items: {},
-    },
-  },
-  // Craft Scarecrow
-  // {
-  //   activity: "Bush Bought",
-  //   description: translate("hank.crafting.scarecrow"),
-  //   requirement: 1,
-  //   reward: {
-  //     items: {
-  //       "Potato Seed": 5,
-  //     },
-  //   },
-  // },
-  // {
-  //   activity: "Pumpkin Harvested",
-  //   description:"Harvest 10 Pumpkins",
-  //   requirement: 10,
-  //   reward: {
-  //     items: {},
-  //   },
-  // },
-  // {
-  //   activity: "Pumpkin Harvested",
-  //   description:"Time to expand",
-  //   requirement: 1,
-  //   reward: {
-  //     items: {},
-  //   },
-  // },
-
-  // Level up
-];
+export const CHORES: Chore[] = [];
