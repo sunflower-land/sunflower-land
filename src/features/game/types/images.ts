70f0b59951b58d372a34f6eb34302dde36edae13
spencer <spencer@sunflower-land.com>
2024-04-28 20:00:32 +1000

    [FEAT] Update banner image to webp

--- src/features/game/types/images.ts
+++ src/features/game/types/images.ts
@@ -243,7 +243,7 @@ import dawnBreakerBanner from "assets/decorations/banners/dawn_breaker_banner.pn
 import witchesEveBanner from "assets/decorations/banners/witches_eve_banner.webp";
 import catchTheKrakenBanner from "assets/decorations/banners/catch_the_kraken_banner.webp";
 import springBlossomBanner from "assets/decorations/banners/spring_banner.gif";
-import clashOfFactionsBanner from "assets/decorations/banners/clash_of_factions_banner.png";
+import clashOfFactionsBanner from "assets/decorations/banners/clash_of_factions_banner.webp";
 import lifetimeFarmerBanner from "assets/decorations/banners/lifetime_farmer_banner.png";
 import budTicket from "assets/icons/bud_ticket.png";
 import prizeTicket from "assets/icons/prize_ticket.png";
