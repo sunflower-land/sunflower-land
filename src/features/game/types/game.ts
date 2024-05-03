4eea5b3ed79c69c3b95dbc8909fde0d55802503d
spencer <spencer@sunflower-land.com>
2024-05-03 11:00:04 +1000

    [FEAT] Add drillOilReserve event

--- src/features/game/types/game.ts
+++ src/features/game/types/game.ts
@@ -26,7 +26,7 @@ import {
   PurchasableItems,
   SoldOutCollectibleName,
 } from "./collectibles";
-import { TreasureToolName } from "./tools";
+import { TreasureToolName, WorkbenchToolName } from "./tools";
 import { ConversationName } from "./announcements";
 import { NPCName } from "lib/npcs";
 import { SeasonalTicket } from "./seasons";
@@ -333,7 +333,8 @@ export type InventoryItemName =
   | MarineMarvelName
   | FlowerName
   | MegaStoreCollectibleName
-  | FactionBanner;
+  | FactionBanner
+  | WorkbenchToolName;
 
 export type Inventory = Partial<Record<InventoryItemName, Decimal>>;
 
@@ -437,7 +438,8 @@ export type Oil = {
 
 export type OilReserve = {
   oil: Oil;
-  drillsLeft: number;
+  drilled: number;
+  createdAt: number;
 } & Position;
 
 export type CropPlot = {
