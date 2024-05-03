99c9f190bfecee059aeeac992ebb46419ab0b5fa
Azaro <qppazaro@gmail.com>
2024-05-03 05:35:37 +0300

    [CHORE] Fixed size of Clash Of Factions and Lifetime banners. (#3655)

--- src/features/island/collectibles/components/LifetimeFarmerBanner.tsx
+++ src/features/island/collectibles/components/LifetimeFarmerBanner.tsx
@@ -5,17 +5,20 @@ import { PIXEL_SCALE } from "features/game/lib/constants";
 
 export const LifetimeFarmerBanner: React.FC = () => {
   return (
-    <>
+    <div
+      className="absolute pointer-events-none"
+      style={{
+        width: `${PIXEL_SCALE * 22}px`,
+        top: `${PIXEL_SCALE * -3}px`,
+      }}
+    >
       <img
         src={banner}
         style={{
           width: `${PIXEL_SCALE * 16}px`,
-          bottom: `${PIXEL_SCALE * 2}px`,
-          left: `${PIXEL_SCALE * 0}px`,
         }}
-        className="absolute"
         alt="Lifetime Farmer Banner"
       />
-    </>
+    </div>
   );
 };
