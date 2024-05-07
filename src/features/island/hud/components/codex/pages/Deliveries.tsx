914c62be04d1467b5ecc4c262270a5eed35e8f77
brian-man <36871683+brian-man@users.noreply.github.com>
2024-05-06 17:44:17 -0700

    [FIX] Hide 'New deliveries' label while tasks are frozen (#3666)

--- src/features/island/hud/components/codex/pages/Deliveries.tsx
+++ src/features/island/hud/components/codex/pages/Deliveries.tsx
@@ -2,26 +2,36 @@ import { SUNNYSIDE } from "assets/sunnyside";
 import { Label } from "components/ui/Label";
 import { DeliveryOrders } from "features/island/delivery/components/Orders";
 import { useAppTranslation } from "lib/i18n/useAppTranslations";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 
 import { secondsToString } from "lib/utils/time";
 import { secondsTillReset } from "features/helios/components/hayseedHank/HayseedHankV2";
 
+import { Context } from "features/game/GameProvider";
+import { getSeasonChangeover } from "lib/utils/getSeasonWeek";
+
 export const Deliveries: React.FC = () => {
+  const { gameService } = useContext(Context);
   const [selected, setSelected] = useState<string>();
 
   const { t } = useAppTranslation();
+
+  const { ticketTasksAreFrozen } = getSeasonChangeover({
+    id: gameService.state.context.farmId,
+  });
 
   return (
     <div className="flex flex-col h-full">
       <div className="p-1">
         <div className="flex justify-between">
           <Label type="default">{t("deliveries")}</Label>
-          <Label type="info" icon={SUNNYSIDE.icons.stopwatch}>
-            {`${t("new.delivery.in")} ${secondsToString(secondsTillReset(), {
-              length: "short",
-            })}`}
-          </Label>
+          {!ticketTasksAreFrozen && (
+            <Label type="info" icon={SUNNYSIDE.icons.stopwatch}>
+              {`${t("new.delivery.in")} ${secondsToString(secondsTillReset(), {
+                length: "short",
+              })}`}
+            </Label>
+          )}
         </div>
         <p className="my-2 ml-1 text-xxs">{t("deliveries.intro")}</p>
       </div>
