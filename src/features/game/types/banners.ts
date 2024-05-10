62fa3e0794672d61c3a76e3753fe357c8d66867b
Elias <101262042+eliasSFL@users.noreply.github.com>
2024-05-10 11:00:58 +0800

    Use existing constants

--- src/features/game/types/banners.ts
+++ src/features/game/types/banners.ts
@@ -1,36 +1,19 @@
+import { SeasonalBanner } from "./seasons";
+import { SEASONAL_BANNERS } from "./seasons";
+import { FactionBanner } from "./game";
 type bannerName =
-  // War Banners
   | "Human War Banner"
   | "Goblin War Banner"
-
-  // Season Banners
   | "Lifetime Farmer Banner"
-  | "Solar Flare Banner"
-  | "Dawn Breaker Banner"
-  | "Witches' Eve Banner"
-  | "Catch the Kraken Banner"
-  | "Spring Blossom Banner"
-  | "Clash of Factions Banner"
-
-  // Faction Banners
-  | "Bumpkin Faction Banner"
-  | "Goblin Faction Banner"
-  | "Sunflorian Faction Banner"
-  | "Nightshade Faction Banner"
-
-  // Collab Banners
-  | "Earn Alliance Banner";
+  | "Earn Alliance Banner"
+  | SeasonalBanner
+  | FactionBanner;
 
 export const BANNERS: Record<bannerName, string> = {
+  ...SEASONAL_BANNERS,
   "Human War Banner": "",
   "Goblin War Banner": "",
   "Lifetime Farmer Banner": "",
-  "Solar Flare Banner": "",
-  "Dawn Breaker Banner": "",
-  "Witches' Eve Banner": "",
-  "Catch the Kraken Banner": "",
-  "Spring Blossom Banner": "",
-  "Clash of Factions Banner": "",
   "Bumpkin Faction Banner": "",
   "Goblin Faction Banner": "",
   "Sunflorian Faction Banner": "",
