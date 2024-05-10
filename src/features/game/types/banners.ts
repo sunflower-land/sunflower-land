d4722689be63cd0caad90e5bab4635e5fa2f4ee9
Elias <101262042+eliasSFL@users.noreply.github.com>
2024-05-10 09:35:33 +0800

    [FEAT] Banner Category in Chest

---
+++ src/features/game/types/banners.ts
@@ -0,0 +1,39 @@
+type bannerName =
+  // War Banners
+  | "Human War Banner"
+  | "Goblin War Banner"
+
+  // Season Banners
+  | "Lifetime Farmer Banner"
+  | "Solar Flare Banner"
+  | "Dawn Breaker Banner"
+  | "Witches' Eve Banner"
+  | "Catch the Kraken Banner"
+  | "Spring Blossom Banner"
+  | "Clash of Factions Banner"
+
+  // Faction Banners
+  | "Bumpkin Faction Banner"
+  | "Goblin Faction Banner"
+  | "Sunflorian Faction Banner"
+  | "Nightshade Faction Banner"
+
+  // Collab Banners
+  | "Earn Alliance Banner";
+
+export const BANNERS: Record<bannerName, string> = {
+  "Human War Banner": "",
+  "Goblin War Banner": "",
+  "Lifetime Farmer Banner": "",
+  "Solar Flare Banner": "",
+  "Dawn Breaker Banner": "",
+  "Witches' Eve Banner": "",
+  "Catch the Kraken Banner": "",
+  "Spring Blossom Banner": "",
+  "Clash of Factions Banner": "",
+  "Bumpkin Faction Banner": "",
+  "Goblin Faction Banner": "",
+  "Sunflorian Faction Banner": "",
+  "Nightshade Faction Banner": "",
+  "Earn Alliance Banner": "",
+};
