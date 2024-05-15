64ce11a598d13eacb48f35258baf9119777630de
Romy Furtado <25412194+Rofrtd@users.noreply.github.com>
2024-05-15 11:04:52 +1000

    [FEAT] boost description

--- src/lib/i18n/dictionaries/portugueseDictionary.ts
+++ src/lib/i18n/dictionaries/portugueseDictionary.ts
@@ -960,6 +960,7 @@ const boostEffectDescriptions: Record<BoostEffectDescriptions, string> = {
   "description.pan.boost": ENGLISH_TERMS["description.pan.boost"],
   "description.paw.shield.boost": ENGLISH_TERMS["description.paw.shield.boost"],
   "description.vinny.boost": ENGLISH_TERMS["description.vinny.boost"],
+  "description.rice.panda.boost": ENGLISH_TERMS["description.rice.panda.boost"],
 };
 
 const bountyDescription: Record<BountyDescription, string> = {
