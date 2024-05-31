3aa68321949e8b0bdd670724e1c5a3764bca4c4c
Adam Hannigan <adamhannigan81@gmail.com>
2024-06-01 09:11:52 +1000

    [FIX] Quieten pete

--- src/features/world/ui/NPCModals.tsx
+++ src/features/world/ui/NPCModals.tsx
@@ -53,7 +53,7 @@ interface Props {
 }
 
 function getInitialNPC(scene: SceneId): NPCName | undefined {
-  return "pumpkin' pete";
+  return undefined;
 }
 
 export const NPCModals: React.FC<Props> = ({ scene, id }) => {
