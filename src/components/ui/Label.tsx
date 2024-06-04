aa5e42c32ca32ac47cb06bd6b4f0d98938f6d590
Adam Hannigan <adamhannigan81@gmail.com>
2024-06-04 10:53:52 +1000

    Extreme pixels

--- src/components/ui/Label.tsx
+++ src/components/ui/Label.tsx
@@ -117,7 +117,7 @@ export const Label: React.FC<Props> = ({
         paddingLeft: icon ? "14px" : "3px",
         paddingRight: secondaryIcon ? "14px" : icon ? "4px" : "3px",
         color: LABEL_STYLES[type].textColour,
-        fontSize: `10px`,
+        fontSize: `20px`,
         ...style,
 
         // Normal font styles
