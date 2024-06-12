dedfe1deb2d9038493baa8f3e66245e918c6fd38
Polysemouse <107602352+Polysemouse@users.noreply.github.com>
2024-06-11 22:07:18 -0700

    [FIX] Update style of fishing reel button (#3858)

--- src/components/ui/SensitiveButton.tsx
+++ src/components/ui/SensitiveButton.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import classnames from "classnames";
-import { pixelLightBorderStyle } from "features/game/lib/style";
+import { useSound } from "lib/utils/hooks/useSound";
+import primaryButton from "assets/ui/light_button.png";
+import { PIXEL_SCALE } from "features/game/lib/constants";
 
 interface Props {
-  onClick?: () => void;
+  onClick?: (event: React.PointerEvent<HTMLButtonElement>) => void;
   disabled?: boolean;
   className?: string;
   type?: "button" | "submit" | undefined;
@@ -19,16 +21,35 @@ export const SensitiveButton: React.FC<Props> = ({
   className,
   type,
 }) => {
+  const button = useSound("button");
+
+  const onPointerDownWithSound = (
+    event: React.PointerEvent<HTMLButtonElement>
+  ) => {
+    button.play();
+    onClick?.(event);
+  };
+
   return (
     <button
       className={classnames(
-        "bg-brown-200 w-full p-1 text-xs object-contain justify-center items-center hover:bg-brown-300 cursor-pointer flex disabled:opacity-50 ",
-        className
+        "w-full p-1 text-sm object-contain justify-center items-center hover:brightness-90 cursor-pointer flex disabled:opacity-50",
+        className,
+        { "cursor-not-allowed": disabled }
       )}
       type={type}
       disabled={disabled}
-      style={pixelLightBorderStyle}
-      onPointerDown={onClick}
+      style={{
+        borderImage: `url(${primaryButton})`,
+        borderStyle: "solid",
+        borderWidth: `8px 8px 10px 8px`,
+        borderImageSlice: "3 3 4 3 fill",
+        imageRendering: "pixelated",
+        borderImageRepeat: "stretch",
+        borderRadius: `${PIXEL_SCALE * 5}px`,
+        color: "#674544",
+      }}
+      onPointerDown={onPointerDownWithSound}
     >
       <div className="mb-1">{children}</div>
     </button>
