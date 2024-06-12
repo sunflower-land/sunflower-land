da26d077dd6058b99e7caeaeb27ceef78fb33551
Elias <101262042+eliasSFL@users.noreply.github.com>
2024-06-12 23:17:48 +0800

    Add comments for the Confirmation Modal

--- src/components/ui/ConfirmationModal.tsx
+++ src/components/ui/ConfirmationModal.tsx
@@ -5,6 +5,20 @@ import { Panel } from "./Panel";
 import { Equipped } from "features/game/types/bumpkin";
 import { Modal } from "./Modal";
 
+/**
+ * The props for Confirmation Modal
+ * @param show condition to show Modal
+ * @param onHide condition to hide Modal
+ * @param messages array of messages. 1 message per span
+ * @param icon icon for an image to be shown in modal
+ * @param imageStyle style for the image
+ * @param onCancel condition to cancel button
+ * @param onConfirm condition to confirm button
+ * @param confirmButtonLabel label for confirm button
+ * @param bumpkinParts to show bumpkin in Modal
+ * @param disabled condition to disable the confirm button
+ */
+
 interface ConfirmProps {
   show: boolean;
   onHide: () => void;
@@ -36,6 +50,7 @@ export const ConfirmationModal: React.FC<ConfirmProps> = ({
       <Panel className="sm:w-4/5 m-auto" bumpkinParts={bumpkinParts}>
         <div className="flex flex-col p-2 items-center">
           {icon && <img src={icon} style={imageStyle} />}
+          {/* Convert each message into separate spans */}
           {messages.map((msg, index) => (
             <span key={index} className="text-sm text-start w-full mb-1">
               {msg}
