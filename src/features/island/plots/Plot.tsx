6fbadef99fe4d73f5a658d560ac2a37e2c23641a
Adam Hannigan <adamhannigan81@gmail.com>
2024-06-19 11:52:31 +1000

    [FEAT] Quick select improvements

--- src/features/island/plots/Plot.tsx
+++ src/features/island/plots/Plot.tsx
@@ -240,9 +240,7 @@ export const Plot: React.FC<Props> = ({ id, index }) => {
     if (!crop) {
       if (
         hasFeatureAccess(state, "CROP_QUICK_SELECT") &&
-        (!selectedItem ||
-          !(selectedItem in CROP_SEEDS()) ||
-          !inventory[selectedItem]?.gte(1))
+        (!seed || !(seed in CROP_SEEDS()) || !inventory[seed]?.gte(1))
       ) {
         setShowQuickSelect(true);
         return;
@@ -267,7 +265,7 @@ export const Plot: React.FC<Props> = ({ id, index }) => {
 
       if (
         planted >= 3 &&
-        selectedItem === "Sunflower Seed" &&
+        seed === "Sunflower Seed" &&
         !newState.context.state.inventory["Sunflower Seed"]?.gt(0) &&
         !newState.context.state.inventory["Basic Scarecrow"]
       ) {
@@ -299,13 +297,13 @@ export const Plot: React.FC<Props> = ({ id, index }) => {
       <Transition
         appear={true}
         show={showQuickSelect}
-        enter="transition-opacity transition-transform duration-200"
+        enter="transition-opacity duration-300"
         enterFrom="opacity-0"
         enterTo="opacity-100"
-        leave="transition-opacity duration-100"
+        leave="transition-opacity duration-300"
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
-        className="flex top-[-255%] left-[-90px] absolute z-40 shadow-md"
+        className="flex top-[-255%] left-[50%] absolute z-40"
       >
         <QuickSelect
           icon={SUNNYSIDE.icons.seeds}
@@ -314,7 +312,10 @@ export const Plot: React.FC<Props> = ({ id, index }) => {
             icon: CROP_SEEDS()[seed].yield as InventoryItemName,
           }))}
           onClose={() => setShowQuickSelect(false)}
-          onSelected={() => setPulsating(true)}
+          onSelected={(seed) => {
+            onClick(seed as SeedName);
+            setShowQuickSelect(false);
+          }}
           type={t("quickSelect.cropSeeds")}
         />
       </Transition>
