bce036919c099cac5df53ccd149ace9b1fc40fa4
Elias <101262042+eliasSFL@users.noreply.github.com>
2024-06-26 09:42:08 +0800

    [TESTNET] Incorporate the Hoarding Check into the Game Options Modal

--- src/features/island/hud/components/settings-menu/amoy-actions/AmoyTestnetActions.tsx
+++ src/features/island/hud/components/settings-menu/amoy-actions/AmoyTestnetActions.tsx
@@ -3,30 +3,34 @@ import { useAppTranslation } from "lib/i18n/useAppTranslations";
 import React, { useState } from "react";
 import { DEV_TimeMachine } from "./DEV_TimeMachine";
 import { createPortal } from "react-dom";
-import { DEV_HoardingCheck } from "./DEV_HoardingCheck";
+import { ContentComponentProps } from "../GameOptions";
 
-export const AmoyTestnetActions: React.FC = () => {
+export const AmoyTestnetActions: React.FC<ContentComponentProps> = ({
+  onSubMenuClick,
+}) => {
   const { t } = useAppTranslation();
   const [showTimeMachine, setShowTimeMachine] = useState(false);
 
   return (
     <>
-      <ul className="list-none">
-        <li className="p-1">
-          <Button
-            onClick={() => setShowTimeMachine(!showTimeMachine)}
-            className="p-1"
-          >
-            {t("gameOptions.amoyActions.timeMachine")}
-          </Button>
-        </li>
-        <li className="p-1">
-          <DEV_HoardingCheck network="mainnet" />
-        </li>
-        <li className="p-1">
-          <DEV_HoardingCheck network="amoy" />
-        </li>
-      </ul>
+      <Button
+        onClick={() => setShowTimeMachine(!showTimeMachine)}
+        className="p-1"
+      >
+        {t("gameOptions.amoyActions.timeMachine")}
+      </Button>
+      <Button
+        onClick={() => onSubMenuClick("mainnetHoardingCheck")}
+        className="p-1"
+      >
+        {`Hoarding Check (Mainnet)`}
+      </Button>
+      <Button
+        onClick={() => onSubMenuClick("amoyHoardingCheck")}
+        className="p-1"
+      >
+        {`Hoarding Check (Amoy)`}
+      </Button>
       {showTimeMachine && createPortal(<DEV_TimeMachine />, document.body)}
     </>
   );
