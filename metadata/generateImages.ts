a3aa896038235c24281c0e37789b1a03250b0293
Adam Hannigan <adamhannigan81@gmail.com>
2024-07-01 10:30:55 +1000

    Generate image script

---
+++ metadata/generateImages.ts
@@ -0,0 +1,44 @@
+import fs from "fs";
+import sharp from "sharp";
+
+import { getKeys } from "features/game/types/decorations";
+import { InventoryItemName } from "features/game/types/game";
+import { KNOWN_IDS } from "features/game/types";
+
+const IMAGES: Partial<Record<InventoryItemName, string>> = {
+  "Goblin Gold Champion": "src/assets/sfts/goblin_gold_champion.png",
+  "Goblin Silver Champion": "src/assets/sfts/goblin_gold_champion.png",
+};
+
+const WIDTH = 1920;
+
+async function generateImage({ name }: { name: InventoryItemName }) {
+  const ID = KNOWN_IDS[name];
+
+  const background = await sharp("public/erc1155/images/3x3_bg.png").toBuffer();
+  const itemImage = await sharp(IMAGES[name]).toBuffer();
+
+  // Composite item image onto background
+  const mergedImage = await sharp(background)
+    .composite([{ input: itemImage }])
+
+    .toBuffer();
+
+  const resized = await sharp(mergedImage)
+    .resize({
+      width: WIDTH,
+
+      kernel: sharp.kernel.nearest,
+    })
+    .toBuffer();
+
+  fs.writeFileSync(`public/erc1155/images/${ID}.png`, resized);
+}
+
+export const generateImages = () => {
+  getKeys(IMAGES).forEach((item) => {
+    generateImage({ name: item });
+  });
+};
+
+generateImages();
