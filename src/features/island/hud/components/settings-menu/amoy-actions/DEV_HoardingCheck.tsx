2242daf0b9834466f27c6c46cb7331a458a0c57b
Elias <101262042+eliasSFL@users.noreply.github.com>
2024-07-10 11:21:40 +0800

    [TESTNET] Incorporate the Hoarding Check into the Game Options Modal (#3939)
    * [TESTNET] Incorporate the Hoarding Check into the Game Options Modal
    
    * Simplify Blockchain Settings

--- src/features/island/hud/components/settings-menu/amoy-actions/DEV_HoardingCheck.tsx
+++ src/features/island/hud/components/settings-menu/amoy-actions/DEV_HoardingCheck.tsx
@@ -1,19 +1,20 @@
 import { Button } from "components/ui/Button";
-import { Panel } from "components/ui/Panel";
 import { KNOWN_IDS } from "features/game/types";
 import React, { ChangeEvent, useState } from "react";
-import { Modal } from "components/ui/Modal";
 import GameABI from "lib/blockchain/abis/SunflowerLandGame.json";
 import Web3 from "web3";
 import { AbiItem } from "web3-utils";
 import { BumpkinItem, ITEM_IDS } from "features/game/types/bumpkin";
 import { useAppTranslation } from "lib/i18n/useAppTranslations";
+import { ContentComponentProps } from "../GameOptions";
 
 interface Props {
   network: "mainnet" | "amoy";
 }
 
-const HoarderCheck: React.FC<Props> = ({ network }) => {
+export const DEV_HoarderCheck: React.FC<Props & ContentComponentProps> = ({
+  network,
+}) => {
   const { t } = useAppTranslation();
   const [loading, setLoading] = useState(false);
   const [farmId, setFarmId] = useState("");
@@ -129,11 +130,11 @@ const HoarderCheck: React.FC<Props> = ({ network }) => {
   }
 
   if (loading) {
-    return <Panel>{t("loading")}</Panel>;
+    return <div>{t("loading")}</div>;
   }
 
   return (
-    <Panel className="flex flex-col p-1">
+    <>
       {network}
       <input
         style={{
@@ -162,24 +163,6 @@ const HoarderCheck: React.FC<Props> = ({ network }) => {
 
       <Button onClick={search} className="pt-2">
         {t("check")}
-      </Button>
-    </Panel>
-  );
-};
-
-export const DEV_HoardingCheck: React.FC<Props> = ({ network }) => {
-  const { t } = useAppTranslation();
-  const [show, setShow] = useState(false);
-  return (
-    <>
-      <Modal show={show} onHide={() => setShow(false)}>
-        <HoarderCheck network={network} />
-      </Modal>
-      <Button onClick={() => setShow(!show)}>
-        {`Hoarding Check`}
-        {" ("}
-        {network}
-        {")"}
       </Button>
     </>
   );
