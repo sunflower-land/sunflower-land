c3794113570ab75000ad8b2bb902aa655f9eba67
Elias <101262042+eliasSFL@users.noreply.github.com>
2024-07-12 07:55:08 +0800

    [FIX] Hourglass not burning on remove (#4011)
    * [FIX] Hourglass not burning on remove
    
    * [FIX] Removal Restriction

--- src/features/game/events/landExpansion/burnCollectible.test.ts
+++ src/features/game/events/landExpansion/burnCollectible.test.ts
@@ -3,7 +3,118 @@ import { burnCollectible } from "./burnCollectible";
 import { TEST_FARM } from "features/game/lib/constants";
 
 describe("burnCollectible", () => {
-  it("requires collectible exists", () => {
+  it("requires Hourglass exists", () => {
+    expect(() =>
+      burnCollectible({
+        state: {
+          ...TEST_FARM,
+          inventory: {
+            "Gourmet Hourglass": new Decimal(2),
+          },
+          home: {
+            collectibles: {},
+          },
+        },
+        action: {
+          id: "1",
+          location: "home",
+          name: "Gourmet Hourglass",
+          type: "collectible.burned",
+        },
+      }),
+    ).toThrow("Invalid collectible");
+  });
+
+  it("burns a Hourglass in the home", () => {
+    const state = burnCollectible({
+      state: {
+        ...TEST_FARM,
+        inventory: {
+          "Gourmet Hourglass": new Decimal(2),
+        },
+        home: {
+          collectibles: {
+            "Gourmet Hourglass": [
+              {
+                coordinates: {
+                  x: 0,
+                  y: 0,
+                },
+                id: "1",
+                createdAt: 0,
+                readyAt: 0,
+              },
+            ],
+          },
+        },
+      },
+      action: {
+        id: "1",
+        location: "home",
+        name: "Gourmet Hourglass",
+        type: "collectible.burned",
+      },
+    });
+
+    expect(state.inventory["Gourmet Hourglass"]).toEqual(new Decimal(1));
+    expect(state.home.collectibles).toEqual({});
+  });
+
+  it("burns a Hourglass in the farm", () => {
+    const state = burnCollectible({
+      state: {
+        ...TEST_FARM,
+        inventory: {
+          "Gourmet Hourglass": new Decimal(1),
+        },
+        collectibles: {
+          "Gourmet Hourglass": [
+            {
+              coordinates: {
+                x: 0,
+                y: 0,
+              },
+              id: "1",
+              createdAt: 0,
+              readyAt: 0,
+            },
+            {
+              coordinates: {
+                x: 0,
+                y: 0,
+              },
+              id: "2",
+              createdAt: 0,
+              readyAt: 0,
+            },
+          ],
+        },
+      },
+      action: {
+        id: "1",
+        location: "farm",
+        name: "Gourmet Hourglass",
+        type: "collectible.burned",
+      },
+    });
+
+    expect(state.inventory["Gourmet Hourglass"]).toEqual(new Decimal(0));
+    expect(state.collectibles).toEqual({
+      "Gourmet Hourglass": [
+        {
+          coordinates: {
+            x: 0,
+            y: 0,
+          },
+          id: "2",
+          createdAt: 0,
+          readyAt: 0,
+        },
+      ],
+    });
+  });
+
+  it("requires Time Warp Totem exists", () => {
     expect(() =>
       burnCollectible({
         state: {
@@ -25,7 +136,7 @@ describe("burnCollectible", () => {
     ).toThrow("Invalid collectible");
   });
 
-  it("burns a collectible in the home", () => {
+  it("burns Time Warp Totem in the home", () => {
     const state = burnCollectible({
       state: {
         ...TEST_FARM,
@@ -60,7 +171,7 @@ describe("burnCollectible", () => {
     expect(state.home.collectibles).toEqual({});
   });
 
-  it("burns a collectible in the farm", () => {
+  it("burns Time Warp Totem in the farm", () => {
     const state = burnCollectible({
       state: {
         ...TEST_FARM,
