5cbd437dd8eb639ce4e880f6464034149874ce68
spencer <spencer@sunflower-land.com>
2024-07-29 13:53:03 +1000

    [FEAT] Fix bugged drill pointer events

--- src/features/island/hud/components/DesertDiggingDisplay.tsx
+++ src/features/island/hud/components/DesertDiggingDisplay.tsx
@@ -6,59 +6,83 @@ import { Context } from "features/game/GameProvider";
 import { GameState } from "features/game/types/game";
 import { isCollectibleBuilt } from "features/game/lib/collectibleBuilt";
 import { Modal } from "components/ui/Modal";
-import { DailyPuzzle } from "features/world/ui/beach/Digby";
-import { CloseButtonPanel } from "features/game/components/CloseablePanel";
-import { NPC_WEARABLES } from "lib/npcs";
+import { Digby } from "features/world/ui/beach/Digby";
 
-const getMaxDigs = (game: GameState) => {
+export const getMaxDigs = (game: GameState) => {
   let maxDigs = 25;
+  const extraDigs = game.desert.digging.extraDigs ?? 0;
 
   if (isCollectibleBuilt({ name: "Heart of Davy Jones", game })) {
     maxDigs += 20;
   }
 
-  return maxDigs;
+  if (
+    isCollectibleBuilt({
+      name: "Pharaoh Chicken",
+      game,
+    })
+  ) {
+    maxDigs += 1;
+  }
+
+  return maxDigs + extraDigs;
 };
 
 export const DesertDiggingDisplay = () => {
   const { gameService } = useContext(Context);
   const [gameState] = useActor(gameService);
   const [show, setShow] = useState(false);
+
+  const handleClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    e.preventDefault();
+
+    setShow(!show);
+  };
+
   const dugItems = gameState.context.state.desert.digging.grid.flat();
   const dugCount = gameState.context.state.desert.digging.grid.length;
   const newDigSite = dugCount === 0;
 
   const maxDigs = getMaxDigs(gameState.context.state);
+  const digsLeft = maxDigs - dugCount;
 
   return (
     <>
       <div
-        id="blahh"
-        className="absolute top-36 left-4 cursor-pointer"
-        onClick={() => setShow(!show)}
+        className="absolute top-36 left-4 cursor-pointer z-50 hover:img-highlight"
+        onClick={handleClick}
       >
-        <Label
-          type="default"
-          icon={SUNNYSIDE.tools.sand_shovel}
-          secondaryIcon={SUNNYSIDE.icons.expression_confused}
-        >
-          <span className="text">{`4 digs left`}</span>
-        </Label>
-        {/* <Label
-          type="default"
-          icon={SUNNYSIDE.icons.sad}
-          secondaryIcon={SUNNYSIDE.icons.expression_confused}
-        >
-          <span className="text">{`No digs left...`}</span>
-        </Label> */}
+        {!!digsLeft && dugCount > 0 && (
+          <Label
+            type="default"
+            icon={SUNNYSIDE.tools.sand_shovel}
+            secondaryIcon={SUNNYSIDE.icons.expression_confused}
+          >
+            <span className="text">{`${digsLeft} digs left`}</span>
+          </Label>
+        )}
+        {digsLeft === 0 && (
+          <Label
+            type="default"
+            icon={SUNNYSIDE.icons.sad}
+            secondaryIcon={SUNNYSIDE.icons.expression_confused}
+          >
+            <span className="text">{`No digs left...`}</span>
+          </Label>
+        )}
+        {dugCount === 0 && (
+          <Label
+            type="default"
+            icon={SUNNYSIDE.icons.happy}
+            secondaryIcon={SUNNYSIDE.icons.expression_confused}
+          >
+            <span className="text">{`New dig site!`}</span>
+          </Label>
+        )}
       </div>
       <Modal show={show}>
-        <CloseButtonPanel bumpkinParts={NPC_WEARABLES.digby}>
-          <DailyPuzzle />
-        </CloseButtonPanel>
-        {/* <CloseButtonPanel>
-          <div className="p-1"></div>
-        </CloseButtonPanel> */}
+        <Digby onClose={() => setShow(false)} />
       </Modal>
     </>
   );
