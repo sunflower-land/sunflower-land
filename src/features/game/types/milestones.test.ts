05ff72b43344e8160265e50d6ba2f461ca59043b
Craig <41215134+c-py@users.noreply.github.com>
2024-08-12 12:36:41 +1000

    [FIX] Expert Angler Requirements

--- src/features/game/types/milestones.test.ts
+++ src/features/game/types/milestones.test.ts
@@ -2,9 +2,12 @@ import "lib/__mocks__/configMock";
 
 import { getEncyclopediaFish } from "features/island/hud/components/codex/lib/utils";
 import { FISH_MILESTONES } from "./milestones";
+import { getKeys } from "./decorations";
+import { FarmActivityName } from "./farmActivity";
+import { FISH } from "./fishing";
 
 describe("fishMilestones", () => {
-  it.only("Makes progress on Deep Sea Diver", () => {
+  it("Makes progress on Deep Sea Diver", () => {
     const totalFish = getEncyclopediaFish().length;
 
     const progress = FISH_MILESTONES["Deep Sea Diver"].percentageComplete({
@@ -24,6 +27,45 @@ describe("fishMilestones", () => {
 
     const progress =
       FISH_MILESTONES["Deep Sea Diver"].percentageComplete(allCaught);
+
+    expect(progress).toBe(100);
+  });
+
+  it.each(getKeys(FISH_MILESTONES))("Completes %s", (milestone) => {
+    const allCaught = getKeys(FISH).reduce(
+      (acc, name) => ({ ...acc, [`${name} Caught`]: 100 }),
+      {},
+    );
+
+    const progress = FISH_MILESTONES[milestone].percentageComplete(allCaught);
+
+    expect(progress).toBe(100);
+  });
+
+  it("Allows Marine Marvel Master to be completed without Crimson Carp", () => {
+    const allCaught = getKeys(FISH).reduce(
+      (acc, name) => ({ ...acc, [`${name} Caught`]: 100 }),
+      {} as Partial<Record<FarmActivityName, number>>,
+    );
+
+    delete allCaught["Crimson Carp Caught"];
+
+    const progress =
+      FISH_MILESTONES["Marine Marvel Master"].percentageComplete(allCaught);
+
+    expect(progress).toBe(100);
+  });
+
+  it("Allows Marine Marvel Master to be completed without Kraken Tentacle", () => {
+    const allCaught = getKeys(FISH).reduce(
+      (acc, name) => ({ ...acc, [`${name} Caught`]: 100 }),
+      {} as Partial<Record<FarmActivityName, number>>,
+    );
+
+    delete allCaught["Kraken Tentacle Caught"];
+
+    const progress =
+      FISH_MILESTONES["Marine Marvel Master"].percentageComplete(allCaught);
 
     expect(progress).toBe(100);
   });
