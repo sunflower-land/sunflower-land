5290cf68f0be2925f3753c18336cde9ee3f62a58
Elias <elias@sunflower-land.com>
2024-08-28 18:11:57 +0800

    Script creates json

--- _scripts/translate.ts
+++ _scripts/translate.ts
@@ -18,34 +18,54 @@ import {
 const client = new TranslateClient({ region: "ap-southeast-2" });
 
 async function translateText(text: string, targetLanguage: LanguageCode) {
-  const params = {
-    Text: text,
-    SourceLanguageCode: "en", // English
-    TargetLanguageCode: targetLanguage, // e.g., 'fr' for French
-  };
-  const command = new TranslateTextCommand(params);
-  const response = await client.send(command);
-  return response.TranslatedText;
+  try {
+    if (!text.trim()) {
+      console.warn("Skipping empty term:", text);
+      return ""; // Return an empty string for empty terms
+    }
+
+    const params = {
+      Text: text,
+      SourceLanguageCode: "en",
+      TargetLanguageCode: targetLanguage,
+    };
+    const command = new TranslateTextCommand(params);
+    const response = await client.send(command);
+    return response.TranslatedText;
+  } catch (error) {
+    console.error("Translation error:", error.message);
+    // You can handle the error here (e.g., retry, log, or provide a fallback)
+    throw error; // Rethrow the error if needed
+  }
 }
 
-async function main() {
+async function main(language: LanguageCode) {
   // Find terms which are missing a translation
   // For string, send to AWS API - Translate
-  let translatedTerms: Record<TranslationKeys, string>;
-  const englishTerms = getKeys(ENGLISH_TERMS);
-  englishTerms.forEach(async (name) => {
-    console.log(ENGLISH_TERMS[name]);
-    const translation = await translateText(ENGLISH_TERMS[name], "fr");
-    console.log(translation);
-  });
-  // const languageJson = path.join(
-  //   __dirname,
-  //   `../src/lib/i18n/dictionaries/fr.json`,
-  // );
-  // fs.writeFile(languageJson);
+  const translatedTerms = {};
+  // Iterate through each English term
+  for (const term in ENGLISH_TERMS) {
+    if (Object.hasOwnProperty.call(ENGLISH_TERMS, term)) {
+      const englishText = ENGLISH_TERMS[term];
+      console.log(englishText);
+      const translatedText = await translateText(englishText, language); // Translate to French (you can change the target language)
+      console.log(translatedText);
+      translatedTerms[term] = translatedText;
+    }
+  }
+  console.log(translatedTerms);
+  const languageJson = path.join(
+    __dirname,
+    `../src/lib/i18n/dictionaries/${language}.json`,
+  );
+  fs.writeFile(languageJson, JSON.stringify(translatedTerms), () => undefined);
 }
 
-main();
+// main("fr");
+// main("pt");
+// main("ru");
+// main("tk");
+main("zh-CN");
 
 // Send to AWS Translate
 
