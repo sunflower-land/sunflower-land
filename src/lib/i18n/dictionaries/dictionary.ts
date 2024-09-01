eb069e0a50623ca6be13ca7eaf44d1383e4dcf22
droidze <evrytngspsble@gmail.com>
2024-09-01 07:56:13 +0330

    [FEAT] Support for the Persian Language

--- src/lib/i18n/dictionaries/dictionary.ts
+++ src/lib/i18n/dictionaries/dictionary.ts
@@ -2,6 +2,7 @@ import ENGLISH_TERMS from "./dictionary.json";
 import TURKISH_TERMS from "./tr.json";
 import CHINESE_SIMPLIFIED_TERMS from "./zh-CN.json";
 import PORTUGUESE_TERMS from "./pt-BR.json";
+import PERSIAN_TERMS from "./fa.json";
 import FRENCH_TERMS from "./fr.json";
 import RUSSIAN_TERMS from "./ru.json";
 import { TranslationKeys } from "./types";
@@ -10,12 +11,13 @@ import britishFlag from "assets/sfts/flags/british_flag.gif";
 import usaFlag from "assets/sfts/flags/usa_flag.gif";
 import brazilFlag from "assets/sfts/flags/brazil_flag.gif";
 import portugalFlag from "assets/sfts/flags/portugal_flag.gif";
+import iranFlag from "assets/sfts/flags/iran_flag.gif";
 import franceFlag from "assets/sfts/flags/france_flag.gif";
 import turkeyFlag from "assets/sfts/flags/turkey_flag.gif";
 import chinaFlag from "assets/sfts/flags/china_flag.gif";
 import russiaFlag from "assets/sfts/flags/russia_flag.gif";
 
-export type LanguageCode = "en" | "fr" | "pt-BR" | "tr" | "zh-CN" | "ru";
+export type LanguageCode = "en" | "fa" | "fr" | "pt-BR" | "tr" | "zh-CN" | "ru";
 
 export type TranslationResource = Partial<Record<TranslationKeys, string>>;
 
@@ -30,6 +32,11 @@ export const languageDetails: Record<LanguageCode, LanguageDetails> = {
     languageName: "English",
     languageImage: [britishFlag, usaFlag],
     imageAlt: ["United Kingdom Flag", "United States of America Flag"],
+  },
+  fa: {
+    languageName: "فارسی",
+    languageImage: [iranFlag],
+    imageAlt: ["Iran Flag"],
   },
   fr: {
     languageName: "Français",
@@ -63,6 +70,9 @@ export const resources: Partial<
 > = {
   en: {
     translation: ENGLISH_TERMS,
+  },
+  fa: {
+    translation: PERSIAN_TERMS,
   },
   fr: {
     translation: FRENCH_TERMS,
