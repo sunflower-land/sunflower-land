d9c10e9b6c5346cb77e1f00e9c645fd5ad3aaae7
Craig <41215134+c-py@users.noreply.github.com>
2024-09-02 17:59:58 +1000

    [FIX] Do not create new functions

--- src/lib/i18n/useAppTranslations.ts
+++ src/lib/i18n/useAppTranslations.ts
@@ -2,20 +2,16 @@
 import { useTranslation as useOriginalTranslation } from "react-i18next";
 import { TranslationKeys } from "./dictionaries/types";
 import Decimal from "decimal.js-light";
-import { useCallback } from "react";
 
 // Define a custom hook that wraps the original useTranslation hook
 export const useAppTranslation = () => {
-  const { t: originalT, i18n } = useOriginalTranslation();
+  const { t, i18n } = useOriginalTranslation();
 
-  // Here we cast the original t function to a more strictly typed version
-  const t = useCallback(
-    (
+  return {
+    t: t as (
       key: TranslationKeys,
       args?: { [key: string]: string | number | Decimal },
-    ) => originalT(key, args),
-    [originalT],
-  );
-
-  return { t, i18n };
+    ) => ReturnType<typeof t>,
+    i18n,
+  };
 };
