1bdde2e659478ff984d298e1a7346b27b6e0a013
Craig <41215134+c-py@users.noreply.github.com>
2024-09-02 17:37:59 +1000

    [FIX] Translate Performance

--- src/lib/i18n/useAppTranslations.ts
+++ src/lib/i18n/useAppTranslations.ts
@@ -2,16 +2,20 @@
 import { useTranslation as useOriginalTranslation } from "react-i18next";
 import { TranslationKeys } from "./dictionaries/types";
 import Decimal from "decimal.js-light";
+import { useCallback } from "react";
 
 // Define a custom hook that wraps the original useTranslation hook
 export const useAppTranslation = () => {
   const { t: originalT, i18n } = useOriginalTranslation();
 
   // Here we cast the original t function to a more strictly typed version
-  const t = (
-    key: TranslationKeys,
-    args?: { [key: string]: string | number | Decimal },
-  ) => originalT(key, args);
+  const t = useCallback(
+    (
+      key: TranslationKeys,
+      args?: { [key: string]: string | number | Decimal },
+    ) => originalT(key, args),
+    [originalT],
+  );
 
   return { t, i18n };
 };
