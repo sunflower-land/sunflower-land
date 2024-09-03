965585947f512f920472118496622739003f80f4
0xSacul <63975424+0xSacul@users.noreply.github.com>
2024-09-03 14:32:58 +0200

    [FIX] Scarecrow AOE Size

--- src/features/game/types/bumpkinSkills.ts
+++ src/features/game/types/bumpkinSkills.ts
@@ -522,7 +522,7 @@ export const BUMPKIN_REVAMP_SKILL_TREE: Record<
       points: 2,
       skill: 2,
     },
-    boosts: "Basic Scarecrow AOE increase size to 7x8",
+    boosts: "Basic Scarecrow AOE increase size to 7x7",
     image: SUNNYSIDE?.skills?.green_thumb_LE,
   },
   "Horror Mike": {
@@ -532,7 +532,7 @@ export const BUMPKIN_REVAMP_SKILL_TREE: Record<
       points: 2,
       skill: 2,
     },
-    boosts: "Scary Mike AOE increase size to 7x8",
+    boosts: "Scary Mike AOE increase size to 7x7",
     image: SUNNYSIDE?.skills?.green_thumb_LE,
   },
   // Crops - Tier 3
@@ -585,7 +585,7 @@ export const BUMPKIN_REVAMP_SKILL_TREE: Record<
       points: 3,
       skill: 5,
     },
-    boosts: "Laurie Crow AOE increase size to 7x8",
+    boosts: "Laurie Crow AOE increase size to 7x7",
     image: SUNNYSIDE?.skills?.green_thumb_LE,
   },
 
