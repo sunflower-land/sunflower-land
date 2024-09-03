8e46bc358c773eb556b7921c48f5aac6d58dfad4
Craig <41215134+c-py@users.noreply.github.com>
2024-09-03 18:55:09 +1000

    [FEAT] Use React 18 createRoot

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import { CONFIG } from "lib/config";
 import { PortalApp } from "features/portal/PortalApp";
 
 // TODO - tree shaking to minimise bundle size
 if (CONFIG.PORTAL_APP) {
-  ReactDOM.render(
+  const root = createRoot(document.getElementById("root")!);
+  root.render(
     <React.StrictMode>
       <PortalApp />
     </React.StrictMode>,
-    document.getElementById("root"),
   );
 } else {
   // Main Game
-  ReactDOM.render(
+  const root = createRoot(document.getElementById("root")!);
+  root.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>,
-    document.getElementById("root"),
   );
 }
