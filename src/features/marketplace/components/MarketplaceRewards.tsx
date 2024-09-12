21d0bc59b3f718635bd1532de3401bcf6d69484e
Elias <elias@sunflower-land.com>
2024-09-12 15:12:00 +0800

    Add Reward UI

--- src/features/marketplace/components/MarketplaceRewards.tsx
+++ src/features/marketplace/components/MarketplaceRewards.tsx
@@ -1,9 +1,40 @@
-import React from "react";
+import { useActor } from "@xstate/react";
+import { Label } from "components/ui/Label";
+import { ResizableBar } from "components/ui/ProgressBar";
+import { Context } from "features/game/GameProvider";
+import React, { useContext } from "react";
+import trade_point from "src/assets/icons/trade_point.webp";
 
 export const MarketplaceRewards: React.FC = () => {
+  const { gameService } = useContext(Context);
+  const [
+    {
+      context: { state },
+    },
+  ] = useActor(gameService);
+  // To be used to access total tradePoints
+
+  return <TradePointsProgressBar />;
+};
+
+const TradePointsProgressBar: React.FC = () => {
   return (
-    <div>
-      <p>{`My points`}</p>
+    <div className="flex items-center p-2">
+      <div className="ml-2">
+        <Label icon={trade_point} type="vibrant">{`Trading Level`}</Label>
+      </div>
+      <div className="ml-4">
+        <ResizableBar
+          percentage={70} // Will replace with trading level
+          type="progress"
+          outerDimensions={{
+            width: 30,
+            height: 7,
+          }}
+        />
+      </div>
+      {/* Will be replaced with the level progress */}
+      <p className="ml-4">{`${70 * 0.7}/70`}</p>
     </div>
   );
 };
