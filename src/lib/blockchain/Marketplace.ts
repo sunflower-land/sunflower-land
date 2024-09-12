79f2bfa2234c025e5ff8bac41928e0228f0f322c
Adam Hannigan <adamhannigan81@gmail.com>
2024-09-12 11:16:20 +1000

    Fix TSC

--- src/lib/blockchain/Marketplace.ts
+++ src/lib/blockchain/Marketplace.ts
@@ -51,7 +51,7 @@ export async function acceptOfferTransaction({
       BigInt(fee),
       offer,
     ],
-    account: sender,
+    account: sender as `0x${string}`,
   });
 
   saveTxHash({ event: "transaction.offerAccepted", hash, sessionId, deadline });
