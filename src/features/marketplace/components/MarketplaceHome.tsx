38b05424b30c0a1ecc13d8fcf73991eb1b4e2420
spencer <spencer@sunflower-land.com>
2024-09-16 12:45:56 +1000

    [FEAT] Remove test components

--- src/features/marketplace/components/MarketplaceHome.tsx
+++ src/features/marketplace/components/MarketplaceHome.tsx
@@ -1,20 +1,15 @@
 import { ButtonPanel, InnerPanel } from "components/ui/Panel";
 import { ITEM_DETAILS } from "features/game/types/images";
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 
 import budIcon from "assets/icons/bud.png";
 import wearableIcon from "assets/icons/wearables.webp";
 import lightning from "assets/icons/lightning.png";
 import filterIcon from "assets/icons/filter_icon.webp";
 
-import { Context } from "features/game/GameProvider";
-import { GameWallet } from "features/wallet/Wallet";
-
 import { CollectionName } from "features/game/types/marketplace";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { Collection } from "./Collection";
-import { signTypedData } from "@wagmi/core";
-import { config } from "features/wallet/WalletProvider";
 import { SUNNYSIDE } from "assets/sunnyside";
 import { TextInput } from "components/ui/TextInput";
 import { TranslationKeys } from "lib/i18n/dictionaries/types";
@@ -211,61 +206,5 @@ const Filters: React.FC<{
         </div>
       ))}
     </div>
-  );
-};
-
-const List: React.FC = () => {
-  const [isListing, setIsListing] = useState(false);
-
-  if (isListing) {
-    return <ListTrade />;
-  }
-
-  return <button onClick={() => setIsListing(true)}>{`List`}</button>;
-};
-
-const ListTrade: React.FC = () => {
-  const { gameService } = useContext(Context);
-
-  const [isSigning, setIsSigning] = useState(false);
-
-  const onClick = async () => {
-    setIsSigning(true);
-
-    const signature = await signTypedData(config, {
-      primaryType: "Listing",
-      types: {
-        Listing: [
-          { name: "item", type: "string" },
-          { name: "quantity", type: "uint256" },
-          { name: "SFL", type: "uint256" },
-        ],
-      },
-      message: {
-        item: "Kuebiko",
-        quantity: BigInt(1),
-        SFL: BigInt(1),
-      },
-      domain: {
-        name: "Sunflower Land",
-      },
-    });
-
-    gameService.send("POST_EFFECT", {
-      effect: {
-        type: "marketplace.onChainCollectibleListed",
-        item: "Kuebiko",
-        signature,
-        sfl: 1,
-      },
-    });
-
-    setIsSigning(false);
-  };
-
-  return (
-    <GameWallet action="listTrade">
-      <button onClick={onClick}>{`Sign`}</button>
-    </GameWallet>
   );
 };
