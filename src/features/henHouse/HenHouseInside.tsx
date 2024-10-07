de4eed3e7291ce23edc49dd9ae13281b0435c28a
spencer <spencer@sunflower-land.com>
2024-10-07 21:12:25 +1100

    [FEAT] Add chickens to house

--- src/features/henHouse/HenHouseInside.tsx
+++ src/features/henHouse/HenHouseInside.tsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect } from "react";
+import React, { useContext, useLayoutEffect } from "react";
 import { SUNNYSIDE } from "assets/sunnyside";
 
 import { GRID_WIDTH_PX, PIXEL_SCALE } from "features/game/lib/constants";
@@ -8,18 +8,51 @@ import { Button } from "components/ui/Button";
 import { Section, useScrollIntoView } from "lib/utils/hooks/useScrollIntoView";
 import { useNavigate } from "react-router-dom";
 import { Hud } from "features/island/hud/Hud";
+import { MachineState } from "features/game/lib/gameMachine";
+import { useSelector } from "@xstate/react";
+import { Context } from "features/game/GameProvider";
+import { getKeys } from "features/game/types/decorations";
+import { MapPlacement } from "features/game/expansion/components/MapPlacement";
+import { ANIMALS } from "features/game/types/animals";
+import { Chicken } from "./Chicken";
 
 const background = SUNNYSIDE.land.tent_inside;
 
+const _henHouse = (state: MachineState) => state.context.state.henHouse;
+
 export const HenHouseInside: React.FC = () => {
+  const { gameService } = useContext(Context);
   const { t } = useAppTranslation();
 
   const [scrollIntoView] = useScrollIntoView();
   const navigate = useNavigate();
 
+  const henHouse = useSelector(gameService, _henHouse);
+
   useLayoutEffect(() => {
     scrollIntoView(Section.GenesisBlock, "auto");
   }, []);
+
+  const mapPlacements: Array<JSX.Element> = [];
+
+  const components = getKeys(henHouse.animals).map((id) => {
+    const animal = henHouse.animals[id];
+
+    return (
+      <MapPlacement
+        key={`chicken-${id}`}
+        x={animal.coordinates.x}
+        y={animal.coordinates.y}
+        height={ANIMALS.Chicken.height}
+        width={ANIMALS.Chicken.width}
+        z={1}
+      >
+        <Chicken id={id} />
+      </MapPlacement>
+    );
+  });
+
+  mapPlacements.push(...components);
 
   return (
     <>
@@ -43,6 +76,8 @@ export const HenHouseInside: React.FC = () => {
                   height: `${192 * PIXEL_SCALE}px`,
                 }}
               />
+
+              {mapPlacements.sort((a, b) => b.props.y - a.props.y)}
 
               <Button
                 className="absolute -bottom-16"
