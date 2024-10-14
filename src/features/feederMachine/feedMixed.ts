3469dd6d1965bfc48858c742f5ab2f6b7f847e7f
eliasSFL <elias@sunflower-land.com>
2024-10-14 08:01:07 +0800

    Restart feedMixed function

--- src/features/feederMachine/feedMixed.ts
+++ src/features/feederMachine/feedMixed.ts
@@ -1,7 +1,4 @@
 import cloneDeep from "lodash.clonedeep";
-import Decimal from "decimal.js-light";
-import { trackActivity } from "features/game/types/bumpkinActivity";
-import { getKeys } from "features/game/types/decorations";
 import { Inventory, GameState, AnimalFoodName } from "features/game/types/game";
 
 export type FeedMixedAction = {
@@ -52,60 +49,6 @@ export const ANIMAL_FOODS: Record<AnimalFoodName, Feed> = {
 
 export function feedMixed({ state, action }: Options) {
   const stateCopy: GameState = cloneDeep(state);
-  const bumpkin = stateCopy.bumpkin;
-
-  const feed = ANIMAL_FOODS[action.feed];
-  const amount = action.amount ?? 1;
-
-  if (!feed) {
-    throw new Error("Feed does not exist");
-  }
-
-  if (!bumpkin) {
-    throw new Error("You do not have a bumpkin!");
-  }
-
-  const { coins } = feed;
-
-  if (stateCopy.coins < coins) {
-    throw new Error("Insufficient Coins");
-  }
-
-  const subtractedInventory = getKeys(feed.ingredients).reduce(
-    (inventory, ingredientName) => {
-      const count = inventory[ingredientName] || new Decimal(0);
-      const totalAmount =
-        feed.ingredients[ingredientName]?.mul(amount) || new Decimal(0);
-
-      if (count.lessThan(totalAmount)) {
-        throw new Error(`Insufficient ingredient: ${ingredientName}`);
-      }
-
-      return {
-        ...inventory,
-        [ingredientName]: count.sub(totalAmount),
-      };
-    },
-    stateCopy.inventory,
-  );
-
-  const oldAmount = stateCopy.inventory[action.feed] || new Decimal(0);
-
-  bumpkin.activity = trackActivity(
-    `${action.feed} Mixed`,
-    bumpkin.activity,
-    new Decimal(amount),
-  );
-  bumpkin.activity = trackActivity(
-    "Coins Spent",
-    bumpkin.activity,
-    new Decimal(coins),
-  );
-  stateCopy.coins -= coins;
-  stateCopy.inventory = {
-    ...subtractedInventory,
-    [action.feed]: oldAmount.add(amount) as Decimal,
-  };
 
   return stateCopy;
 }
