98ad90f1c8d19ae5052b6e54a315bdb92ec01240
LittleEinstein ID 73795 | HSP#0439 <104184348+LittleEins@users.noreply.github.com>
2024-10-14 14:33:29 +0800

    [FEAT] Add Large Cow Image

--- src/assets/sunnyside.ts
+++ src/assets/sunnyside.ts
@@ -279,6 +279,7 @@ export const SUNNYSIDE = {
     chickenIdle: `${CONFIG.PROTECTED_IMAGE_URL}/animals/chickens/idle_chicken.gif`,
     chickenAsleep: `${CONFIG.PROTECTED_IMAGE_URL}/animals/chickens/sleeping_chicken.gif`,
     cow: `${CONFIG.PROTECTED_IMAGE_URL}/animals/cows/cow_md.webp`,
+    cow_large: `${CONFIG.PROTECTED_IMAGE_URL}/animals/cows/cow_lg.webp`,
     sheep: `${CONFIG.PROTECTED_IMAGE_URL}/animals/sheep/sheep_md.webp`,
 
     bird: `${CONFIG.PROTECTED_IMAGE_URL}/animals/bird_sheet.png`,
