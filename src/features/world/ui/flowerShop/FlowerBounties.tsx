097719f4546ac78d494d894c3c41ce0dce59c2ee
Adam Hannigan <adamhannigan81@gmail.com>
2024-10-21 12:35:58 +1100

    [FEAT] Flower Bounties

---
+++ src/features/world/ui/flowerShop/FlowerBounties.tsx
@@ -0,0 +1,244 @@
+import { useSelector } from "@xstate/react";
+import { SUNNYSIDE } from "assets/sunnyside";
+import classNames from "classnames";
+import { Button } from "components/ui/Button";
+import { HudContainer } from "components/ui/HudContainer";
+import { Label } from "components/ui/Label";
+import { ButtonPanel, InnerPanel, Panel } from "components/ui/Panel";
+import { CloseButtonPanel } from "features/game/components/CloseablePanel";
+import { SpeakingModal } from "features/game/components/SpeakingModal";
+import { getAnimalLevel } from "features/game/events/landExpansion/sellAnimal";
+import { Context } from "features/game/GameProvider";
+import { PIXEL_SCALE } from "features/game/lib/constants";
+import { weekResetsAt } from "features/game/lib/factions";
+import { MachineState } from "features/game/lib/gameMachine";
+import { AnimalType } from "features/game/types/animals";
+import { getKeys } from "features/game/types/decorations";
+import { FlowerName, FLOWERS } from "features/game/types/flowers";
+import { Animal, BountyRequest } from "features/game/types/game";
+import { ITEM_DETAILS } from "features/game/types/images";
+import { TimerDisplay } from "features/retreat/components/auctioneer/AuctionDetails";
+import { useAppTranslation } from "lib/i18n/useAppTranslations";
+import { NPC_WEARABLES } from "lib/npcs";
+import { useCountdown } from "lib/utils/hooks/useCountdown";
+import React, { useContext, useState } from "react";
+
+const _exchange = (state: MachineState) => state.context.state.bounties;
+
+interface Props {
+  onClose: () => void;
+}
+
+function acknowledgeIntro() {
+  localStorage.setItem(
+    "flower.bounties.acknowledged",
+    new Date().toISOString(),
+  );
+}
+
+function hasReadIntro() {
+  return !!localStorage.getItem("flower.bounties.acknowledged");
+}
+
+export const FlowerBounties: React.FC<Props> = ({ onClose }) => {
+  const { gameService } = useContext(Context);
+  const exchange = useSelector(gameService, _exchange);
+
+  const { t } = useAppTranslation();
+
+  const [showIntro, setShowIntro] = useState(!hasReadIntro());
+  const [deal, setDeal] = useState<BountyRequest>();
+
+  const state = gameService.getSnapshot().context.state;
+
+  const deals = exchange.requests.filter((deal) => deal.name in FLOWERS);
+
+  const expiresAt = useCountdown(weekResetsAt());
+
+  if (showIntro) {
+    return (
+      <SpeakingModal
+        message={[
+          {
+            text: t("bounties.flower.intro.one"),
+          },
+          {
+            text: t("bounties.flower.intro.two"),
+          },
+        ]}
+        bumpkinParts={NPC_WEARABLES.poppy}
+        onClose={() => {
+          acknowledgeIntro();
+          setShowIntro(false);
+        }}
+      />
+    );
+  }
+
+  const sell = () => {};
+
+  if (deal) {
+    return <Deal deal={deal} onClose={onClose} onSold={sell} />;
+  }
+
+  return (
+    <CloseButtonPanel bumpkinParts={NPC_WEARABLES.poppy} onClose={onClose}>
+      <div className="p-1">
+        <div className="flex justify-between items-center mb-2">
+          <Label type="default">{t("bounties.board")}</Label>
+          <Label type="info" icon={SUNNYSIDE.icons.stopwatch}>
+            <TimerDisplay time={expiresAt} />
+          </Label>
+        </div>
+
+        <p className="text-xs mb-2">{t("bounties.board.info")}</p>
+        <div className="flex flex-wrap">
+          {deals.length === 0 && (
+            <p className="text-sm">{t("bounties.board.empty")}</p>
+          )}
+          {deals.map((deal) => {
+            const isSold = !!state.bounties.completed.find(
+              (request) => request.id === deal.id,
+            );
+
+            return (
+              <div
+                key={deal.id}
+                className={classNames("w-1/3 sm:w-1/4 pr-1.5 pb-1.5", {
+                  "pointer-events-none": isSold,
+                })}
+              >
+                <ButtonPanel
+                  // disabled={isDisabled}
+                  onClick={() => setDeal(deal)}
+                >
+                  <div className="flex justify-center items-center my-2 mb-6">
+                    <div className="relative">
+                      <img
+                        src={ITEM_DETAILS[deal.name].image}
+                        className="w-8 z-20"
+                      />
+                    </div>
+                  </div>
+
+                  {!!isSold && (
+                    <Label
+                      type="success"
+                      className={"absolute -top-3.5 text-center p-1 "}
+                      style={{
+                        right: `${PIXEL_SCALE * -3}px`,
+                        // width: `calc(100% + ${PIXEL_SCALE * 6}px)`,
+                        height: "25px",
+                      }}
+                    >
+                      {t("bounties.sold")}
+                    </Label>
+                  )}
+
+                  {!!deal.coins && (
+                    <Label
+                      type="warning"
+                      icon={SUNNYSIDE.ui.coinsImg}
+                      className={"absolute -bottom-2 text-center p-1 "}
+                      style={{
+                        left: `${PIXEL_SCALE * -3}px`,
+                        right: `${PIXEL_SCALE * -3}px`,
+                        width: `calc(100% + ${PIXEL_SCALE * 6}px)`,
+                        height: "25px",
+                      }}
+                    >
+                      {deal.coins}
+                    </Label>
+                  )}
+
+                  {getKeys(deal.items ?? {}).map((name) => {
+                    return (
+                      <Label
+                        key={name}
+                        type="warning"
+                        icon={ITEM_DETAILS[name].image}
+                        className={"absolute -bottom-2 text-center p-1 "}
+                        style={{
+                          left: `${PIXEL_SCALE * -3}px`,
+                          right: `${PIXEL_SCALE * -3}px`,
+                          width: `calc(100% + ${PIXEL_SCALE * 6}px)`,
+                          height: "25px",
+                        }}
+                      >
+                        {deal.items?.[name]}
+                      </Label>
+                    );
+                  })}
+                </ButtonPanel>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </CloseButtonPanel>
+  );
+};
+
+const Deal: React.FC<{
+  deal: BountyRequest;
+  onClose: () => void;
+  onSold: () => void;
+}> = ({ deal, onClose, onSold }) => {
+  const { gameService } = useContext(Context);
+
+  const { t } = useAppTranslation();
+  const sell = () => {
+    gameService.send("bounty.sold", {
+      requestId: deal.id,
+    });
+
+    onSold();
+  };
+
+  if (!deal) {
+    return null;
+  }
+
+  return (
+    <Panel>
+      <div className="p-2">
+        <div className="mb-2 flex flex-wrap">
+          <Label
+            type="default"
+            icon={ITEM_DETAILS[deal.name].image}
+            className="mr-2"
+          >
+            {name}
+          </Label>
+          {!!deal.coins && (
+            <Label type="warning" icon={SUNNYSIDE.ui.coinsImg}>
+              {deal.coins}
+            </Label>
+          )}
+
+          {getKeys(deal.items ?? {}).map((name) => {
+            <Label key={name} type="warning" icon={ITEM_DETAILS[name].image}>
+              {deal.items?.[name]}
+            </Label>;
+          })}
+        </div>
+
+        <p>
+          {deal.coins
+            ? t("bounties.sell.coins", { amount: deal.coins })
+            : t("bounties.sell.items", {
+                amount: getKeys(deal.items ?? {})
+                  .map((name) => `${deal.items?.[name]} x ${name}`)
+                  .join(" - "),
+              })}
+        </p>
+      </div>
+      <div className="flex">
+        <Button className="mr-1" onClick={onClose}>
+          {t("cancel")}
+        </Button>
+        <Button onClick={sell}>{t("confirm")}</Button>
+      </div>
+    </Panel>
+  );
+};
