be01ea451c865fd83db8310b8d7ad4929a84b2fa
Elias <101262042+eliasSFL@users.noreply.github.com>
2024-10-28 03:41:20 +0000

    [FEAT] Update start date to Nov 4th

--- src/lib/flags.ts
+++ src/lib/flags.ts
@@ -48,13 +48,13 @@ const featureFlags = {
   SKILLS_REVAMP: testnetFeatureFlag,
   FSL: betaTimeBasedFeatureFlag(new Date("2024-10-10T00:00:00Z")),
   NEW_RESOURCES_GE: defaultFeatureFlag,
-  ANIMAL_BUILDINGS: betaTimeBasedFeatureFlag(new Date("2024-11-01T00:00:00Z")),
-  BARLEY: betaTimeBasedFeatureFlag(new Date("2024-11-01T00:00:00Z")),
+  ANIMAL_BUILDINGS: betaTimeBasedFeatureFlag(new Date("2024-11-04T00:00:00Z")),
+  BARLEY: betaTimeBasedFeatureFlag(new Date("2024-11-04T00:00:00Z")),
   GEM_BOOSTS: (game: GameState) => game.experiments.includes("GEM_BOOSTS"),
-  CHICKEN_GARBO: betaTimeBasedFeatureFlag(new Date("2024-11-01T00:00:00Z")),
-  CRAFTING_BOX: betaTimeBasedFeatureFlag(new Date("2024-11-01T00:00:00Z")),
-  FLOWER_BOUNTIES: timeBasedFeatureFlag(new Date("2024-11-01T00:00:00Z")),
-  BEDS: timeBasedFeatureFlag(new Date("2024-11-01T00:00:00Z")),
+  CHICKEN_GARBO: betaTimeBasedFeatureFlag(new Date("2024-11-04T00:00:00Z")),
+  CRAFTING_BOX: betaTimeBasedFeatureFlag(new Date("2024-11-04T00:00:00Z")),
+  FLOWER_BOUNTIES: timeBasedFeatureFlag(new Date("2024-11-04T00:00:00Z")),
+  BEDS: timeBasedFeatureFlag(new Date("2024-11-04T00:00:00Z")),
 } satisfies Record<string, FeatureFlag>;
 
 export type FeatureName = keyof typeof featureFlags;
