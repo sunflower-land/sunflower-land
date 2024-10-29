7765efcd114ccb405fb8f45bfe3ed7033686fc1b
Adam Hannigan <adamhannigan81@gmail.com>
2024-10-29 17:12:45 +1100

    [FIX] Chicken rescue

--- src/features/world/ui/InteractableModals.tsx
+++ src/features/world/ui/InteractableModals.tsx
@@ -141,7 +141,6 @@ class InteractableModalManager {
 export const interactableModalManager = new InteractableModalManager();
 
 function getInitialModal(scene: SceneId): InteractableName | undefined {
-  return "chicken_rescue";
   if (
     !hasReadFactionIntro() &&
     (scene === "goblin_house" ||
