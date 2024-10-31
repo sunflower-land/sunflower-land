b887ec3f554d6b4b6716d544138905f86ed29d04
LittleEinstein ID 73795 | HSP#0439 <104184348+LittleEins@users.noreply.github.com>
2024-10-31 09:58:05 +0800

    [FEAT] Auction Wearable Item Buff Label

--- src/features/game/types/bumpkinItemBuffs.ts
+++ src/features/game/types/bumpkinItemBuffs.ts
@@ -560,6 +560,36 @@ export const BUMPKIN_ITEM_BUFF_LABELS: Partial<Record<BumpkinItem, BuffLabel>> =
       boostTypeIcon: SUNNYSIDE.icons.stopwatch,
       boostedItemIcon: SUNNYSIDE.animals.sheepSleeping,
     },
+    "Infernal Bullwhip": {
+      shortDescription: translate("description.infernalBullwhip.boost"),
+      labelType: "vibrant",
+      boostTypeIcon: lightning,
+      boostedItemIcon: SUNNYSIDE.animalFoods.mixed_grain,
+    },
+    "Chicken Suit": {
+      shortDescription: translate("description.chickenSuit.boost"),
+      labelType: "vibrant",
+      boostTypeIcon: powerup,
+      boostedItemIcon: SUNNYSIDE.resource.feather,
+    },
+    "Cowbell Necklace": {
+      shortDescription: translate("description.cowbellNecklace.boost"),
+      labelType: "info",
+      boostTypeIcon: powerup,
+      boostedItemIcon: SUNNYSIDE.resource.milk,
+    },
+    "Black Sheep Onesie": {
+      shortDescription: translate("description.blackSheepOnesie.boost"),
+      labelType: "info",
+      boostTypeIcon: powerup,
+      boostedItemIcon: SUNNYSIDE.resource.wool,
+    },
+    "Merino Jumper": {
+      shortDescription: translate("description.merinoJumper.boost"),
+      labelType: "info",
+      boostTypeIcon: powerup,
+      boostedItemIcon: SUNNYSIDE.resource.merino_wool,
+    },
 
     ...SPECIAL_ITEM_LABELS,
   };
