e1a10833e04aa51e0745654731e6b3cab582e71f
droidze <evrytngspsble@gmail.com>
2024-11-01 09:16:50 +0330

    [FIX] FIx Next Drop Supply/Ingredient Label + Minor UI Fixes

--- src/features/island/hud/components/codex/components/SeasonalAuctions.tsx
+++ src/features/island/hud/components/codex/components/SeasonalAuctions.tsx
@@ -127,10 +127,17 @@ const NextDrop: React.FC<{ auctions: AuctionItems }> = ({ auctions }) => {
           <Label className="-ml-1 mb-1" type="default">
             {t("season.codex.nextDrop")}
           </Label>
+          {nextDrop.sfl > 0 && (
+            <Label type="formula" icon={sfl} className="mb-1">
+              {t("season.codex.nextDrop.available", {
+                dropSupply: nextDrop.supply,
+              })}
+            </Label>
+          )}
           {getKeys(nextDrop.ingredients).map((name) => (
             <Label
               type="formula"
-              icon={nextDrop.sfl > 0 ? sfl : ITEM_DETAILS[name].image}
+              icon={ITEM_DETAILS[name].image}
               className="mb-1"
               key={name}
             >
@@ -159,7 +166,7 @@ const NextDrop: React.FC<{ auctions: AuctionItems }> = ({ auctions }) => {
                         type="vibrant"
                         icon={lightning}
                         style={{
-                          marginLeft: "4px",
+                          marginLeft: "3px",
                         }}
                       >
                         {nextDrop.type === "collectible"
@@ -172,7 +179,7 @@ const NextDrop: React.FC<{ auctions: AuctionItems }> = ({ auctions }) => {
                         icon={buffLabel.boostTypeIcon}
                         secondaryIcon={buffLabel.boostedItemIcon}
                         style={{
-                          marginLeft: "4px",
+                          marginLeft: "3px",
                         }}
                       >
                         {buffLabel.shortDescription}
@@ -185,7 +192,7 @@ const NextDrop: React.FC<{ auctions: AuctionItems }> = ({ auctions }) => {
                       type="default"
                       icon={SUNNYSIDE.icons.heart}
                       style={{
-                        marginLeft: "7px",
+                        marginLeft: "6px",
                       }}
                     >
                       {nextDrop.type === "collectible"
@@ -194,13 +201,23 @@ const NextDrop: React.FC<{ auctions: AuctionItems }> = ({ auctions }) => {
                     </Label>
                   </div>
                 )}
+                {isMobile && (
+                  <div className="flex flex-col text-start text-base">
+                    <TimerDisplay time={starts} />
+                    <span className="text-xs">
+                      {new Date(nextDrop.startAt).toLocaleString().slice(0, -3)}
+                    </span>
+                  </div>
+                )}
               </div>
-              <div className="flex flex-col text-center mt-0.5 text-base">
-                <TimerDisplay time={starts} />
-                <span className="text-xs">
-                  {new Date(nextDrop.startAt).toLocaleString().slice(0, -3)}
-                </span>
-              </div>
+              {!isMobile && (
+                <div className="flex flex-col text-end text-base">
+                  <TimerDisplay time={starts} />
+                  <span className="text-xs">
+                    {new Date(nextDrop.startAt).toLocaleString().slice(0, -3)}
+                  </span>
+                </div>
+              )}
             </div>
           </div>
         </div>
