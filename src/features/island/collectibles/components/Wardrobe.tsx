ff5a53561003e614c1d71d46bdcdf1ea25f05a45
Droid <109084288+Droidze@users.noreply.github.com>
2024-11-05 03:21:36 +0330

    [CHORE] Remove Craft Decorations and Wardrobe Modal (#4700)
    * [CHORE] Remove Craft Decorations and Wardrobe Modal
    
    * remove craft decorations codes
    
    * remove unused terms
    
    * fix tsc

--- src/features/island/collectibles/components/Wardrobe.tsx
+++ src/features/island/collectibles/components/Wardrobe.tsx
@@ -1,37 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
 import { PIXEL_SCALE } from "features/game/lib/constants";
 import { SUNNYSIDE } from "assets/sunnyside";
-import { Modal } from "components/ui/Modal";
-import { CloseButtonPanel } from "features/game/components/CloseablePanel";
-import { Button } from "components/ui/Button";
-import { useAppTranslation } from "lib/i18n/useAppTranslations";
-import { Label } from "components/ui/Label";
-import lockIcon from "assets/icons/lock.png";
 
 export const Wardrobe: React.FC = () => {
-  const [showModal, setShowModal] = useState(false);
-
-  const { t } = useAppTranslation();
-
-  const open = () => {
-    setShowModal(true);
-  };
-
   return (
     <>
-      <Modal show={showModal} onHide={() => setShowModal(false)}>
-        <CloseButtonPanel>
-          <div className="p-2">
-            <Label type="default" icon={lockIcon} className="mb-2">
-              {t("coming.soon")}
-            </Label>
-            <p className="text-sm">{t("crafting.coming.soon")}</p>
-          </div>
-          <Button onClick={() => setShowModal(false)}>{t("close")}</Button>
-        </CloseButtonPanel>
-      </Modal>
-
       <img
         src={SUNNYSIDE.decorations.wardrobe}
         style={{
@@ -39,9 +13,8 @@ export const Wardrobe: React.FC = () => {
           bottom: `${PIXEL_SCALE * 6}px`,
           left: `${PIXEL_SCALE * 1.5}px`,
         }}
-        className="absolute cursor-pointer hover:img-highlight"
+        className="absolute"
         alt="Wardrobe"
-        onClick={open}
       />
     </>
   );
