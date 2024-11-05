f1b20d12ee37dc63a9d836e051242a0466953d17
LittleEins | Sunflower <104184348+LittleEins@users.noreply.github.com>
2024-11-05 14:48:52 +0800

    [FIX] Key Reduction When No History in Megastore (#4697)
    * [FIX] Key Reduction When No History in Megastore
    
    * [FIX] Key Reduction When No Key in Inventory
    
    Adds another layer to make sure key is not counted if none is in inventory and no history

--- src/features/game/events/landExpansion/buySeasonalItem.ts
+++ src/features/game/events/landExpansion/buySeasonalItem.ts
@@ -200,15 +200,19 @@ export function isKeyBoughtWithinSeason(
   const keyBoughtAt =
     game.pumpkinPlaza.keysBought?.megastore[tierKey as Keys]?.boughtAt;
   const seasonTime = SEASONS[getCurrentSeason()];
+  const currentKey = game.inventory[tierKey]; // used to check if player has key in inventory
+  //If player has no history of buying keys at megastore
+  if (!keyBoughtAt && isLowerTier && !currentKey) return true;
 
+  // Returns false if key is bought outside current season, otherwise, true
   if (keyBoughtAt) {
     const isWithinSeason =
       new Date(keyBoughtAt) >= seasonTime.startDate &&
       new Date(keyBoughtAt) <= seasonTime.endDate;
-
     return isWithinSeason;
   }
 
+  // This will only be triggered if isLowerTier is false
   return false;
 }
 
