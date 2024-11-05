d66acf54ebb8922f53ecc33c49a5757727dae5a0
LittleEinstein ID 73795 | HSP#0439 <104184348+LittleEins@users.noreply.github.com>
2024-11-05 12:45:35 +0800

    [FEAT] Halloween Wearable Withdrawal Restriction

--- src/features/game/types/withdrawables.ts
+++ src/features/game/types/withdrawables.ts
@@ -1755,4 +1755,7 @@ export const BUMPKIN_WITHDRAWABLES: Record<
   Sickle: () => false,
   "Speed Boots": () => false,
   "Tomato Apron": () => false,
+  "Adventurer's Suit": () => false,
+  "Adventurer's Torch": () => false,
+  "Pumpkin Head": () => false,
 };
