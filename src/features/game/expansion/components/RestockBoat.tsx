2d309e8289dcff3079e5f4f10227b215f0956a0c
Elias <101262042+eliasSFL@users.noreply.github.com>
2024-11-09 06:10:02 +0000

    Reapply "[FIX] Shipment clears stock of items not in shipment (#4741)"
    This reverts commit 4dc4176157ffcda349bc36eab8af3df54da04d59.

--- src/features/game/expansion/components/RestockBoat.tsx
+++ src/features/game/expansion/components/RestockBoat.tsx
@@ -4,10 +4,7 @@ import { MapPlacement } from "./MapPlacement";
 import { PIXEL_SCALE, StockableName } from "features/game/lib/constants";
 import { useSelector } from "@xstate/react";
 import { MachineState } from "features/game/lib/gameMachine";
-import {
-  canRestockShipment,
-  SHIPMENT_STOCK,
-} from "features/game/events/landExpansion/shipmentRestocked";
+import { canRestockShipment } from "features/game/events/landExpansion/shipmentRestocked";
 import { Context } from "features/game/GameProvider";
 import { Modal } from "components/ui/Modal";
 import { Label } from "components/ui/Label";
@@ -17,9 +14,10 @@ import { Button } from "components/ui/Button";
 import confetti from "canvas-confetti";
 import { hasFeatureAccess } from "lib/flags";
 import { useAppTranslation } from "lib/i18n/useAppTranslations";
-import { Decimal } from "decimal.js-light";
 import { Box } from "components/ui/Box";
 import { ITEM_DETAILS } from "features/game/types/images";
+import { CROP_LIFECYCLE } from "features/island/plots/lib/plant";
+import Decimal from "decimal.js-light";
 
 const expansions = (state: MachineState) =>
   state.context.state.inventory["Basic Land"]?.toNumber() ?? 3;
@@ -28,7 +26,11 @@ const canRestock = (state: MachineState) =>
   canRestockShipment({ game: state.context.state }) &&
   !!state.context.state.shipments.restockedAt;
 
-export const RestockBoat: React.FC = () => {
+export const RestockBoat: React.FC<{
+  restockSeeds: [string, number][];
+  restockTools: [string, number][];
+  getShipmentAmount: (item: StockableName, amount: number) => Decimal;
+}> = ({ restockSeeds, restockTools, getShipmentAmount }) => {
   const { t } = useAppTranslation();
   const [isOpen, setIsOpen] = useState(false);
   const { gameService, showAnimations } = useContext(Context);
@@ -73,14 +75,55 @@ export const RestockBoat: React.FC = () => {
             <p className="text-sm mb-2">{t("gems.shipment.success")}</p>
             <p className="text-sm mb-2">{t("gems.shipment.shops")}</p>
           </div>
-          <div className="mt-1 flex flex-wrap">
-            {Object.entries(SHIPMENT_STOCK).map(([item, amount]) => (
-              <Box
-                key={item}
-                count={new Decimal(amount)}
-                image={ITEM_DETAILS[item as StockableName].image}
-              />
-            ))}
+          <div className="mt-1 h-auto overflow-y-auto overflow-x-hidden scrollable pl-1">
+            {restockTools.length > 0 && (
+              <Label
+                icon={ITEM_DETAILS.Axe.image}
+                type="default"
+                className="ml-2 mb-1"
+              >
+                {t("tools")}
+              </Label>
+            )}
+            <div className="flex flex-wrap mb-2">
+              {restockTools.map(([item, amount]) => {
+                const shipmentAmount = getShipmentAmount(
+                  item as StockableName,
+                  amount,
+                );
+                return (
+                  <Box
+                    key={item}
+                    count={shipmentAmount}
+                    image={ITEM_DETAILS[item as StockableName].image}
+                  />
+                );
+              })}
+            </div>
+            {restockSeeds.length > 0 && (
+              <Label
+                icon={CROP_LIFECYCLE.Sunflower.seed}
+                type="default"
+                className="ml-2 mb-1"
+              >
+                {t("seeds")}
+              </Label>
+            )}
+            <div className="flex flex-wrap mb-2">
+              {restockSeeds.map(([item, amount]) => {
+                const shipmentAmount = getShipmentAmount(
+                  item as StockableName,
+                  amount,
+                );
+                return (
+                  <Box
+                    key={item}
+                    count={shipmentAmount}
+                    image={ITEM_DETAILS[item as StockableName].image}
+                  />
+                );
+              })}
+            </div>
           </div>
           <Button
             onClick={() => {
