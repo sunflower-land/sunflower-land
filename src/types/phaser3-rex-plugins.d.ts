83f41f3192ba94f6dbc696647c94e359770986f4
0xSacul <63975424+0xSacul@users.noreply.github.com>
2024-11-11 00:11:01 +0100

    [FIX] Declare rexNinePatch2

--- src/types/phaser3-rex-plugins.d.ts
+++ src/types/phaser3-rex-plugins.d.ts
@@ -2,3 +2,7 @@ declare module "phaser3-rex-plugins/plugins/ninepatch-plugin.js" {
   const value: any;
   export default value;
 }
+declare module "phaser3-rex-plugins/plugins/ninepatch2-plugin.js" {
+  const value: any;
+  export default value;
+}
