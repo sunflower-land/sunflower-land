19304aa88d4da0efe09b94e660683e4ad2e2fad4
Elias <101262042+eliasSFL@users.noreply.github.com>
2024-11-12 16:24:30 +0800

    Use lightning icon

--- src/features/game/types/bumpkinItemBuffs.ts
+++ src/features/game/types/bumpkinItemBuffs.ts
@@ -82,7 +82,7 @@ export const BUMPKIN_ITEM_BUFF_LABELS: Partial<Record<BumpkinItem, BuffLabel>> =
     "Sunflower Shield": {
       shortDescription: translate("bumpkinItemBuff.sunflower.shield.boost"),
       labelType: "success",
-      boostTypeIcon: powerup,
+      boostTypeIcon: lightning,
       boostedItemIcon: CROP_LIFECYCLE.Sunflower.crop,
     },
     "Sunflower Amulet": {
