1adbb72d81dfca8d75ec4a523ff5f1eb8ea56cd0
0xSacul <63975424+0xSacul@users.noreply.github.com>
2024-11-13 03:14:50 +0100

    [FEAT] Initial Commit

--- src/features/game/types/chests.test.ts
+++ src/features/game/types/chests.test.ts
@@ -2,40 +2,40 @@ import { BASIC_REWARDS, LUXURY_REWARDS, RARE_REWARDS } from "./chests";
 import { SEASONS } from "./seasons";
 
 describe("SEASONAL_REWARDS", () => {
-  it("does not include Grape Pants during Clash of Factions Season", () => {
+  it("does not include Cow Scratcher during Pharaoh's Treasure Season", () => {
     jest.useFakeTimers();
-    jest.setSystemTime(SEASONS["Clash of Factions"].startDate);
+    jest.setSystemTime(SEASONS["Pharaoh's Treasure"].startDate);
 
     expect(BASIC_REWARDS()).not.toContainEqual({
-      wearables: { "Grape Pants": 1 },
+      items: { "Cow Scratcher": 1 },
       weighting: 5,
     });
     expect(RARE_REWARDS()).not.toContainEqual({
-      wearables: { "Grape Pants": 1 },
+      items: { "Cow Scratcher": 1 },
       weighting: 25,
     });
     expect(LUXURY_REWARDS()).not.toContainEqual({
-      wearables: { "Grape Pants": 1 },
+      items: { "Cow Scratcher": 1 },
       weighting: 25,
     });
 
     jest.clearAllTimers();
   });
 
-  it("includes Grape Pants during Pharaoh's Treasure Season", () => {
+  it("includes Cow Scratcher during Bull Run Season because we love to see Cows scratching their back", () => {
     jest.useFakeTimers();
-    jest.setSystemTime(SEASONS["Pharaoh's Treasure"].startDate);
+    jest.setSystemTime(SEASONS["Bull Run"].startDate);
 
     expect(BASIC_REWARDS()).toContainEqual({
-      wearables: { "Grape Pants": 1 },
+      items: { "Cow Scratcher": 1 },
       weighting: 2250,
     });
     expect(RARE_REWARDS()).toContainEqual({
-      wearables: { "Grape Pants": 1 },
+      items: { "Cow Scratcher": 1 },
       weighting: 11250,
     });
     expect(LUXURY_REWARDS()).toContainEqual({
-      wearables: { "Grape Pants": 1 },
+      items: { "Cow Scratcher": 1 },
       weighting: 11250,
     });
 
