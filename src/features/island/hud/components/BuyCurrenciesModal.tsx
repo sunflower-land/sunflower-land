98f36a6268318471067936030f5bfc6ca0fa9df0
Droid <109084288+Droidze@users.noreply.github.com>
2024-11-14 04:53:12 +0330

    [CHORE] Update SFL/Coins Tab Style to Better Show on Small Screens (#4767)

--- src/features/island/hud/components/BuyCurrenciesModal.tsx
+++ src/features/island/hud/components/BuyCurrenciesModal.tsx
@@ -32,6 +32,7 @@ import { useAppTranslation } from "lib/i18n/useAppTranslations";
 import { VIPItems } from "../../../game/components/modal/components/VIPItems";
 import { PIXEL_SCALE } from "features/game/lib/constants";
 import { ITEM_DETAILS } from "features/game/types/images";
+import { isMobile } from "mobile-device-detect";
 
 const COIN_IMAGES = [coinsScattered, coinsIcon, coinsStack];
 
@@ -201,7 +202,7 @@ export const BuyCurrenciesModal: React.FC<Props> = ({
               </Label>
               {/* Exchange packages */}
               {!exchangePackageId && (
-                <div className="flex px-1 pb-2 justify-between gap-1  sm:text-sm sm:gap-2 overflow-x-scroll overflow-y-hidden scrollable">
+                <div className="flex px-1 pb-1 justify-between gap-1  sm:text-sm sm:gap-2">
                   {Object.keys(SFL_TO_COIN_PACKAGES).map((packageId, index) => {
                     const option = SFL_TO_COIN_PACKAGES[Number(packageId)];
 
@@ -211,14 +212,17 @@ export const BuyCurrenciesModal: React.FC<Props> = ({
                         className="flex relative flex-col flex-1 items-center p-2 cursor-pointer hover:bg-brown-300"
                         onClick={() => setExchangePackageId(Number(packageId))}
                       >
-                        <span className="whitespace-nowrap mb-2">{`${option.coins} coins`}</span>
+                        <span className="whitespace-nowrap mb-2">{`${option.coins} x`}</span>
                         <div className="flex flex-1 justify-center items-center mb-6 w-full relative">
-                          <SquareIcon width={24} icon={COIN_IMAGES[index]} />
+                          <SquareIcon
+                            width={isMobile ? 18 : 22}
+                            icon={COIN_IMAGES[index]}
+                          />
                         </div>
                         <Label
                           icon={sflIcon}
                           type="warning"
-                          iconWidth={11}
+                          iconWidth={13}
                           className="absolute h-7  -bottom-2"
                           style={{
                             left: `${PIXEL_SCALE * -3}px`,
@@ -226,7 +230,7 @@ export const BuyCurrenciesModal: React.FC<Props> = ({
                             width: `calc(100% + ${PIXEL_SCALE * 6}px)`,
                           }}
                         >
-                          {`${option.sfl} SFL`}
+                          <span className="pl-1 sm:pl-0">{`${option.sfl} SFL`}</span>
                         </Label>
                       </ButtonPanel>
                     );
