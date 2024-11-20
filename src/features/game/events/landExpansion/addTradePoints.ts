25da13748d6fb42f41fc6b2fd7ba1b39c958fa0d
Elias <elias@sunflower-land.com>
2024-11-20 09:43:43 +0800

    Fix tests

--- src/features/game/events/landExpansion/addTradePoints.ts
+++ src/features/game/events/landExpansion/addTradePoints.ts
@@ -1,6 +1,5 @@
 import Decimal from "decimal.js-light";
 import { GameState } from "features/game/types/game";
-import { produce } from "immer";
 
 export function addTradePoints({
   state,
@@ -11,17 +10,15 @@ export function addTradePoints({
   points: number;
   sfl: number;
 }) {
-  return produce(state, (game) => {
-    // Define Constants
-    const TRADE_POINTS_MULTIPLIER = 1; // Value adjustable
-    const pointsCalculation = 1 + sfl ** TRADE_POINTS_MULTIPLIER;
-    const multipliedPoints = points * pointsCalculation;
+  // Define Constants
+  const TRADE_POINTS_MULTIPLIER = 1; // Value adjustable
+  const pointsCalculation = 1 + sfl ** TRADE_POINTS_MULTIPLIER;
+  const multipliedPoints = points * pointsCalculation;
 
-    // Add points to gamestate
-    game.trades.tradePoints = (game.trades.tradePoints ?? 0) + multipliedPoints;
-    game.inventory["Trade Point"] = (
-      game.inventory["Trade Point"] ?? new Decimal(0)
-    ).add(multipliedPoints);
-    return game;
-  });
+  // Add points to gamestate
+  state.trades.tradePoints = (state.trades.tradePoints ?? 0) + multipliedPoints;
+  state.inventory["Trade Point"] = (
+    state.inventory["Trade Point"] ?? new Decimal(0)
+  ).add(multipliedPoints);
+  return state;
 }
