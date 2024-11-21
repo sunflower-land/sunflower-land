a7e6184dce3b07fbfa535aa20fa908c84db1ee6f
LittleEinstein ID 73795 | HSP#0439 <104184348+LittleEins@users.noreply.github.com>
2024-11-21 09:52:09 +0800

    [CHORE] Fix Shadows remaining Decoration

--- src/features/island/collectibles/components/Candles.tsx
+++ src/features/island/collectibles/components/Candles.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import candles from "assets/decorations/candles.png";
-import candlesOn from "assets/decorations/candles_on.gif";
+import candlesOn from "assets/decorations/candles_on.webp";
 import { PIXEL_SCALE } from "features/game/lib/constants";
 
 const isHalloween = () => {
