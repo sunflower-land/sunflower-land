3f9380116384f460cad36bfb05db00f7f562b81c
Brian Manthos <manthos@msn.com>
2024-11-26 15:48:00 -0800

    lint

--- src/features/game/lib/vipAccess.ts
+++ src/features/game/lib/vipAccess.ts
@@ -2,8 +2,12 @@ import Decimal from "decimal.js-light";
 import { Inventory } from "../types/game";
 import { getSeasonalBanner } from "../types/seasons";
 
-export const hasVipAccess = (inventory: Inventory, now = new Date()): boolean => {
-  const seasonBannerQuantity = inventory[getSeasonalBanner(now)] ?? new Decimal(0);
+export const hasVipAccess = (
+  inventory: Inventory,
+  now = new Date(),
+): boolean => {
+  const seasonBannerQuantity =
+    inventory[getSeasonalBanner(now)] ?? new Decimal(0);
   const hasSeasonPass = seasonBannerQuantity.gt(0);
 
   const lifetimeBannerQuantity =
