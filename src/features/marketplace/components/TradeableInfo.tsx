95416021b71dc7edb6efc293e4bca6f957a190a2
Spencer Dezart-Smith <17863697+spencerdezartsmith@users.noreply.github.com>
2024-11-29 09:31:43 +1100

    [FEAT] Add background-less wearables (#4832)

--- src/features/marketplace/components/TradeableInfo.tsx
+++ src/features/marketplace/components/TradeableInfo.tsx
@@ -6,23 +6,31 @@ import { TradeableDetails } from "features/game/types/marketplace";
 import { useAppTranslation } from "lib/i18n/useAppTranslations";
 import { TradeableDisplay } from "../lib/tradeables";
 
-import bg from "assets/ui/3x3_bg.png";
+import grassBg from "assets/ui/3x3_bg.png";
+import brownBg from "assets/brand/brown_background.png";
+
 import { InventoryItemName } from "features/game/types/game";
 import { getKeys } from "features/game/types/craftables";
 import { TRADE_LIMITS } from "features/game/actions/tradeLimits";
+import { useParams } from "react-router-dom";
 
 export const TradeableInfo: React.FC<{
   display: TradeableDisplay;
   tradeable?: TradeableDetails;
 }> = ({ display, tradeable }) => {
   const { t } = useAppTranslation();
+  const params = useParams();
   const isResource = getKeys(TRADE_LIMITS).includes(
     display.name as InventoryItemName,
   );
 
+  const isBackground = display.name.includes("Background");
+  const useBrownBackground = params.collection === "wearables" || isResource;
+  const background = useBrownBackground ? brownBg : grassBg;
+
   return (
     <>
-      <InnerPanel className="w-full flex relative mb-1">
+      <InnerPanel className="w-full flex relative mb-1" style={{ padding: 0 }}>
         <div className="flex flex-wrap absolute top-2 right-2">
           {/* {tradeable && (
             <Label
@@ -37,16 +45,21 @@ export const TradeableInfo: React.FC<{
           )}
         </div>
 
-        <img src={bg} className="w-full rounded-sm" />
         <img
-          src={display.image}
-          className="w-1/3 absolute"
-          style={{
-            left: "50%",
-            transform: "translate(-50%, 50%)",
-            bottom: "50%",
-          }}
+          src={isBackground ? display.image : background}
+          className="w-full rounded-sm"
         />
+        {!isBackground && (
+          <img
+            src={display.image}
+            className="w-1/3 absolute"
+            style={{
+              left: "50%",
+              transform: "translate(-50%, 50%)",
+              bottom: "50%",
+            }}
+          />
+        )}
       </InnerPanel>
       <InnerPanel>
         <div className="p-2">
