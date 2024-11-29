072877195b0322e6cfe6ab98b6fa5fe498f4c464
Craig <41215134+c-py@users.noreply.github.com>
2024-11-29 19:24:04 +1300

    [FIX] Hoarding Warning for resources

--- src/features/marketplace/components/PurchaseModalContent.tsx
+++ src/features/marketplace/components/PurchaseModalContent.tsx
@@ -8,6 +8,15 @@ import walletIcon from "assets/icons/wallet.png";
 import { Context } from "features/game/GameProvider";
 import { TradeableItemDetails } from "./TradeableSummary";
 import { KNOWN_ITEMS } from "features/game/types";
+import { useSelector } from "@xstate/react";
+import { MachineState } from "features/game/lib/gameMachine";
+import { hasMaxItems } from "features/game/lib/processEvent";
+import Decimal from "decimal.js-light";
+import { ModalContext } from "features/game/components/modal/ModalProvider";
+
+const _inventory = (state: MachineState) => state.context.state.inventory;
+const _previousInventory = (state: MachineState) =>
+  state.context.state.previousInventory;
 
 type PurchaseModalContentProps = {
   authToken: string;
@@ -27,12 +36,31 @@ export const PurchaseModalContent: React.FC<PurchaseModalContentProps> = ({
   listing,
 }) => {
   const { gameService } = useContext(Context);
+  const { openModal } = useContext(ModalContext);
   const { t } = useAppTranslation();
+
+  const inventory = useSelector(gameService, _inventory);
+  const previousInventory = useSelector(gameService, _previousInventory);
 
   const collection = getCollectionName(KNOWN_ITEMS[tradeable.id]);
   const display = getTradeableDisplay({
     id: tradeable.id,
     type: collection,
+  });
+
+  let updatedInventory = inventory;
+  if (collection === "resources" || collection === "collectibles") {
+    updatedInventory = {
+      ...inventory,
+      [KNOWN_ITEMS[tradeable.id]]: (
+        inventory[KNOWN_ITEMS[tradeable.id]] ?? new Decimal(0)
+      ).add(listing.quantity),
+    };
+  }
+
+  const hasMax = hasMaxItems({
+    current: updatedInventory,
+    old: previousInventory,
   });
 
   const confirm = async () => {
@@ -47,6 +75,39 @@ export const PurchaseModalContent: React.FC<PurchaseModalContentProps> = ({
     onClose();
   };
 
+  if (hasMax && listing.type === "instant") {
+    return (
+      <>
+        <div className="p-2">
+          <div className="flex justify-between">
+            <Label type="danger" className="mb-2 ml-1" icon={walletIcon}>
+              {t("marketplace.hoarding")}
+            </Label>
+          </div>
+          <p className="mb-3">{t("marketplace.hoarding.description")}</p>
+          <div className="opacity-50">
+            <TradeableItemDetails
+              display={display}
+              sfl={price}
+              quantity={listing.quantity}
+            />
+          </div>
+        </div>
+        <div className="flex space-x-1">
+          <Button onClick={onClose}>{t("cancel")}</Button>
+          <Button
+            onClick={() => {
+              onClose();
+              openModal("STORE_ON_CHAIN");
+            }}
+            className="relative"
+          >
+            <span>{t("gameOptions.blockchainSettings.storeOnChain")}</span>
+          </Button>
+        </div>
+      </>
+    );
+  }
   return (
     <>
       <div className="p-2">
@@ -67,7 +128,11 @@ export const PurchaseModalContent: React.FC<PurchaseModalContentProps> = ({
       </div>
       <div className="flex space-x-1">
         <Button onClick={onClose}>{t("cancel")}</Button>
-        <Button onClick={() => confirm()} className="relative">
+        <Button
+          onClick={() => confirm()}
+          disabled={hasMax}
+          className="relative"
+        >
           <span>{t("confirm")}</span>
           {listing.type === "onchain" && (
             <img src={walletIcon} className="absolute right-1 top-0.5 h-7" />
