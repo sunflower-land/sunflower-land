60345d80c70ff63ed27cf2adc053a9c807915833
Elias <101262042+eliasSFL@users.noreply.github.com>
2024-12-18 06:54:11 +0800

    [FEAT] Gem Boost feedback (#4970)
    * Track bumpkin activity when speeding up gems
    
    * Enhance InProgressInfo component with instant cooking feature and confirmation modal
    
    * Translations
    
    * - Updated button styling for better usability and visual appeal.
    - Changed gem display to include an image alongside the gem count for improved clarity.
    
    * Add fastforward icon to make the button clearer on its function

--- src/features/island/buildings/components/building/InProgressInfo.tsx
+++ src/features/island/buildings/components/building/InProgressInfo.tsx
@@ -5,28 +5,39 @@ import { PIXEL_SCALE } from "features/game/lib/constants";
 import { COOKABLES } from "features/game/types/consumables";
 import { ITEM_DETAILS } from "features/game/types/images";
 import { secondsToString } from "lib/utils/time";
-import React from "react";
+import React, { useState } from "react";
 import { MachineInterpreter } from "../../lib/craftingMachine";
 import { useAppTranslation } from "lib/i18n/useAppTranslations";
 import { Label } from "components/ui/Label";
 import { SUNNYSIDE } from "assets/sunnyside";
+import { Button } from "components/ui/Button";
+import { getInstantGems } from "features/game/events/landExpansion/speedUpRecipe";
+import { GameState } from "features/game/types/game";
+import { ConfirmationModal } from "components/ui/ConfirmationModal";
+import fastForward from "assets/icons/fast_forward.png";
 
 interface Props {
   craftingService: MachineInterpreter;
   onClose: () => void;
   isOilBoosted: boolean;
+  onInstantCooked: (gems: number) => void;
+  state: GameState;
 }
 
 export const InProgressInfo: React.FC<Props> = ({
   craftingService,
   onClose,
   isOilBoosted,
+  onInstantCooked,
+  state,
 }) => {
   const { t } = useAppTranslation();
 
+  const [showConfirmation, setShowConfirmation] = useState(false);
+
   const [
     {
-      context: { secondsTillReady, name },
+      context: { secondsTillReady, name, readyAt },
     },
   ] = useActor(craftingService);
 
@@ -37,9 +48,15 @@ export const InProgressInfo: React.FC<Props> = ({
   }
 
   const { cookingSeconds } = COOKABLES[name];
+  const { inventory } = state;
+
+  const gems = getInstantGems({
+    readyAt: readyAt ?? 0,
+    game: state,
+  });
 
   return (
-    <div className="flex flex-col mb-2">
+    <div className="flex flex-col mb-2 w-full">
       <Label
         className="mr-3 ml-2 mb-1"
         icon={SUNNYSIDE.icons.stopwatch}
@@ -47,7 +64,7 @@ export const InProgressInfo: React.FC<Props> = ({
       >
         {t("in.progress")}
       </Label>
-      <div className="flex">
+      <div className="flex items-center justify-between">
         <Box
           image={ITEM_DETAILS[name].image}
           // alternateIcon={isOilBoosted ? ITEM_DETAILS["Oil"].image : null}
@@ -69,6 +86,31 @@ export const InProgressInfo: React.FC<Props> = ({
             type="progress"
           />
         </div>
+        <Button
+          disabled={!inventory.Gem?.gte(gems)}
+          className="w-36 sm:w-44 px-3 h-12 mr-[6px]"
+          onClick={() => setShowConfirmation(true)}
+        >
+          <div className="flex items-center justify-center gap-1 mx-2">
+            <img src={fastForward} className="h-5" />
+            <span className="text-sm flex items-center">{gems}</span>
+            <img src={ITEM_DETAILS["Gem"].image} className="h-5" />
+          </div>
+        </Button>
+        <ConfirmationModal
+          show={showConfirmation}
+          onHide={() => setShowConfirmation(false)}
+          onCancel={() => setShowConfirmation(false)}
+          onConfirm={() => {
+            onInstantCooked(gems);
+            setShowConfirmation(false);
+          }}
+          messages={[
+            t("instantCook.confirmationMessage"),
+            t("instantCook.costMessage", { gems }),
+          ]}
+          confirmButtonLabel={t("instantCook.finish")}
+        />
       </div>
     </div>
   );
