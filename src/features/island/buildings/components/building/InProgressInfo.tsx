ad0447f40cddd5f0ecce6f52e3bcb92cc6c300bd
Elias <elias@sunflower-land.com>
2024-12-17 10:12:56 +0800

    Enhance InProgressInfo component with instant cooking feature and confirmation modal

--- src/features/island/buildings/components/building/InProgressInfo.tsx
+++ src/features/island/buildings/components/building/InProgressInfo.tsx
@@ -5,28 +5,38 @@ import { PIXEL_SCALE } from "features/game/lib/constants";
 import { COOKABLES } from "features/game/types/consumables";
 import { ITEM_DETAILS } from "features/game/types/images";
 import { secondsToString } from "lib/utils/time";
-import React from "react";
+import React, { useState } from "react";
 import { MachineInterpreter } from "../../lib/craftingMachine";
 import { useAppTranslation } from "lib/i18n/useAppTranslations";
 import { Label } from "components/ui/Label";
 import { SUNNYSIDE } from "assets/sunnyside";
+import { Button } from "components/ui/Button";
+import { getInstantGems } from "features/game/events/landExpansion/speedUpRecipe";
+import { GameState } from "features/game/types/game";
+import { ConfirmationModal } from "components/ui/ConfirmationModal";
 
 interface Props {
   craftingService: MachineInterpreter;
   onClose: () => void;
   isOilBoosted: boolean;
+  onInstantCooked: (gems: number) => void;
+  state: GameState;
 }
 
 export const InProgressInfo: React.FC<Props> = ({
   craftingService,
   onClose,
   isOilBoosted,
+  onInstantCooked,
+  state,
 }) => {
   const { t } = useAppTranslation();
 
+  const [showConfirmation, setShowConfirmation] = useState(false);
+
   const [
     {
-      context: { secondsTillReady, name },
+      context: { secondsTillReady, name, readyAt },
     },
   ] = useActor(craftingService);
 
@@ -37,9 +47,15 @@ export const InProgressInfo: React.FC<Props> = ({
   }
 
   const { cookingSeconds } = COOKABLES[name];
+  const { inventory } = state;
+
+  const gems = getInstantGems({
+    readyAt: readyAt ?? 0,
+    game: state,
+  });
 
   return (
-    <div className="flex flex-col mb-2">
+    <div className="flex flex-col mb-2 w-full">
       <Label
         className="mr-3 ml-2 mb-1"
         icon={SUNNYSIDE.icons.stopwatch}
@@ -47,7 +63,7 @@ export const InProgressInfo: React.FC<Props> = ({
       >
         {t("in.progress")}
       </Label>
-      <div className="flex">
+      <div className="flex items-center justify-between">
         <Box
           image={ITEM_DETAILS[name].image}
           // alternateIcon={isOilBoosted ? ITEM_DETAILS["Oil"].image : null}
@@ -69,6 +85,32 @@ export const InProgressInfo: React.FC<Props> = ({
             type="progress"
           />
         </div>
+        <Button
+          disabled={!inventory.Gem?.gte(gems)}
+          className="w-16 h-12 mr-[6px]"
+          onClick={() => setShowConfirmation(true)}
+        >
+          <Label
+            type={inventory.Gem?.gte(gems) ? "default" : "danger"}
+            icon={ITEM_DETAILS.Gem.image}
+          >
+            {gems}
+          </Label>
+        </Button>
+        <ConfirmationModal
+          show={showConfirmation}
+          onHide={() => setShowConfirmation(false)}
+          onCancel={() => setShowConfirmation(false)}
+          onConfirm={() => {
+            onInstantCooked(gems);
+            setShowConfirmation(false);
+          }}
+          messages={[
+            "Are you sure you want to finish this recipe now?",
+            `This will cost you ${gems} Gems`,
+          ]}
+          confirmButtonLabel={"Finish Now"}
+        />
       </div>
     </div>
   );
