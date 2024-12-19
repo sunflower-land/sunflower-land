1a21c70a4fd77bba75a8552d98fd75c1ec78470d
Elias <elias@sunflower-land.com>
2024-12-19 17:51:42 +0800

    Buff fishing limit skills

--- src/features/game/types/bumpkinSkills.ts
+++ src/features/game/types/bumpkinSkills.ts
@@ -1021,8 +1021,8 @@ export const BUMPKIN_REVAMP_SKILL_TREE = {
   },
 
   // Fishing - Tier 1
-  "Fisherman's 2 Fold": {
-    name: "Fisherman's 2 Fold",
+  "Fisherman's 5 Fold": {
+    name: "Fisherman's 5 Fold",
     tree: "Fishing",
     requirements: {
       points: 1,
@@ -1031,7 +1031,7 @@ export const BUMPKIN_REVAMP_SKILL_TREE = {
     },
     boosts: {
       buff: {
-        shortDescription: translate("skill.fishermansTwoFold"),
+        shortDescription: translate("skill.fishermansFiveFold"),
         labelType: "success",
         boostTypeIcon: powerup,
         boostedItemIcon: ITEM_DETAILS["Rod"].image,
@@ -1098,8 +1098,8 @@ export const BUMPKIN_REVAMP_SKILL_TREE = {
     image: SUNNYSIDE?.skills?.green_thumb_LE,
   },
   // Fishing - Tier 2
-  "Fisherman's 5 Fold": {
-    name: "Fisherman's 5 Fold",
+  "Fisherman's 10 Fold": {
+    name: "Fisherman's 10 Fold",
     tree: "Fishing",
     requirements: {
       points: 2,
@@ -1108,7 +1108,7 @@ export const BUMPKIN_REVAMP_SKILL_TREE = {
     },
     boosts: {
       buff: {
-        shortDescription: translate("skill.fishermansFiveFold"),
+        shortDescription: translate("skill.fishermansTenFold"),
         labelType: "success",
         boostTypeIcon: powerup,
         boostedItemIcon: ITEM_DETAILS["Rod"].image,
