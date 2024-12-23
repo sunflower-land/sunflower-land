b0f96473bc61ad058a654fea46286f117bc6fc7d
Elias <elias@sunflower-land.com>
2024-12-23 10:28:57 +0800

    Reward no trade points if trade is less than 5 SFL

--- src/features/game/events/landExpansion/addTradePoints.ts
+++ src/features/game/events/landExpansion/addTradePoints.ts
@@ -10,12 +10,31 @@ const TRADE_POINTS_MULTIPLIER = 0.25; // Value adjustable
 export function calculateTradePoints({
   points,
   sfl,
+  items,
 }: {
   points: number;
   sfl: number;
+  items?: Partial<Record<MarketplaceTradeableName, number>>;
 }) {
+  let multipliedPoints = 0;
+
+  if (items) {
+    const name = getKeys(items).filter(
+      (itemName) => itemName in KNOWN_IDS,
+    )[0] as InventoryItemName;
+    const isResource = getKeys(TRADE_LIMITS).includes(name);
+
+    if (isResource) {
+      return { multipliedPoints };
+    }
+  }
+
+  if (sfl < 5) {
+    return { multipliedPoints };
+  }
+
   const pointsCalculation = sfl * TRADE_POINTS_MULTIPLIER;
-  const multipliedPoints = points * pointsCalculation;
+  multipliedPoints = points * pointsCalculation;
 
   return { multipliedPoints };
 }
@@ -43,7 +62,11 @@ export function addTradePoints({
     }
   }
 
-  const { multipliedPoints } = calculateTradePoints({ points, sfl });
+  if (sfl < 5) {
+    return state;
+  }
+
+  const { multipliedPoints } = calculateTradePoints({ points, sfl, items });
 
   // Add points to gamestate
   state.trades.tradePoints = (state.trades.tradePoints ?? 0) + multipliedPoints;
