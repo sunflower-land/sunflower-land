bf2a3b61b207df9398698a42c6948ab24d4e4397
spencer <spencer@sunflower-land.com>
2025-01-06 21:11:03 +1100

    [FEAT] Add messaging functions

--- src/lib/messaging.ts
+++ src/lib/messaging.ts
@@ -1,10 +1,5 @@
 import { app } from "./firebase";
-import {
-  getMessaging,
-  getToken,
-  MessagePayload,
-  onMessage,
-} from "firebase/messaging";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
 export const FIREBASE_VAPID_PUBLIC_KEY = import.meta.env
   .VITE_FIREBASE_PUBLIC_VAPID_KEY;
@@ -12,32 +7,30 @@ export const FIREBASE_VAPID_PUBLIC_KEY = import.meta.env
 const messaging = getMessaging(app);
 
 export const requestForToken = async () => {
+  const registration = await navigator.serviceWorker.ready;
   try {
     const currentToken = await getToken(messaging, {
       vapidKey: FIREBASE_VAPID_PUBLIC_KEY,
+      serviceWorkerRegistration: registration,
     });
 
     if (!currentToken) {
-      alert(
-        "No registration token available. Request permission to generate one.",
-      );
-
       return null;
     }
 
     return currentToken;
   } catch (error) {
-    alert(`Error requesting for token: ${error}`);
+    // eslint-disable-next-line no-console
+    console.log(`Error requesting for token: ${error}`);
 
     return null;
   }
 };
 
-onMessage(messaging, ({ notification }: MessagePayload) => {
-  if (!notification) return;
-
-  new Notification(notification.title as string, {
-    body: notification.body,
-    icon: notification.icon,
-  });
+onMessage(messaging, (payload) => {
+  // eslint-disable-next-line no-console
+  console.log(
+    "[firebase-messaging-sw.js] Received foreground message ",
+    payload,
+  );
 });
