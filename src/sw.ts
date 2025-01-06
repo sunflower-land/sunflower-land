ab458af4bc3815024e048fe32dbcf583a11fa920
spencer <spencer@sunflower-land.com>
2025-01-06 21:10:43 +1100

    [FEAT] Add messaging to service worker

--- src/sw.ts
+++ src/sw.ts
@@ -8,7 +8,9 @@ import { cleanupOutdatedCaches, precacheAndRoute } from "workbox-precaching";
 import { StaleWhileRevalidate } from "workbox-strategies";
 import { ExpirationPlugin } from "workbox-expiration";
 import { CONFIG } from "./lib/config";
-
+import { getMessaging } from "firebase/messaging/sw";
+import { onBackgroundMessage } from "firebase/messaging/sw";
+import { app } from "./lib/firebase";
 declare let self: ServiceWorkerGlobalScope;
 
 const isTestnet = CONFIG.NETWORK === "amoy";
@@ -50,6 +52,25 @@ if (import.meta.env.PROD) {
   // Google Fonts
   googleFontsCache();
 }
+
+// Firebase Messaging
+const messaging = getMessaging(app);
+
+onBackgroundMessage(messaging, (payload) => {
+  // eslint-disable-next-line no-console
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload,
+  );
+  // Customize notification here
+  const notificationTitle = payload.notification?.title as string;
+  const notificationOptions = {
+    body: payload.notification?.body,
+    icon: "../public/icons/favicon.ico",
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
 
 // Offline fallback html page
 offlineFallback();
