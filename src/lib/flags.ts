c671e0b12ccf43387b5793914ff8da4b53f7ee64
Adam Hannigan <adamhannigan81@gmail.com>
2025-01-20 11:35:13 +1100

    [FIX] Admin testers

--- src/lib/flags.ts
+++ src/lib/flags.ts
@@ -6,7 +6,7 @@ const adminFeatureFlag = ({ wardrobe, inventory }: GameState) =>
   (!!((wardrobe["Gift Giver"] ?? 0) > 0) && !!inventory["Beta Pass"]?.gt(0));
 
 const seasonAdminFeatureFlag = ({ username }: GameState) =>
-  testnetFeatureFlag() || ["adam", "elias", "dcol"].includes(username ?? "");
+  testnetFeatureFlag() || ["adam", "eliassfl", "dcol"].includes(username ?? "");
 
 const defaultFeatureFlag = ({ inventory }: GameState) =>
   CONFIG.NETWORK === "amoy" || !!inventory["Beta Pass"]?.gt(0);
