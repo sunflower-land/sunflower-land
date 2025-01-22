127226f3f19961727b76c974db5b392a3f02dd86
Craig <41215134+c-py@users.noreply.github.com>
2025-01-23 09:45:53 +1100

    [FEAT] Lava Pit Modal

--- src/features/game/expansion/components/lavaPit/LavaPit.tsx
+++ src/features/game/expansion/components/lavaPit/LavaPit.tsx
@@ -1,15 +1,28 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { ITEM_DETAILS } from "features/game/types/images";
 import { PIXEL_SCALE } from "features/game/lib/constants";
 import { Modal } from "components/ui/Modal";
 import { LavaPitModalContent } from "./LavaPitModalContent";
+import { useSelector } from "@xstate/react";
+import { Context } from "features/game/GameProvider";
+import { MachineState } from "features/game/lib/gameMachine";
+import { SUNNYSIDE } from "assets/sunnyside";
+
+const _lavaPit = (id: string) => (state: MachineState) =>
+  state.context.state.lavaPits[id];
 
 interface Props {
   id: string;
 }
 
 export const LavaPit: React.FC<Props> = ({ id }) => {
-  const [showModal, setShowModal] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+
+  const { gameService, showAnimations } = useContext(Context);
+  const lavaPit = useSelector(gameService, _lavaPit(id));
+
+  const lavaPitReady =
+    (lavaPit?.startedAt ?? Infinity) < Date.now() && !lavaPit?.collectedAt;
 
   return (
     <div className="relative w-full h-full">
@@ -17,15 +30,25 @@ export const LavaPit: React.FC<Props> = ({ id }) => {
         className="w-full h-full cursor-pointer hover:img-highlight"
         onClick={() => setShowModal(true)}
       >
+        {lavaPitReady && (
+          <img
+            src={SUNNYSIDE.icons.expression_alerted}
+            className={
+              "absolute -top-2 left-[calc(50%-4px)] transform -translate-x-1/2 z-20" +
+              (showAnimations ? " ready" : "")
+            }
+            style={{ width: `${PIXEL_SCALE * 4}px` }}
+          />
+        )}
         <img
-          id="lavap"
+          id={`lavapit-${id}`}
           src={ITEM_DETAILS["Lava Pit"].image}
           width={36 * PIXEL_SCALE}
         />
       </div>
 
       <Modal show={showModal} onHide={() => setShowModal(false)}>
-        <LavaPitModalContent onClose={() => setShowModal(false)} />
+        <LavaPitModalContent onClose={() => setShowModal(false)} id={id} />
       </Modal>
     </div>
   );
--- src/features/game/expansion/components/lavaPit/LavaPitModalContent.tsx
+++ src/features/game/expansion/components/lavaPit/LavaPitModalContent.tsx
@@ -2,28 +2,67 @@ import React, { useContext } from "react";
 import { CloseButtonPanel } from "features/game/components/CloseablePanel";
 import { Label } from "components/ui/Label";
 import { useTranslation } from "react-i18next";
-import { RequirementLabel } from "components/ui/RequirementsLabel";
 import { getKeys } from "features/game/types/craftables";
 import { LAVA_PIT_REQUIREMENTS } from "features/game/events/landExpansion/startLavaPit";
 import { useSelector } from "@xstate/react";
 import { MachineState } from "features/game/lib/gameMachine";
 import { Context } from "features/game/GameProvider";
-import Decimal from "decimal.js-light";
 import { Button } from "components/ui/Button";
 import { ITEM_DETAILS } from "features/game/types/images";
 import { SUNNYSIDE } from "assets/sunnyside";
+import { Box } from "components/ui/Box";
+import { formatNumber } from "lib/utils/formatNumber";
+
+import { InnerPanel, OuterPanel } from "components/ui/Panel";
+import Decimal from "decimal.js-light";
+import useUiRefresher from "lib/utils/hooks/useUiRefresher";
+import { secondsToString } from "lib/utils/time";
+import { LAVA_PIT_MS } from "features/game/events/landExpansion/collectLavaPit";
+import { SEASON_ICONS } from "features/island/buildings/components/building/market/SeasonalSeeds";
 
 const _inventory = (state: MachineState) => state.context.state.inventory;
+const _lavaPit = (id: string) => (state: MachineState) =>
+  state.context.state.lavaPits[id];
+const _season = (state: MachineState) => state.context.state.season;
 
 interface Props {
   onClose: () => void;
+  id: string;
 }
 
-export const LavaPitModalContent: React.FC<Props> = ({ onClose }) => {
+export const LavaPitModalContent: React.FC<Props> = ({ onClose, id }) => {
+  const { t } = useTranslation();
+
   const { gameService } = useContext(Context);
   const inventory = useSelector(gameService, _inventory);
+  const lavaPit = useSelector(gameService, _lavaPit(id));
+  const season = useSelector(gameService, _season);
 
-  const { t } = useTranslation();
+  useUiRefresher();
+
+  const throwResourcesIntoPit = () => {
+    gameService.send("lavaPit.started", {
+      id,
+    });
+  };
+
+  const collectLavaPit = () => {
+    gameService.send("lavaPit.collected", {
+      id,
+    });
+  };
+
+  const hasIngredients = getKeys(LAVA_PIT_REQUIREMENTS).every((itemName) =>
+    (inventory[itemName] ?? new Decimal(0)).gte(
+      LAVA_PIT_REQUIREMENTS[itemName] ?? new Decimal(0),
+    ),
+  );
+
+  const lavaPitInProgress = lavaPit?.startedAt !== undefined;
+  const timeRemaining = LAVA_PIT_MS - (Date.now() - (lavaPit?.startedAt ?? 0));
+  const canCollect = lavaPitInProgress && timeRemaining <= 0;
+  const wasRecentlyCollected = Date.now() - (lavaPit?.collectedAt ?? 0) < 1000;
+
   return (
     <CloseButtonPanel
       onClose={onClose}
@@ -32,30 +71,127 @@ export const LavaPitModalContent: React.FC<Props> = ({ onClose }) => {
           name: "Lava Pit",
           icon: ITEM_DETAILS["Lava Pit"].image,
         },
-        {
-          name: "Guide",
-          icon: SUNNYSIDE.icons.angry,
-        },
       ]}
+      container={OuterPanel}
     >
-      <Label type="default" className="mt-2">
-        {"Requirements"}
-      </Label>
-      <div className="flex flex-col gap-2">
-        {getKeys(LAVA_PIT_REQUIREMENTS).map((itemName) => {
-          return (
-            <RequirementLabel
-              key={itemName}
-              type="item"
-              item={itemName}
-              balance={inventory[itemName] ?? new Decimal(0)}
-              showLabel
-              requirement={new Decimal(LAVA_PIT_REQUIREMENTS[itemName] ?? 0)}
+      <InnerPanel>
+        <Label
+          type="default"
+          icon={SEASON_ICONS[season.season]}
+          className="mt-2 ml-2"
+        >
+          {t(`season.${season.season}`)}
+        </Label>
+        <p className="text-xxs p-2">{t("lavaPit.description")}</p>
+        <div className="flex flex-col gap-2 p-2">
+          {getKeys(LAVA_PIT_REQUIREMENTS).map((itemName) => {
+            return (
+              <>
+                <div className="flex items-start cursor-context-menu hover:brightness-100">
+                  <Box
+                    image={ITEM_DETAILS[itemName].image}
+                    className="-mt-2 -ml-2 -mb-1"
+                  />
+                  <div>
+                    <div className="flex flex-wrap items-start">
+                      <Label type="default" className="mr-1 mb-1">
+                        {`${formatNumber(LAVA_PIT_REQUIREMENTS[itemName])} x ${itemName}`}
+                      </Label>
+                    </div>
+
+                    <p className="text-xs ml-0.5">
+                      {ITEM_DETAILS[itemName]?.description
+                        ? ITEM_DETAILS[itemName].description
+                        : t("reward.collectible")}
+                    </p>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </InnerPanel>
+
+      <div className="pt-1" />
+
+      <InnerPanel>
+        <div className="flex justify-between">
+          {lavaPitInProgress && (
+            <Label
+              type="default"
+              className="ml-2"
+              icon={ITEM_DETAILS["Lava Pit"].image}
+            >
+              {t("active")}
+            </Label>
+          )}
+          {!lavaPitInProgress && (
+            <Label
+              type="warning"
+              icon={ITEM_DETAILS["Lava Pit"].image}
+              className="ml-2"
+            >
+              {t("reward")}
+            </Label>
+          )}
+
+          {lavaPitInProgress && canCollect && (
+            <Label type="success">{"Ready"}</Label>
+          )}
+          {lavaPitInProgress && !canCollect && (
+            <Label type="info" icon={SUNNYSIDE.icons.stopwatch}>
+              {`${t("ready.in")} ${secondsToString(timeRemaining / 1000, {
+                length: "medium",
+              })}`}
+            </Label>
+          )}
+          {!lavaPitInProgress && (
+            <Label icon={SUNNYSIDE.icons.stopwatch} type="transparent">
+              {secondsToString(LAVA_PIT_MS / 1000, {
+                length: "short",
+              })}
+            </Label>
+          )}
+        </div>
+        <div className="flex flex-col gap-2 p-2">
+          <div className="flex items-start cursor-context-menu hover:brightness-100">
+            <Box
+              image={ITEM_DETAILS["Obsidian"].image}
+              className="-mt-2 -ml-1 -mb-1"
             />
-          );
-        })}
-      </div>
-      <Button>{"Start Lava Pit"}</Button>
+            <div>
+              <div className="flex flex-wrap items-start">
+                <Label type="default" className="mr-1 mb-1">
+                  {`1 x Obsidian`}
+                </Label>
+              </div>
+              <p className="text-xs ml-0.5">
+                {ITEM_DETAILS["Obsidian"].description}
+              </p>
+            </div>
+          </div>
+        </div>
+        {lavaPitInProgress && canCollect && (
+          <Button onClick={collectLavaPit} disabled={!canCollect}>
+            {t("lavaPit.collect")}
+          </Button>
+        )}
+        {lavaPitInProgress && !canCollect && (
+          <Button onClick={collectLavaPit} disabled={!canCollect}>
+            {t("lavaPit.burning")}
+          </Button>
+        )}
+        {!lavaPitInProgress && (
+          <Button
+            disabled={
+              !hasIngredients || lavaPitInProgress || wasRecentlyCollected
+            }
+            onClick={throwResourcesIntoPit}
+          >
+            {t("lavaPit.throw")}
+          </Button>
+        )}
+      </InnerPanel>
     </CloseButtonPanel>
   );
 };
