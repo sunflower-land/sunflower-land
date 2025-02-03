df0bdd75c85bb6fee6bd98b69d229c06e5917400
Elias <101262042+eliasSFL@users.noreply.github.com>
2025-02-03 13:11:36 +0800

    [FIX] Premium Composter Ingredients

--- src/features/game/types/composters.ts
+++ src/features/game/types/composters.ts
@@ -143,17 +143,16 @@ export const SEASON_COMPOST_REQUIREMENTS: Record<
       Egg: 5,
     },
     summer: {
-      Apple: 4,
-      Kale: 5,
+      Banana: 3,
+      Egg: 5,
     },
     autumn: {
-      Orange: 6,
-      Egg: 4,
+      Apple: 4,
+      Tomato: 5,
     },
     winter: {
+      Lemon: 3,
       Apple: 3,
-      Blueberry: 3,
-      Egg: 5,
     },
   },
 };
