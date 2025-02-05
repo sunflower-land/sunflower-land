58a1db36ae62b56032e6607157a985d3d11d34da
Elias <101262042+eliasSFL@users.noreply.github.com>
2025-02-06 07:28:54 +0800

    [FEAT] New Crops Hoarding Limit (#5209)

--- src/features/game/actions/tradeLimits.test.ts
+++ src/features/game/actions/tradeLimits.test.ts
@@ -1,15 +1,14 @@
-import { getEntries } from "../expansion/lib/utils";
+import { Decimal } from "decimal.js-light";
+import { getObjectEntries } from "../expansion/lib/utils";
 import { MAX_INVENTORY_ITEMS } from "../lib/processEvent";
 import { TRADE_LIMITS } from "./tradeLimits";
 
 describe("TRADE_LIMITS", () => {
   it("allows the hoarding limit to be at least 3x more than the trade limits", () => {
-    getEntries(TRADE_LIMITS).forEach(([item, quantity]) => {
-      const maxInventory = MAX_INVENTORY_ITEMS[item];
+    getObjectEntries(TRADE_LIMITS).forEach(([item, quantity]) => {
+      const maxInventory = MAX_INVENTORY_ITEMS[item] ?? new Decimal(0);
       const tradeLimit = quantity ?? 0;
-      if (maxInventory) {
-        expect(maxInventory.toNumber()).toBeGreaterThanOrEqual(tradeLimit * 3);
-      }
+      expect(Number(maxInventory)).toBeGreaterThanOrEqual(tradeLimit * 3);
     });
   });
 });
