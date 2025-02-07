6c7fb9e55f89d6e63ca9d608920b66a4942578b6
Elias <101262042+eliasSFL@users.noreply.github.com>
2025-02-07 19:15:57 +0800

    Remove old pickSkill function

--- src/features/game/events/index.ts
+++ src/features/game/events/index.ts
@@ -54,7 +54,6 @@ import {
 } from "./landExpansion/collectRecipe";
 import { feedBumpkin, FeedBumpkinAction } from "./landExpansion/feedBumpkin";
 import { detectBot, DetectBotAction } from "./detectBot";
-import { pickSkill, PickSkillAction } from "./landExpansion/pickSkill";
 import { choseSkill, ChoseSkillAction } from "./landExpansion/choseSkill";
 import { resetSkills, ResetSkillsAction } from "./landExpansion/resetSkills";
 import { seedBought, SeedBoughtAction } from "./landExpansion/seedBought";
@@ -476,7 +475,6 @@ export type PlayingEvent =
   | CollectRecipeAction
   | FeedBumpkinAction
   | DetectBotAction
-  | PickSkillAction
   | ChoseSkillAction
   | ResetSkillsAction
   | SeedBoughtAction
@@ -686,7 +684,6 @@ export const PLAYING_EVENTS: Handlers<PlayingEvent> = {
   "recipe.cooked": cook,
   "recipe.collected": collectRecipe,
   "bumpkin.feed": feedBumpkin,
-  "skill.picked": pickSkill,
   "skill.chosen": choseSkill,
   "skills.reset": resetSkills,
   "seed.bought": seedBought,
