e2eafbf306f6d1accfc675e99a7085383f5d396f
spencer <spencer@sunflower-land.com>
2025-02-12 15:35:56 +1100

    [FEAT] Cancel queued recipe

--- src/features/island/buildings/components/building/QueueSlot.tsx
+++ src/features/island/buildings/components/building/QueueSlot.tsx
@@ -1,32 +1,32 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { BuildingProduct } from "features/game/types/game";
 import { ITEM_DETAILS } from "features/game/types/images";
 import { Box } from "components/ui/Box";
 import { SUNNYSIDE } from "assets/sunnyside";
+import { ModalOverlay } from "components/ui/ModalOverlay";
+import { useTranslation } from "react-i18next";
+import { Button } from "components/ui/Button";
+import { Context } from "features/game/GameProvider";
+import { BuildingName } from "features/game/types/buildings";
+import { Panel } from "components/ui/Panel";
 
 interface QueueSlotProps {
+  buildingName: BuildingName;
+  buildingId: string;
   item?: BuildingProduct;
   isLocked?: boolean;
   readyRecipes: BuildingProduct[];
 }
 
 export const QueueSlot: React.FC<QueueSlotProps> = ({
+  buildingName,
+  buildingId,
   item,
-  isLocked,
   readyRecipes,
 }) => {
-  if (isLocked) {
-    return (
-      <Box
-        showOverlay={true}
-        overlayIcon={
-          <div className="flex justify-center items-center">
-            <img src={SUNNYSIDE.icons.lock} alt="locked slot" className="w-5" />
-          </div>
-        }
-      />
-    );
-  }
+  const { gameService } = useContext(Context);
+  const { t } = useTranslation();
+  const [showConfirm, setShowConfirm] = useState(false);
 
   if (!item) return <Box />;
 
@@ -34,15 +34,57 @@ export const QueueSlot: React.FC<QueueSlotProps> = ({
     (recipe) => recipe.name === item.name && recipe.readyAt === item.readyAt,
   );
 
+  const handleCancel = () => {
+    gameService.send("recipe.cancelled", {
+      buildingName,
+      buildingId,
+      queueItem: item,
+    });
+  };
+
   return (
-    <div className="relative">
-      {isReady && (
-        <img
-          className="absolute top-1 right-1 w-4 z-10"
-          src={SUNNYSIDE.icons.confirm}
-        />
-      )}
-      <Box image={ITEM_DETAILS[item.name].image} />
-    </div>
+    <>
+      <div className="relative">
+        {isReady && (
+          <img
+            className="absolute top-1 right-1 w-4 z-10"
+            src={SUNNYSIDE.icons.confirm}
+          />
+        )}
+        {!isReady && (
+          <img
+            className="absolute top-1 right-1 w-4 z-10"
+            src={SUNNYSIDE.icons.cancel}
+          />
+        )}
+        <div
+          className={!isReady ? "cursor-pointer" : ""}
+          onClick={!isReady ? () => setShowConfirm(true) : undefined}
+        >
+          <Box image={ITEM_DETAILS[item.name].image} />
+        </div>
+      </div>
+      <ModalOverlay
+        show={showConfirm}
+        onBackdropClick={() => setShowConfirm(false)}
+      >
+        <Panel>
+          <p className="p-1.5 mb-1.5">
+            {t("recipes.confirmCancel", { recipe: item.name })}
+          </p>
+          <div className="flex space-x-1 justify-end">
+            <Button onClick={() => setShowConfirm(false)}>{t("cancel")}</Button>
+            <Button
+              onClick={() => {
+                setShowConfirm(false);
+                handleCancel();
+              }}
+            >
+              {t("confirm")}
+            </Button>
+          </div>
+        </Panel>
+      </ModalOverlay>
+    </>
   );
 };
