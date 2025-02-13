ea194327c40f9c3bc3e93737d7251f1b406f72df
Adam Hannigan <adamhannigan81@gmail.com>
2025-02-13 19:02:50 +1100

    [FIX] Robust marketplace is resource check

--- src/features/marketplace/components/TradeableList.tsx
+++ src/features/marketplace/components/TradeableList.tsx
@@ -290,7 +290,7 @@ export const TradeableListItem: React.FC<TradeableListItemProps> = ({
           points: tradeType === "instant" ? 1 : 3,
         }).multipliedPoints;
 
-  const isLessThanOffer = price <= highestOffer && display.type !== "resources";
+  const isLessThanOffer = price <= highestOffer && !isResource;
 
   if (showConfirmation) {
     return (
