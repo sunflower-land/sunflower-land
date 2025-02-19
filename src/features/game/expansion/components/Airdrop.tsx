a909c4d862e9d8cf35239d1bc1a5f761449fd4e4
Elias <101262042+eliasSFL@users.noreply.github.com>
2025-02-19 21:22:45 +0800

    [CHORE] Use ClaimReward for RoninWelcomePack instead of AirdropModal

--- src/features/game/expansion/components/Airdrop.tsx
+++ src/features/game/expansion/components/Airdrop.tsx
@@ -14,7 +14,25 @@ export const AirdropModal: React.FC<{
   onClose?: () => void;
   onClaimed: () => void;
 }> = ({ airdrop, onClose, onClaimed }) => {
-  return <ClaimReward reward={airdrop} onClaim={onClaimed} onClose={onClose} />;
+  const { gameService } = useContext(Context);
+
+  const claim = () => {
+    gameService.send("airdrop.claimed", {
+      id: airdrop.id,
+    });
+
+    if (airdrop.items["Time Warp Totem"]) {
+      gameService.send("LANDSCAPE", {
+        placeable: "Time Warp Totem",
+        action: "collectible.placed",
+        location: "farm",
+      });
+    }
+
+    onClaimed();
+  };
+
+  return <ClaimReward reward={airdrop} onClaim={claim} onClose={onClose} />;
 };
 
 interface Props {
@@ -23,7 +41,6 @@ interface Props {
 export const Airdrop: React.FC<Props> = ({ airdrop }) => {
   const { showAnimations } = useContext(Context);
   const [showModal, setShowModal] = useState(false);
-  const { gameService } = useContext(Context);
 
   return (
     <>
@@ -31,12 +48,7 @@ export const Airdrop: React.FC<Props> = ({ airdrop }) => {
         <CloseButtonPanel onClose={() => setShowModal(false)}>
           <AirdropModal
             airdrop={airdrop}
-            onClaimed={() => {
-              gameService.send("airdrop.claimed", {
-                id: airdrop.id,
-              });
-              setShowModal(false);
-            }}
+            onClaimed={() => setShowModal(false)}
           />
         </CloseButtonPanel>
       </Modal>
@@ -92,9 +104,6 @@ export const AirdropPopup: React.FC = () => {
     <AirdropModal
       airdrop={airdrop}
       onClaimed={() => {
-        gameService.send("airdrop.claimed", {
-          id: airdrop.id,
-        });
         gameService.send("CLOSE");
       }}
       onClose={() => {
