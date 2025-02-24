6302f3fc15cf2064b5380cbdcc6a6a3c02012e6a
Craig <41215134+c-py@users.noreply.github.com>
2025-02-24 15:50:28 +1100

    [FEAT] Ronin Network Daily Chest

--- src/lib/blockchain/DailyReward.ts
+++ src/lib/blockchain/DailyReward.ts
@@ -2,12 +2,13 @@ import ABI from "./abis/SunflowerDailyRewards";
 import { CONFIG } from "lib/config";
 import { parseMetamaskError } from "./utils";
 import {
+  getAccount,
   readContract,
   waitForTransactionReceipt,
   writeContract,
 } from "@wagmi/core";
 import { config } from "features/wallet/WalletProvider";
-import { polygonAmoy } from "viem/chains";
+import { polygonAmoy, ronin, saigon } from "viem/chains";
 import { polygon } from "viem/chains";
 
 export async function getDailyCode(
@@ -17,13 +18,31 @@ export async function getDailyCode(
   await new Promise((res) => setTimeout(res, 3000 * attempts));
 
   try {
+    const chainAccount = getAccount(config);
+
+    // Polygon
+    if (
+      chainAccount.chainId === polygonAmoy.id ||
+      chainAccount.chainId === polygon.id
+    ) {
+      const code = await readContract(config, {
+        chainId: CONFIG.NETWORK === "mainnet" ? polygon.id : polygonAmoy.id,
+        abi: ABI,
+        address: CONFIG.DAILY_REWARD_CONTRACT as `0x${string}`,
+        functionName: "counts",
+        args: [account],
+      });
+
+      return Number(code ?? "0");
+    }
+
+    // Ronin
     const code = await readContract(config, {
-      chainId: CONFIG.NETWORK === "mainnet" ? polygon.id : polygonAmoy.id,
+      chainId: CONFIG.NETWORK === "mainnet" ? ronin.id : saigon.id,
       abi: ABI,
-      address: CONFIG.DAILY_REWARD_CONTRACT as `0x${string}`,
+      address: CONFIG.RONIN_DAILY_REWARD_CONTRACT as `0x${string}`,
       functionName: "counts",
       args: [account],
-      account,
     });
 
     return Number(code ?? "0");
@@ -44,13 +63,32 @@ export async function trackDailyReward({
   account: `0x${string}`;
   code: number;
 }): Promise<void> {
+  const chainAccount = getAccount(config);
+
+  // Polygon
+  if (
+    chainAccount.chainId === polygonAmoy.id ||
+    chainAccount.chainId === polygon.id
+  ) {
+    const hash = await writeContract(config, {
+      chainId: CONFIG.NETWORK === "mainnet" ? polygon.id : polygonAmoy.id,
+      abi: ABI,
+      address: CONFIG.DAILY_REWARD_CONTRACT as `0x${string}`,
+      functionName: "reward",
+      args: [code],
+      account,
+    });
+    await waitForTransactionReceipt(config, { hash });
+  }
+
   const hash = await writeContract(config, {
-    chainId: CONFIG.NETWORK === "mainnet" ? polygon.id : polygonAmoy.id,
+    chainId: CONFIG.NETWORK === "mainnet" ? ronin.id : saigon.id,
     abi: ABI,
-    address: CONFIG.DAILY_REWARD_CONTRACT as `0x${string}`,
+    address: CONFIG.RONIN_DAILY_REWARD_CONTRACT as `0x${string}`,
     functionName: "reward",
     args: [code],
     account,
   });
+
   await waitForTransactionReceipt(config, { hash });
 }
