a914b877922e18c40cbedb879f1f4c23996948ea
LittleEinstein ID 73795 | HSP#0439 <104184348+LittleEins@users.noreply.github.com>
2025-02-25 15:02:09 +0800

    [FEAT] Woodlands Seasonal Map Update

--- src/features/world/scenes/WoodlandsScene.ts
+++ src/features/world/scenes/WoodlandsScene.ts
@@ -1,6 +1,7 @@
-import woodlandsJSON from "assets/map/woodlands.json";
+import seasonal_woodland from "assets/map/seasonal_woodlands.json";
 import rabbitJson from "assets/map/rabbit_woodlands.json";
 import rabbitTileset from "assets/map/rabbit-tileset.json";
+import seasonal_tileset from "assets/map/seasonal_tileset.json";
 
 import { SceneId } from "../mmoMachine";
 import { BaseScene, NPCBumpkin } from "./BaseScene";
@@ -46,9 +47,9 @@ export class WoodlandsScene extends BaseScene {
     super({
       name: "woodlands",
       map: {
-        json: IS_EASTER ? rabbitJson : woodlandsJSON,
-        imageKey: IS_EASTER ? "easter-tileset" : "tileset",
-        defaultTilesetConfig: IS_EASTER ? rabbitTileset : undefined,
+        json: IS_EASTER ? rabbitJson : seasonal_woodland,
+        imageKey: IS_EASTER ? "easter-tileset" : "seasonal-tileset",
+        defaultTilesetConfig: IS_EASTER ? rabbitTileset : seasonal_tileset,
       },
       audio: { fx: { walk_key: "dirt_footstep" } },
     });
@@ -66,5 +67,53 @@ export class WoodlandsScene extends BaseScene {
     super.create();
 
     this.initialiseNPCs(BUMPKINS);
+
+    const season = this.gameState.season.season;
+
+    // List of all seasonal elements
+    const seasonElements = [
+      "Ground",
+      "Ground Decorations",
+      "Flowers & Grass",
+      "Paths",
+      "Paths Layer 2",
+      "Decoration Base",
+      "Decoration Base 2",
+      "Decoration Base 3",
+      "Decorations Layer 2",
+      "Decorations Layer 3",
+      "Decorations Layer 4",
+      "Building Base",
+      "Building Base Decorations",
+      "Building Layer 2",
+      "Building Layer 3",
+    ];
+    const seasons = ["Spring", "Summer", "Autumn", "Winter"];
+
+    const topElements = [
+      "Decorations Layer 2",
+      "Decorations Layer 3",
+      "Decorations Layer 4",
+      "Building Layer 2",
+      "Building Layer 3",
+    ];
+
+    const activeSeasonName = season.charAt(0).toUpperCase() + season.slice(1);
+
+    // Hide all seasonal layers that are not used for the active season
+    seasons.forEach((seasonName) => {
+      seasonElements.forEach((element) => {
+        const layerName = `${element}/${seasonName} ${element}`;
+        if (seasonName !== activeSeasonName)
+          this.layers[layerName]?.setVisible(false);
+
+        // Add Elements here that are drawn on top of the map
+        topElements.forEach((layer) => {
+          if (element === layer) {
+            this.layers[layerName]?.setDepth(1000000);
+          }
+        });
+      });
+    });
   }
 }
