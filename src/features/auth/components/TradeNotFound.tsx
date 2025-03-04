40040d84b46d54760b54dcc07964421f37ecf527
Elias <101262042+eliasSFL@users.noreply.github.com>
2025-03-04 14:21:23 +0800

    [FEAT] Add SWR Revalidation to Trade Not Found Component

--- src/features/auth/components/TradeNotFound.tsx
+++ src/features/auth/components/TradeNotFound.tsx
@@ -3,10 +3,14 @@ import { Label } from "components/ui/Label";
 import { useGame } from "features/game/GameProvider";
 import { useAppTranslation } from "lib/i18n/useAppTranslations";
 import React from "react";
+import { useSWRConfig } from "swr";
 
 export const TradeNotFound: React.FC = () => {
   const { t } = useAppTranslation();
   const { gameService } = useGame();
+
+  const { mutate } = useSWRConfig();
+
   return (
     <div className="flex flex-col gap-1">
       <div className="flex flex-col p-2">
@@ -16,7 +20,12 @@ export const TradeNotFound: React.FC = () => {
 
         <p>{`This trade is not found. Please try again.`}</p>
       </div>
-      <Button onClick={() => gameService.send("CONTINUE")}>
+      <Button
+        onClick={() => {
+          mutate(() => true, undefined, { revalidate: true });
+          gameService.send("CONTINUE");
+        }}
+      >
         {t("continue")}
       </Button>
     </div>
