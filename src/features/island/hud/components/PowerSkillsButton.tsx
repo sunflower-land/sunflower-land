8c5271cb7d2b1bf2f03a7c7696500b420250d553
Elias <101262042+eliasSFL@users.noreply.github.com>
2025-03-07 20:51:42 +0800

    [FEAT] Add Power Skills Ready Indicator

--- src/features/island/hud/components/PowerSkillsButton.tsx
+++ src/features/island/hud/components/PowerSkillsButton.tsx
@@ -3,9 +3,37 @@ import lightning from "assets/icons/lightning.png";
 import { PIXEL_SCALE } from "features/game/lib/constants";
 import { SUNNYSIDE } from "assets/sunnyside";
 import { PowerSkills } from "./PowerSkills";
+import { useGame } from "features/game/GameProvider";
+import {
+  BumpkinSkillRevamp,
+  getPowerSkills,
+} from "features/game/types/bumpkinSkills";
+import { BumpkinRevampSkillName } from "features/game/types/bumpkinSkills";
+import { useSelector } from "@xstate/react";
 
 export const PowerSkillsButton: React.FC = () => {
   const [show, setShow] = useState(false);
+  const { gameService } = useGame();
+  const bumpkin = useSelector(
+    gameService,
+    (state) => state.context.state.bumpkin,
+  );
+  const { skills, previousPowerUseAt } = bumpkin;
+
+  const powerSkills = getPowerSkills();
+  const powerSkillsUnlocked = powerSkills.filter(
+    (skill) => !!skills[skill.name as BumpkinRevampSkillName],
+  );
+
+  const powerSkillsReady = powerSkillsUnlocked.some(
+    (skill: BumpkinSkillRevamp) => {
+      const nextSkillUse =
+        (previousPowerUseAt?.[skill.name as BumpkinRevampSkillName] ?? 0) +
+        (skill.requirements.cooldown ?? 0);
+      return nextSkillUse < Date.now();
+    },
+  );
+
   return (
     <div className="relative">
       <div
@@ -44,6 +72,17 @@ export const PowerSkillsButton: React.FC = () => {
             width: `${PIXEL_SCALE * 8}px`,
           }}
         />
+        {powerSkillsReady && (
+          <img
+            src={SUNNYSIDE.icons.expression_alerted}
+            className="absolute animate-pulsate"
+            style={{
+              width: `${PIXEL_SCALE * 4}px`,
+              top: `-${PIXEL_SCALE * 2}px`,
+              right: `-${PIXEL_SCALE * 2}px`,
+            }}
+          />
+        )}
       </div>
       <PowerSkills show={show} onHide={() => setShow(false)} />
     </div>
