bbcd2344575807bbbfdf940e2fa50dd9badc72b3
Elias <101262042+eliasSFL@users.noreply.github.com>
2025-03-12 11:44:09 +0800

    Skeuomorphic Round Button

--- src/components/ui/RoundButton.tsx
+++ src/components/ui/RoundButton.tsx
@@ -6,10 +6,10 @@ import classNames from "classnames";
  * @param children - The content of the button.
  * @param onClick - The function to call when the button is clicked.
  * @param disabled - Whether the button is disabled.
+ * @param buttonSize - The size of the button. Default is 22.
  * additional styling around the button should be handled a separate div around the button
  */
 interface RoundButtonProps {
-  children: React.ReactNode;
   onClick?: React.MouseEventHandler<HTMLDivElement> | undefined;
   disabled?: boolean;
   className?: string;
@@ -36,15 +36,14 @@ export const RoundButton: React.FC<RoundButtonProps> = ({
     >
       <img
         src={SUNNYSIDE.ui.round_button_pressed}
-        className="absolute group-active:hidden"
+        className="absolute"
         style={{
           width: `${PIXEL_SCALE * buttonSize}px`,
-          height: `${PIXEL_SCALE * buttonSize}px`,
         }}
       />
       <img
         src={SUNNYSIDE.ui.round_button}
-        className="absolute group-active:translate-y-[2px]"
+        className="absolute group-active:hidden"
         style={{
           width: `${PIXEL_SCALE * buttonSize}px`,
         }}
