a19aa8d321abb00bb17e0a2efbbdd8a01fb775ed
vitt0 <vitt0ne.jc@gmail.com>
2025-03-12 22:30:21 -0300

    [CHORE] Add SFT's Boost Warnings (#5352)

--- src/features/game/types/collectibleItemBuffs.ts
+++ src/features/game/types/collectibleItemBuffs.ts
@@ -74,6 +74,10 @@ function getCollectibleBuffLabels(
         labelType: "info",
         boostTypeIcon: SUNNYSIDE.icons.stopwatch,
       },
+      {
+        shortDescription: translate("description.nancy.warning"),
+        labelType: "danger",
+      },
     ],
     Scarecrow: [
       {
@@ -85,6 +89,10 @@ function getCollectibleBuffLabels(
         shortDescription: translate("description.scarecrow.boost"),
         labelType: "success",
         boostTypeIcon: powerup,
+      },
+      {
+        shortDescription: translate("description.scarecrow.warning"),
+        labelType: "danger",
       },
     ],
     Kuebiko: [
@@ -114,6 +122,10 @@ function getCollectibleBuffLabels(
         shortDescription: translate("description.gnome.boost.aoe"),
         labelType: "vibrant",
         boostTypeIcon: lightning,
+      },
+      {
+        shortDescription: translate("description.gnome.warning"),
+        labelType: "danger",
       },
     ],
     "Sir Goldensnout": [
@@ -202,6 +214,10 @@ function getCollectibleBuffLabels(
               labelType: "success",
               boostTypeIcon: powerup,
               boostedItemIcon: CROP_LIFECYCLE.basic.Cabbage.crop,
+            },
+            {
+              shortDescription: translate("description.Karkinos.warning"),
+              labelType: "danger",
             },
           ] as BuffLabel[])),
     ],
@@ -555,6 +571,10 @@ function getCollectibleBuffLabels(
         boostTypeIcon: powerup,
         boostedItemIcon: SUNNYSIDE.resource.wood,
       },
+      {
+        shortDescription: translate("description.woody.beaver.warning"),
+        labelType: "danger",
+      },
     ],
     "Apprentice Beaver": [
       {
@@ -568,6 +588,10 @@ function getCollectibleBuffLabels(
         labelType: "info",
         boostTypeIcon: SUNNYSIDE.icons.stopwatch,
         boostedItemIcon: SUNNYSIDE.resource.wood,
+      },
+      {
+        shortDescription: translate("description.apprentice.beaver.warning"),
+        labelType: "danger",
       },
     ],
     "Foreman Beaver": [
