343d79c950e5b589cfa0e732dd21d0bef2af148e
Adam Hannigan <adamhannigan81@gmail.com>
2025-03-17 09:59:21 +1100

    Telegram instructions

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import { VitePWA } from "vite-plugin-pwa";
 import preload from "vite-plugin-preload";
 import mkcert from "vite-plugin-mkcert";
 
+const isRunningOnPort443 = process.env.PORT === "443";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -17,9 +19,10 @@ export default defineConfig({
       // TODO with API environment variables
       injectData: {},
     }),
-    mkcert({
-      hosts: ["https://127.0.0.1"],
-    }),
+    isRunningOnPort443 &&
+      mkcert({
+        hosts: ["https://127.0.0.1"],
+      }),
     VitePWA({
       devOptions: {
         enabled: true,
