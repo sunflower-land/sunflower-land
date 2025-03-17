9c63f9c9797af6953ae738fd969a10403a61597a
Elias <101262042+eliasSFL@users.noreply.github.com>
2025-03-17 18:47:21 +0800

    Implement rewards button logic to display alert icon for completed social tasks; integrate game context and task completion checks

--- src/features/island/hud/components/referral/RewardsButton.tsx
+++ src/features/island/hud/components/referral/RewardsButton.tsx
@@ -1,12 +1,31 @@
 import { RoundButton } from "components/ui/RoundButton";
 import { PIXEL_SCALE } from "features/game/lib/constants";
-import React, { useState } from "react";
+import React, { useCallback, useContext, useState } from "react";
 import giftIcon from "assets/icons/gift.png";
 import { Rewards } from "./Rewards";
 import { SUNNYSIDE } from "assets/sunnyside";
+import { Context } from "features/game/GameProvider";
+import { useSelector } from "@xstate/react";
+import {
+  SocialTaskName,
+  TASKS,
+} from "features/game/events/landExpansion/completeSocialTask";
 
 export const RewardsButton: React.FC = () => {
   const [showRewardsModal, setShowRewardsModal] = useState(false);
+  const { gameService } = useContext(Context);
+  const state = useSelector(gameService, (state) => state.context.state);
+
+  const isTaskCompleted = useCallback(
+    (task: SocialTaskName) => TASKS[task].requirement(state),
+    [state],
+  );
+  const isAnyTaskCompleted = Object.values(TASKS).some(
+    (task) =>
+      isTaskCompleted(task.title as SocialTaskName) &&
+      !state.socialTasks?.completed[task.title as SocialTaskName]?.completedAt,
+  );
+
   return (
     <div
       className="absolute"
@@ -22,14 +41,16 @@ export const RewardsButton: React.FC = () => {
             top: `${PIXEL_SCALE * 4}px`,
           }}
         />
-        <img
-          src={SUNNYSIDE.icons.expression_alerted}
-          className="absolute animate-pulsate"
-          style={{
-            width: `${PIXEL_SCALE * 3}px`,
-            right: `${PIXEL_SCALE * 0}px`,
-          }}
-        />
+        {isAnyTaskCompleted && (
+          <img
+            src={SUNNYSIDE.icons.expression_alerted}
+            className="absolute animate-pulsate"
+            style={{
+              width: `${PIXEL_SCALE * 3}px`,
+              right: `${PIXEL_SCALE * 0}px`,
+            }}
+          />
+        )}
       </RoundButton>
       <Rewards
         show={showRewardsModal}
