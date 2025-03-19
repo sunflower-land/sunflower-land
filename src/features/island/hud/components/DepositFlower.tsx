eb0b7581f7d7924a154906d52589897518bde584
spencer <spencer@sunflower-land.com>
2025-03-19 14:23:13 +1100

    [FEAT] Deposit UI

--- src/features/island/hud/components/DepositFlower.tsx
+++ src/features/island/hud/components/DepositFlower.tsx
@@ -1,45 +1,104 @@
-import React, { useContext, useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useContext, useEffect, useState } from "react";
 import { Label } from "components/ui/Label";
 import { SUNNYSIDE } from "assets/sunnyside";
-
+import * as AuthProvider from "features/auth/lib/Provider";
 import baseIcon from "assets/icons/chains/base.png";
-import polygonIcon from "assets/icons/chains/polygon.webp";
-import {
-  DropdownOptionsPanel,
-  ButtonPanel,
-  DropdownButtonPanel,
-} from "components/ui/Panel";
+import { ButtonPanel } from "components/ui/Panel";
 import flowerIcon from "assets/icons/flower_token.webp";
 import { MachineState } from "features/game/lib/gameMachine";
 import { Context } from "features/game/GameProvider";
 import { useSelector } from "@xstate/react";
 import { CopySvg } from "components/ui/CopyField";
 import { useTranslation } from "react-i18next";
+import { shortAddress } from "lib/utils/shortAddress";
+import { Button } from "components/ui/Button";
+import { CONFIG } from "lib/config";
+import { DropdownPanel } from "components/ui/DropdownPanel";
 
-const networkOptions = [
-  { value: "Base", icon: baseIcon },
-  { value: "Polygon", icon: polygonIcon }, // You might want to use a different icon for Polygon
+type NetworkName = "Base" | "Base Sepolia";
+
+type NetworkOption = {
+  value: NetworkName;
+  icon: string;
+  chainId: number;
+};
+
+const MAINNET_NETWORKS: NetworkOption[] = [
+  {
+    value: "Base",
+    icon: baseIcon,
+    chainId: 8453,
+  },
 ];
 
+const TESTNET_NETWORKS: NetworkOption[] = [
+  {
+    value: "Base Sepolia",
+    icon: baseIcon,
+    chainId: 84532,
+  },
+];
+
+// Select appropriate network options based on config
+const networkOptions =
+  CONFIG.NETWORK === "mainnet" ? MAINNET_NETWORKS : TESTNET_NETWORKS;
+
+type ProcessedDeposit = {
+  from: string | null;
+  value: string;
+  transactionHash: string;
+  createdAt: number;
+};
+
+const _deposits = (state: MachineState): ProcessedDeposit[] =>
+  state.context.data["depositingFlower"]?.deposits ?? [];
 const _depositAddress = (state: MachineState): string =>
-  state.context.data["depositingFlower"]?.depositAddress ??
-  "0xdef0123456789abcdef0123456789abcdef012345";
+  state.context.data["depositingFlower"]?.depositAddress ?? "";
+const _pending = (state: MachineState) => state.matches("depositingFlower");
+const _success = (state: MachineState) =>
+  state.matches("depositingFlowerSuccess");
 
 export const DepositFlower: React.FC<{ onClose: () => void }> = ({
   onClose,
 }) => {
+  const { authService } = useContext(AuthProvider.Context);
   const { gameService } = useContext(Context);
   const { t } = useTranslation();
-  const [selectedNetwork, setSelectedNetwork] = useState<string>();
+  const [selectedNetwork, setSelectedNetwork] = useState<
+    NetworkOption | undefined
+  >();
   const [acknowledged, setAcknowledged] = useState(false);
   const [showLabel, setShowLabel] = useState(false);
   const [tooltipMessage, setTooltipMessage] = useState<string>(t("copied"));
 
   const depositAddress = useSelector(gameService, _depositAddress);
+  const deposits = useSelector(gameService, _deposits);
+  const pending = useSelector(gameService, _pending);
+  const success = useSelector(gameService, _success);
 
-  const handleNetworkChange = (network: string) => {
+  useEffect(() => {
+    if (selectedNetwork?.value) {
+      refreshDeposits();
+    }
+  }, [selectedNetwork]);
+
+  useEffect(() => {
+    if (success) {
+      gameService.send("CONTINUE");
+    }
+  }, [success]);
+
+  const handleNetworkChange = (networkName: NetworkName) => {
+    // Use proper type checking to ensure networkName is a valid key
+    const networkOption = networkOptions.find(
+      (option) => option.value === networkName,
+    ) as NetworkOption;
+
+    if (networkOption.value === selectedNetwork?.value) return;
+
     setAcknowledged(false);
-    setSelectedNetwork(network);
+    setSelectedNetwork(networkOption);
   };
 
   const copyToClipboard = async () => {
@@ -58,6 +117,16 @@ export const DepositFlower: React.FC<{ onClose: () => void }> = ({
     }, 2000);
   };
 
+  const refreshDeposits = async () => {
+    gameService.send("flower.depositStarted", {
+      effect: {
+        type: "flower.depositStarted",
+        chainId: selectedNetwork?.chainId,
+      },
+      authToken: authService.getSnapshot().context.user.rawToken as string,
+    });
+  };
+
   return (
     <>
       <div>
@@ -72,45 +141,63 @@ export const DepositFlower: React.FC<{ onClose: () => void }> = ({
           </Label>
         </div>
 
-        <Dropdown
+        <DropdownPanel<NetworkName>
           options={networkOptions}
-          value={selectedNetwork}
+          value={selectedNetwork?.value}
           onChange={handleNetworkChange}
           placeholder="Select network"
         />
 
-        {!acknowledged && selectedNetwork && (
+        {/* Acknowledge conditions */}
+        {!acknowledged && selectedNetwork?.value && (
           <>
-            <div className="flex flex-col gap-2 p-2 mt-2">
-              <div className="flex  gap-2">
-                <img src={SUNNYSIDE.icons.expression_alerted} className="h-5" />
-                <span className="text-xxs sm:text-xs">
-                  {`Only deposit $FLOWER from ${selectedNetwork}. Other deposits will be lost.`}
-                </span>
+            <div className="flex flex-col p-2 mt-1 text-xxs sm:text-xs">
+              <div className="w-full flex gap-1 h-8">
+                <div className="w-6 flex items-center justify-center">
+                  <img
+                    src={SUNNYSIDE.icons.expression_alerted}
+                    className="object-contain h-5"
+                  />
+                </div>
+                <div className="w-[90%] flex items-center">
+                  <span>{`Only deposit $FLOWER from ${selectedNetwork.value}. Other deposits will be lost.`}</span>
+                </div>
               </div>
-              <div className="flex gap-2">
-                <img src={flowerIcon} className="h-5" />
-                <span className="text-xxs sm:text-xs">
-                  {`Minimum deposit is 2 $FLOWER.`}
-                </span>
+              <div className="w-full flex gap-1 h-8">
+                <div className="w-6 flex items-center justify-center">
+                  <img src={flowerIcon} className="object-contain w-5" />
+                </div>
+                <div className="w-[90%] flex items-center">
+                  <span>{`Minimum deposit is 5 $FLOWER.`}</span>
+                </div>
               </div>
-              <div className="flex gap-2">
-                <img src={SUNNYSIDE.icons.stopwatch} className="h-5" />
-                <span className="text-xxs sm:text-xs">
-                  {`Processing times may vary.`}
-                </span>
+              <div className="w-full flex gap-1 h-8">
+                <div className="w-6 flex items-center justify-center">
+                  <img
+                    src={SUNNYSIDE.icons.stopwatch}
+                    className="object-contain w-5"
+                  />
+                </div>
+                <div className="w-[90%] flex items-center">
+                  <span>{`Processing times may vary.`}</span>
+                </div>
               </div>
             </div>
+
             <ButtonPanel
+              disabled={!depositAddress}
               className="w-full text-center"
               onClick={() => setAcknowledged(true)}
             >
-              <span>{`I understand`}</span>
+              <div className="mb-1">
+                {!depositAddress ? `Loading...` : `I understand`}
+              </div>
             </ButtonPanel>
           </>
         )}
 
-        {acknowledged && selectedNetwork && (
+        {/* Deposit address */}
+        {acknowledged && selectedNetwork?.value && (
           <div className="relative flex flex-col items-center justify-center gap-2 p-2 mt-2">
             <span className="px-3 sm:px-[50px]  text-center break-all select-text">
               {depositAddress}
@@ -128,78 +215,64 @@ export const DepositFlower: React.FC<{ onClose: () => void }> = ({
             >
               <Label type="success">{tooltipMessage}</Label>
             </div>
+            <Label type="danger">
+              <span className="text-center">
+                {`Do not save this address as its subject to change`}
+              </span>
+            </Label>
           </div>
         )}
       </div>
-    </>
-  );
-};
-
-interface DropdownOption {
-  value: string;
-  icon?: string;
-}
-
-interface DropdownProps {
-  options: DropdownOption[];
-  value?: string;
-  onChange: (value: string) => void;
-  placeholder?: string;
-  className?: string;
-}
-
-export const Dropdown: React.FC<DropdownProps> = ({
-  options,
-  value,
-  onChange,
-  placeholder = "Select an option",
-  className,
-}) => {
-  const [showDropdown, setShowDropdown] = useState(false);
-
-  const selectedOption = options.find((option) => option.value === value);
-
-  return (
-    <div className={`flex flex-col gap-2 relative ${className}`}>
-      <DropdownButtonPanel
-        className="flex items-center justify-between gap-2"
-        onClick={() => setShowDropdown(!showDropdown)}
-      >
-        <div className="flex items-center gap-2">
-          {selectedOption?.icon && (
-            <img src={selectedOption.icon} className="w-5" />
-          )}
-          <p className="mb-1 ml-1">
-            {selectedOption ? selectedOption.value : placeholder}
-          </p>
-        </div>
-        <img
-          src={SUNNYSIDE.icons.chevron_down}
-          className={`w-5 ${showDropdown ? "rotate-180" : ""}`}
-        />
-      </DropdownButtonPanel>
-
-      {showDropdown && (
-        <div className="absolute top-[78%] left-0 right-0 z-50">
-          <DropdownOptionsPanel className="flex flex-col">
-            {options
-              .filter((option) => option.value !== value)
-              .map((option) => (
-                <div
-                  key={option.value}
-                  className="flex items-center gap-2 py-2 cursor-pointer hover:brightness-90"
-                  onClick={() => {
-                    setShowDropdown(false);
-                    onChange(option.value);
-                  }}
-                >
-                  {option.icon && <img src={option.icon} className="w-5" />}
-                  <span className="text-sm">{option.value}</span>
+      {/* Deposits history */}
+      {selectedNetwork?.value && deposits.length > 0 && (
+        <div className="flex flex-col mt-2">
+          <div className="pb-2 border-b border-white -px-2">
+            <span className="text-sm ml-1">{`Processed deposits`}</span>
+          </div>
+          <div className="h-[120px] scrollable overflow-y-auto">
+            {deposits.map((deposit) => (
+              <div
+                key={deposit.transactionHash}
+                className="flex items-center gap-1 border-b border-white -px-2 py-1.5"
+              >
+                <div>
+                  <img
+                    src={selectedNetwork.icon}
+                    alt="chain logo"
+                    className="w-6"
+                  />
                 </div>
-              ))}
-          </DropdownOptionsPanel>
+                <div className="flex flex-col flex-1">
+                  <span className="text-xxs">
+                    {new Date(deposit.createdAt).toLocaleDateString("en-US", {
+                      weekday: "short",
+                      month: "short",
+                      day: "numeric",
+                      year: "numeric",
+                    })}
+                  </span>
+                  <span className="text-xxs">
+                    {shortAddress(deposit.from ?? "")}
+                  </span>
+                </div>
+                <div className="flex gap-1 mr-2">
+                  <span className="text-xxs">{deposit.value}</span>
+                  <img src={flowerIcon} alt="flower icon" className="w-4" />
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       )}
-    </div>
+      {acknowledged && (
+        <Button
+          disabled={pending || !selectedNetwork}
+          className="w-full text-center mt-2"
+          onClick={refreshDeposits}
+        >
+          {pending ? "Refreshing..." : "Refresh Deposit"}
+        </Button>
+      )}
+    </>
   );
 };
