7b6b44dcfa7af5a8b87c74286d7a23838f9299f1
jcads <50954098+jcads@users.noreply.github.com>
2025-04-23 10:40:15 +0800

    feat: replace Trending with a What's New section

--- src/features/marketplace/components/WhatsNew.tsx
+++ src/features/marketplace/components/WhatsNew.tsx
@@ -1,80 +1,81 @@
-import { Loading } from "features/auth/components";
-import { Marketplace as ICollection } from "features/game/types/marketplace";
-import React, { useContext, useEffect, useState } from "react";
-import { loadMarketplace as loadMarketplace } from "../actions/loadMarketplace";
-import * as Auth from "features/auth/lib/Provider";
-import { useActor, useSelector } from "@xstate/react";
-import { useNavigate, useLocation } from "react-router";
-import { ListViewCard } from "./ListViewCard";
-import Decimal from "decimal.js-light";
+import React, { useContext } from "react";
 import { getTradeableDisplay } from "../lib/tradeables";
+import { useLocation, useNavigate } from "react-router";
+import { Loading } from "features/auth/components";
 import { Context } from "features/game/GameProvider";
 import { MachineState } from "features/game/lib/gameMachine";
+import { useActor, useSelector } from "@xstate/react";
+import { INVENTORY_RELEASES } from "features/game/types/withdrawables";
+import { KNOWN_ITEMS } from "features/game/types";
+import { ListViewCard } from "./ListViewCard";
+import * as Auth from "features/auth/lib/Provider";
+import useSWR from "swr";
+import { collectionFetcher } from "./Collection";
+import Decimal from "decimal.js-light";
 
 const _state = (state: MachineState) => state.context.state;
 export const WhatsNew: React.FC = () => {
-  const { authService } = useContext(Auth.Context);
-  const [authState] = useActor(authService);
-
-  const filters = "collectibles,wearables";
-
-  const navigate = useNavigate();
-
-  const [isLoading, setIsLoading] = useState(true);
-  const [collection, setCollection] = useState<ICollection>();
   const isWorldRoute = useLocation().pathname.includes("/world");
   const { gameService } = useContext(Context);
   const state = useSelector(gameService, _state);
+  const navigate = useNavigate();
+  const { authService } = useContext(Auth.Context);
+  const [authState] = useActor(authService);
 
-  const load = async () => {
-    setIsLoading(true);
+  const token = authState.context.user.rawToken as string;
+  const {
+    data: collectibles,
+    isLoading,
+    error,
+  } = useSWR(["collectibles", token], collectionFetcher);
 
-    const data = await loadMarketplace({
-      filters: filters ?? "",
-      token: authState.context.user.rawToken as string,
-    });
+  if (error) throw error;
 
-    setCollection(data);
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    load();
-  }, []);
+  const sortedCollectibles =
+    collectibles?.items
+      .sort((a, b) => b.id - a.id)
+      .sort(
+        (a, b) =>
+          (INVENTORY_RELEASES[KNOWN_ITEMS[b.id]]?.tradeAt.getTime() ?? 0) -
+          (INVENTORY_RELEASES[KNOWN_ITEMS[a.id]]?.tradeAt.getTime() ?? 0),
+      ) ?? [];
 
   if (isLoading) {
     return <Loading />;
   }
 
-  const items = collection?.items?.slice(0, 4) ?? [];
-
   return (
-    <div className="flex flex-wrap w-full">
-      {items.map((item) => {
-        const display = getTradeableDisplay({
-          type: item.collection,
-          id: item.id,
-          state,
-        });
+    <div className="flex flex-wrap">
+      {sortedCollectibles
+        .slice(0, 7)
+        .map(({ id, floor, lastSalePrice, expiresAt }) => {
+          const display = getTradeableDisplay({
+            type: "collectibles",
+            id,
+            state,
+          });
 
-        return (
-          <div
-            className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 xl:w-[14.2%] pr-1 pb-1"
-            key={`${item.collection}-${item.id}`}
-          >
-            <ListViewCard
-              details={display}
-              price={new Decimal(item.floor)}
-              lastSalePrice={new Decimal(item.lastSalePrice)}
-              onClick={() => {
-                navigate(
-                  `${isWorldRoute ? "/world" : ""}/marketplace/${item.collection}/${item.id}`,
-                );
-              }}
-            />
-          </div>
-        );
-      })}
+          return (
+            <div
+              key={id}
+              className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 xl:w-[14.2%] pr-1 pb-1"
+            >
+              <ListViewCard
+                details={display}
+                price={floor ? new Decimal(floor) : undefined}
+                lastSalePrice={
+                  lastSalePrice ? new Decimal(lastSalePrice) : undefined
+                }
+                onClick={() => {
+                  navigate(
+                    `${isWorldRoute ? "/world" : ""}/marketplace/collectibles/${id}`,
+                  );
+                }}
+                expiresAt={expiresAt}
+              />
+            </div>
+          );
+        })}
     </div>
   );
 };
