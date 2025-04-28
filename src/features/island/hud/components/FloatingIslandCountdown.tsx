d9e05fc2cf1853173fd15fa93fa46cc79278899d
Adam Hannigan <adamhannigan81@gmail.com>
2025-04-28 11:48:03 +1000

    [FIX] Hide love island widget

--- src/features/island/hud/components/FloatingIslandCountdown.tsx
+++ src/features/island/hud/components/FloatingIslandCountdown.tsx
@@ -11,6 +11,7 @@ import { getActiveFloatingIsland } from "features/game/types/floatingIsland";
 import { useGame } from "features/game/GameProvider";
 import { Modal } from "components/ui/Modal";
 import { HotAirBalloon } from "features/loveIsland/HotAirBalloon";
+import { hasFeatureAccess } from "lib/flags";
 
 const Countdown: React.FC<{
   startAt: number;
@@ -58,6 +59,10 @@ export const FloatingIslandCountdown: React.FC = () => {
   });
 
   if (!event || hide) {
+    return null;
+  }
+
+  if (!hasFeatureAccess(gameState.context.state, "LOVE_ISLAND")) {
     return null;
   }
 
