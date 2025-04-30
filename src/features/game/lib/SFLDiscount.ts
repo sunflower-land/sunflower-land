a97779dc324db8ade0fd05f7439aa302d9ee2545
Adam Hannigan <adamhannigan81@gmail.com>
2025-04-30 15:16:04 +1000

    [FEAT] Add flower perk (#5580)

--- src/features/game/lib/SFLDiscount.ts
+++ src/features/game/lib/SFLDiscount.ts
@@ -6,8 +6,13 @@ export function SFLDiscount(state: GameState | undefined, sfl: Decimal) {
   if (!state) return sfl;
 
   if (hasVipAccess({ game: state })) {
-    // 25% discount
-    return sfl.times(0.75);
+    if (Date.now() > new Date("2025-05-01").getTime()) {
+      // 50% discount
+      return sfl.times(0.5);
+    } else {
+      // 25% discount
+      return sfl.times(0.75);
+    }
   }
 
   return sfl;
