697e375273bbe49f8cb73782da37b7c7290a1f56
Elias <elias@sunflower-land.com>
2025-05-09 08:10:32 +0800

    fix tsc

--- src/features/game/events/landExpansion/collectLavaPit.test.ts
+++ src/features/game/events/landExpansion/collectLavaPit.test.ts
@@ -81,8 +81,6 @@ describe("collectLavaPit", () => {
           1: {
             x: 0,
             y: 0,
-            width: 2,
-            height: 2,
             startedAt: now - 36 * 60 * 60 * 1000,
             createdAt: 0,
           },
@@ -129,7 +127,7 @@ describe("collectLavaPit", () => {
           ],
         },
         lavaPits: {
-          1: { x: 0, y: 0, width: 2, height: 2, startedAt: 0, createdAt: 0 },
+          1: { x: 0, y: 0, startedAt: 0, createdAt: 0 },
         },
       },
       action: { type: "lavaPit.collected", id: "1" },
