a3f7ab078b4fac15181495a45039c1f9601a13a0
Adam Hannigan <adamhannigan81@gmail.com>
2025-05-11 10:39:52 +1000

    [FEAT] Coin Head Reward (#5636)
    * [FEAT] Add in game rewards
    
    * Update rewards

--- src/features/game/components/FLOWERTeaser.tsx
+++ src/features/game/components/FLOWERTeaser.tsx
@@ -1,5 +1,5 @@
 import { useAppTranslation } from "lib/i18n/useAppTranslations";
-import React from "react";
+import React, { useState } from "react";
 import { acknowledgeFLOWERTeaser } from "features/announcements/announcementsStorage";
 import { useGame } from "../GameProvider";
 import { Button } from "components/ui/Button";
@@ -9,8 +9,10 @@ import flowerIcon from "assets/icons/flower_token.webp";
 import { Label } from "components/ui/Label";
 import { SUNNYSIDE } from "assets/sunnyside";
 import { NoticeboardItems } from "features/world/ui/kingdom/KingdomNoticeboard";
+import { Merkl } from "./modal/components/Merkl";
 
 export const FLOWERTeaserContent: React.FC = () => {
+  const [page, setPage] = useState(0);
   const { t } = useAppTranslation();
   const { gameService } = useGame();
   const onAcknowledge = () => {
@@ -18,12 +20,16 @@ export const FLOWERTeaserContent: React.FC = () => {
     gameService.send("ACKNOWLEDGE");
   };
 
+  if (page === 1) {
+    return <Merkl onClose={onAcknowledge} />;
+  }
+
   return (
     <div className="relative scrollable overflow-auto pr-0.5 max-h-[500px]">
       <img
         src={SUNNYSIDE.icons.close}
         className="absolute top-0 right-0.5 w-8 cursor-pointer"
-        onClick={onAcknowledge}
+        onClick={() => setPage(1)}
       />
       <Label icon={newsIcon} type="default" className="ml-2 mt-1">
         {t("news.title")}
@@ -63,7 +69,7 @@ export const FLOWERTeaserContent: React.FC = () => {
       </p>
 
       <div className="flex justify-center">
-        <Button onClick={onAcknowledge} className="mt-2 mr-1">
+        <Button onClick={() => setPage(1)} className="mt-2 mr-1">
           {t("close")}
         </Button>
         <Button
