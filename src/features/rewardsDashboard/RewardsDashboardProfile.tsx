cb00478508e951aebe5d82957cb52730ed2d7dbc
spencer <spencer@sunflower-land.com>
2025-05-12 09:30:02 +1000

    [FEAT] Add token panel

--- src/features/rewardsDashboard/RewardsDashboardProfile.tsx
+++ src/features/rewardsDashboard/RewardsDashboardProfile.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Panel } from "components/ui/Panel";
+import { InnerPanel, Panel } from "components/ui/Panel";
 import { useContext } from "react";
 import * as AuthProvider from "features/auth/lib/Provider";
 import useSWR from "swr";
@@ -7,8 +7,8 @@ import { AuthMachineState } from "features/auth/lib/authMachine";
 import { useSelector } from "@xstate/react";
 import { getRewardsDashboard } from "./actions/getFlowerDashboardProfile";
 import { useAppTranslation } from "lib/i18n/useAppTranslations";
-import { Label } from "components/ui/Label";
-import { Button } from "components/ui/Button";
+import flowerToken from "assets/icons/flower_token.webp";
+import { PIXEL_SCALE } from "features/game/lib/constants";
 
 const fetcher = async ([url, token]: [string, string]) => {
   return getRewardsDashboard(token);
@@ -35,24 +35,50 @@ export const RewardsDashboardProfile = () => {
     return () => clearInterval(interval);
   }, [mutate]);
 
-  if (error) {
-    return (
-      <Panel className="inset-0 fixed pointer-events-auto">
-        <Label type="danger">{t("transaction.somethingWentWrong")}</Label>
-        <Button
-          onClick={() => {
-            window.location.reload();
-          }}
-        >
-          {t("try.again")}
-        </Button>
-      </Panel>
-    );
-  }
+  // if (error) {
+  //   return (
+  //     <Panel className="inset-0 fixed pointer-events-auto">
+  //       <Label type="danger">{t("transaction.somethingWentWrong")}</Label>
+  //       <Button
+  //         onClick={() => {
+  //           window.location.reload();
+  //         }}
+  //       >
+  //         {t("try.again")}
+  //       </Button>
+  //     </Panel>
+  //   );
+  // }
 
   return (
     <Panel className="inset-0 fixed pointer-events-auto">
-      {`Rewards Dashboard`}
+      <InnerPanel className="relative">
+        <div className="flex w-full space-x-3 p-1">
+          <img
+            src={flowerToken}
+            alt="Flower Token"
+            className="img-highlight"
+            style={{ width: PIXEL_SCALE * 12, height: PIXEL_SCALE * 12 }}
+          />
+          <div className="flex-1 flex flex-col -mt-1">
+            <span className="text-base text-black">{`$0.11`}</span>
+            <span className="text-xs">{`Supply: 265,000,000`}</span>
+            <span className="text-xs">{`FDV: $28,000,000`}</span>
+          </div>
+          <div className="w-1/4"></div>
+        </div>
+        <a
+          href="https://app.uniswap.org/swap?chain=base&inputCurrency=0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913&outputCurrency=0x3e12b9d6a4d12cd9b4a6d613872d0eb32f68b380&value=1&field=input"
+          className="text-xs absolute top-1 right-1 underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {`Buy/Sell`}
+        </a>
+      </InnerPanel>
+      <InnerPanel></InnerPanel>
+      <InnerPanel></InnerPanel>
+      {/* {`Rewards Dashboard`}
       <hr className="w-full my-4 border-gray-200" />
       <div className="flex">
         <div>
@@ -65,7 +91,7 @@ export const RewardsDashboardProfile = () => {
             ))}
           </div>
         </div>
-      </div>
+      </div> */}
     </Panel>
   );
 };
