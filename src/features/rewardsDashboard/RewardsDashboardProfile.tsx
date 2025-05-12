6cbebf75120b0b6395bf74380a9ab687e7a89ea5
spencer <spencer@sunflower-land.com>
2025-05-12 11:39:31 +1000

    [FEAT] Add player spend

--- src/features/rewardsDashboard/RewardsDashboardProfile.tsx
+++ src/features/rewardsDashboard/RewardsDashboardProfile.tsx
@@ -7,8 +7,10 @@ import { AuthMachineState } from "features/auth/lib/authMachine";
 import { useSelector } from "@xstate/react";
 import { getRewardsDashboard } from "./actions/getFlowerDashboardProfile";
 import { useAppTranslation } from "lib/i18n/useAppTranslations";
-import flowerToken from "assets/icons/flower_token.webp";
 import { PIXEL_SCALE } from "features/game/lib/constants";
+
+import flowerToken from "assets/icons/flower_token.webp";
+import walletIcon from "assets/icons/wallet.png";
 
 const fetcher = async ([url, token]: [string, string]) => {
   return getRewardsDashboard(token);
@@ -52,32 +54,64 @@ export const RewardsDashboardProfile = () => {
 
   return (
     <Panel className="inset-0 fixed pointer-events-auto">
-      <InnerPanel className="relative">
-        <div className="flex w-full space-x-3 p-1">
-          <img
-            src={flowerToken}
-            alt="Flower Token"
-            className="img-highlight"
-            style={{ width: PIXEL_SCALE * 12, height: PIXEL_SCALE * 12 }}
-          />
-          <div className="flex-1 flex flex-col -mt-1">
-            <span className="text-base text-black">{`$0.11`}</span>
-            <span className="text-xs">{`Supply: 265,000,000`}</span>
-            <span className="text-xs">{`FDV: $28,000,000`}</span>
+      {/* Top Row */}
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
+        <InnerPanel className="relative">
+          <div className="flex w-full space-x-3 p-1">
+            <img
+              src={flowerToken}
+              alt="Flower Token"
+              className="img-highlight"
+              style={{ width: PIXEL_SCALE * 12, height: PIXEL_SCALE * 12 }}
+            />
+            <div className="flex-1 flex flex-col -mt-1">
+              <span className="text-base text-black">{`$0.11`}</span>
+              <span className="text-xs">{`Supply: 265,000,000`}</span>
+              <span className="text-xs">{`FDV: $28,000,000`}</span>
+            </div>
           </div>
-          <div className="w-1/4"></div>
-        </div>
-        <a
-          href="https://app.uniswap.org/swap?chain=base&inputCurrency=0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913&outputCurrency=0x3e12b9d6a4d12cd9b4a6d613872d0eb32f68b380&value=1&field=input"
-          className="text-xs absolute top-1 right-1 underline"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {`Buy/Sell`}
-        </a>
-      </InnerPanel>
-      <InnerPanel></InnerPanel>
-      <InnerPanel></InnerPanel>
+          <a
+            href="https://app.uniswap.org/swap?chain=base&inputCurrency=0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913&outputCurrency=0x3e12b9d6a4d12cd9b4a6d613872d0eb32f68b380&value=1&field=input"
+            className="text-xs absolute top-1 right-1 underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {`Buy/Sell`}
+          </a>
+        </InnerPanel>
+        <InnerPanel>
+          <div className="flex w-full space-x-3 p-1">
+            <img
+              src={walletIcon}
+              alt="Wallet Icon"
+              className="img-highlight"
+              style={{ width: PIXEL_SCALE * 14 }}
+            />
+            <div className="flex-1 flex flex-col -mt-1">
+              <span className="text-base text-black">{`102,168`}</span>
+              <span className="text-xs">{`7 day player spend`}</span>
+              <span className="text-xs">{`10,246 players spent flower`}</span>
+            </div>
+          </div>
+        </InnerPanel>
+        <InnerPanel>
+          {/* <div className="flex w-full space-x-3 p-1">
+            <div>
+              <img
+                src={flowerToken}
+                alt="Flower Token"
+                className="img-highlight"
+                style={{ width: PIXEL_SCALE * 12, height: PIXEL_SCALE * 12 }}
+              />
+              <img
+                src={decreaseRightArrow}
+                alt="Decrease Right Arrow"
+                className="img-highlight"
+              />
+            </div>
+          </div> */}
+        </InnerPanel>
+      </div>
       {/* {`Rewards Dashboard`}
       <hr className="w-full my-4 border-gray-200" />
       <div className="flex">
