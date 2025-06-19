57e554fb5cca61894eb6b645aefe27b673efc5dc
Elias <101262042+eliasSFL@users.noreply.github.com>
2025-06-20 07:25:10 +0800

    [FEAT] Add Landscaping Decorations Modal Back (#5765)
    * Add the Landscaping button back
    
    * Enhance RoundButton and LandscapingHud with buttonPressed state
    
    - Added buttonPressed prop to RoundButton component to control visibility.
    - Implemented Escape key handling in LandscapingHud to toggle buttonPressed state.
    - Updated button class names to reflect pressed state for improved UI feedback.
    
    * Add Crafting Decorations Back
    
    * Remove decorations tab
    
    * Refactor LANDSCAPING_DECORATIONS to remove function calls and improve access patterns
    
    - Changed LANDSCAPING_DECORATIONS from a function to a constant object for direct access.
    - Updated references throughout the codebase to reflect this change, enhancing performance and readability.
    
    * Revert "Enhance RoundButton and LandscapingHud with buttonPressed state"
    
    This reverts commit 81d805983ec650c0f89900221377709b17b3adfd.
    
    * Set default to first item in object
    
    * Feature flag landscaping

---
+++ src/features/island/hud/components/decorations/LandscapingDecorations.tsx
@@ -0,0 +1,118 @@
+import { useActor } from "@xstate/react";
+import { Box } from "components/ui/Box";
+import { Button } from "components/ui/Button";
+import { CraftingRequirements } from "components/ui/layouts/CraftingRequirements";
+import { SplitScreenView } from "components/ui/SplitScreenView";
+import {
+  LandscapingDecorationName,
+  LANDSCAPING_DECORATIONS,
+  getKeys,
+  Decoration,
+} from "features/game/types/decorations";
+import { ITEM_DETAILS } from "features/game/types/images";
+import { gameAnalytics } from "lib/gameAnalytics";
+import { useAppTranslation } from "lib/i18n/useAppTranslations";
+import React, { useContext, useState } from "react";
+import { Context } from "features/game/GameProvider";
+import { ITEM_ICONS } from "../inventory/Chest";
+import { MachineInterpreter } from "features/game/expansion/placeable/landscapingMachine";
+
+interface Props {
+  onClose: () => void;
+}
+
+export const LandscapingDecorations: React.FC<Props> = ({ onClose }) => {
+  const { t } = useAppTranslation();
+  const [selectedName, setSelectedName] = useState<LandscapingDecorationName>(
+    getKeys(LANDSCAPING_DECORATIONS)[0],
+  );
+
+  const selected = LANDSCAPING_DECORATIONS[selectedName];
+
+  const { gameService } = useContext(Context);
+  const [
+    {
+      context: { state },
+    },
+  ] = useActor(gameService);
+
+  const inventory = state.inventory;
+
+  const price = selected.coins ?? 0;
+
+  const landscapingMachine = gameService.state.children
+    .landscaping as MachineInterpreter;
+
+  const buy = () => {
+    landscapingMachine.send("SELECT", {
+      action: "decoration.bought",
+      placeable: selected.name,
+      requirements: {
+        coins: price,
+        ingredients: selected.ingredients,
+      },
+      multiple: true,
+    });
+
+    if (selected.ingredients["Gem"]) {
+      gameAnalytics.trackSink({
+        currency: "Gem",
+        amount: selected.ingredients["Gem"].toNumber() ?? 1,
+        item: selected.name,
+        type: "Collectible",
+      });
+    }
+
+    onClose();
+  };
+
+  const lessFunds = () => {
+    if (!price) return false;
+
+    return state.coins < price;
+  };
+
+  const lessIngredients = () =>
+    getKeys(selected.ingredients).some((name) =>
+      selected.ingredients[name]?.greaterThan(inventory[name] || 0),
+    );
+
+  return (
+    <SplitScreenView
+      panel={
+        <CraftingRequirements
+          gameState={state}
+          details={{
+            item: selected.name,
+          }}
+          requirements={{
+            resources: selected.ingredients,
+            coins: price,
+          }}
+          actionView={
+            <Button disabled={lessFunds() || lessIngredients()} onClick={buy}>
+              {t("buy")}
+            </Button>
+          }
+        />
+      }
+      content={
+        <>
+          {Object.values(LANDSCAPING_DECORATIONS).map((item: Decoration) => (
+            <Box
+              isSelected={selected.name === item.name}
+              key={item.name}
+              onClick={() =>
+                setSelectedName(item.name as LandscapingDecorationName)
+              }
+              image={
+                ITEM_ICONS(state.island.type, state.season.season)[item.name] ??
+                ITEM_DETAILS[item.name].image
+              }
+            />
+          ))}
+        </>
+      }
+    />
+  );
+};
