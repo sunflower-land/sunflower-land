945e3111bdc40394a8710a991df4787dbf918c3d
Elias <101262042+eliasSFL@users.noreply.github.com>
2025-06-19 13:52:07 +0800

    [FEAT] Add back Wardrobe (#5758)
    * [FEAT] Add back Wardrobe
    
    * Add pulsating effect to arrow
    
    * useSelector
    
    * use actual gameState in case we do bring it to beta testing
    
    * Refactor WardrobeWearables component to improve buy functionality and UI. Introduced BuyWearableAction for better separation of concerns, added timeout to prevent double buying, and updated isNotReady logic to return detailed readiness status.
    
    * Hide Streamer Helmet from Wardrobe

--- src/features/island/collectibles/components/Wardrobe.tsx
+++ src/features/island/collectibles/components/Wardrobe.tsx
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 
 import { PIXEL_SCALE } from "features/game/lib/constants";
 import { SUNNYSIDE } from "assets/sunnyside";
+import { Modal } from "components/ui/Modal";
+import { CloseButtonPanel } from "features/game/components/CloseablePanel";
+import { WardrobeWearables } from "features/home/components/WardrobeWearables";
+import { OuterPanel } from "components/ui/Panel";
+import { hasFeatureAccess } from "lib/flags";
+import { Context } from "features/game/GameProvider";
+import { useSelector } from "@xstate/react";
+
+function hasOpened() {
+  return !!localStorage.getItem("hasOpenedNewWardrobe");
+}
+function acknowledge() {
+  localStorage.setItem("hasOpenedNewWardrobe", "true");
+}
 
 export const Wardrobe: React.FC = () => {
+  const [showModal, setShowModal] = useState(false);
+  const { gameService } = useContext(Context);
+
+  // TODO: Delete this after release
+  const hasAccess = useSelector(gameService, (state) =>
+    hasFeatureAccess(state.context.state, "WARDROBE"),
+  );
+
+  const open = () => {
+    acknowledge();
+    setShowModal(true);
+  };
   return (
     <>
+      {!hasOpened() && hasAccess && (
+        <img
+          src={SUNNYSIDE.icons.click_icon}
+          className="absolute bottom-0 right-0 z-20 cursor-pointer animate-pulsate"
+          style={{
+            width: `${PIXEL_SCALE * 18}px`,
+            right: `${PIXEL_SCALE * -8}px`,
+          }}
+          onClick={open}
+        />
+      )}
       <img
         src={SUNNYSIDE.decorations.wardrobe}
         style={{
@@ -13,9 +50,26 @@ export const Wardrobe: React.FC = () => {
           bottom: `${PIXEL_SCALE * 6}px`,
           left: `${PIXEL_SCALE * 1.5}px`,
         }}
-        className="absolute"
+        className={`absolute ${
+          hasAccess ? "cursor-pointer hover:img-highlight" : ""
+        }`}
         alt="Wardrobe"
+        onClick={() => hasAccess && open()}
       />
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <CloseButtonPanel
+          onClose={() => setShowModal(false)}
+          container={OuterPanel}
+          tabs={[
+            {
+              icon: SUNNYSIDE.decorations.wardrobe,
+              name: "Wardrobe",
+            },
+          ]}
+        >
+          <WardrobeWearables />
+        </CloseButtonPanel>
+      </Modal>
     </>
   );
 };
