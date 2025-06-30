e02c4901c15da1c423928468152a7ffe779d7aca
Elias <elias@sunflower-land.com>
2025-06-30 14:28:12 +0800

    Button Width changes

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -71,9 +71,9 @@ export const Button: React.FC<React.PropsWithChildren<Props>> = ({
     <>
       <button
         className={classnames(
-          `w-full p-1 text-sm object-contain justify-center items-center hover:brightness-90 cursor-pointer flex disabled:opacity-50 [border-image:var(--button-image)_3_3_4_3_fill] active:[border-image:var(--button-pressed-image)_3_3_4_3_fill] transition-transform active:scale-[0.99]`,
-          className,
+          `p-1 text-sm object-contain justify-center items-center hover:brightness-90 cursor-pointer flex disabled:opacity-50 [border-image:var(--button-image)_3_3_4_3_fill] active:[border-image:var(--button-pressed-image)_3_3_4_3_fill] transition-transform active:scale-[0.99]`,
           !className?.match(/\bw-/g) && "w-full",
+          className,
           { "cursor-not-allowed": disabled },
         )}
         type={type}
