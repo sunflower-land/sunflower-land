006a59622a137fbf86a566c3e3b80e6e7b99456b
Spencer Dezart-Smith <17863697+spencerdezartsmith@users.noreply.github.com>
2025-07-09 15:06:03 +1000

    Update src/features/social/components/ChatInput.tsx
    Co-authored-by: Craig <41215134+c-py@users.noreply.github.com>

--- src/features/social/components/ChatInput.tsx
+++ src/features/social/components/ChatInput.tsx
@@ -60,7 +60,8 @@ export const ChatInput: React.FC<Props> = ({ disabled, onEnter }) => {
         className="text-xs w-full px-2 py-2 max-h-min focus:outline-none focus:ring-0"
       />
       <Validation text={text} />
-      <Button onClick={() => onEnter(text)} disabled={text.length === 0}>
+      <Button onClick={() => onEnter(text)} disabled={        disabled={text.length === 0 || !isValidText()}
+}>
         {`Message`}
       </Button>
     </div>
