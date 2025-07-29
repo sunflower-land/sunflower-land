65924ec39e56cb2a29600fb0be68556dc4ac716f
Elias <elias@sunflower-land.com>
2025-07-29 10:02:09 +0800

    Place Sunstone time calculation

--- src/features/game/events/landExpansion/placeSunstone.test.ts
+++ src/features/game/events/landExpansion/placeSunstone.test.ts
@@ -110,4 +110,46 @@ describe("placeSunstone", () => {
       },
     });
   });
+  it("reinstates current progress when stone was mined", () => {
+    const dateNow = Date.now();
+    const state = placeSunstone({
+      action: {
+        coordinates: {
+          x: 2,
+          y: 2,
+        },
+        id: "1", // ID doesn't matter since it's an existing stone
+        name: "Sunstone Rock",
+        type: "sunstone.placed",
+      },
+      state: {
+        ...INITIAL_FARM,
+        buildings: {},
+        inventory: {
+          "Sunstone Rock": new Decimal(2),
+        },
+        sunstones: {
+          "123": {
+            createdAt: dateNow,
+            stone: { minedAt: dateNow - 180000 },
+            removedAt: dateNow - 120000,
+            minesLeft: 5,
+          },
+        },
+      },
+      createdAt: dateNow,
+    });
+
+    expect(state.sunstones).toEqual({
+      "123": {
+        createdAt: expect.any(Number),
+        stone: {
+          minedAt: dateNow - 60000,
+        },
+        x: 2,
+        y: 2,
+        minesLeft: 5,
+      },
+    });
+  });
 });
