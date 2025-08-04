5795e8954aaf2c31d8e79b24256425ed2331ee22
Elias <elias@sunflower-land.com>
2025-08-04 20:51:25 +0800

    [FEAT] Remove Restrictions to Claiming Cheers

--- src/features/game/events/landExpansion/claimDailyCheers.test.ts
+++ src/features/game/events/landExpansion/claimDailyCheers.test.ts
@@ -13,50 +13,6 @@ describe("claimDailyCheers", () => {
 
     expect(game?.socialFarming.cheers.cheersUsed).toBe(0);
     expect(game?.socialFarming.cheers.freeCheersClaimedAt).toBe(now);
-  });
-
-  it("should prevent claiming a daily cheers twice in one day", () => {
-    const now = Date.now();
-
-    const game = claimDailyCheers({
-      state: INITIAL_FARM,
-      action: { type: "cheers.claimed" },
-      createdAt: now,
-    });
-
-    expect(() =>
-      claimDailyCheers({
-        state: game,
-        action: { type: "cheers.claimed" },
-        createdAt: now,
-      }),
-    ).toThrow("Already claimed your daily free cheers");
-  });
-
-  it("gives three free cheers to the player", () => {
-    const now = Date.now();
-
-    const game = claimDailyCheers({
-      state: {
-        ...INITIAL_FARM,
-        socialFarming: {
-          points: 0,
-          villageProjects: {},
-          cheersGiven: {
-            date: new Date(now).toISOString().split("T")[0],
-            projects: {},
-            farms: [],
-          },
-          cheers: {
-            freeCheersClaimedAt: now - 24 * 60 * 60 * 1000,
-            cheersUsed: 3,
-          },
-        },
-      },
-      action: { type: "cheers.claimed" },
-      createdAt: now,
-    });
-
     expect(game?.inventory.Cheer?.toNumber()).toBe(3);
   });
 
@@ -80,7 +36,7 @@ describe("claimDailyCheers", () => {
           },
           cheers: {
             freeCheersClaimedAt: now - 24 * 60 * 60 * 1000,
-            cheersUsed: 3,
+            cheersUsed: 0,
           },
         },
       },
@@ -88,117 +44,26 @@ describe("claimDailyCheers", () => {
       createdAt: now,
     });
 
+    expect(game?.socialFarming.cheers.cheersUsed).toBe(0);
+    expect(game?.socialFarming.cheers.freeCheersClaimedAt).toBe(now);
     expect(game?.inventory.Cheer?.toNumber()).toBe(6);
   });
 
-  it("gives three free cheers to the player if they didn't claim yesterday", () => {
+  it("should prevent claiming a daily cheers twice in one day", () => {
     const now = Date.now();
 
     const game = claimDailyCheers({
-      state: {
-        ...INITIAL_FARM,
-        socialFarming: {
-          points: 0,
-          villageProjects: {},
-          cheersGiven: {
-            date: new Date(now).toISOString().split("T")[0],
-            projects: {},
-            farms: [],
-          },
-          cheers: {
-            freeCheersClaimedAt: now - 48 * 60 * 60 * 1000,
-            cheersUsed: 0,
-          },
-        },
-      },
+      state: INITIAL_FARM,
       action: { type: "cheers.claimed" },
       createdAt: now,
     });
 
-    expect(game?.inventory.Cheer?.toNumber()).toBe(3);
-  });
-
-  it("should only give bonus cheers if the player has used their free cheers", () => {
-    const now = Date.now();
-
-    const game = claimDailyCheers({
-      state: {
-        ...INITIAL_FARM,
-        socialFarming: {
-          points: 0,
-          villageProjects: {},
-          cheersGiven: {
-            date: new Date(now).toISOString().split("T")[0],
-            projects: {},
-            farms: [],
-          },
-          cheers: {
-            freeCheersClaimedAt: now - 24 * 60 * 60 * 1000,
-            cheersUsed: 2,
-          },
-        },
-      },
-      action: { type: "cheers.claimed" },
-      createdAt: now,
-    });
-
-    expect(game?.inventory.Cheer?.toNumber()).toBe(2);
-  });
-
-  it("should only give a maximum of 3 bonus cheers if the player has used more than 3 cheers yesterday", () => {
-    const now = Date.now();
-
-    const game = claimDailyCheers({
-      state: {
-        ...INITIAL_FARM,
-        socialFarming: {
-          points: 0,
-          villageProjects: {},
-          cheersGiven: {
-            date: new Date(now).toISOString().split("T")[0],
-            projects: {},
-            farms: [],
-          },
-          cheers: {
-            freeCheersClaimedAt: now - 24 * 60 * 60 * 1000,
-            cheersUsed: 4,
-          },
-        },
-      },
-      action: { type: "cheers.claimed" },
-      createdAt: now,
-    });
-
-    expect(game?.inventory.Cheer?.toNumber()).toBe(3);
-  });
-
-  it("should give 3 free cheers to player on 4th August 2025, regardless of previous day's value", () => {
-    jest.useFakeTimers();
-    jest.setSystemTime(new Date("2025-08-04T00:00:00.000Z"));
-    const now = Date.now();
-    const game = claimDailyCheers({
-      state: {
-        ...INITIAL_FARM,
-        socialFarming: {
-          points: 0,
-          villageProjects: {},
-          cheersGiven: {
-            date: new Date(now).toISOString().split("T")[0],
-            projects: {},
-            farms: [],
-          },
-          cheers: {
-            freeCheersClaimedAt: now - 24 * 60 * 60 * 1000,
-            cheersUsed: 0,
-          },
-        },
-      },
-      action: { type: "cheers.claimed" },
-      createdAt: now,
-    });
-
-    expect(game?.inventory.Cheer?.toNumber()).toBe(3);
-
-    jest.useRealTimers();
+    expect(() =>
+      claimDailyCheers({
+        state: game,
+        action: { type: "cheers.claimed" },
+        createdAt: now,
+      }),
+    ).toThrow("Already claimed your daily free cheers");
   });
 });
--- src/features/game/events/landExpansion/claimDailyCheers.ts
+++ src/features/game/events/landExpansion/claimDailyCheers.ts
@@ -1,6 +1,7 @@
 import Decimal from "decimal.js-light";
 import { hasVipAccess } from "features/game/lib/vipAccess";
 import { GameState } from "features/game/types/game";
+import { getCurrentSeason } from "features/game/types/seasons";
 import { produce } from "immer";
 import { hasFeatureAccess } from "lib/flags";
 
@@ -29,41 +30,23 @@ export function claimDailyCheers({
     } = draft;
 
     const today = new Date(createdAt).toISOString().split("T")[0];
-    const yesterday = new Date(createdAt - 24 * 60 * 60 * 1000)
-      .toISOString()
-      .split("T")[0];
 
     if (cheers.freeCheersClaimedAt >= new Date(today).getTime()) {
       throw new Error("Already claimed your daily free cheers");
     }
 
-    const dayFreeCheersClaimed = new Date(cheers.freeCheersClaimedAt)
-      .toISOString()
-      .split("T")[0];
-
-    const isBetterTogetherStartDate =
-      new Date(today).getTime() === new Date("2025-08-04").getTime();
-
     let amount = 3;
 
-    if (hasVipAccess({ game: draft })) {
+    if (
+      hasVipAccess({ game: draft }) &&
+      getCurrentSeason(new Date(createdAt)) === "Better Together"
+    ) {
       amount = 6;
     }
 
-    // If today is the start date, give 3 regardless of previous day's value
-    const cheersUsedYesterday = isBetterTogetherStartDate
-      ? amount
-      : // Otherwise, give the amount of cheers used yesterday, or 3 if cheers wasn't claimed yesterday
-        Math.min(
-          dayFreeCheersClaimed === yesterday ? cheers.cheersUsed : amount,
-          amount,
-        );
-
-    if (cheersUsedYesterday < 0) {
-      throw new Error("Not enough cheers to claim");
-    }
-
-    draft.inventory.Cheer = new Decimal(cheersUsedYesterday);
+    draft.inventory.Cheer = (draft.inventory.Cheer ?? new Decimal(0)).add(
+      amount,
+    );
 
     if (cheers.freeCheersClaimedAt < new Date(today).getTime()) {
       cheers.freeCheersClaimedAt = createdAt;
