c6aaa5682ee160f18fd431d98706d33a0f496019
Elias <elias@sunflower-land.com>
2025-09-03 10:42:18 +0800

    Remove summary confirmation for individual pets

--- src/features/island/collectibles/components/pets/PetFeed.tsx
+++ src/features/island/collectibles/components/pets/PetFeed.tsx
@@ -5,10 +5,8 @@ import { Box } from "components/ui/Box";
 import { ITEM_DETAILS } from "features/game/types/images";
 import { CookableName } from "features/game/types/consumables";
 import { Label } from "components/ui/Label";
-import { getExperienceToNextLevel } from "features/game/types/pets";
 import { Button } from "components/ui/Button";
 import { Inventory } from "features/game/types/game";
-import levelUp from "assets/icons/level_up.png";
 import { SUNNYSIDE } from "assets/sunnyside";
 import { InnerPanel } from "components/ui/Panel";
 import xpIcon from "assets/icons/xp.png";
@@ -123,24 +121,7 @@ const PetFeedPanel: React.FC<
     !inventory[selectedFood] ||
     inventory[selectedFood].lessThan(1);
 
-  const { level, currentProgress, experienceBetweenLevels } =
-    getExperienceToNextLevel(petData.experience);
-
   const foodXp = getPetRequestXP(selectedFood);
-  const experienceAfterFeed = petData.experience + foodXp;
-  const {
-    level: levelAfterFeed,
-    currentProgress: currentProgressAfterFeed,
-    experienceBetweenLevels: experienceBetweenLevelsAfterFeed,
-  } = getExperienceToNextLevel(experienceAfterFeed);
-
-  const levelChange = isDisabled ? 0 : levelAfterFeed - level;
-  const experienceChange = isDisabled
-    ? 0
-    : experienceAfterFeed - petData.experience;
-  const beforeEnergy = petData.energy;
-  const afterEnergy = petData.energy + foodXp;
-  const energyChange = isDisabled ? 0 : afterEnergy - beforeEnergy;
 
   return (
     <div className="flex flex-col items-center gap-1">
@@ -167,79 +148,20 @@ const PetFeedPanel: React.FC<
         </div>
       </div>
 
-      {showConfirm ? (
-        <div className="flex flex-col gap-2 w-full">
-          <Label type="default">{t("pet.changes")}</Label>
-          <div className="flex flex-row sm:flex-col gap-2 w-full justify-between">
-            <InnerPanel className="flex flex-col gap-2 w-[40%] sm:w-full">
-              <div className="flex flex-row items-center gap-2">
-                <img src={levelUp} className="w-3" />
-                <span className="text-xs font-medium">
-                  {t("pets.level", { level })}
-                </span>
-              </div>
-              <div className="flex flex-row gap-2 items-center">
-                <img src={xpIcon} className="w-4" />
-                <div className="flex flex-row sm:items-center gap-1 text-xs">
-                  <span>
-                    {`${currentProgress} / ${experienceBetweenLevels}`}
-                  </span>
-                </div>
-              </div>
-              <div className="flex flex-row items-center gap-2">
-                <img src={SUNNYSIDE.icons.lightning} className="w-3" />
-                <span className="text-xs font-medium">{`${beforeEnergy}`}</span>
-              </div>
-            </InnerPanel>
-            <div className="w-[20%] sm:w-full flex justify-center items-center">
-              <img
-                src={SUNNYSIDE.icons.arrow_right}
-                alt="Arrow Right"
-                className="w-4 block sm:hidden"
-              />
-              <img
-                src={SUNNYSIDE.icons.arrow_down}
-                alt="Arrow Down"
-                className="w-4 hidden sm:block"
-              />
-            </div>
-            <InnerPanel className="flex flex-col gap-2 w-[40%] sm:w-full">
-              <div className="flex flex-row items-center gap-2">
-                <img src={levelUp} className="w-3" />
-                <span className="text-xs font-medium">
-                  {t("pets.level", { level: levelAfterFeed })}{" "}
-                  {levelChange > 0 ? `(+${levelChange})` : ""}
-                </span>
-              </div>
-              <div className="flex flex-row gap-2 items-center">
-                <img src={xpIcon} className="w-4" />
-                <div className="flex flex-row sm:items-center gap-1 text-xs">
-                  <span>
-                    {`${currentProgressAfterFeed} / ${experienceBetweenLevelsAfterFeed} ${experienceChange > 0 ? `(+${experienceChange})` : ""}`}
-                  </span>
-                </div>
-              </div>
-              <div className="flex flex-row items-center gap-2">
-                <img src={SUNNYSIDE.icons.lightning} className="w-3" />
-                <span className="text-xs font-medium">{`${afterEnergy} ${energyChange > 0 ? `(+${energyChange})` : ""}`}</span>
-              </div>
-            </InnerPanel>
-          </div>
+      <div className="flex flex-row sm:flex-col gap-2 justify-between w-full p-1">
+        <div className="flex flex-row gap-2 items-center">
+          <img src={xpIcon} className="w-4" />
+          <span className="text-xs">{t("pets.plusFoodXp", { foodXp })}</span>
         </div>
-      ) : (
-        <div className="flex flex-row sm:flex-col gap-2 justify-between w-full p-1">
-          <div className="flex flex-row gap-2 items-center">
-            <img src={xpIcon} className="w-4" />
-            <span className="text-xs">{t("pets.plusFoodXp", { foodXp })}</span>
-          </div>
-          <div className="flex flex-row gap-2">
+        <div className="flex flex-row gap-2 items-center">
+          <div className="w-4">
             <img src={SUNNYSIDE.icons.lightning} className="w-3" />
-            <span className="text-xs">
-              {t("pets.plusFoodEnergy", { foodXp })}
-            </span>
           </div>
+          <span className="text-xs">
+            {t("pets.plusFoodEnergy", { foodXp })}
+          </span>
         </div>
-      )}
+      </div>
 
       {/* Labels for today's feed and insufficient food */}
       {isToday && petData.requests.foodFed?.includes(selectedFood) ? (
