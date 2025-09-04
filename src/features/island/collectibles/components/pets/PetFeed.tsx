508467635310aa0ee510d080e321675ce7fcc52b
Elias <elias@sunflower-land.com>
2025-09-04 10:49:49 +0800

    Move ResetFoodRequests to own file

--- src/features/island/collectibles/components/pets/PetFeed.tsx
+++ src/features/island/collectibles/components/pets/PetFeed.tsx
@@ -12,12 +12,7 @@ import { InnerPanel } from "components/ui/Panel";
 import xpIcon from "assets/icons/xp.png";
 import { Loading } from "features/auth/components/Loading";
 import { useAppTranslation } from "lib/i18n/useAppTranslations";
-
-function getGemCost(resets: number) {
-  const baseCost = 40;
-  const nextPrice = baseCost * Math.pow(1.5, resets);
-  return Math.round(nextPrice);
-}
+import { ResetFoodRequests } from "./ResetFoodRequests";
 
 interface Props {
   petName: PetName;
@@ -48,6 +43,8 @@ export const PetFeed: React.FC<
     petData.requests.food[0] ?? null,
   );
   const [showConfirm, setShowConfirm] = useState(false);
+
+  // States for reset Requests
   const [showResetRequests, setShowResetRequests] = useState(false);
   const [isRevealing, setIsRevealing] = useState(false);
   const [isPicking, setIsPicking] = useState(false);
@@ -310,74 +307,5 @@ const PetFeedContent: React.FC<
         })}
       </div>
     </div>
-  );
-};
-
-const ResetFoodRequests: React.FC<{
-  petName: PetName;
-  petData: Pet;
-  inventory: Inventory;
-  todayDate: string;
-  resetRequests: () => Promise<void>;
-  setShowResetRequests: (showResetRequests: boolean) => void;
-}> = ({
-  petName,
-  petData,
-  inventory,
-  todayDate,
-  resetRequests,
-  setShowResetRequests,
-}) => {
-  const { t } = useAppTranslation();
-  const [showResetRequestsConfirmation, setShowResetRequestsConfirmation] =
-    useState(false);
-  const resetGemCost = getGemCost(petData.requests.resets?.[todayDate] ?? 0);
-  const hasEnoughGem = inventory.Gem?.gte(resetGemCost);
-  return (
-    <InnerPanel className="flex flex-col gap-2">
-      <div className="flex flex-row gap-2 items-center justify-between">
-        <div className="flex flex-row gap-2">
-          <img
-            src={SUNNYSIDE.icons.arrow_left}
-            className="h-6 cursor-pointer"
-            onClick={() => setShowResetRequests(false)}
-          />
-          <Label type="default">
-            {t("pets.getNewRequests", { pet: petName })}
-          </Label>
-        </div>
-        <Label type="info" secondaryIcon={ITEM_DETAILS.Gem.image}>
-          {`${resetGemCost} ${t("gems")}`}
-        </Label>
-      </div>
-      <p className="text-xs px-1">
-        {t("pets.requestsResetedAt", { pet: petName })}
-      </p>
-      <p className="text-xs px-1">
-        {t("pets.requestsResetedAtDescription", { pet: petName })}
-      </p>
-      <div className="flex flex-row gap-2 items-center justify-between">
-        <Button
-          onClick={() => {
-            if (showResetRequestsConfirmation) {
-              resetRequests();
-              setShowResetRequestsConfirmation(false);
-            } else {
-              setShowResetRequestsConfirmation(true);
-            }
-          }}
-          disabled={!hasEnoughGem}
-        >
-          {showResetRequestsConfirmation
-            ? t("confirm")
-            : t("pets.resetRequests")}
-        </Button>
-        {showResetRequestsConfirmation && (
-          <Button onClick={() => setShowResetRequestsConfirmation(false)}>
-            {t("cancel")}
-          </Button>
-        )}
-      </div>
-    </InnerPanel>
   );
 };
