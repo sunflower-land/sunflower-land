0829fe38d5ee3f55a9972adf9faee269d60dbc91
Craig <41215134+c-py@users.noreply.github.com>
2025-09-05 15:03:04 +1000

    [FEAT] Show Global Data in Local Feed

--- src/features/island/hud/Hud.tsx
+++ src/features/island/hud/Hud.tsx
@@ -72,7 +72,7 @@ const HudComponent: React.FC<{
 
   return (
     <HudContainer>
-      <Feed type="local" showFeed={showFeed} setShowFeed={setShowFeed} />
+      <Feed type="world" showFeed={showFeed} setShowFeed={setShowFeed} />
       <div
         className={classNames(
           "absolute left-0 top-0 bottom-0 p-2.5 transition-transform duration-200",
