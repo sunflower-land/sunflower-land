dc97439a8549eb98d889509332f1070d513af59b
Elias <elias@sunflower-land.com>
2025-09-05 12:36:32 +0800

    Fetch Pet events

---
+++ src/features/game/events/pets/fetchPet.ts
@@ -0,0 +1,68 @@
+import Decimal from "decimal.js-light";
+import {
+  getExperienceToNextLevel,
+  getPetFetches,
+  isPetNapping,
+  isPetNeglected,
+  PET_RESOURCES,
+  PetName,
+  PetResourceName,
+} from "features/game/types/pets";
+import { GameState } from "features/game/types/game";
+import { produce } from "immer";
+
+export type FetchPetAction = {
+  type: "pet.fetched";
+  pet: PetName;
+  fetch: PetResourceName;
+};
+
+type Options = {
+  state: GameState;
+  action: FetchPetAction;
+  createdAt?: number;
+};
+
+export function fetchPet({ state, action, createdAt = Date.now() }: Options) {
+  return produce(state, (stateCopy) => {
+    const { pet, fetch } = action;
+
+    const petData = stateCopy.pets?.common?.[pet];
+    if (!petData) {
+      throw new Error("Pet not found");
+    }
+
+    if (isPetNapping(petData, createdAt)) {
+      throw new Error("Pet is napping");
+    }
+
+    if (isPetNeglected(petData, createdAt)) {
+      throw new Error("Pet is neglected");
+    }
+
+    const fetchData = getPetFetches(pet);
+
+    const fetchEntry = fetchData.fetches.find(
+      (fetchEntry) => fetchEntry.name === fetch,
+    );
+
+    if (!fetchEntry) {
+      throw new Error("Fetch not found");
+    }
+
+    const { level: petLevel } = getExperienceToNextLevel(petData.experience);
+    if (petLevel < fetchEntry.level) {
+      throw new Error("Pet level doesn't match fetch required level");
+    }
+
+    const energyRequired = PET_RESOURCES[fetch].energy;
+    if (petData.energy < energyRequired) {
+      throw new Error("Pet doesn't have enough energy");
+    }
+
+    petData.energy -= energyRequired;
+    state.inventory[fetch] = (state.inventory[fetch] ?? new Decimal(0)).add(1);
+
+    return stateCopy;
+  });
+}
