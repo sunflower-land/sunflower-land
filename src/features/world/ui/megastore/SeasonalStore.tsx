34a68b368b6a36fcd88f58c5d02bf14c5413afd3
Brian Manthos <manthos@msn.com>
2025-09-06 08:56:15 -0700

    Rename seasonalstore_components dir to chapterstore_components

--- src/features/world/ui/megastore/SeasonalStore.tsx
+++ src/features/world/ui/megastore/SeasonalStore.tsx
@@ -14,8 +14,8 @@ import {
   SeasonalStoreWearable,
 } from "features/game/types/megastore";
 
-import { ItemsList } from "./seasonalstore_components/ItemsList";
-import { ItemDetail } from "./seasonalstore_components/ItemDetail";
+import { ItemsList } from "./chapterstore_components/ItemsList";
+import { ItemDetail } from "./chapterstore_components/ItemDetail";
 import { ITEM_IDS } from "features/game/types/bumpkin";
 import { ITEM_DETAILS } from "features/game/types/images";
 import { getImageUrl } from "lib/utils/getImageURLS";
