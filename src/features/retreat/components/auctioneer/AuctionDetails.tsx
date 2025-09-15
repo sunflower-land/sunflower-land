81e32cb01f6c6f87df23c442d9c5ae71f1931520
spencer <spencer@sunflower-land.com>
2025-09-15 14:36:38 +1000

    [FEAT] Update UI

--- src/features/retreat/components/auctioneer/AuctionDetails.tsx
+++ src/features/retreat/components/auctioneer/AuctionDetails.tsx
@@ -136,7 +136,7 @@ export const AuctionDetails: React.FC<Props> = ({
         </div>
 
         {buffLabels && (
-          <div className="flex flex-col gap-1">
+          <div className="flex flex-col gap-1 mb-1">
             {buffLabels.map(
               (
                 { labelType, boostTypeIcon, boostedItemIcon, shortDescription },
