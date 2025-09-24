e49fa5057b8efdbbb763b1289f52c18f4cdf8ae0
Adam Hannigan <adamhannigan81@gmail.com>
2025-09-24 18:36:06 +0100

    Tweaks

--- src/lib/flags.ts
+++ src/lib/flags.ts
@@ -91,7 +91,7 @@ const FEATURE_FLAGS = {
   RONIN_AIRDROP: (game: GameState) => {
     if (Date.now() > RONIN_AIRDROP_ENDDATE.getTime()) return false;
 
-    return defaultFeatureFlag(game);
+    return adminFeatureFlag(game);
   },
 
   // Permanent Feature Flags
