7089f5bbeb48cb8612c027f4718cf143dc3c9439
Spencer Dezart-Smith <17863697+spencerdezartsmith@users.noreply.github.com>
2025-09-25 10:43:15 +1000

    [FEAT] Pet NFT Deposit (#6322)
    * [FEAT] Add addresses
    
    * [FEAT] Allow error text to be highlighted
    
    * [FEAT] Add blockchain functions
    
    * [FEAT] Add toast logic
    
    * [FEAT] Update deposit game items. Add flag
    
    * [CHORE] Add flag condition

--- src/features/game/toast/ToastPanel.tsx
+++ src/features/game/toast/ToastPanel.tsx
@@ -18,6 +18,8 @@ import { Bud } from "../types/buds";
 import { KNOWN_IDS } from "../types";
 import { getTradeableDisplay } from "features/marketplace/lib/tradeables";
 import { useVisiting } from "lib/utils/visitUtils";
+import { PetNFT } from "../types/pets";
+import { ITEM_DETAILS } from "../types/images";
 
 const MAX_TOAST = 6;
 
@@ -58,6 +60,11 @@ const getToastIcon = (item: ToastItem, faction?: FactionName) => {
     }).image;
   }
 
+  if (item.startsWith("Pet #")) {
+    // TODO: Update with pet image
+    return ITEM_DETAILS["Pet Egg"].image;
+  }
+
   return "";
 };
 
@@ -77,6 +84,7 @@ export const ToastPanel: React.FC = () => {
     setFactionPoints,
     setWardrobe,
     setBuds,
+    setPetNFTs,
   } = useContext(ToastContext);
   const [visibleToasts, setVisibleToasts] = useState<Toast[]>([]);
   const [showToasts, setShowToasts] = useState<boolean>(false);
@@ -98,6 +106,8 @@ export const ToastPanel: React.FC = () => {
   const newWardrobe = useRef<Partial<Record<BumpkinItem, number>>>();
   const oldBuds = useRef<Partial<Record<number, Bud>>>();
   const newBuds = useRef<Partial<Record<number, Bud>>>();
+  const oldPetNFTs = useRef<Partial<Record<number, PetNFT>>>();
+  const newPetNFTs = useRef<Partial<Record<number, PetNFT>>>();
 
   /**
    * Listens to game state transitions.
@@ -124,6 +134,8 @@ export const ToastPanel: React.FC = () => {
     newWardrobe.current = state.context.state.wardrobe;
     oldBuds.current = newBuds.current;
     newBuds.current = state.context.state.buds;
+    oldPetNFTs.current = newPetNFTs.current;
+    newPetNFTs.current = state.context.state.pets?.nfts ?? {};
 
     // inventory is set and changed
     if (
@@ -173,6 +185,11 @@ export const ToastPanel: React.FC = () => {
     // buds is set and changed
     if (!!newBuds.current && oldBuds.current !== newBuds.current) {
       setBuds(newBuds.current);
+    }
+
+    // pet nfts is set and changed
+    if (!!newPetNFTs.current && oldPetNFTs.current !== newPetNFTs.current) {
+      setPetNFTs(newPetNFTs.current);
     }
   });
 
--- src/lib/blockchain/Deposit.ts
+++ src/lib/blockchain/Deposit.ts
@@ -1,11 +1,12 @@
 import { CONFIG } from "lib/config";
 import BudDepositAbi from "./abis/BudDeposit";
+import DepositV2Abi from "./abis/DepositV2";
 import { waitForTransactionReceipt, writeContract } from "@wagmi/core";
 import { config } from "features/wallet/WalletProvider";
 import { polygon } from "viem/chains";
 import { polygonAmoy } from "viem/chains";
-
-const budsDepositAddress = CONFIG.BUD_DEPOSIT_CONTRACT;
+import { INITIAL_FARM } from "features/game/lib/constants";
+import { hasFeatureAccess } from "lib/flags";
 
 export interface DepositArgs {
   account: `0x${string}`;
@@ -16,6 +17,7 @@ export interface DepositArgs {
   wearableIds: number[];
   wearableAmounts: number[];
   budIds: number[];
+  petIds: number[];
 }
 
 export async function depositToFarm({
@@ -27,11 +29,26 @@ export async function depositToFarm({
   wearableAmounts,
   wearableIds,
   budIds,
+  petIds,
 }: DepositArgs) {
+  if (hasFeatureAccess(INITIAL_FARM, "PET_NFT_DEPOSIT")) {
+    return depositToFarmV2({
+      account,
+      farmId,
+      sfl,
+      itemIds,
+      itemAmounts,
+      wearableAmounts,
+      wearableIds,
+      budIds,
+      petIds,
+    });
+  }
+
   const hash = await writeContract(config, {
     chainId: CONFIG.NETWORK === "mainnet" ? polygon.id : polygonAmoy.id,
     abi: BudDepositAbi,
-    address: budsDepositAddress as `0x${string}`,
+    address: CONFIG.BUD_DEPOSIT_CONTRACT as `0x${string}`,
     functionName: "depositToFarm",
     account,
     args: [
@@ -42,6 +59,39 @@ export async function depositToFarm({
       wearableIds.map(BigInt),
       wearableAmounts.map(BigInt),
       budIds.map(BigInt),
+    ],
+  });
+  await waitForTransactionReceipt(config, { hash });
+}
+
+export async function depositToFarmV2({
+  account,
+  farmId,
+  itemIds,
+  itemAmounts,
+  wearableAmounts,
+  wearableIds,
+  budIds,
+  petIds,
+}: Exclude<DepositArgs, "sfl">) {
+  if (!hasFeatureAccess(INITIAL_FARM, "PET_NFT_DEPOSIT")) {
+    throw new Error("Deposit V2 is not available");
+  }
+
+  const hash = await writeContract(config, {
+    chainId: CONFIG.NETWORK === "mainnet" ? polygon.id : polygonAmoy.id,
+    abi: DepositV2Abi,
+    address: CONFIG.DEPOSIT_V2_CONTRACT as `0x${string}`,
+    functionName: "depositToFarm",
+    account,
+    args: [
+      BigInt(farmId),
+      itemIds.map(BigInt),
+      itemAmounts.map(BigInt),
+      wearableIds.map(BigInt),
+      wearableAmounts.map(BigInt),
+      budIds.map(BigInt),
+      petIds.map(BigInt),
     ],
   });
   await waitForTransactionReceipt(config, { hash });
