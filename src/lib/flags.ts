79c498e0c279365a7bfa3f68b36a7ba337c16fe9
Craig <41215134+c-py@users.noreply.github.com>
2025-10-09 11:58:37 +1100

    [FEAT] Update Ronin Withdraw flag

--- src/lib/flags.ts
+++ src/lib/flags.ts
@@ -128,7 +128,7 @@ const FEATURE_FLAGS = {
   GASLESS_AUCTIONS: () => true,
   NODE_FORGING: defaultFeatureFlag,
   DEPOSIT_SFL: adminTimeBasedFeatureFlag(new Date("2025-08-28T00:00:00.000Z")),
-  RONIN_FLOWER: testnetFeatureFlag,
+  RONIN_FLOWER: adminFeatureFlag,
   MEMORY_BETA: defaultFeatureFlag,
   PET_NFT_DEPOSIT: testnetFeatureFlag,
 } satisfies Record<string, FeatureFlag>;
