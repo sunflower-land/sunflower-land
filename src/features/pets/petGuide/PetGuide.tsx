de38eaf6797222542298f28d0df83280bd0706e3
Elias <elias@sunflower-land.com>
2025-10-12 19:55:28 +0800

    [FEAT] Enhance Pet Guide with interactive views and navigation

--- src/features/pets/petGuide/PetGuide.tsx
+++ src/features/pets/petGuide/PetGuide.tsx
@@ -1,20 +1,74 @@
 import { Button } from "components/ui/Button";
 import { InnerPanel } from "components/ui/Panel";
-import React from "react";
+import React, { useState } from "react";
+import { PetEgg } from "./PetEgg";
+import { Label } from "components/ui/Label";
+import { Feed } from "./Feed";
+import { Fetch } from "./Fetch";
+import { PetMaintenance } from "./PetMaintenance";
+import { PetLevelsAndPerks } from "./PetLevelsAndPerks";
+import { PetCategories } from "./PetCategories";
+import { Shrines } from "./Shrines";
+import { Social } from "./Social";
 
 export const PetGuide: React.FC = () => {
+  const [view, setView] = useState<
+    | "Pet Egg"
+    | "Feed"
+    | "Fetch"
+    | "Pet Maintenance"
+    | "Pet Categories"
+    | "Levels & Perks"
+    | "Shrines"
+    | "Social"
+  >();
+
+  const setToDefault = () => setView(undefined);
+
+  if (view === "Pet Egg") {
+    return <PetEgg onBack={setToDefault} />;
+  }
+  if (view === "Feed") {
+    return <Feed onBack={setToDefault} />;
+  }
+  if (view === "Fetch") {
+    return <Fetch onBack={setToDefault} />;
+  }
+  if (view === "Pet Maintenance") {
+    return <PetMaintenance onBack={setToDefault} />;
+  }
+  if (view === "Pet Categories") {
+    return <PetCategories onBack={setToDefault} />;
+  }
+  if (view === "Levels & Perks") {
+    return <PetLevelsAndPerks onBack={setToDefault} />;
+  }
+  if (view === "Shrines") {
+    return <Shrines onBack={setToDefault} />;
+  }
+  if (view === "Social") {
+    return <Social onBack={setToDefault} />;
+  }
+
   return (
     <InnerPanel>
-      <p>{`Pet Guide`}</p>
+      <Label type="default">{`Pet Guide`}</Label>
+      <p className="text-xs p-1">{`Learn about pets and how to care for them.`}</p>
       <div id="Buttons" className="grid grid-cols-2 gap-1">
-        <Button>{`Pet Egg`}</Button>
-        <Button>{`Feed`}</Button>
-        <Button>{`Fetch`}</Button>
-        <Button>{`Pet Maintenance`}</Button>
-        <Button>{`Pet Categories`}</Button>
-        <Button>{`Levels & Perks`}</Button>
-        <Button>{`Shrines`}</Button>
-        <Button>{`Social`}</Button>
+        <Button onClick={() => setView("Pet Egg")}>{`Pet Egg`}</Button>
+        <Button onClick={() => setView("Feed")}>{`Feed`}</Button>
+        <Button onClick={() => setView("Fetch")}>{`Fetch`}</Button>
+        <Button onClick={() => setView("Pet Maintenance")}>
+          {`Pet Maintenance`}
+        </Button>
+        <Button onClick={() => setView("Pet Categories")}>
+          {`Pet Categories`}
+        </Button>
+        <Button onClick={() => setView("Levels & Perks")}>
+          {`Levels & Perks`}
+        </Button>
+        <Button onClick={() => setView("Shrines")}>{`Shrines`}</Button>
+        <Button onClick={() => setView("Social")}>{`Social`}</Button>
       </div>
     </InnerPanel>
   );
---
+++ src/features/pets/petGuide/Social.tsx
@@ -0,0 +1,15 @@
+import { SUNNYSIDE } from "assets/sunnyside";
+import { InnerPanel } from "components/ui/Panel";
+import React from "react";
+
+export const Social: React.FC<{ onBack: () => void }> = ({ onBack }) => {
+  return (
+    <InnerPanel className="relative">
+      <img
+        src={SUNNYSIDE.icons.arrow_left}
+        onClick={onBack}
+        className="cursor-pointer w-6 h-6 absolute top-3 left-1"
+      />
+    </InnerPanel>
+  );
+};
