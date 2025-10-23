7eecbee32b7d12bfc533b3ac450011d87b1337c9
Elias <elias@sunflower-land.com>
2025-10-23 10:23:42 +0800

    [FEAT] Restrict Pet NFT Placement when pet is not revealed

--- src/features/game/events/landExpansion/placeNFT.test.ts
+++ src/features/game/events/landExpansion/placeNFT.test.ts
@@ -3,11 +3,53 @@ import { TEST_FARM } from "../../lib/constants";
 import { GameState } from "../../types/game";
 import { placeNFT } from "./placeNFT";
 
-const date = Date.now();
 const GAME_STATE: GameState = TEST_FARM;
 
 describe("Place NFT", () => {
+  it("requires the Pet NFT is revealed", () => {
+    jest.useFakeTimers();
+    jest.setSystemTime(new Date("2025-10-23T00:00:00.000Z"));
+    const date = Date.now();
+
+    expect(() =>
+      placeNFT({
+        state: {
+          ...GAME_STATE,
+          pets: {
+            nfts: {
+              1: {
+                id: 1,
+                name: "Pet #1",
+                requests: {
+                  food: [],
+                  fedAt: date,
+                },
+                energy: 100,
+                experience: 0,
+                pettedAt: date,
+              },
+            },
+          },
+        },
+        action: {
+          id: "1",
+          type: "nft.placed",
+          nft: "Pet",
+          coordinates: {
+            x: 0,
+            y: 0,
+          },
+          location: "farm",
+        },
+        createdAt: date,
+      }),
+    ).toThrow("This Pet NFT is not revealed");
+
+    jest.useRealTimers();
+  });
+
   it("requires the NFT ID is on the game state", () => {
+    const date = Date.now();
     expect(() =>
       placeNFT({
         state: {
@@ -27,11 +69,13 @@ describe("Place NFT", () => {
           },
           location: "farm",
         },
+        createdAt: date,
       }),
     ).toThrow("This NFT does not exist");
   });
 
   it("requires the NFT is not already placed", () => {
+    const date = Date.now();
     expect(() =>
       placeNFT({
         state: {
@@ -63,11 +107,13 @@ describe("Place NFT", () => {
           },
           location: "farm",
         },
+        createdAt: date,
       }),
     ).toThrow("This NFT is already placed");
   });
 
   it("places the NFT", () => {
+    const date = Date.now();
     const state = placeNFT({
       state: {
         ...GAME_STATE,
@@ -94,6 +140,7 @@ describe("Place NFT", () => {
         },
         location: "farm",
       },
+      createdAt: date,
     });
 
     expect(state.buds?.[1].coordinates).toStrictEqual({ x: 0, y: 0 });
