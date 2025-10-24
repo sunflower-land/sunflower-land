454a7221a0f88dddd0508fce3844704603f84a67
spencer <spencer@sunflower-land.com>
2025-10-24 11:03:19 +1100

    [CHORE] Remove comp code. Update date for competition winners.

--- src/features/game/events/landExpansion/collectCrafting.ts
+++ src/features/game/events/landExpansion/collectCrafting.ts
@@ -1,21 +1,7 @@
 import Decimal from "decimal.js-light";
-import {
-  CompetitionTaskName,
-  getCompetitionPointsPerTask,
-} from "features/game/types/competitions";
 import { trackFarmActivity } from "features/game/types/farmActivity";
 import { GameState } from "features/game/types/game";
 import { produce } from "immer";
-import { hasFeatureAccess } from "lib/flags";
-
-export const CRAFT_TO_COMPETITION_TASK: Record<string, CompetitionTaskName> = {
-  "Moo Doll": "Craft a Moo Doll",
-  "Wooly Doll": "Craft a Wooly Doll",
-  "Lumber Doll": "Craft a Lumber Doll",
-  "Gilded Doll": "Craft a Gilded Doll",
-  "Lunar Doll": "Craft a Lunar Doll",
-  "Ember Doll": "Craft an Ember Doll",
-};
 
 export type CollectCraftingAction = {
   type: "crafting.collected";
@@ -59,27 +45,6 @@ export function collectCrafting({
       `${item.collectible || item.wearable} Crafted`,
       copy.farmActivity,
     );
-
-    const craftedName = item.collectible || item.wearable;
-    const task = CRAFT_TO_COMPETITION_TASK[craftedName];
-
-    if (
-      task &&
-      hasFeatureAccess(copy, "BUILDING_FRIENDSHIPS") &&
-      copy.competitions.progress.BUILDING_FRIENDSHIPS
-    ) {
-      const points = getCompetitionPointsPerTask({
-        game: copy,
-        name: "BUILDING_FRIENDSHIPS",
-        task,
-      });
-
-      copy.competitions.progress.BUILDING_FRIENDSHIPS.points += points;
-      copy.competitions.progress.BUILDING_FRIENDSHIPS.currentProgress[task] =
-        (copy.competitions.progress.BUILDING_FRIENDSHIPS.currentProgress[
-          task
-        ] || 0) + points;
-    }
 
     copy.craftingBox.status = "idle";
     copy.craftingBox.item = undefined;
