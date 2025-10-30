a07660d9c2b875037b2041c08c06b5558bf7779b
Elias <elias@sunflower-land.com>
2025-10-30 12:08:04 +0800

    fix tsc

--- src/components/ui/SFTDetailPopover.tsx
+++ src/components/ui/SFTDetailPopover.tsx
@@ -114,10 +114,12 @@ export const SFTDetailPopoverTradeDetails = ({
           <span>{`${t("marketplace.price", { price: formatNumber(tradeable.floor, { decimalPlaces: 2 }) })} FLOWER`}</span>
         </Label>
       )}
-      {tradeable.supply !== 0 && (
+      {tradeable.supply !== undefined && tradeable.supply > 0 && (
         <Label type="transparent" className="text-xs">
           <span className="text-xs -ml-1">
-            {t("marketplace.supply", { supply: tradeable.supply })}
+            {t("marketplace.supply", {
+              supply: formatNumber(tradeable.supply, { decimalPlaces: 0 }),
+            })}
           </span>
         </Label>
       )}
