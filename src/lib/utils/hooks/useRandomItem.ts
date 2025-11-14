605ef7c81925961ef4781b85bbfe01ed11e4a257
spencer <spencer@sunflower-land.com>
2025-11-14 16:48:30 +1100

    [CHORE] Update hooks that reference refs during render

--- src/lib/utils/hooks/useRandomItem.ts
+++ src/lib/utils/hooks/useRandomItem.ts
@@ -1,15 +1,16 @@
-import { useRef } from "react";
+import { useState } from "react";
 
 /**
  * This hook allows you to get a random item from an array that will persist between renders.
  * @param array Array of items
  * @returns A random item from the array
  */
+
 export const useRandomItem = <T>(array: T[]): T => {
   if (array.length === 0) {
     throw new Error("Array must not be empty");
   }
 
-  const randomIndex = useRef(Math.floor(Math.random() * array.length));
-  return array[randomIndex.current];
+  const [index] = useState(() => Math.floor(Math.random() * array.length));
+  return array[index];
 };
