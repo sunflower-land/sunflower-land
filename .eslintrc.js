e24a3def5ebeabf24297f40d48a4c83b63fdae70
spencer <spencer@sunflower-land.com>
2025-11-17 14:13:57 +1100

    [CHORE] Use stable version of eslint. Update args.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:.eslintrc.js
 /* eslint-env node */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const reactHooks = require("eslint-plugin-react-hooks");
@@ -7,8 +6,6 @@ const hasSetStateInEffectRule =
   reactHooks?.rules?.["set-state-in-effect"] !== undefined;
 const hasPurityRule = reactHooks?.rules?.purity !== undefined;
 
-=======
->>>>>>> 477b85679 ([FEAT] Install react compiler and eslint upgrades):.eslintrc.cjs
 module.exports = {
   env: {
     browser: true,
@@ -18,7 +15,6 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
-    "plugin:react-hooks/recommended-latest",
     "plugin:@typescript-eslint/recommended",
     "prettier",
     "plugin:prettier/recommended",
@@ -68,8 +64,10 @@ module.exports = {
         endOfLine: "auto",
       },
     ],
-    // Important React Compiler Rules. Will un-warn them individually over time.
-    "react-hooks/set-state-in-effect": "warn",
-    "react-hooks/purity": "warn",
+    // Important React Compiler Rules. Enable when available.
+    ...(hasSetStateInEffectRule
+      ? { "react-hooks/set-state-in-effect": "warn" }
+      : {}),
+    ...(hasPurityRule ? { "react-hooks/purity": "warn" } : {}),
   },
 };
