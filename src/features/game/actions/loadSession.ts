f702a4c1afa6f81a6ed164c241fddf3ea427b7f0
Elias <elias@sunflower-land.com>
2026-01-16 10:18:27 +0800

    [FEAT] Store Language in Metadata
    Co-authored-by: Craig <41215134+c-py@users.noreply.github.com>

--- src/features/game/actions/loadSession.ts
+++ src/features/game/actions/loadSession.ts
@@ -7,11 +7,13 @@ import { GameState, Purchase } from "../types/game";
 import { Announcements } from "../types/announcements";
 import { getSignupMethod } from "features/auth/actions/createAccount";
 import { Moderation } from "../lib/gameMachine";
+import { LanguageCode } from "lib/i18n/dictionaries/language";
 
 type Request = {
   token: string;
   transactionId: string;
   wallet?: string;
+  language?: LanguageCode;
 };
 
 type Response = {
@@ -71,6 +73,7 @@ export async function loadSession(
       signUpMethod,
       timezone,
       wallet: request.wallet,
+      language: request.language ?? "en",
     }),
   });
 
