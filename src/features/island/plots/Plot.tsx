8f5bf89658f5376cf6ea05876ffaac6901d610bf
Elias <elias@sunflower-land.com>
2026-01-23 15:17:15 +0800

    remove test code

--- src/features/island/plots/Plot.tsx
+++ src/features/island/plots/Plot.tsx
@@ -30,7 +30,7 @@ import {
   SEASONAL_SEEDS,
   SeedName,
 } from "features/game/types/seeds";
-// import { getBumpkinLevel } from "features/game/lib/level";
+import { getBumpkinLevel } from "features/game/lib/level";
 import { ModalContext } from "features/game/components/modal/ModalProvider";
 import { getKeys } from "features/game/types/craftables";
 import { Transition } from "@headlessui/react";
@@ -41,8 +41,8 @@ import { TsunamiPlot } from "./components/TsunamiPlot";
 import { GreatFreezePlot } from "./components/GreatFreezePlot";
 import { SeasonalSeed } from "./components/SeasonalSeed";
 import { Modal } from "components/ui/Modal";
-// import { hasReputation, Reputation } from "features/game/lib/reputation";
-// import { isFaceVerified } from "features/retreat/components/personhood/lib/faceRecognition";
+import { hasReputation, Reputation } from "features/game/lib/reputation";
+import { isFaceVerified } from "features/retreat/components/personhood/lib/faceRecognition";
 import { useNow } from "lib/utils/hooks/useNow";
 
 export function getYieldColour(yieldAmount: number) {
@@ -79,16 +79,15 @@ const selectCropsSold = (state: MachineState) =>
   state.context.state.farmActivity?.["Sunflower Sold"] ?? 0;
 
 // A player that has been vetted and is engaged in the season.
-const isSeasonedPlayer = (_state: MachineState): boolean => {
-  return false;
+const isSeasonedPlayer = (state: MachineState): boolean => {
   // - level 60+
-  // return (
-  //   getBumpkinLevel(state.context.state.bumpkin?.experience ?? 0) >= 60 &&
-  //   // - verified (personhood verification)
-  //   (state.context.verified || isFaceVerified({ game: state.context.state })) &&
-  //   // - has grower reputation
-  //   hasReputation({ game: state.context.state, reputation: Reputation.Grower })
-  // );
+  return (
+    getBumpkinLevel(state.context.state.bumpkin?.experience ?? 0) >= 60 &&
+    // - verified (personhood verification)
+    (state.context.verified || isFaceVerified({ game: state.context.state })) &&
+    // - has grower reputation
+    hasReputation({ game: state.context.state, reputation: Reputation.Grower })
+  );
 };
 
 interface Props {
