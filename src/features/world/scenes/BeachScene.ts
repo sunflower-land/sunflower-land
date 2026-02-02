ceb4436c7f1553fd728d7d60efd4c27d2cdc2b12
spencer <spencer@sunflower-land.com>
2026-02-02 15:24:45 +1100

    [CHORE] Load missing texture

--- src/features/world/scenes/BeachScene.ts
+++ src/features/world/scenes/BeachScene.ts
@@ -157,6 +157,7 @@ export class BeachScene extends BaseScene {
     this.load.image("coprolite", "world/coprolite.webp");
     this.load.image("moon_crystal", "world/moon_crystal.webp");
     this.load.image("sand", "world/sand.webp");
+    this.load.image("ammonite_shell", "world/ammonite_shell.webp");
 
     this.load.image("shovel_select", "world/shovel_select_new.webp");
     this.load.image("confirm_select", "world/select_confirm_new.webp");
