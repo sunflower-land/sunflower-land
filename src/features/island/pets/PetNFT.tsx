5b649e1d2b8270a51db6a29b1a6c8843cbe7859d
Elias <elias@sunflower-land.com>
2026-02-03 13:55:25 +0800

    Fix nft pet moving issue

--- src/features/island/pets/PetNFT.tsx
+++ src/features/island/pets/PetNFT.tsx
@@ -4,22 +4,36 @@ import React, { useContext } from "react";
 import { MoveableComponent } from "../collectibles/MovableComponent";
 import { Context } from "features/game/GameProvider";
 import { PetNFT as _PetNFT } from "../collectibles/components/petNFT/PetNFT";
+import { PlaceableLocation } from "features/game/types/collectibles";
 
 export interface PetNFTProps {
   id: string;
   x: number;
   y: number;
+  location?: PlaceableLocation;
 }
 
 const isLandscaping = (state: MachineState) => state.matches("landscaping");
 
-const PetNFTComponent: React.FC<PetNFTProps> = ({ id, x, y }) => {
+const PetNFTComponent: React.FC<PetNFTProps> = ({
+  id,
+  x,
+  y,
+  location = "farm",
+}) => {
   const { gameService } = useContext(Context);
   const landscaping = useSelector(gameService, isLandscaping);
 
   if (landscaping)
     return (
-      <MoveableComponent name="Pet" id={String(id)} index={0} x={x} y={y}>
+      <MoveableComponent
+        name="Pet"
+        id={String(id)}
+        index={0}
+        x={x}
+        y={y}
+        location={location}
+      >
         <_PetNFT id={String(id)} />
       </MoveableComponent>
     );
