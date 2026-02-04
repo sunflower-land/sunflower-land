4aa499a0a6d255822800c12bbf3c8efef13dc0ee
jcads <50954098+jcads@users.noreply.github.com>
2026-02-04 12:16:58 +0100

    [FIX] Update mutant fish trophies

--- src/features/island/collectibles/components/BattleFish.tsx
+++ src/features/island/collectibles/components/BattleFish.tsx
@@ -1,24 +1,21 @@
 import React from "react";
 
-import battleFish from "assets/fish/battle_fish.webp";
+import battleFish from "assets/fish/battle_fish_trophy.webp";
 import { PIXEL_SCALE } from "features/game/lib/constants";
 import { SFTDetailPopover } from "components/ui/SFTDetailPopover";
 
 export const BattleFish: React.FC = () => {
   return (
     <SFTDetailPopover name="Battle Fish">
-      <>
-        <img
-          src={battleFish}
-          style={{
-            width: `${PIXEL_SCALE * 32}px`,
-            bottom: `${PIXEL_SCALE * 0}px`,
-            left: `${PIXEL_SCALE * 0}px`,
-          }}
-          className="absolute"
-          alt="Battle Fish"
-        />
-      </>
+      <img
+        src={battleFish}
+        style={{
+          width: `${PIXEL_SCALE * 32}px`,
+          left: `${PIXEL_SCALE * 0}px`,
+        }}
+        className="absolute"
+        alt="Battle Fish"
+      />
     </SFTDetailPopover>
   );
 };
