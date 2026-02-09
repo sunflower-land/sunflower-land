18e6805e2ad64061e0684013690a8e59a4cfac80
Elias <101262042+eliasSFL@users.noreply.github.com>
2026-02-09 11:10:38 +0800

    [FEAT] Add Fertilisers to Offchain (#6928)

--- src/features/game/lib/offChainItems.ts
+++ src/features/game/lib/offChainItems.ts
@@ -11,7 +11,7 @@ import { TREASURE_TOOLS } from "../types/tools";
 import { PET_SHRINES } from "../types/pets";
 import { HOURGLASSES } from "../events/landExpansion/burnCollectible";
 import { RESOURCES } from "../types/resources";
-import { WORM } from "../types/composters";
+import { FRUIT_COMPOST, CROP_COMPOST, WORM } from "../types/composters";
 import { REWARD_BOXES } from "../types/rewardBoxes";
 import { PROCESSED_RESOURCES } from "../types/processedFood";
 import { SELLABLE_TREASURES } from "../types/treasure";
@@ -52,6 +52,7 @@ export const OFFCHAIN_ITEMS_SET = new Set<InventoryItemName>([
     (ticket): ticket is ChapterRaffleTicket => ticket !== undefined,
   ),
   ...getKeys(CONSUMABLES),
+  ...getKeys({ ...CROP_COMPOST, ...FRUIT_COMPOST }),
 ]);
 
 export const OFFCHAIN_ITEMS: InventoryItemName[] =
