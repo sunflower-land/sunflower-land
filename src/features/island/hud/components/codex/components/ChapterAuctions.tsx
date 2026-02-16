e0e2b5d25e024753c4b4d5d65ae512ecda5a6317
Craig <41215134+c-py@users.noreply.github.com>
2026-02-16 15:08:33 +1100

    [FIX] Show Total Supply on Chapter Dashboard (#6963)
    * [FIX] Show Total Supply on Chapter Dashboard
    
    * [FEAT] default to zero
    
    * [FEAT] Add TSC

--- src/features/island/hud/components/codex/components/ChapterAuctions.tsx
+++ src/features/island/hud/components/codex/components/ChapterAuctions.tsx
@@ -56,11 +56,9 @@ type AuctionItems = Record<
  */
 function getChapterAuctions({
   auctions,
-  totalSupply,
   chapter,
 }: {
   auctions: Auction[];
-  totalSupply: Record<string, number>;
   chapter: ChapterName;
 }) {
   const { startDate, endDate } = CHAPTERS[chapter];
@@ -91,19 +89,7 @@ function getChapterAuctions({
     return acc;
   }, {} as AuctionItems);
 
-  // Use chapter-only supply totals
-  const filteredTotalSupply = getKeys(details).reduce(
-    (acc, name) => ({
-      ...acc,
-      [name]: details[name].auctions.reduce(
-        (total, auction) => total + auction.supply,
-        0,
-      ),
-    }),
-    {} as Record<string, number>,
-  );
-
-  return { details, filteredTotalSupply };
+  return { details };
 }
 
 const NextDrop: React.FC<{ auctions: AuctionItems; game: GameState }> = ({
@@ -456,12 +442,10 @@ export const ChapterAuctions: React.FC<Props> = ({
     return <Loading />;
   }
 
-  const { details: auctionItems, filteredTotalSupply: totalItems } =
-    getChapterAuctions({
-      auctions: auctioneerState.context.auctions,
-      totalSupply: auctioneerState.context.totalSupply,
-      chapter,
-    });
+  const { details: auctionItems } = getChapterAuctions({
+    auctions: auctioneerState.context.auctions,
+    chapter,
+  });
 
   return (
     <>
@@ -477,7 +461,7 @@ export const ChapterAuctions: React.FC<Props> = ({
             <Drops
               name={selected}
               detail={auctionItems[selected]}
-              maxSupply={totalItems[selected]}
+              maxSupply={auctioneerState.context.totalSupply[selected] ?? 0}
               game={gameState}
             />
           )}
