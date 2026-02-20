0bdafc7cf02eb1482d5aa09bc0fc4eaecab90fd7
Elias <elias@sunflower-land.com>
2026-02-20 14:10:48 +0800

    revert landdata

--- src/features/game/lib/landData.ts
+++ src/features/game/lib/landData.ts
@@ -1,20 +1,4 @@
 import { GameState } from "../types/game";
-import { makeGame } from "./transforms";
-import FARM128727 from "../../../../128727.json";
-import craftingRecipes from "../../../../craftingRecipes.json";
-import { Recipes } from "./crafting";
+import { STATIC_OFFLINE_FARM } from "./landDataStatic";
 
-const madeGame: GameState = makeGame({ ...FARM128727.farm });
-export const OFFLINE_FARM: GameState = {
-  ...madeGame,
-  blessing: {
-    offering: {
-      item: "Green Amulet",
-      prize: "Green Amulet",
-    },
-  },
-  craftingBox: {
-    ...madeGame.craftingBox,
-    recipes: craftingRecipes as Partial<Recipes>,
-  },
-};
+export const OFFLINE_FARM: GameState = STATIC_OFFLINE_FARM;
