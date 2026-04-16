efffe30a396ca31919b47aa84faa968208164346
Elias <elias@sunflower-land.com>
2026-04-16 12:45:35 +0800

    Release Salt Sculpture for testing

--- src/lib/flags.ts
+++ src/lib/flags.ts
@@ -141,7 +141,7 @@ const FEATURE_FLAGS = {
   AGING_SHED: betaFeatureFlag,
 
   SALT_SKILLS: betaFeatureFlag,
-  SALT_SCULPTURE: testnetFeatureFlag,
+  SALT_SCULPTURE: betaFeatureFlag,
 
   /** Player economies: token dashboard, portal player-economy API, marketplace minigames row. */
   PLAYER_ECONOMIES: (game) => !!game.settings.economiesEnabled,
