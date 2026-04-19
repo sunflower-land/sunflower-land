6dcecb65776633d0250c5f6fd3751c52a251cd09
Alexandr Dragin <one.eastside@gmail.com>
2026-04-19 13:28:31 +0300

    [CHORE ]Adding offers and listing at main page markertplace
    UI changes at Market place with out change logic
    
    Summary
    Adding offers and listing at main page market place
    
    Context / motivation
    just improving user experience
    
    How to test
    At the bottom main page market place, aftet What New will appear 2 new sections from Profile/Trades
    
    Screenshots or screen recording
    should look something like that
    image

--- src/features/marketplace/components/MarketplaceHotNow.tsx
+++ src/features/marketplace/components/MarketplaceHotNow.tsx
@@ -19,6 +19,8 @@ import { TopTrades } from "./TopTrades";
 import useSWR, { preload } from "swr";
 import { CONFIG } from "lib/config";
 import { useGame } from "features/game/GameProvider";
+import { MyOffers } from "./profile/MyOffers";
+import { MyListings } from "./profile/MyListings";
 
 const hotNowFetcher = ([, token]: [string, string]) => {
   if (CONFIG.API_URL) return loadTrends({ token });
@@ -163,7 +165,7 @@ export const MarketplaceHotNow: React.FC = () => {
 
           <TopTrades trends={data} />
         </div>
-      </InnerPanel>
+      <InnerPanel className="mb-1">
 
       <InnerPanel>
         <div className="p-2">
@@ -174,6 +176,9 @@ export const MarketplaceHotNow: React.FC = () => {
           <WhatsNew />
         </div>
       </InnerPanel>
+
+      <MyOffers />
+      <MyListings />
     </div>
   );
 };
