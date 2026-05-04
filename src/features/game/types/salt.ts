c1d01e947647a682a2df9e87657e4f7259658f80
Elias <elias@sunflower-land.com>
2026-05-04 10:31:55 +0800

    [CHORE] Simplify salt node coordinate calculation
    Refactor the getSaltNodeCoordinates function to streamline the logic for determining offset values based on the expansions count. The previous switch statement has been replaced with conditional statements for improved readability and maintainability.

--- src/features/game/types/salt.ts
+++ src/features/game/types/salt.ts
@@ -350,47 +350,14 @@ export function getSaltNodeCoordinates(
   let offsetX = 0;
   let offsetY = 0;
 
-  switch (expansions) {
-    case 0:
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-      offsetX = 6;
-      offsetY = 10;
-      break;
-    case 7:
-    case 8:
-    case 9:
-    case 10:
-    case 11:
-    case 12:
-    case 13:
-    case 14:
-    case 15:
-    case 16:
-    case 18:
-    case 19:
-    case 20:
-      offsetX = 6;
-      offsetY = 6;
-      break;
-    case 21:
-    case 22:
-    case 23:
-    case 24:
-    case 25:
-    case 26:
-    case 27:
-    case 28:
-    case 29:
-    case 30:
-    default:
-      offsetX = 0;
-      offsetY = 0;
-      break;
+  if (expansions < 7) {
+    offsetX = 6;
+    offsetY = 10;
+  }
+
+  if (expansions >= 7 && expansions < 21) {
+    offsetX = 6;
+    offsetY = 6;
   }
 
   return {
