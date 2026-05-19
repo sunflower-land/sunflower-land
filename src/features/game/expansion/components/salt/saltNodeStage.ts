1b4b82ee0b184a3fa4ea95cddcb4d3ed40d995eb
Vitt0c <11968934+vitt0@users.noreply.github.com>
2026-05-19 12:23:46 -0300

    add-visual-art-for-salt-nodes-4-and-5

--- src/features/game/expansion/components/salt/saltNodeStage.ts
+++ src/features/game/expansion/components/salt/saltNodeStage.ts
@@ -2,9 +2,13 @@ import saltNodeEmpty from "assets/buildings/salt/salt_node_empty.webp";
 import saltNodeStage1 from "assets/buildings/salt/salt_node_stage_1.webp";
 import saltNodeStage2 from "assets/buildings/salt/salt_node_stage_2.webp";
 import saltNodeStage3 from "assets/buildings/salt/salt_node_stage_3.webp";
+import saltNodeStage3 from "assets/buildings/salt/salt_node_stage_4.webp";
+import saltNodeStage3 from "assets/buildings/salt/salt_node_stage_5.webp";
 
 export function getSaltNodeSprite(storedCharges: number): string {
-  if (storedCharges >= 3) return saltNodeStage3;
+  if (storedCharges >= 5) return saltNodeStage5;
+  if (storedCharges === 4) return saltNodeStage4;
+  if (storedCharges === 3) return saltNodeStage3;
   if (storedCharges === 2) return saltNodeStage2;
   if (storedCharges === 1) return saltNodeStage1;
   return saltNodeEmpty;
