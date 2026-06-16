962c6f3731f5456f593da921678b147174b496c6
Elias <101262042+eliasSFL@users.noreply.github.com>
2026-06-16 17:05:36 +0800

    fix lint

--- src/features/game/lib/landDataStatic.ts
+++ src/features/game/lib/landDataStatic.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable unused-imports/no-unused-vars */
-import Decimal from "decimal.js-light";
 import type { GameState } from "../types/game";
 
 import { INITIAL_FARM } from "./constants";
